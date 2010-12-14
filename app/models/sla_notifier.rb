0c5f00e319fc1db13072a012ab51c88b68cd7e4f
shihab <shihab@freshdesk.com>
2010-12-14 12:39:58 +0530

    sla notifier model has been added

---
+++ app/models/sla_notifier.rb
@@ -0,0 +1,24 @@
+class SlaNotifier < ActionMailer::Base
+  
+
+  def sla_escalation(ticket, email)
+    subject       'SlaNotifier#sla_escalation'
+    recipients    email
+    from          ticket.account.default_email
+    sent_on       Time.now    
+    body          :ticket => ticket, :host => ticket.account.full_domain
+    headers       "Reply-to" => "#{ticket.account.default_email}"
+    content_type  "text/plain"
+  end
+  
+  def fr_sla_escalation(ticket, email)
+    subject       'SlaNotifier#first_response_sla_escalation'
+    recipients    email
+    from          ticket.account.default_email
+    sent_on       Time.now    
+    body          :ticket => ticket, :host => ticket.account.full_domain
+    headers       "Reply-to" => "#{ticket.account.default_email}"
+    content_type  "text/plain"
+  end
+
+end
