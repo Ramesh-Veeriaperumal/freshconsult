c7f440046b8e8d7e6e7eb4c4a9c05c3a45445fd2
shihab <shihab@freshdesk.com>
2010-12-31 16:03:01 +0530

    people and tickets from companies is provided

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,10 @@
 class Customer < ActiveRecord::Base
   
+  
+  has_many :users , :class_name =>'User'
+  
+  has_many :tickets , :through =>:users , :class_name => 'Helpdesk::Ticket' ,:foreign_key => "requester_id"
+  
   CUST_TYPES = [
     [ :customer,    "Customer",         1 ], 
     [ :prospect,    "Prospect",      2 ], 
@@ -11,4 +16,8 @@ class Customer < ActiveRecord::Base
   CUST_TYPE_OPTIONS = CUST_TYPES.map { |i| [i[1], i[2]] }
   CUST_TYPE_BY_KEY = Hash[*CUST_TYPES.map { |i| [i[2], i[1]] }.flatten]
   CUST_TYPE_BY_TOKEN = Hash[*CUST_TYPES.map { |i| [i[0], i[2]] }.flatten]
+  
+  
+  
+  
 end
