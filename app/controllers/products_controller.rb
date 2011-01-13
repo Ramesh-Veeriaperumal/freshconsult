cd9eb673bd91e5c623fc86b75a04815bfb5309d0
Parsuram Vijayasankar <venom@Parsuram-Vijayasankars-MacBook-Pro.local>
2011-01-13 16:19:46 +0530

    Products page Stage 2 UI
    - Product create, delete and edit pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,33 @@
 class ProductsController < ApplicationController
-  def index
-  end
+  include ModelControllerMethods
+  
+#  def index
+#  end
+#
+#  def new
+#  end
+#
+#  def create
+#    p "+++++++++++++++Testing venom ========="
+#    if @product.save
+#      flash[:notice] = "Product successfully created."
+#      redirect_back_or_default redirect_url
+#    else
+#      render :action => 'new'
+#    end
+#  end
+#
+#  def edit
+#  end
+#
+#  def update
+#  end
+#
+#  def destroy
+#  end
 
-  def new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
+  protected
+    def scoper
+      current_account.products
+    end
 end
