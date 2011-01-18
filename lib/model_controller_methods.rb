460c1b38a5b1e0354addb228ff67c4655127fe17
Shan <shan@freshdesk.com>
2011-01-18 17:37:24 +0530

    role_token hardcoding removed
    Signed-off-by: Shan <shan@freshdesk.com>

--- lib/model_controller_methods.rb
+++ lib/model_controller_methods.rb
@@ -6,8 +6,7 @@ module ModelControllerMethods
   
   def index
     @users = self.instance_variable_set('@' + self.controller_name,
-      scoper.find(:all, :order => 'name', :conditions =>{:role_token=>'customer'}))
-     
+      scoper.find(:all, :order => 'name'))     
 
     respond_to do |format|
       format.html  do
