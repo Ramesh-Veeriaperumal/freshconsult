2fee29c0585be921131c8edff244dea268d2cf29
shihab <shihab@freshdesk.com>
2011-01-28 20:22:42 +0530

    made categories dynamic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,27 @@ class HomeController < ApplicationController
     
     @folder = Solution::Folder
   end
+  
+  
+  def get_folders
+    
+    logger.debug "Folder is is  :::  #{params[:id]}"
+    
+    @folder = Solution::Folder.find(:first, :include =>:categories , :conditions =>{:id =>params[:id]})
+    
+        
+     render :partial => "solution_toc", :locals => { :folder => @folder }
+    
+  end
+  
+  def get_article
+    
+    logger.debug "Folder is is  :::  #{params[:id]}"
+    
+    @solution = Helpdesk::Article.find(params[:id])
+       
+     render :partial => 'solution_article', :locals => { :article => @solution } 
+    
+  end
+  
 end
