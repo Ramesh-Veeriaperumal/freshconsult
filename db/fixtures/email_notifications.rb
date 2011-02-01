39b733497d2906bff20e71b8268a5418aab3b26c
Shan <shan@freshdesk.com>
2011-02-01 17:56:20 +0530

    Commented_by_requester notification removed
    Signed-off-by: Shan <shan@freshdesk.com>

--- db/fixtures/email_notifications.rb
+++ db/fixtures/email_notifications.rb
@@ -4,7 +4,7 @@ EmailNotification.seed_many(:notification_type, [
   { :notification_type => EmailNotification::TICKET_ASSIGNED_TO_GROUP, :account => account, :requester_notification => false, :agent_notification => true },
   { :notification_type => EmailNotification::TICKET_ASSIGNED_TO_AGENT, :account => account, :requester_notification => false, :agent_notification => true },
   { :notification_type => EmailNotification::COMMENTED_BY_AGENT, :account => account, :requester_notification => true, :agent_notification => false },
-  { :notification_type => EmailNotification::COMMENTED_BY_REQUESTER, :account => account, :requester_notification => false, :agent_notification => true },
+  #{ :notification_type => EmailNotification::COMMENTED_BY_REQUESTER, :account => account, :requester_notification => false, :agent_notification => true },
   { :notification_type => EmailNotification::REPLIED_BY_REQUESTER, :account => account, :requester_notification => false, :agent_notification => true },
   { :notification_type => EmailNotification::TICKET_RESOLVED, :account => account, :requester_notification => true, :agent_notification => false },
   { :notification_type => EmailNotification::TICKET_CLOSED, :account => account, :requester_notification => true, :agent_notification => false },
