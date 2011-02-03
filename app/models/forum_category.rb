fa9984f6e0da584dbe9aa0a342d3e45085c49cfd
kirandarisi <kiran.darisi@gmail.com>
2011-02-03 11:40:20 +0530

    relationships added with forums as well as account

--- app/models/forum_category.rb
+++ app/models/forum_category.rb
@@ -1,3 +1,11 @@
 class ForumCategory < ActiveRecord::Base
+  validates_presence_of :name
   has_many :forums
-end
+  belongs_to :account
+   
+   
+   # retrieves forums ordered by position
+  def self.find_ordered(account, options = {})
+    find :all, options.update(:conditions => {:account_id => account}, :order => 'name')
+  end
+ end
