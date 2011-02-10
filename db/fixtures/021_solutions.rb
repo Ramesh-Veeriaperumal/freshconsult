d168430f92f63239b12fc08c05d7bdbbd5ac65b6
Shan <shan@freshdesk.com>
2011-02-10 12:21:45 +0530

    A few changes/hacks to deploy in engineyard.
    Signed-off-by: Shan <shan@freshdesk.com>

---
+++ db/fixtures/021_solutions.rb
@@ -0,0 +1,23 @@
+account = Account.first
+
+folder = Solution::Folder.seed(:account_id, :name) do |s|
+  s.account_id = account.id
+  s.name = 'General'
+  s.description = 'Default solution category, feel free to edit or delete it.'
+end
+
+guide = Helpdesk::Guide.seed_many(:account_id, :folder_id, :name, [
+  {
+    :account_id => account.id,
+    :folder_id => folder.id,
+    :name => 'FAQ',
+    :description => 'Default solution folder, feel free to edit or delete it.'
+  },
+  
+  {
+    :account_id => account.id,
+    :folder_id => folder.id,
+    :name => 'Getting Started',
+    :description => 'Default solution folder, feel free to edit or delete it.'
+  }
+])
