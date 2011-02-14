11921acb46fb1c8b5eb06e37f0c2d5793d536905
Shan <shan@freshdesk.com>
2011-02-14 15:32:30 +0530

    Sample ticket seed data..
    Signed-off-by: Shan <shan@freshdesk.com>

---
+++ db/fixtures/041_tickets.rb
@@ -0,0 +1,23 @@
+account = Account.current
+
+ticket = Helpdesk::Ticket.seed(:account_id, :subject) do |s|
+  s.account_id = account.id
+  s.subject = "This is a sample ticket"
+  s.description = 'This is a sample ticket, feel free to delete it.'
+  s.requester_id = account.admin.id
+  s.responder_id = account.admin.id
+  s.status = TicketConstants::STATUS_KEYS_BY_TOKEN[:open]
+  s.source = TicketConstants::SOURCE_KEYS_BY_TOKEN[:portal]
+  s.priority = TicketConstants::PRIORITY_KEYS_BY_TOKEN[:low]
+  s.ticket_type = TicketConstants::TYPE_KEYS_BY_TOKEN[:how_to]
+end
+
+Helpdesk::Activity.seed(:account_id, :notable_id, :notable_type) do |s|
+  s.account_id = account.id
+  s.description = "{{user_path}} created a new ticket {{notable_path}}"
+  s.notable_id = ticket.id
+  s.notable_type = "Helpdesk::Ticket"
+  s.user_id = account.admin.id
+  s.short_descr = "{{user_path}} created the ticket"
+  s.activity_data = {}
+end
