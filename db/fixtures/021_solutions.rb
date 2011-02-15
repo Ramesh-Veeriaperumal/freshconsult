d36c12b9b93ae1273d3fd7d349eaad4cc64c76ab
Shan <shan@freshdesk.com>
2011-02-15 10:47:52 +0530

    Solutions table name change..
    Signed-off-by: Shan <shan@freshdesk.com>

--- db/fixtures/021_solutions.rb
+++ db/fixtures/021_solutions.rb
@@ -1,22 +1,20 @@
 account = Account.current
 
-folder = Solution::Folder.seed(:account_id, :name) do |s|
+category = Solution::Category.seed(:account_id, :name) do |s|
   s.account_id = account.id
   s.name = 'General'
   s.description = 'Default solution category, feel free to edit or delete it.'
 end
 
-guide = Helpdesk::Guide.seed_many(:account_id, :folder_id, :name, [
+Solution::Folder.seed_many(:category_id, :name, [
   {
-    :account_id => account.id,
-    :folder_id => folder.id,
+    :category_id => category.id,
     :name => 'FAQ',
     :description => 'Default solution folder, feel free to edit or delete it.'
   },
   
   {
-    :account_id => account.id,
-    :folder_id => folder.id,
+    :category_id => category.id,
     :name => 'Getting Started',
     :description => 'Default solution folder, feel free to edit or delete it.'
   }
