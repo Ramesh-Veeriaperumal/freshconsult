7bd544a8ba4901cbc413bbb04b080b8236dc0a7e
Shan <shan@freshdesk.com>
2011-02-18 17:41:29 +0530

    Validation for helpdesk activities.
    Signed-off-by: Shan <shan@freshdesk.com>

--- app/models/helpdesk/activity.rb
+++ app/models/helpdesk/activity.rb
@@ -9,12 +9,19 @@ class Helpdesk::Activity < ActiveRecord::Base
   
   attr_protected :notable_id
   
-  validates_presence_of :description, :notable_id
+  validates_presence_of :description, :notable_id, :user_id
+  
+  before_create :set_short_descr
   
   named_scope :freshest, lambda { |account|
     { :conditions => ["account_id = ? ", account], 
       :order => "helpdesk_activities.created_at DESC"
     }
   }
+  
+  private
+    def set_short_descr
+      self.short_descr ||= descr
+    end
 
 end
