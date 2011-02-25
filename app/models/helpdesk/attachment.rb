8ca19c5f8eacaff8865ce77bc2d3047bb22ca863
kiran.darisi@gmail.com <kiran.darisi@gmail.com>
2011-02-25 15:58:35 +0530

    attachement sizes has been changed

--- app/models/helpdesk/attachment.rb
+++ app/models/helpdesk/attachment.rb
@@ -9,16 +9,24 @@ class Helpdesk::Attachment < ActiveRecord::Base
     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
     :path => "/data/helpdesk/attachments/:id/:style/:filename",
     :url => "/:class/:id",
-    :styles => { 
-        :medium => "127x177>", 
-        :thumb  => "50x50#" 
-    }
+    :styles => Proc.new  { |attachment| attachment.instance.attachment_sizes }
+    
     #:bucket => 'fdesk-attachments'
     
   before_post_process :image?
     
   def image?
     !(content_content_type =~ /^image.*/).nil?
+  end
+  
+  def attachment_sizes
+   if self.description == "logo"
+      return {:logo => "x50>"}
+   elsif  self.description == "fav_icon"
+      return {:fav_icon  => "16x16>" }
+   else
+      return {:medium => "127x177>",:thumb  => "50x50#" }
+    end
   end
 
 end
