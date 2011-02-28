a5cc8fc488f94f9ecf24bc378fd5f494334ba1ea
kiran.darisi@gmail.com <kiran.darisi@gmail.com>
2011-02-28 19:04:13 +0530

    trigger has been modified

--- db/migrate/20110225045033_add_ticket_display_trigger.rb
+++ db/migrate/20110225045033_add_ticket_display_trigger.rb
@@ -5,8 +5,14 @@ class AddTicketDisplayTrigger < ActiveRecord::Migration
    execute "create trigger add_ticket_display_id before insert on helpdesk_tickets for each row 
       begin
         declare ticket_dis_id integer;
-        select max(dis_ids.dis_id) into ticket_dis_id from ((select max(display_id) as dis_id from helpdesk_tickets where account_id = new.account_id) union (select ticket_display_id as dis_id from accounts where id = new.account_id)) dis_ids;
-        set new.display_id = ticket_dis_id + 1;
+        declare max_dis_id integer;
+        select max(display_id) into  max_dis_id from helpdesk_tickets where account_id = new.account_id;
+        select ticket_display_id into  ticket_dis_id from accounts where id = new.account_id;
+        if ticket_dis_id > max_dis_id then
+         set new.display_id = ticket_dis_id + 1;
+        else
+         set new.display_id = max_dis_id + 1;
+        end if;
       end;"
   
  
