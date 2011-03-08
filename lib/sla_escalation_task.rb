056e2e1affeb3b646bfbc4b55f2bd396bb8cf7b9
shihab <shihab@freshdesk.com>
2011-03-08 12:59:33 +0530

    print is changed

--- lib/sla_escalation_task.rb
+++ lib/sla_escalation_task.rb
@@ -2,13 +2,13 @@ module Slainit
   # This will initialize SLA scheduler which will be called in every 1 min 
   require 'rufus/scheduler'
   
-  RAILS_DEFAULT_LOGGER.debug "-------SLA escalation Initializes--------"
+  puts "-------SLA escalation Initializes--------"
   
   scheduler = Rufus::Scheduler.start_new
 
   scheduler.every '3m' do
     
-  RAILS_DEFAULT_LOGGER.debug "-------SLA escalation check starts--------"
+  puts "-------SLA escalation check starts--------"
   @overdue_tickets = Helpdesk::Ticket.find(:all, :readonly => false, :conditions =>['due_by <=? AND isescalated=? AND status=?', Time.zone.now.to_s(:db),false,Helpdesk::Ticket::STATUS_KEYS_BY_TOKEN[:open]] )
   
   @overdue_tickets.each do |ticket|
@@ -37,7 +37,7 @@ module Slainit
           
       
     end
-      RAILS_DEFAULT_LOGGER.debug "-------SLA escalation check ends--------"
+      puts "-------SLA escalation check ends--------"
    end
    
 end
