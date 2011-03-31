3d4409a69a25971c8d87092a314fbdfda5933f70
kirandarisi <kiran.darisi@gmail.com>
2011-03-31 15:46:12 +0530

    Index for companies has been implemented

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -13,8 +13,9 @@ class ContactsController < ApplicationController
   
   def index
     
-    @contacts = self.instance_variable_set('@' + self.controller_name,
-    scoper.find(:all, :order => 'name' ))       
+    #@contacts = self.instance_variable_set('@' + self.controller_name,  scoper.find(:all, :order => 'name' ))
+    
+
     
     respond_to do |format|
       format.html  do
