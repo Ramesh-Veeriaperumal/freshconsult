3ed91962f24f9b7050391930e71ce4d1de8e0b41
Shan <shan@freshdesk.com>
2011-04-20 12:22:42 +0530

    Send Activation Invite backend support..
    Signed-off-by: Shan <shan@freshdesk.com>

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -1,9 +1,14 @@
 class ActivationsController < ApplicationController
   #before_filter :require_no_user, :only => [:new, :create] #Guess we don't really need this - Shan
+  before_filter :only => :send_invite do |c| 
+    c.requires_permission :manage_users
+  end
   
   def send_invite
-    puts "Send Activation"
-    flash[:notice] = "Activation email has been sent to the User!"
+    contact = current_account.contacts.find params[:id]
+    contact.deliver_contact_activation if contact
+    
+    flash[:notice] = "Activation email has been sent to the contact!"
     redirect_to(:back)
   end
   
