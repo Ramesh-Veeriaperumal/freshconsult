75ec6de7df09a67e2020ba95abef556897f75b5b
kirandarisi <kiran.darisi@gmail.com>
2011-04-25 17:00:30 +0530

    from email has been changed to rachel

--- app/models/freshdesk_errors_mailer.rb
+++ app/models/freshdesk_errors_mailer.rb
@@ -2,7 +2,7 @@ class FreshdeskErrorsMailer < ActionMailer::Base
   
   def error_email(object, params, e, options={}) 
     recipients    "support@freshdesk.com"
-    from          "support@freshdesk.com"
+    from          "rachel@freshdesk.com"
     subject       (options[:subject] || "Error in #{object.class.name}")
     sent_on       Time.now
     body(:object => object, :params => params, :error => e, :additional_info => options[:additional_info])
