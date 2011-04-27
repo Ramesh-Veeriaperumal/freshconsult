28c701fefa3211b23e71d19b6f5596f450fd1ec1
kirandarisi <kiran.darisi@gmail.com>
2011-04-27 05:05:00 -0700

    Edited config/initializers/omniauth.rb via GitHub

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,18 +1,13 @@
 #config/initializers/omniauth.rb
 require 'openid/store/filesystem'
 
-ActionController::Dispatcher.middleware.use OmniAuth::Builder do
-  provider :twitter,  'KEY', 'SECRET'
-  provider :facebook, 'APP_ID', 'APP_SECRET'
-  provider :linked_in, 'KEY', 'SECRET'
-  provider :open_id,  OpenID::Store::Filesystem.new('/tmp')
-end
+
 # you will be able to access the above providers by the following url
 # /auth/providername for example /auth/twitter /auth/facebook
 
 ActionController::Dispatcher.middleware do
   RAILS_DEFAULT_LOGGER.debug " ################################################## INSIDE omniauth  "
-  use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('./tmp'), :name => "google",  :identifier => "https://www.google.com/accounts/o8/id"
+  use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "google",  :identifier => "https://www.google.com/accounts/o8/id"
   #use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "yahoo",   :identifier => "https://me.yahoo.com"
   #use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "aol",     :identifier => "https://openid.aol.com"
   #use OmniAuth::Strategies::OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => "myspace", :identifier => "http://myspace.com"
