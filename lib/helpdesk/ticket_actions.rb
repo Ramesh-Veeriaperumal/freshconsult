d02b21c2fefb96a12172354820ff5b717802b108
kirandarisi <kiran.darisi@gmail.com>
2011-04-28 15:35:55 +0530

    issue regarding creating the ticket has been fixed

--- lib/helpdesk/ticket_actions.rb
+++ lib/helpdesk/ticket_actions.rb
@@ -2,12 +2,10 @@ module Helpdesk::TicketActions
   
   def create_the_ticket(need_captcha = nil)
     @ticket = current_account.tickets.build(params[:helpdesk_ticket])
-    set_default_values
-
+     set_default_values
     return false if need_captcha && !(current_user || verify_recaptcha(:model => @ticket, 
                                                         :message => "Captcha verification failed, try again!"))
     return false unless @ticket.save
-    
     handle_attachments
     @ticket.create_activity(@ticket.requester, "{{user_path}} submitted a new ticket {{notable_path}}", {}, 
                                  "{{user_path}} submitted the ticket")
@@ -21,11 +19,12 @@ module Helpdesk::TicketActions
       )
     end
     @ticket
+    
   end
 
   def set_default_values
     @ticket.status = TicketConstants::STATUS_KEYS_BY_TOKEN[:open] unless TicketConstants::STATUS_NAMES_BY_KEY.key?(@ticket.status)
-    @ticket.source ||= TicketConstants::SOURCE_KEYS_BY_TOKEN[:portal]
+    @ticket.source = TicketConstants::SOURCE_KEYS_BY_TOKEN[:portal] if @ticket.source == 0
     @ticket.ticket_type ||= TicketConstants::TYPE_KEYS_BY_TOKEN[:how_to]
     @ticket.email ||= current_user && current_user.email
   end
