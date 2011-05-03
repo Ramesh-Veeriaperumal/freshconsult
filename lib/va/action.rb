f76191317801076e603b3715fab0d262bc373e67
Shan <shan@freshdesk.com>
2011-05-03 16:17:32 +0530

    Deleted/Empty group things handled for scenario emails as well.
    Signed-off-by: Shan <shan@freshdesk.com>

--- lib/va/action.rb
+++ lib/va/action.rb
@@ -107,7 +107,7 @@ class Va::Action
     
     def send_email_to_group(act_on)
       group = get_group(act_on)
-      if group
+      if group && !group.agent_emails.empty?
         send_internal_email(act_on, group.agent_emails)
         add_activity("Sent an email to the group <b>#{group.name}</b>")
       end
@@ -123,8 +123,11 @@ class Va::Action
     
   private
     def get_group(act_on) # this (g == 0) is kind of hack, same goes for agents also.
-      g_id = act_hash[:email_to].to_i
-      (g_id == 0) ? (act_on.group_id ? act_on.group : nil) : act_on.account.groups.find(g_id)
+      begin
+        g_id = act_hash[:email_to].to_i
+        (g_id == 0) ? (act_on.group_id ? act_on.group : nil) : act_on.account.groups.find(g_id)
+      rescue ActiveRecord::RecordNotFound
+      end
     end
 
     def get_agent(act_on)
