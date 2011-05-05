7834f1a40dae803c2ba5112dfe838ab00a162929
Parsuram Vijayasankar <venom@Parsus-MacBook-Pro.local>
2011-05-05 15:42:21 +0530

    Jammit runtime script

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,4 @@
 #delayed_job monit has some problem with file permissions.
 #refer http://community.engineyard.com/discussions/problems/1485-delayed_job-worker-not-starting-permission-denied
 run "sudo chmod 755 #{release_path}/script/runner"
-run "Jammit.package!"
+run "bundle exec jammit"
