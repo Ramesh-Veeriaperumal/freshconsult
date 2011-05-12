70af9919a3a1f8488b3a4ae8412b38c480b31d6c
Kiran Darisi <kiran.darisi@gmail.com>
2011-05-12 14:27:41 +0530

    changed when there are no admins exist

--- db/migrate/20110511125405_create_admin_account_for_all_accounts.rb
+++ db/migrate/20110511125405_create_admin_account_for_all_accounts.rb
@@ -2,8 +2,10 @@ class CreateAdminAccountForAllAccounts < ActiveRecord::Migration
   def self.up
     Account.all.each do |account|
       account_admin = account.admins.first
-  	  account_admin.user_role = User::USER_ROLES_KEYS_BY_TOKEN[:account_admin] 
-  		account_admin.save!
+      unless account_admin.nil?
+  	    account_admin.user_role = User::USER_ROLES_KEYS_BY_TOKEN[:account_admin] 
+  		  account_admin.save!
+  		end
     end
   end
 
