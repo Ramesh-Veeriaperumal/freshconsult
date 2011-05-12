ce46abe565965c9c238b87690bc5c05504091720
kirandarisi <kiran.darisi@gmail.com>
2011-05-12 03:34:13 -0700

    Edited db/migrate/20110511125405_create_admin_account_for_all_accounts.rb via GitHub

--- db/migrate/20110511125405_create_admin_account_for_all_accounts.rb
+++ db/migrate/20110511125405_create_admin_account_for_all_accounts.rb
@@ -1,9 +1,10 @@
 class CreateAdminAccountForAllAccounts < ActiveRecord::Migration
   def self.up
     Account.all.each do |account|
-     puts account.full_domain
+     
      account_admin = account.admins.first
      unless account_admin.nil?
+       puts account.full_domain
        puts account_admin.email
        puts account_admin.user_role
        account_admin.user_role = User::USER_ROLES_KEYS_BY_TOKEN[:account_admin] 
