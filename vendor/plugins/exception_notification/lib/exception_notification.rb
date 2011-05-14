5ebd34cf3f9b0f3afde605ca02fb534db4509619
Kiran Darisi <kiran.darisi@gmail.com>
2011-05-14 14:29:38 +0530

    Except notifications has been done

---
+++ vendor/plugins/exception_notification/lib/exception_notification.rb
@@ -0,0 +1,8 @@
+require "actionmailer"
+
+module ExceptionNotification
+  autoload :Notifiable,     'exception_notification/notifiable'
+  autoload :Notifier,       'exception_notification/notifier'
+  #autoload :NotifierHelper, 'exception_notification/notifier_helper'
+  autoload :ConsiderLocal,  'exception_notification/consider_local'
+end
