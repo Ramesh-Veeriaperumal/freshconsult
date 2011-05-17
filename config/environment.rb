ed548bd19788f1dd1883ddba76864022bfc66031
shihab <shihab@freshdesk.com>
2011-05-17 01:43:26 -0700

    Edited config/environment.rb via GitHub

--- config/environment.rb
+++ config/environment.rb
@@ -53,9 +53,8 @@ Rails::Initializer.run do |config|
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
-  config.time_zone = 'Chennai'
-  
-  config.gem "calendar_date_select"
+  config.time_zone = 'Chennai'  
+ 
   
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
