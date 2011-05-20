cac08e076068f26719e46f763200bd2625243af2
shihab <shihab@freshdesk.com>
2011-05-20 18:51:16 +0530

    trigger has been changed

--- lib/trigger_sql.rb
+++ lib/trigger_sql.rb
@@ -3,20 +3,23 @@ module TriggerSql
     #Current mysql doesn't have support to check the existence of a trigger while creating one.
     'create trigger add_ticket_display_id before insert on helpdesk_tickets for each row 
       begin
-        declare ticket_dis_id integer;
-        declare max_dis_id integer;
-        select max(display_id) into  max_dis_id from helpdesk_tickets where account_id = new.account_id;
-        select ticket_display_id into  ticket_dis_id from accounts where id = new.account_id;
-        if max_dis_id is null then 
-            set max_dis_id = 1;
-        else
-            set max_dis_id = max_dis_id + 1; 
+        if new.display_id is null then
+          declare ticket_dis_id integer;
+          declare max_dis_id integer;
+          select max(display_id) into  max_dis_id from helpdesk_tickets where account_id = new.account_id;
+          select ticket_display_id into  ticket_dis_id from accounts where id = new.account_id;
+          if max_dis_id is null then 
+              set max_dis_id = 1;
+          else
+              set max_dis_id = max_dis_id + 1; 
+          end if;
+          if ticket_dis_id > max_dis_id then
+            set new.display_id = ticket_dis_id;
+          else
+            set new.display_id = max_dis_id;
+          end if;
         end if;
-        if ticket_dis_id > max_dis_id then
-         set new.display_id = ticket_dis_id;
-        else
-         set new.display_id = max_dis_id;
-        end if;
+                
       end;'
   end
 end
