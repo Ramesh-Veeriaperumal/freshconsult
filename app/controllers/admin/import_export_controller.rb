0ee7a36a3c8e85cd24d9d54f9888db09e6f96448
Parsuram Vijayasankar <venom@Parsus-MacBook-Pro.local>
2011-05-23 16:55:33 +0530

    Moved add admin pref to a helper method which will construct the admin links in the admin home page Added Export data link to the accounts settings page

--- app/controllers/admin/import_export_controller.rb
+++
@@ -1,4 +0,0 @@
-class Admin::ImportExportController < Admin::AdminController
-  def index
-  end
-end
--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -1,5 +1,63 @@
 module Admin::HomeHelper
+  
+  def admin_link(list_array)
+    link_item = list_array.map do |pref|
+                  next if !pref[2].nil? && !pref[2]
+                    link_content = image_tag( "/images/spacer.gif", :class => "admin-icon-#{ pref[1] }" ) +
+                                   content_tag( :div, t(".#{pref[1]}") )
+                    content_tag( :li, link_to( link_content, pref[0] ) )
+                end
+    link_item
+  end
+  
+  # Defining the Array and constructing the Admin Page links
+  # !!! IMPORTANT: 
+  #     The name listed below in the Array next to the link is the key value for the Item
+  # =>  This key value will be used as the 
+  # =>       1. i18n key
+  # =>       2. Name of the image that should be placed under the admin-icon folder
+  
   def admin_pref_links
-    admin_links = [ "Helpdesk", [['/ticket_fields', 'Ticket Fields', '']]]
+    admin_links = [
+                    [ t(".everything_helpdesk"), [
+                                    ['/account/edit',               'rebranding'              ],
+                                    ['/admin/portal',               'customer-portal'         ],
+                                    ['/ticket_fields',              'ticket-fields'           ],
+                                    ['/admin/email_configs',        'email-settings'          ],
+                                    ['/admin/email_notifications',  'email-notifications'     ],
+                                    ['/helpdesk/sla_policies',      'sla'                     ],
+                                    ['/admin/va_rules',             'dispatcher'              ],
+                                    ['/admin/automations',          'scenario',         feature?(:scenario_automations)  ],
+                                    ['/admin/business_calendars',   'business-hours',   feature?(:business_hours)        ]
+                    ]],
+                    [ t(".everything_else"), [
+                                    ['/account',                    'account-settings'        ],
+                                    ['/admin/security',             'remote-authentication'   ],
+                                    ['/admin/zip_readers',          'import'                  ],
+                                    ['/agents',                     'agent'                   ],
+                                    ['/groups',                     'group'                   ],
+                                    ['/admin/widget_config',        'feedback'                ]
+                    ]]
+
+                    # [ "People & Roles", [
+                    #                 ['/agents',   'agent' ],
+                    #                 ['/groups',   'group' ]
+                    # ]],
+                    # [ "Account", [
+                    #                 ['/account',            'account-settings' ],
+                    #                 ['/admin/security',     'remote-authentication' ],
+                    #                 ['/admin/zip_readers',  'import' ]
+                    # ]]
+                  ]
+
+    admin_html = 
+      admin_links.map do |group|
+        content_tag( :div, 
+                        content_tag(:h3, group[0], :class => "title") +
+                        content_tag(:ul, admin_link(group[1]), :class => "admin_icons"),
+                        :class => "admin #{ cycle('odd', 'even') }" )
+      end
+
+    admin_html
   end
 end
