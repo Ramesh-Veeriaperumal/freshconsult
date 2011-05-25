fe5831fc07678ec0819b7fd5d048596e327f6ea7
Kiran Darisi <kiran.darisi@gmail.com>
2011-05-25 15:19:24 +0530

    monitorships in posts has been done

--- app/models/post_mailer.rb
+++ app/models/post_mailer.rb
@@ -1,8 +1,8 @@
 class PostMailer < ActionMailer::Base
   
   def monitor_email(emailcoll,post,user)
-    to    emailcoll
-    from          curren_account.default_email
+    recipients    emailcoll
+    from          user.account.default_email
     subject       post.topic.title
     sent_on       Time.now
     body(:post => post,:user => user)
