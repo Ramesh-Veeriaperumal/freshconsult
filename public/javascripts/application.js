4aebda1c8a59ba8d8e67c66789720597e41b3a1f
Parsuram Vijayasankar <venom@Parsus-MacBook-Pro.local>
2011-06-13 12:30:43 +0530

    Moved the content css of fresh to the tinymce folder

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -73,7 +73,7 @@ var ReplyForm = {
   init: function() {
     EditForm.cancel();
     $('reply').toggle();
-    $('post_body').focus();
+    $('topic_body').focus();
     // for Safari which is sometime weird
 //    setTimeout('$(\"post_body\").focus();',50);
   }
