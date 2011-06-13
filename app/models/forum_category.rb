594c8fe5c938768ce0c5baff103ccc9efeef5b8d
Shan <shan@freshdesk.com>
2011-06-13 15:37:52 +0530

    Tab changes for current_portal..
    Signed-off-by: Shan <shan@freshdesk.com>

--- app/models/forum_category.rb
+++ app/models/forum_category.rb
@@ -5,6 +5,8 @@ class ForumCategory < ActiveRecord::Base
   has_many :forums, :dependent => :destroy
   has_many :portal_forums, :class_name => 'Forum', :conditions =>{:forum_visibility => Forum::VISIBILITY_KEYS_BY_TOKEN[:anyone]} 
   has_many :user_forums, :class_name => 'Forum', :conditions =>['forum_visibility != ?', Forum::VISIBILITY_KEYS_BY_TOKEN[:agents]] 
+  has_many :portal_topics, :through => :portal_forums
+  has_many :user_topics, :through => :user_forums
   
   attr_accessible :name,:description , :import_id
   belongs_to :account
