7e0070c4da7280f841d12ecaa3a4e0de25312cf7
Parsuram Vijayasankar <venom@Parsus-MacBook-Pro.local>
2011-06-28 12:29:43 +0530

    Intrim local commit for ticket_view

--- app/models/helpdesk/ticket_field.rb
+++ app/models/helpdesk/ticket_field.rb
@@ -7,9 +7,25 @@ class Helpdesk::TicketField < ActiveRecord::Base
   belongs_to :flexifield_def_entry
   has_many :picklist_values, :as => :pickable, :class_name => 'Helpdesk::PicklistValue',
     :dependent => :destroy
-
   
   acts_as_list
+  
+  # Enumerator constant for mapping the CSS class name to the field type
+  FIELD_CLASS = { :default_subject      => "text",
+                  :default_requester    => "text",
+                  :default_ticket_type  => "dropdown",
+                  :default_status       => "dropdown", 
+                  :default_priority     => "dropdown",
+                  :default_group        => "dropdown",
+                  :default_agent        => "dropdown",
+                  :default_source       => "dropdown",
+                  :default_description  => "paragraph",
+                  :custom_text          => "text",
+                  :custom_paragraph     => "paragraph",
+                  :custom_checkbox      => "checkbox",
+                  :custom_number        => "number",
+                  :custom_dropdown      => "dropdown"
+                }
   
   # scope_condition for acts_as_list
   def scope_condition
