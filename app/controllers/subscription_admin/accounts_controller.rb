82565e8c87ff37435ebd16dd6a0dea75655e6318
kirandarisi <kiran.darisi@gmail.com>
2011-07-04 03:36:32 -0700

    Order added

--- app/controllers/subscription_admin/accounts_controller.rb
+++ app/controllers/subscription_admin/accounts_controller.rb
@@ -6,7 +6,7 @@ class SubscriptionAdmin::AccountsController < ApplicationController
   
   def index
     @accounts = search(params[:search])
-    @accounts = @accounts.paginate( :page => params[:page], :per_page => 30, :order => 'accounts.created_at')
+    @accounts = @accounts.paginate( :page => params[:page], :per_page => 30, :order => 'accounts.created_at desc')
   end
   
   def agents
