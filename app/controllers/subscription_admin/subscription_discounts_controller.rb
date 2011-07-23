f142f3cefe8af9a3a0aaab558735b56a3e306a06
Kiran Darisi <kiran.darisi@gmail.com>
2011-07-23 17:04:49 +0530

    not checking the account state for subscriptions discounts

--- app/controllers/subscription_admin/subscription_discounts_controller.rb
+++ app/controllers/subscription_admin/subscription_discounts_controller.rb
@@ -1,4 +1,5 @@
 class SubscriptionAdmin::SubscriptionDiscountsController < ApplicationController
+  skip_before_filter :check_account_state
   include ModelControllerMethods
   include AdminControllerMethods
 end
