c1bf0954657381237a11f355df8e0b1a7a7b1904
Kiran Darisi <kiran.darisi@gmail.com>
2011-08-02 18:03:20 +0530

    code re factored moved to lib

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,52 +3,11 @@ class ReportsController < Admin::AdminController
   include Reports::ConstructReport
   
   def index
-    
-   val = "responder"
-   
-   rep_tkts_by_agts = fetch_tkts_by_status(val)
-   @agts_tkts_by_status = tkts_by_status(rep_tkts_by_agts,val)
-   @agts_tkts_res_by_time = fetch_tkt_res_on_time(val)
-   @agts_tkts_over_due = fetch_overdue_tkts(val)
-   
-   val = "group"
-   
-   rep_tkts_by_grps = fetch_tkts_by_status(val)
-   @grps_tkts_by_status = tkts_by_status(rep_tkts_by_grps,val)
-   @grps_tkts_res_by_time = fetch_tkt_res_on_time(val)
-   @grps_tkts_over_due = fetch_overdue_tkts(val)
-   
+   @global_hash_agts = build_tkts_hash("responder")
+   @global_hash_grps = build_tkts_hash("group")
   end
  
  protected
- 
- def fetch_tkts_by_status(val)
-   scoper.tickets.find( :all,
-     :include => val, 
-     :select => "count(*) count, #{val}_id,status", 
-     :group => "#{val}_id,status")
- end
- 
- def fetch_tkt_res_on_time(val)
-   scoper.tickets.find(
-     :all, 
-     :select => "count(*) count, #{val}_id", 
-     :include => val,
-     :joins => "INNER JOIN helpdesk_ticket_states on helpdesk_tickets.id = helpdesk_ticket_states.ticket_id", 
-     :conditions => ['helpdesk_tickets.status IN (?,?) and helpdesk_tickets.due_by >  helpdesk_ticket_states.resolved_at',TicketConstants::STATUS_KEYS_BY_TOKEN[:resolved],TicketConstants::STATUS_KEYS_BY_TOKEN[:closed]],
-     :group => "#{val}_id")
- end
- 
-  
- def fetch_overdue_tkts(val)
-   scoper.tickets.find(
-     :all, 
-     :select => "count(*) count, #{val}_id", 
-     :include => val,
-     :joins => "INNER JOIN helpdesk_ticket_states on helpdesk_tickets.id = helpdesk_ticket_states.ticket_id", 
-     :conditions => " (helpdesk_tickets.due_by <  helpdesk_ticket_states.resolved_at  || (helpdesk_ticket_states.resolved_at is null and   helpdesk_tickets.due_by < now() )) ",
-     :group => "#{val}_id")
- end
  
  def scoper
    current_account
--- lib/reports/construct_report.rb
+++ lib/reports/construct_report.rb
@@ -1,11 +1,19 @@
 module Reports::ConstructReport
   
-  def tkts_by_status(tkts,info)
+  def build_tkts_hash(val)
+    @val = val
+    @global_hash = tkts_by_status(fetch_tkts_by_status)
+    merge_hash(:tkt_res_on_time,fetch_tkt_res_on_time)
+    merge_hash(:over_due_tkts,fetch_overdue_tkts)
+    @global_hash
+  end
+  
+  def tkts_by_status(tkts)
    data = {}
    tkts.each do |tkt|
     status_hash = {}
-    info_val = info.eql?("responder") ? "email" : "name"
-    responder = tkt.send("#{info}").blank? ? "Unassigned" : tkt.send("#{info}").send("#{info_val}")
+    #info_val = info.eql?("responder") ? "email" : "name"
+    responder = tkt.send("#{@val}_id").blank? ? "Unassigned" : tkt.send("#{@val}_id")
     if data.has_key?(responder)
       status_hash = data.fetch(responder)
     end
@@ -15,5 +23,52 @@ module Reports::ConstructReport
     data.store(responder,status_hash)
    end
      data
+ end
+ 
+ def merge_hash(key,res_hash)
+   res_hash.each do |tkt|
+     responder = tkt.send("#{@val}_id").blank? ? "Unassigned" : tkt.send("#{@val}_id")
+     status_hash = @global_hash.fetch(responder)
+     status_hash.store(key,tkt.count)
+     @global_hash.store(responder,status_hash)
   end
+ end
+ 
+ def date_condition
+   " helpdesk_tickets.created_at between '#{30.days.ago}' and now() "
+ end
+ 
+ def fetch_tkts_by_status
+   scoper.tickets.find( :all,
+     :include => @val, 
+     :select => "count(*) count, #{@val}_id,status", 
+     :conditions => "#{date_condition}",
+     :group => "#{@val}_id,status")
+ end
+ 
+ def fetch_tkt_res_on_time
+   scoper.tickets.find(
+     :all, 
+     :select => "count(*) count, #{@val}_id", 
+     :include => @val,
+     :joins => "INNER JOIN helpdesk_ticket_states on helpdesk_tickets.id = helpdesk_ticket_states.ticket_id", 
+     :conditions => ["helpdesk_tickets.status IN (?,?) and helpdesk_tickets.due_by >  helpdesk_ticket_states.resolved_at and #{date_condition}",TicketConstants::STATUS_KEYS_BY_TOKEN[:resolved],TicketConstants::STATUS_KEYS_BY_TOKEN[:closed]],
+     :group => "#{@val}_id")
+ end
+ 
+  
+ def fetch_overdue_tkts
+   scoper.tickets.find(
+     :all, 
+     :select => "count(*) count, #{@val}_id", 
+     :include => @val,
+     :joins => "INNER JOIN helpdesk_ticket_states on helpdesk_tickets.id = helpdesk_ticket_states.ticket_id", 
+     :conditions => " (helpdesk_tickets.due_by <  helpdesk_ticket_states.resolved_at  || (helpdesk_ticket_states.resolved_at is null and   helpdesk_tickets.due_by < now() ))  and #{date_condition} ",
+     :group => "#{@val}_id")
+ end
+ 
+ def scoper
+   Account.current
+ end
+ 
 end
