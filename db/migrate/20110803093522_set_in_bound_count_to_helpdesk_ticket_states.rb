46ed060d236be43a53edfc328bd7e96c4e132160
shihab <shihab@freshdesk.com>
2011-08-04 16:57:53 +0530

    migration for ticket states has been handled

--- db/migrate/20110803093522_set_in_bound_count_to_helpdesk_ticket_states.rb
+++ db/migrate/20110803093522_set_in_bound_count_to_helpdesk_ticket_states.rb
@@ -5,6 +5,12 @@ class SetInBoundCountToHelpdeskTicketStates < ActiveRecord::Migration
       inbound_count = tkt_state.tickets.notes.find(:all,:conditions => {:incoming => true}).count
       inbound_count ||= 0
       tkt_state.inbound_count = inbound_count + 1
+      tkt_state.opened_at ||= tkt_state.created_at
+      tkt_state.assigned_at ||= tkt_state.created_at if tkt_states.tickets.responder_id
+      tkt_state.first_assigned_at  ||= tkt_state.created_at if tkt_states.tickets.responder_id
+      tkt_state.pending_since  ||= tkt_state.created_at if tkt_states.tickets.pending?
+      tkt_state.resolved_at   ||= tkt_state.created_at if tkt_states.tickets.resolved?
+      tkt_state.resolved_at ||= ticket_states.closed_at  ||= ticket_states.created_at if tkt_states.tickets.closed?      
       tkt_state.save
     end
   end
