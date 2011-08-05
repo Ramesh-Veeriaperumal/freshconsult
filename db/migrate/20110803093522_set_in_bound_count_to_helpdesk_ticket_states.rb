25a4efc833710b1ab1bad294008060a8494da889
shihab <shihab@freshdesk.com>
2011-08-05 11:45:10 +0530

    reverted the change

---
+++ db/migrate/20110803093522_set_in_bound_count_to_helpdesk_ticket_states.rb
@@ -0,0 +1,20 @@
+class SetInBoundCountToHelpdeskTicketStates < ActiveRecord::Migration
+  def self.up
+    tkt_states = Helpdesk::TicketState.find(:all)
+    tkt_states.each do |tkt_state|
+      inbound_count = tkt_state.tickets.notes.find(:all,:conditions => {:incoming => true}).count
+      inbound_count ||= 0
+      tkt_state.inbound_count = inbound_count + 1
+      tkt_state.opened_at ||= tkt_state.created_at
+      tkt_state.assigned_at ||= tkt_state.created_at if tkt_state.tickets.responder_id
+      tkt_state.first_assigned_at  ||= tkt_state.created_at if tkt_state.tickets.responder_id
+      tkt_state.pending_since  ||= tkt_state.created_at if tkt_state.tickets.pending?
+      tkt_state.resolved_at   ||= tkt_state.created_at if tkt_state.tickets.resolved?
+      tkt_state.resolved_at ||= tkt_state.closed_at  ||= tkt_state.created_at if tkt_state.tickets.closed?      
+      tkt_state.save
+    end
+  end
+
+  def self.down
+  end
+end
