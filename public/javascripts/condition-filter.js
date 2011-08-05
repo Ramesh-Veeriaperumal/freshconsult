9a5aa346590b6a6a5fe6165b9d0efef83a338099
Parsuram <parsu@freshdesk.com>
2011-08-05 12:02:57 +0530

    Validation for supervisor, dispatcher and scenario automation
    - Fixed i18 bug for forums

--- public/javascripts/condition-filter.js
+++ public/javascripts/condition-filter.js
@@ -188,7 +188,7 @@ rules_filter = function(_name, filter_data, parentDom, options){
 			jQuery(parentDom).parents('form:first').submit(function(e){
 				domUtil.get_filter_list('json', this);
 				//console.log(hidden_.val());
-				//return true;
+			   //return false;
 			});
 
 			jQuery('.l_placeholder').live("click", function(ev){
