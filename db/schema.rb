3e9ca8d23264603d21877186737f64ca201af5c9
Shan <shan@freshdesk.com>
2011-08-16 17:13:12 +0530

    Migration and seed data for surveys.
    Signed-off-by: Shan <shan@freshdesk.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110816055438) do
+ActiveRecord::Schema.define(:version => 20110816100818) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
