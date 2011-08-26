8b3ecc853b2240b72bdf90135591ff9c21f3d674
Parsuram <parsu@freshdesk.com>
2011-08-26 16:36:20 +0530

    Company view for admin

--- app/controllers/support/company_tickets_controller.rb
+++ app/controllers/support/company_tickets_controller.rb
@@ -9,6 +9,7 @@ class Support::CompanyTicketsController < ApplicationController
   before_filter :verify_permission
   
   def index    
+    @page_title = t('helpdesk.tickets.views.all')
     build_tickets
     respond_to do |format|
       format.html
@@ -17,38 +18,39 @@ class Support::CompanyTicketsController < ApplicationController
   end
   
   def filter   
+    @page_title = t("helpdesk.tickets.views.#{current_filter}")
     build_tickets
     render :index
   end
   
   def requester    
     @requested_by = params[:id]
+    @page_title = "Tickets by #{current_account.users.find_by_id(@requested_by).name}"
     build_tickets
     render :index
   end
   
-  protected
-  
-  def current_filter
-    params[:id] || 'visible'
-  end
-  
-  def build_tickets
-     @tickets = TicketsFilter.filter(current_filter.to_sym , current_user , ticket_scope.tickets)
-     @tickets ||= []    
-  end
-  
-  def ticket_scope
-    current_account.users.find_by_id(@requested_by) || current_user.customer
-  end
-  
-  def verify_permission
-    params.symbolize_keys!      
-    unless current_user && current_user.client_manager?
-      flash[:notice] = t("flash.general.access_denied")
-      #redirect_to Helpdesk::ACCESS_DENIED_ROUTE 
-      redirect_to send(Helpdesk::ACCESS_DENIED_ROUTE) 
+  protected  
+    def current_filter
+      params[:id] || 'visible'
     end
-  end
+  
+    def build_tickets
+       @tickets = TicketsFilter.filter(current_filter.to_sym, current_user, ticket_scope.tickets)
+       @tickets ||= []    
+    end
+  
+    def ticket_scope
+      current_account.users.find_by_id(@requested_by) || current_user.customer
+    end
+  
+    def verify_permission
+      params.symbolize_keys!      
+      unless current_user && current_user.client_manager?
+        flash[:notice] = t("flash.general.access_denied")
+        #redirect_to Helpdesk::ACCESS_DENIED_ROUTE 
+        redirect_to send(Helpdesk::ACCESS_DENIED_ROUTE) 
+      end
+    end
   
 end
