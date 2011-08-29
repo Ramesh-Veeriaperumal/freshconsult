2c66d6acfbda6e1f7e91483d62e50dd49355ebaa
Shan <shan@freshdesk.com>
2011-08-29 11:51:15 +0530

    Survey feedback backend completed.
    Signed-off-by: Shan <shan@freshdesk.com>

--- app/models/survey_result.rb
+++ app/models/survey_result.rb
@@ -1,2 +1,21 @@
 class SurveyResult < ActiveRecord::Base
+  has_one :survey_remark, :dependent => :destroy
+  belongs_to :surveyable, :polymorphic => true
+  
+  def add_feedback(feedback)
+    note = surveyable.notes.build({
+      :user_id => customer_id,
+      :body => feedback,
+      :source => Helpdesk::Note::SOURCE_KEYS_BY_TOKEN["feedback"],
+      :incoming => true,
+      :private => false
+    })
+    
+    note.account_id = account_id
+    note.save
+    
+    create_survey_remark({
+      :note_id => note.id
+    })
+  end
 end
