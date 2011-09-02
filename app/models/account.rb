b2c4a320d18c016a2b6c41bbcabc0c3e9d244d23
shihab <shihab@freshdesk.com>
2011-09-02 11:22:29 +0530

    facebook_pages added in model
    Signed-off-by: shihab <shihab@freshdesk.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -104,9 +104,11 @@ class Account < ActiveRecord::Base
   has_many :canned_responses , :class_name =>'Admin::CannedResponse' , :dependent => :destroy  
   has_many :user_accesses , :class_name =>'Admin::UserAccess' , :dependent => :destroy
   
-  has_many :twitter_handles, :class_name =>'Social::TwitterHandle' 
+  has_many :twitter_handles, :class_name =>'Social::TwitterHandle' ,:dependent => :destroy
   
-  has_many :tweets, :class_name =>'Social::Tweet' 
+  has_many :tweets, :class_name =>'Social::Tweet' ,:dependent => :destroy
+  
+  has_many :facebook_pages, :class_name =>'Social::FacebookPage' ,:dependent => :destroy
   
   
   has_one :survey, :dependent => :destroy
