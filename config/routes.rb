5427006e694f9158fb6c447c7f958d9b60f6678c
Kiran Darisi <kiran.darisi@gmail.com>
2011-09-21 18:45:32 +0530

    export csv for customers and trials

--- config/routes.rb
+++ config/routes.rb
@@ -85,8 +85,8 @@ ActionController::Routing::Routes.draw do |map|
   map.with_options(:conditions => {:subdomain => AppConfig['admin_subdomain']}) do |subdom|
     subdom.root :controller => 'subscription_admin/subscriptions', :action => 'index'
     subdom.with_options(:namespace => 'subscription_admin/', :name_prefix => 'admin_', :path_prefix => nil) do |admin|
-      admin.resources :subscriptions, :member => { :charge => :post, :extend_trial => :post }, :collection => {:customers => :get}
-      admin.resources :accounts, :collection => {:agents => :get, :helpdesk_urls => :get, :tickets => :get}
+      admin.resources :subscriptions, :member => { :charge => :post, :extend_trial => :post }, :collection => {:customers => :get, :customers_csv => :get}
+      admin.resources :accounts, :collection => {:agents => :get, :helpdesk_urls => :get, :tickets => :get, :renewal_csv => :get}
       admin.resources :subscription_plans, :as => 'plans'
       admin.resources :subscription_discounts, :as => 'discounts'
       admin.resources :subscription_affiliates, :as => 'affiliates'
