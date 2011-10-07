12ce5dc244ff1e5620e26e693faa848802897eef
Shan <shan@freshdesk.com>
2011-10-07 14:56:36 +0530

    Some more tweaking for sphinx..
    Signed-off-by: Shan <shan@freshdesk.com>

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,11 +1,10 @@
 #To restart delayed_job workers..
 run "sudo monit -g dj_helpkit restart all"
 
-puts "***** AFTER_RESTART is being called"
+#Not a smart way...
 on_utilities("sphinx_sla") do
-  puts "******* GOING to configure and index for thinking_sphinx"
-  
   run "bundle exec rake thinking_sphinx:configure"
   run "bundle exec rake thinking_sphinx:index"
+  run "bundle exec rake thinking_sphinx:start"
   execute "monit reload"
 end
