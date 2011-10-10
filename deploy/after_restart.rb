2ae42b6411426950cf045cfad70bfd0409390299
Shan <shan@freshdesk.com>
2011-10-10 11:52:40 +0530

    blank? doesn't work in deploy hooks..
    Signed-off-by: Shan <shan@freshdesk.com>

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,7 +2,8 @@
 run "sudo monit -g dj_helpkit restart all"
 
 on_utilities("sphinx_sla") do
-  if `ps aux | grep search[d]`.blank? #Need to revisit this again..
+  #1. Need to revisit this again. 2. blank? doesn't work in deploy hooks.
+  if `ps aux | grep search[d]` == ""
     run "bundle exec rake thinking_sphinx:configure"
     run "bundle exec rake thinking_sphinx:index"
     run "bundle exec rake thinking_sphinx:start"
