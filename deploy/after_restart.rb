4d804b872090a06226146a062ac0a93108a60ee0
Shan <shan@freshdesk.com>
2011-10-10 10:53:22 +0530

    coldstart check for sphinx configuration..
    Signed-off-by: Shan <shan@freshdesk.com>

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -3,8 +3,10 @@ run "sudo monit -g dj_helpkit restart all"
 
 #Not a smart way...
 on_utilities("sphinx_sla") do
-  run "bundle exec rake thinking_sphinx:configure"
-  run "bundle exec rake thinking_sphinx:index"
-  run "bundle exec rake thinking_sphinx:start"
-  #execute "monit reload"
+  unless FileTest.exist?("#{release_path}/config/#{environment}.sphinx.conf")
+    run "bundle exec rake thinking_sphinx:configure"
+    run "bundle exec rake thinking_sphinx:index"
+    run "bundle exec rake thinking_sphinx:start"
+    #execute "monit reload"
+  end
 end
