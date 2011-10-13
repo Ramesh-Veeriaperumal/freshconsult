64f6416130fcf41305ec1e2b3a78d7283635a4f2
kirandarisi <kiran.darisi@gmail.com>
2011-10-13 23:04:56 +0530

    ne mime type added

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,2 @@
-Mime::Type.register "application/vnd.openxmlformats-officedocument.wordprocessingml.document", :docx
-Mime::Type.register "application/vnd.openxmlformats-officedocument.presentationml.presentation", :pptx
-Mime::Type.register "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", :xlsx
+MIME::Types.add(MIME::Type.from_array("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", %(xlsx)))
 
