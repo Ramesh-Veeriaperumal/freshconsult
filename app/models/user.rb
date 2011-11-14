1672fb1c52fdbfd454e3766b37bb92159ddff1e4
shihab <shihab@freshdesk.com>
2011-11-14 16:20:11 +0530

    password reset url should be product url - fixed
    Signed-off-by: shihab <shihab@freshdesk.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -218,7 +218,7 @@ class User < ActiveRecord::Base
   end
   ##Authorization copy ends here
   
-  def deliver_password_reset_instructions! #Do we need delayed_jobs here?! by Shan
+  def deliver_password_reset_instructions!(portal) #Do we need delayed_jobs here?! by Shan
     reset_perishable_token!
     
     e_notification = account.email_notifications.find_by_notification_type(EmailNotification::PASSWORD_RESET)
@@ -231,7 +231,7 @@ class User < ActiveRecord::Base
     
     UserNotifier.deliver_password_reset_instructions(self, 
         :email_body => Liquid::Template.parse(template).render((user_key ||= 'agent') => self, 
-          'helpdesk_name' => account.portal_name, 'password_reset_url' => edit_password_reset_url(perishable_token, :host => account.host)))
+          'helpdesk_name' => account.portal_name, 'password_reset_url' => edit_password_reset_url(perishable_token, :host => (!portal.portal_url.blank?) ? portal.portal_url : account.host)))
   end
   
   def deliver_activation_instructions! #Need to refactor this.. Almost similar structure with the above one.
