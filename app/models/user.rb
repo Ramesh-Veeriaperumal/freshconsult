18afc30c09cf44dbc46ad35207618a4550ce0a4d
shihab <shihab@freshdesk.com>
2011-11-15 17:23:16 +0530

    subject in passowrd reset has been changed
    Signed-off-by: shihab <shihab@freshdesk.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -231,7 +231,8 @@ class User < ActiveRecord::Base
     
     UserNotifier.deliver_password_reset_instructions(self, 
         :email_body => Liquid::Template.parse(template).render((user_key ||= 'agent') => self, 
-          'helpdesk_name' => (!portal.name.blank?) ? portal.name : account.portal_name , 'password_reset_url' => edit_password_reset_url(perishable_token, :host => (!portal.portal_url.blank?) ? portal.portal_url : account.host)) , :reply_email => portal.product.friendly_email)
+          'helpdesk_name' => (!portal.name.blank?) ? portal.name : account.portal_name , 'password_reset_url' => edit_password_reset_url(perishable_token, :host => (!portal.portal_url.blank?) ? portal.portal_url : account.host)) , 
+          :subject => "#{ (!portal.name.blank?) ? portal.name : account.portal_name} password reset instructions" ,:reply_email => portal.product.friendly_email)
   end
   
   def deliver_activation_instructions!(portal) #Need to refactor this.. Almost similar structure with the above one.
