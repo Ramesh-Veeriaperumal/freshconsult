453e1cba6e9e31d98dd06874a555cb4f247723d8
Parsuram <parsu@freshdesk.com>
2011-12-20 17:02:53 +0530

    Time tracking with icons and gif

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -238,10 +238,7 @@ module ApplicationHelper
  end
  
  def get_time_in_hours time_in_second
-   hours = time_in_second.div(60*60)
-   minutes_as_percent = (time_in_second.div(60) % 60)*(1.667).round
-   total_time = hours.to_s()+"."+ minutes_as_percent.to_s()
-   total_time
+   [time_in_second.div(60*60), (time_in_second.div(60) % 60)*(1.667).round].map{ |t| t.to_s.rjust(2, '0') }.join(".")
  end
  
  def get_total_time time_sheets
