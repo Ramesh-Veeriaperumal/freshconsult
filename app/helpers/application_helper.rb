21c07ae4681d6b36c9de9ee5eb49a1db4962c023
shihab <shihab@freshdesk.com>
2012-01-05 17:18:14 +0530

    agent role ...report changed to reports
    Signed-off-by: shihab <shihab@freshdesk.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,7 +64,7 @@ module ApplicationHelper
       forums_tab,
       ['/contacts',           :customers,    permission?(:manage_tickets)],
       ['support/tickets',     :checkstatus, !permission?(:manage_tickets)],
-      ['/reports',            :reports,      permission?(:manage_report) ],
+      ['/reports',            :reports,      permission?(:manage_reports) ],
       ['/admin/home',         :admin,        permission?(:manage_users)],
       company_tickets_tab
     ]
