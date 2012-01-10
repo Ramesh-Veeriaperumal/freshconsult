538aa414d5f5c1722416a72ebfa9c81bce06423b
Kiran Darisi <kiran.darisi@gmail.com>
2012-01-10 14:25:56 +0530

    some more changes for the sphinx un instrumented time

--- config/initializers/new_relic_instrumentation.rb
+++ config/initializers/new_relic_instrumentation.rb
@@ -1,13 +1,15 @@
-if defined? ActiveRecord
-  ActiveRecord::Base.class_eval do
-    class << self
-      add_method_tracer :search, 'ActiveRecord/#{self.name}/search'
-      add_method_tracer :search, 'ActiveRecord/search', :push_scope => false
-      add_method_tracer :search, 'ActiveRecord/all', :push_scope => false
+require 'new_relic/agent/method_tracer.rb'
 
-      add_method_tracer :search_count, 'ActiveRecord/#{self.name}/search_count'
-      add_method_tracer :search_count, 'ActiveRecord/search_count', :push_scope => false
-      add_method_tracer :search_count, 'ActiveRecord/all', :push_scope => false
-    end
+if defined?(ThinkingSphinx::Search)
+  ThinkingSphinx::Search.class_eval do
+    include NewRelic::Agent::MethodTracer
+    add_method_tracer :search
+    add_method_tracer :populate
   end
+
+  Riddle::Client.class_eval do
+    include NewRelic::Agent::MethodTracer
+    add_method_tracer :query
+  end
+  
 end
