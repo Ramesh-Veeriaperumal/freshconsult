2300eb8a89d82815ceba8172881e101e84ab3e7a
Kiran Darisi <kiran.darisi@gmail.com>
2012-01-12 16:34:38 +0530

    new gem is added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,5 @@
+require 'ruby-prof'
+
 class SearchController < ApplicationController
   
   extend NewRelic::Agent::MethodTracer
@@ -13,7 +15,20 @@ class SearchController < ApplicationController
   end
   
   def suggest
+    RubyProf.start
     search
+    results = RubyProf.stop
+    File.open "#{RAILS_ROOT}/tmp/profile-graph.html", 'w' do |file|
+      RubyProf::GraphHtmlPrinter.new(results).print(file)
+    end
+ 
+    File.open "#{RAILS_ROOT}/tmp/profile-flat.txt", 'w' do |file|
+      RubyProf::FlatPrinter.new(results).print(file)
+    end
+ 
+    File.open "#{RAILS_ROOT}/tmp/profile-tree.prof", 'w' do |file|
+      RubyProf::CallTreePrinter.new(results).print(file)
+    end
     render :partial => '/search/navsearch_items'    
   end
   
