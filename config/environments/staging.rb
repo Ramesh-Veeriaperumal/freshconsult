0d7ee7186639503628144f9518085ebbdea12cfb
kirandarisi <kiran.darisi@gmail.com>
2012-01-12 17:54:45 +0530

    mimicing production configuration

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -13,7 +13,7 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
-config.reload_plugins = true
+#config.reload_plugins = true
 
 config.after_initialize do
   ActiveMerchant::Billing::Base.gateway_mode = :test
