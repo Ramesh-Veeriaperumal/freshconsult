6d488767dab358ad8665254e4a7f802ade3b36dc
shihab <shihab@freshdesk.com>
2012-01-13 12:38:27 +0530

    setting default source as phone, when agent create tickets..
    Signed-off-by: shihab <shihab@freshdesk.com>

--- app/controllers/helpdesk/tickets_controller.rb
+++ app/controllers/helpdesk/tickets_controller.rb
@@ -309,6 +309,7 @@ class Helpdesk::TicketsController < ApplicationController
       @item.description = @topic.posts.first.body
       @item.requester   = @topic.user
     end
+    @item.source = Helpdesk::Ticket::SOURCE_KEYS_BY_TOKEN[:phone] #setting for agent new ticket- as phone
   end
  
   def create
