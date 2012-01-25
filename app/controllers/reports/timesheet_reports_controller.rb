c41483d876c2ec6dddbd6108096b199ba0fd82b2
shihab <shihab@freshdesk.com>
2012-01-25 15:37:10 +0530

    time sheet export problem -corrected
    Signed-off-by: shihab <shihab@freshdesk.com>

--- app/controllers/reports/timesheet_reports_controller.rb
+++ app/controllers/reports/timesheet_reports_controller.rb
@@ -4,7 +4,9 @@ class Reports::TimesheetReportsController < ApplicationController
   
   before_filter { |c| c.requires_permission :manage_tickets }
   before_filter :set_selected_tab
-  before_filter :build_time_sheet, :only => [:index, :export_csv ,:report_filter]
+  before_filter :build_item ,  :only => [:index,:export_csv,:report_filter]
+  before_filter :time_sheet_list, :only => [:index,:report_filter]
+  before_filter :time_sheet_for_export, :only => [:export_csv]
 
   
   def report_filter
@@ -28,18 +30,14 @@ class Reports::TimesheetReportsController < ApplicationController
             :disposition => "attachment; filename=time_sheet.csv"
   end
   
-   def build_time_sheet
-    @start_date = params[:start_date] ?  Date.parse(params[:start_date]).beginning_of_day : start_of_month(Time.now.month.to_i)
-    @end_date = params[:end_date] ? Date.parse(params[:end_date]).end_of_day : Time.now.end_of_day
-    @customer_id = params[:customer_id] || []
-    @user_id = params[:user_id] || []
-    group_by_caluse = params[:group_by] || :ticket 
-    group_by_caluse = group_by_caluse.to_sym()
-    list_view_items = [:ticket , :customer_name , :note , :group_by_day_criteria ,:agent_name, :hours]
-    @headers = list_view_items.delete_if{|item| item == group_by_caluse }
-    @billable = (!params[:billable].blank? && !params[:billable].to_s.eql?("falsetrue")) ? [params[:billable].to_s.to_bool] : [true,false]
-    @time_sheets = current_account.time_sheets.for_customers(@customer_id).by_agent(@user_id).created_at_inside(@start_date,@end_date).hour_billable(@billable).group_by(&group_by_caluse)
+   def time_sheet_list
+     @time_sheets = current_account.time_sheets.for_customers(@customer_id).by_agent(@user_id).created_at_inside(@start_date,@end_date).hour_billable(@billable).group_by(&group_by_caluse)
+   end
+  
+  def time_sheet_for_export
+     @time_sheets = current_account.time_sheets.for_customers(@customer_id).by_agent(@user_id).created_at_inside(@start_date,@end_date).hour_billable(@billable)
   end
+
 
 
 end
