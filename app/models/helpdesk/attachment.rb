3af6230a1ddef4431a101eea9b22984943df33f5
kirandarisi <kiran.darisi@gmail.com>
2012-01-27 13:34:03 +0530

    click software pdf issue has been fixed

--- app/models/helpdesk/attachment.rb
+++ app/models/helpdesk/attachment.rb
@@ -18,13 +18,13 @@ class Helpdesk::Attachment < ActiveRecord::Base
   
     #before_validation_on_create :set_random_secret
     before_post_process :image?
-    #before_create :set_content_type
+    before_create :set_content_type
   
-  def set_content_type
-    mime_content_type = MIME::Types.type_for(content_file_name).to_s
-    self.content_content_type = mime_content_type unless mime_content_type.blank?
-  end
-  
+    def set_content_type
+      mime_content_type = File.extname(self.content_file_name).gsub('.','')
+      self.content_content_type = "application/pdf" if !mime_content_type.blank? and mime_content_type.eql?("pdf")
+   end
+   
   def attachment_url
     class_string =  self.class
     "#{class_string.to_s.tableize}/#{id}/#{content_file_name}"
