6d722b820f47e31fb7264cc9cbb905e5797a44b3
Kiran Darisi <kiran.darisi@gmail.com>
2012-02-11 18:39:20 +0530

    contact is save

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -28,6 +28,7 @@ require 'openssl'
       @current_user = current_account.users.find_by_email(params[:email])  
       unless @current_user
         @current_user = create_user(params[:email],current_account,nil,{:name => params[:name]})
+        @current_user.save
       else
         @current_user.update_attributes(:name => params[:name])
       end
