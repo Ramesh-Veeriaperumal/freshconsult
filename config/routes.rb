edf92e908541b429a48002a3945d0afa51265007
shihab <shihab@freshdesk.com>
2012-02-13 12:30:29 +0530

    reports route problem fix
    Signed-off-by: shihab <shihab@freshdesk.com>

--- config/routes.rb
+++ config/routes.rb
@@ -90,11 +90,8 @@
     admin.resources :email_commands_setting, :member => { :update => :put }
   end
   
-  map.resources :reports do |report|
-    #report.resources :timesheet_reports
-  end
-
-  map.timesheet_report    '/timesheet_reports', :controller => 'reports/timesheet_reports', :action => 'index'
+  map.resources :reports
+  map.resources :timesheet_reports , :controller => 'reports/timesheet_reports' , :collection => {:report_filter => :post} 
   map.customer_activity   '/activity_reports/customer', :controller => 'reports/customer_reports', :action => 'index'
   map.helpdesk_activity   '/activity_reports/helpdesk', :controller => 'reports/helpdesk_reports', :action => 'index'
   map.customer_activity_generate   '/activity_reports/customer/generate', :controller => 'reports/customer_reports', :action => 'generate'
