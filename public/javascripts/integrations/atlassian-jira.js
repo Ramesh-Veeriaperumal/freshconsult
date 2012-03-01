98711ac8e7952c19f87da593db119df9aa2d8b7f
Sathappan Muthu <sathappan.m@gmail.com>
2012-03-01 11:37:11 +0530

    jira integration changes v 1.7
    Signed-off-by: Sathappan Muthu <sathappan.m@gmail.com>

--- public/javascripts/integrations/atlassian-jira.js
+++ public/javascripts/integrations/atlassian-jira.js
@@ -588,10 +588,10 @@ JiraWidget.prototype= {
 	processFailure: function(evt) {
 		if (evt.status == 401) {
 			alert("Username or password is incorrect.");
-			//harvestWidget.freshdeskWidget.display_login();
 		} else if (evt.status == 404) {
 			alert("Issue not available or Premission denied");
-			jiraWidget.unlinkJiraIssue();
+			jiraWidget.freshdeskWidget.delete_integrated_resource(jiraBundle.integrated_resource_id);
+			jiraWidget.displayCreateWidget();
 		} 
 		else{
 			// log("Server Error")
