4fbd6e41d5dafb5f787e83be27e55b87bc107c5f
Thanashyam Raj <thanashyam@gmail.com>
2012-03-21 21:57:38 +0530

    Changes to Agent and Group summary reports
    Fixed the issues pertaining to Agent and Group Ticket summary
    
    Fixed the Single Date issue.
    
    Added Avg 1st response time.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,37 @@ module ApplicationHelper
   def format_float_value(val)
     sprintf( "%0.02f", val) unless val.nil? 
   end
+
+  def timediff_in_words(interval)
+    secs  = interval.to_i
+    mins  = (secs / 60).to_i
+    secs = secs - (mins * 60)
+
+    hours = (mins / 60).to_i
+    mins = mins - (hours * 60)
+
+    days  = (hours / 24).to_i
+    hours = hours - (days * 24)
+
+    if days > 0
+      "#{days} days and #{hours % 24} hours"
+    elsif hours > 0
+      "#{hours} hours and #{mins % 60} minutes"
+    elsif mins > 0
+      "#{mins} minutes and #{secs % 60} seconds"
+    elsif secs >= 0
+      "#{secs} seconds"
+    end
+
+  end
+
+  def percentage(numerator, denominator)
+    if denominator == 0
+      "-"
+    else
+      format_float_value(100 * numerator / denominator) + '%'
+    end
+  end
   
   def show_flash
     [:notice, :warning, :error].collect {|type| content_tag('div', flash[type], :id => type, :class => "flash_info #{type}") if flash[type] }
