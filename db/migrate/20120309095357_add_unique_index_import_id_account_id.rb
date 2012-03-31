eb848374c04dbfeb89d9201bdb2f668335414453
Kiran Darisi <kiran.darisi@gmail.com>
2012-03-31 11:45:29 +0530

    import index is removed

--- db/migrate/20120309095357_add_unique_index_import_id_account_id.rb
+++ db/migrate/20120309095357_add_unique_index_import_id_account_id.rb
@@ -1,11 +1,11 @@
 class AddUniqueIndexImportIdAccountId < ActiveRecord::Migration
   def self.up
-    add_index :helpdesk_tickets, [:account_id, :import_id], :name => 'index_helpdesk_tickets_on_account_id_and_import_id', :unique => true
-    add_index :users, [:account_id, :import_id], :name => 'index_users_on_account_id_and_import_id', :unique => true
+#    add_index :helpdesk_tickets, [:account_id, :import_id], :name => 'index_helpdesk_tickets_on_account_id_and_import_id', :unique => true
+#    add_index :users, [:account_id, :import_id], :name => 'index_users_on_account_id_and_import_id', :unique => true
   end
 
   def self.down
-    remove_index :helpdesk_tickets, [:account_id, :import_id]
-    remove_index :users, [:account_id, :import_id]
+#    remove_index :helpdesk_tickets, [:account_id, :import_id]
+#    remove_index :users, [:account_id, :import_id]
   end
 end
