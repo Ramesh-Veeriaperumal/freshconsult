4efcc4ee80738055f0cdb8bc843e1613ebe8d270
Kiran Darisi <kiran.darisi@gmail.com>
2012-04-12 19:49:07 +0530

    not null for account id has been added

--- db/migrate/20120402095925_add_account_id_to_helpdesk_ticket_states.rb
+++ db/migrate/20120402095925_add_account_id_to_helpdesk_ticket_states.rb
@@ -1,6 +1,6 @@
 class AddAccountIdToHelpdeskTicketStates < ActiveRecord::Migration
   def self.up
-    add_column :helpdesk_ticket_states, :account_id, "bigint unsigned"
+    add_column :helpdesk_ticket_states, :account_id, "bigint unsigned",:null => false
   end
 
   def self.down
--- db/migrate/20120406093504_add_account_id_to_flexifields.rb
+++ db/migrate/20120406093504_add_account_id_to_flexifields.rb
@@ -1,6 +1,6 @@
 class AddAccountIdToFlexifields < ActiveRecord::Migration
   def self.up
-    add_column :flexifields, :account_id,  "bigint unsigned"
+    add_column :flexifields, :account_id,  "bigint unsigned",:null => false
   end
 
   def self.down
