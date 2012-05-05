65f0329c50bc718b0b3ec210e2b0a1016a0ff7bf
Vivekanandan M <vivekanandan@gmail.com>
2012-05-05 12:26:32 +0530

    default value as others for referrer_type

--- db/migrate/20120412073942_conversion_metrics.rb
+++ db/migrate/20120412073942_conversion_metrics.rb
@@ -2,9 +2,10 @@ class ConversionMetrics < ActiveRecord::Migration
   def self.up
     add_column :conversion_metrics, :referrer_type, :integer
     ConversionMetric.all.each { |metric|      
-      metric.update_keywords(metric.first_referrer)
+      metric.update_keywords(metric.first_referrer) unless metric.first_referrer.blank?
+      referer_type_val = metric.get_referrer_type(metric.first_referrer,true)
       metric.update_attributes!({
-        :referrer_type => metric.get_referrer_type(metric.first_referrer,true),
+        :referrer_type =>  referer_type_val || 8,
         :keywords => metric.keywords
         })
     }
