82b869aef780913258c199ae6ce3c12a0330909a
Vivekanandan M <vivekanandan@gmail.com>
2012-05-05 12:42:11 +0530

    ignored the update_keywords on exception

--- db/migrate/20120412073942_conversion_metrics.rb
+++ db/migrate/20120412073942_conversion_metrics.rb
@@ -1,17 +1,25 @@
 class ConversionMetrics < ActiveRecord::Migration
   def self.up
     add_column :conversion_metrics, :referrer_type, :integer
-    ConversionMetric.all.each { |metric|      
+    ConversionMetric.all.each { |metric|
+    begin
       metric.update_keywords(metric.first_referrer) unless metric.first_referrer.blank?
-      referer_type_val = metric.get_referrer_type(metric.first_referrer,true)
+      referrer_type_val = metric.get_referrer_type(metric.first_referrer,true)
       metric.update_attributes!({
-        :referrer_type =>  referer_type_val || 8,
+        :referrer_type =>  referrer_type_val || 8,
         :keywords => metric.keywords
         })
+        rescue
+                 referrer_type_val = metric.get_referrer_type(metric.first_referrer,true)
+                       metric.update_attributes!({
+                               :referrer_type =>  referrer_type_val || 8,
+                                               })
+
+        end
     }
   end
 
   def self.down
-   remove_column :conversion_metrics, :referrer_type    
+   remove_column :conversion_metrics, :referrer_type
   end
 end
