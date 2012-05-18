1bcea47e88d29ee39c82a31ed7ed209a4ebcc251
Kiran Darisi <kiran.darisi@gmail.com>
2012-05-18 14:46:46 +0530

    added affiliate support

--- app/models/subscription_affiliate.rb
+++ app/models/subscription_affiliate.rb
@@ -1,18 +1,43 @@
-# SubscriptionAffiliate keeps track of the affiliates who are paid
-# for sending traffic to your site.  The token is used for
-# constructing affiliate URLs, like 
-# http://your.domain.com/signup?ref=foo.  The rate is the percentage
-# of each subscription payment that the affiliate should receive.
-# For example, a $99 monthly subscription linked to an affiliate 
-# with a rate of 0.10 will earn the affiliate $9 per month.
 class SubscriptionAffiliate < ActiveRecord::Base
   has_many :subscriptions
   has_many :subscription_payments
+  
+  COMMISSION = 0.20
+  AFILIATE_SOWFTWARE = "ShareASale"
   
   validates_presence_of :token
   validates_uniqueness_of :token
   validates_numericality_of :rate, :greater_than_or_equal_to => 0,
     :less_than_or_equal_to => 1
+    
+  def self.merchant_id
+    40631
+  end
+  
+  def self.affiliate_param
+    "SSAID"
+  end
+  
+  def self.add_affiliate(account)
+    if !account.conversion_metric.nil? and 
+      account.conversion_metric.first_referrer.include?(affiliate_param)
+      uri = account.conversion_metric.first_referrer
+      env = Rack::MockRequest.env_for(uri)
+      req = Rack::Request.new(env)
+      params = req.params
+      affiliate_id = params.fetch(affiliate_param)
+      unless affiliate_id.nil?
+        affiliate = find_by_token(affiliate_id)
+        affiliate = create({:name => AFILIATE_SOWFTWARE,
+                            :rate => COMMISSION,
+                            :token => affiliate_id}) unless affiliate
+       account.subscription.affiliate = affiliate  
+       account.subscription.save
+      end
+    end
+  end
+  
+  
   
   # Return the fees owed to an affiliate for a particular time
   # period. The period defaults to the previous month.
