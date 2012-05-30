1cd95e190847e91ce88e5bdb9bdd0562f0d224b5
johnpaulh <johnpaul@freshdesk.com>
2012-05-30 19:47:27 +0530

    email_notification html editor :changes for the initial templates loaded for the new account.

--- db/fixtures/040_email_notifications.rb
+++ db/fixtures/040_email_notifications.rb
@@ -6,30 +6,8 @@ EmailNotification.seed_many(:account_id, :notification_type, [
     :account_id => account.id, 
     :requester_notification => true, 
     :agent_notification => true,
-    :agent_template => 'Hi {{agent.name}},
-
-Your {{helpdesk_name}} account has been created.
-
-Click the url below to activate your account!
-
-{{activation_url}}
-
-If the above URL does not work try copying and pasting it into your browser. If you continue to have problems, please feel free to contact us.
-
-Regards,
-{{helpdesk_name}}',
-    :requester_template => 'Hi {{contact.name}},
-
-A new {{helpdesk_name}} account has been created for you.
-
-Click the url below to activate your account and select a password!
-
-{{activation_url}}
-
-If the above URL does not work try copying and pasting it into your browser. If you continue to have problems, please feel free to contact us.
-
-Regards,
-{{helpdesk_name}}',
+    :agent_template => 'Hi {{agent.name}},<br /><br />Your {{helpdesk_name}} account has been created.<br /><br />Click the url below to activate your account!<br /><br />{{activation_url}}<br /><br />If the above URL does not work try copying and pasting it into your browser. If you continue to have problems, please feel free to contact us.<br /><br />Regards,<br />{{helpdesk_name}}',
+    :requester_template => '<p>Hi {{contact.name}},<br /><br />A new {{helpdesk_name}} account has been created for you.<br /><br />Click the url below to activate your account and select a password!<br /><br />{{activation_url}}<br /><br />If the above URL does not work try copying and pasting it into your browser. If you continue to have problems, please feel free to contact us.<br /><br />Regards,<br />{{helpdesk_name}}</p>',
     :requester_subject_template => "{{ticket.portal_name}} user activation",
     :agent_subject_template => "{{ticket.portal_name}} user activation"
   },
@@ -38,22 +16,8 @@ Regards,
     :account_id => account.id, 
     :requester_notification => true, 
     :agent_notification => true,
-    :agent_template => 'A request to reset your password has been made. If you did not make this request, simply ignore this email. If you did make this request, just click the link below:
-
-{{password_reset_url}}
-
-If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.
-
-Regards,
-{{helpdesk_name}}',
-    :requester_template => 'A request to reset your password has been made. If you did not make this request, simply ignore this email. If you did make this request, just click the link below:
-
-{{password_reset_url}}
-
-If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.
-
-Regards,
-{{helpdesk_name}}',
+    :agent_template => '<p>A request to reset your password has been made. If you did not make this request, simply ignore this email. If you did make this request, just click the link below:<br /><br />{{password_reset_url}}<br /><br />If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.<br /><br />Regards,<br />{{helpdesk_name}}</p>',
+    :requester_template => '<p>A request to reset your password has been made. If you did not make this request, simply ignore this email. If you did make this request, just click the link below:<br /><br />{{password_reset_url}}<br /><br />If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.<br /><br />Regards,<br />{{helpdesk_name}}</p>',
     :requester_subject_template => "{{ticket.portal_name}} password reset instructions",
     :agent_subject_template => "{{ticket.portal_name}} password reset instructions"
   },
@@ -61,38 +25,15 @@ Regards,
     :notification_type => EmailNotification::NEW_TICKET, 
     :account_id => account.id, :requester_notification => true, 
     :agent_notification => false,
-    :agent_template => 'Hi,
-
-A new ticket has been created. 
-You may view and respond to the ticket here {{ticket.url}}
-
-Regards,
-{{helpdesk_name}}',
-    :requester_template => 'Dear {{ticket.requester.name}},
-
-We would like to acknowledge that we have received your request and a ticket has been created with Ticket ID - {{ticket.id}}.
-A support representative will be reviewing your request and will send you a personal response.(usually within 24 hours).
-
-To view the status of the ticket or add comments, please visit 
-{{ticket.url}}
-
-Thank you for your patience.
-
-Sincerely,
-{{helpdesk_name}} Support Team',
+    :agent_template => 'Hi,<br /><br />A new ticket has been created. <br />You may view and respond to the ticket here {{ticket.url}}<br /><br />Regards,<br />{{helpdesk_name}}',
+    :requester_template => '<p>Dear {{ticket.requester.name}},<br /><br />We would like to acknowledge that we have received your request and a ticket has been created with Ticket ID - {{ticket.id}}.<br />A support representative will be reviewing your request and will send you a personal response.(usually within 24 hours).<br /><br />To view the status of the ticket or add comments, please visit <br />{{ticket.url}}<br /><br />Thank you for your patience.<br /><br />Sincerely,<br />{{helpdesk_name}} Support Team</p>',
     :requester_subject_template => "Ticket Received - [\#{{ticket.id}}] {{ticket.subject}}",
     :agent_subject_template => "New Ticket has been created - [\#{{ticket.id}}] {{ticket.subject}}"
   },
   { :notification_type => EmailNotification::TICKET_ASSIGNED_TO_GROUP, 
       :account_id => account.id, :requester_notification => false, 
       :agent_notification => true,
-      :agent_template => 'Hi
-
-A new ticket ({{ticket.id}}) has been assigned to your group "{{ticket.group.name}}". Please follow the link below to view the ticket.
-
-{{ticket.subject}}
-{{ticket.description}}
-{{ticket.url}}',
+      :agent_template => '<p>Hi<br /><br />A new ticket ({{ticket.id}}) has been assigned to your group "{{ticket.group.name}}". Please follow the link below to view the ticket.<br /><br />{{ticket.subject}}<br />{{ticket.description}}<br />{{ticket.url}}</p>',
     :agent_subject_template => "Assigned to Group - [\#{{ticket.id}}] {{ticket.subject}}"
 },
   {
@@ -100,59 +41,31 @@ A new ticket ({{ticket.id}}) has been assigned to your group "{{ticket.group.nam
     :account_id => account.id, 
     :requester_notification => false, 
     :agent_notification => true,
-    :agent_template => 'Hi {{agent.name}},
-
-A new ticket (#{{ticket.id}}) in group {{ticket.group.name}} is currently unassigned for more than {{ticket.group.assign_time_mins}} minutes.
-
-Ticket Details: 
-
-Subject - {{ticket.subject}}
-
-Description  - {{ticket.description}}
-
-This is an escalation email for the {{ticket.group.name}} group in {{helpdesk_name}}',
+    :agent_template => '<p>Hi {{agent.name}},<br /><br />A new ticket (#{{ticket.id}}) in group {{ticket.group.name}} is currently unassigned for more than {{ticket.group.assign_time_mins}} minutes.<br /><br />Ticket Details: <br /><br />Subject - {{ticket.subject}}<br /><br />Description  - {{ticket.description}}<br /><br />This is an escalation email for the {{ticket.group.name}} group in {{helpdesk_name}}</p>',
     :agent_subject_template => "Unattended Ticket - [\#{{ticket.id}}] {{ticket.subject}}"
   },
   { :notification_type => EmailNotification::TICKET_ASSIGNED_TO_AGENT, 
       :account_id => account.id, :requester_notification => false, 
       :agent_notification => true,
-      :agent_template => 'Hi {{ticket.agent.name}},
-
-A new ticket (Ticket ID - {{ticket.id}}) has been assigned to you. Please follow the link below to view the ticket.
-
-{{ticket.subject}}
-{{ticket.description}}
-{{ticket.url}}',
+      :agent_template => '<p>Hi {{ticket.agent.name}},<br /><br />A new ticket (Ticket ID - {{ticket.id}}) has been assigned to you. Please follow the link below to view the ticket.<br /><br />{{ticket.subject}}<br />{{ticket.description}}<br />{{ticket.url}}</p>',
     :agent_subject_template => "Ticket Assigned - [\#{{ticket.id}}] {{ticket.subject}}"
 },
   { :notification_type => EmailNotification::COMMENTED_BY_AGENT, 
       :account_id => account.id, :requester_notification => true, 
       :agent_notification => false,
-      :requester_template => 'Dear {{ticket.requester.name}},
-
-There is a new comment on your Ticket (#{{ticket.id}}). You can view your ticket by visiting {{ticket.url}}
-You can also reply to this email to add your comment.
-
-Ticket comment 
-{{comment.commenter.name}} - {{comment.body}}
-
-Sincerely,
-{{helpdesk_name}} Support Team',
+      :requester_template => '<p>Dear {{ticket.requester.name}},</p>
+<p>There is a new comment on your Ticket (#{{ticket.id}}). You can view your ticket by visiting {{ticket.url}}<br />
+You can also reply to this email to add your comment.</p>
+<p>Ticket comment <br />
+{{comment.commenter.name}} &#8211; {{comment.body}}</p>
+<p>Sincerely,<br />
+{{helpdesk_name}} Support Team</p>',
     :requester_subject_template => "Ticket Updated - [\#{{ticket.id}}] {{ticket.subject}}"
 },
   { :notification_type => EmailNotification::REPLIED_BY_REQUESTER, 
       :account_id => account.id, :requester_notification => false, 
       :agent_notification => true,
-      :agent_template => 'Hi {{ticket.agent.name}},
-
-The customer has responded to the ticket (#{{ticket.id}})
-
-{{ticket.subject}}
-
-Ticket comment
-{{comment.body}}
-
-{{ticket.url}}',
+      :agent_template => '<p>Hi {{ticket.agent.name}},<br /><br />The customer has responded to the ticket (#{{ticket.id}})<br /><br />{{ticket.subject}}<br /><br />Ticket comment<br />{{comment.body}}<br /><br />{{ticket.url}}</p>',
     :agent_subject_template => "New Reply Received - [\#{{ticket.id}}] {{ticket.subject}}"
 },
   {
@@ -160,17 +73,7 @@ Ticket comment
     :account_id => account.id, 
     :requester_notification => false, 
     :agent_notification => true,
-    :agent_template => 'Hi {{agent.name}},
-
-There has been no response from the helpdesk for Ticket ID #{{ticket.id}}. The first response was due by {{ticket.fr_due_by_hrs}} today.
-
-Ticket Details: 
-
-Subject - {{ticket.subject}}
-
-Requestor - {{ticket.requester.email}}
-
-This is an escalation email from {{helpdesk_name}}',
+    :agent_template => '<p>Hi {{agent.name}},<br /><br />There has been no response from the helpdesk for Ticket ID #{{ticket.id}}. The first response was due by {{ticket.fr_due_by_hrs}} today.<br /><br />Ticket Details: <br /><br />Subject - {{ticket.subject}}<br /><br />Requestor - {{ticket.requester.email}}<br /><br />This is an escalation email from {{helpdesk_name}}</p>',
     :agent_subject_template => "Response time SLA violated - [\#{{ticket.id}}] {{ticket.subject}}"
   },
   {
@@ -178,52 +81,22 @@ This is an escalation email from {{helpdesk_name}}',
     :account_id => account.id, 
     :requester_notification => false, 
     :agent_notification => true,
-    :agent_template => 'Hi {{agent.name}},
-
-Ticket #{{ticket.id}} has not been resolved within the SLA time period. The ticket was due by {{ticket.due_by_hrs}} today.
-
-Ticket Details: 
-
-Subject - {{ticket.subject}}
-
-Requestor - {{ticket.requester.email}}
-
-This is an escalation email from {{helpdesk_name}}',
+    :agent_template => '<p>Hi {{agent.name}},<br /><br />Ticket #{{ticket.id}} has not been resolved within the SLA time period. The ticket was due by {{ticket.due_by_hrs}} today.<br /><br />Ticket Details: <br /><br />Subject - {{ticket.subject}}<br /><br />Requestor - {{ticket.requester.email}}<br /><br />This is an escalation email from {{helpdesk_name}}</p>',
     :agent_subject_template => "Resolution time SLA violated - [\#{{ticket.id}}] {{ticket.subject}}"
   },
   { :notification_type => EmailNotification::TICKET_RESOLVED, 
       :account_id => account.id, :requester_notification => true, :agent_notification => false,
-      :requester_template => 'Dear {{ticket.requester.name}},
-
-Our Support Rep has indicated that your Ticket (#{{ticket.id}}) has been Resolved. 
-
-If you believe that the ticket has not been resolved, please reply to this email to automatically reopen the ticket.
-If there is no response from you, we will assume that the ticket has been resolved and the ticket will be automatically closed after 48 hours.
-
-Sincerely,
-{{helpdesk_name}} Support Team',
+      :requester_template => '<p>Dear {{ticket.requester.name}},<br /><br />Our Support Rep has indicated that your Ticket (#{{ticket.id}}) has been Resolved. <br /><br />If you believe that the ticket has not been resolved, please reply to this email to automatically reopen the ticket.<br />If there is no response from you, we will assume that the ticket has been resolved and the ticket will be automatically closed after 48 hours.<br /><br />Sincerely,<br />{{helpdesk_name}} Support Team</p>',
     :requester_subject_template => "Ticket Resolved - [\#{{ticket.id}}] {{ticket.subject}}"
 },
   { :notification_type => EmailNotification::TICKET_CLOSED, 
       :account_id => account.id, :requester_notification => true, :agent_notification => false,
-      :requester_template => 'Dear {{ticket.requester.name}},
-
-Your Ticket #{{ticket.id}} - {{ticket.subject}} -  has been closed.
-
-We hope that the ticket was resolved to your satisfaction. If you feel that the ticket should not be closed or if the ticket has not been resolved, please reply to this email.
-
-Sincerely,
-{{helpdesk_name}} Support Team',
+      :requester_template => '<p>Dear {{ticket.requester.name}},<br /><br />Your Ticket #{{ticket.id}} - {{ticket.subject}} -  has been closed.<br /><br />We hope that the ticket was resolved to your satisfaction. If you feel that the ticket should not be closed or if the ticket has not been resolved, please reply to this email.<br /><br />Sincerely,<br />{{helpdesk_name}} Support Team</p>',
     :requester_subject_template => "Ticket Closed - [\#{{ticket.id}}] {{ticket.subject}}"
 },
   { :notification_type => EmailNotification::TICKET_REOPENED, 
       :account_id => account.id, :requester_notification => false, :agent_notification => true,
-      :agent_template => 'Hi {{ticket.agent.name}},
-
-Ticket "#{{ticket.id}} - {{ticket.subject}}" has been reopened, please visit {{ticket.url}} to view the ticket.
-
-Ticket comment
-{{comment.body}}',
+      :agent_template => 'Hi {{ticket.agent.name}},<br /><br />Ticket "#{{ticket.id}} - {{ticket.subject}}" has been reopened, please visit {{ticket.url}} to view the ticket.<br /><br />Ticket comment<br />{{comment.body}}',
     :agent_subject_template => "Ticket re-opened - [\#{{ticket.id}}] {{ticket.subject}}"
 }
 ])
