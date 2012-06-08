a40a21ad566c4126fe1e61bbe71dd213995349c0
Vivekanandan M <vivekanandan@gmail.com>
2012-06-08 15:28:23 +0530

    pagination with pageless navigation and enhancements for satisfaction survey

--- app/models/survey_remark.rb
+++ app/models/survey_remark.rb
@@ -1,3 +1,7 @@
 class SurveyRemark < ActiveRecord::Base
+	
 	belongs_to_account
+	belongs_to :survey_result,:class_name => 'SurveyResult', :foreign_key => :survey_result_id
+	belongs_to :feedback,:class_name => 'Helpdesk::Note', :foreign_key => :note_id
+
 end
