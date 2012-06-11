ad01df85dad24072be6470654ec9dc288a9c7ae4
Vivekanandan M <vivekanandan@gmail.com>
2012-06-11 17:47:30 +0530

    survey pagination with page less

--- app/models/survey_remark.rb
+++ app/models/survey_remark.rb
@@ -1,3 +1,5 @@
 class SurveyRemark < ActiveRecord::Base
 	belongs_to_account
+	belongs_to :survey_result,:class_name => 'SurveyResult', :foreign_key => :survey_result_id
+	belongs_to :feedback,:class_name => 'Helpdesk::Note', :foreign_key => :note_id
 end
