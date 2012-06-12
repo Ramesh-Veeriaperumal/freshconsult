a52e0bddb9e46e1d4b96835fae1e75c24ffa3c45
asrinivasa <vasu.addanki@gmail.com>
2012-06-12 15:14:12 +0530

    Sla timer will be stopped for Pending status by default

--- db/fixtures/035_ticket_statuses.rb
+++ db/fixtures/035_ticket_statuses.rb
@@ -4,7 +4,7 @@ ticket_field_id = account.ticket_fields.find_by_field_type("default_status")
 Helpdesk::TicketStatus.seed_many(:account_id, :status_id,[
   { :status_id => 2, :name => 'Open', :customer_display_name => 'Being Processed', :is_default => true, 
     :account_id => account.id, :ticket_field => ticket_field_id },
-  { :status_id => 3, :name => 'Pending', :customer_display_name => 'Awaiting your Reply', :is_default => true,
+  { :status_id => 3, :name => 'Pending', :customer_display_name => 'Awaiting your Reply', :stop_sla_timer => true, :is_default => true,
     :account_id => account.id, :ticket_field => ticket_field_id },
   { :status_id => 4, :name => 'Resolved', :customer_display_name => 'This ticket has been Resolved', :stop_sla_timer => true, :is_default => true, 
     :account_id => account.id, :ticket_field => ticket_field_id },
