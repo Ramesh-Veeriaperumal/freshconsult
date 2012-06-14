4d31e23ae74a95fb3ee662a2821f0ff3b96867b5
johnpaulh <johnpaul@freshdesk.com>
2012-06-14 14:52:35 +0530

    Changes for the merging

--- db/migrate/20120530090010_add_version_column_to_email_notifications.rb
+++ db/migrate/20120530090010_add_version_column_to_email_notifications.rb
@@ -1,6 +1,6 @@
 class AddVersionColumnToEmailNotifications < ActiveRecord::Migration
   def self.up
-    add_column :email_notifications, :version, :integer,:default=>2
+    add_column :email_notifications, :version, :integer,:default=>1
   end
 
   def self.down
