8ee9398bdb66c5250319109c2d13d8068bd7bf58
Sathappan Muthu <sathappan.m@gmail.com>
2012-06-22 03:14:01 +0530

    fixtures salesforce
    Signed-off-by: Sathappan Muthu <sathappan.m@gmail.com>

--- db/fixtures/004_integrations.rb
+++ db/fixtures/004_integrations.rb
@@ -158,10 +158,10 @@ unless Account.current
   #Populate Salesforce
   salesforce_app = Integrations::Application.seed(:name) do |s|
     s.name = "salesforce"
-    s.display_name => "integrations.salesforce.label"
-    s.description => "integrations.salesforce.desc" 
-    s.listing_order => 9
-    s.options => {:direct_install => true, :oauth_url => "/auth/salesforce?origin={{account_id}}"})
+    s.display_name = "integrations.salesforce.label"
+    s.description = "integrations.salesforce.desc" 
+    s.listing_order = 9
+    s.options = {:direct_install => true, :oauth_url => "/auth/salesforce?origin={{account_id}}"}
   end
 
   Integrations::Widget.seed(:application_id, :name) do |s|
