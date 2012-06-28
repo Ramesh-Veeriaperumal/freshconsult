0a235cdd2e4c9b1d31207077c8a78e1312d22e0b
Vikram Chaitanya <vikramnet2003@gmail.com>
2012-06-28 13:17:14 +0530

    fix for dispatcher issue with to_email
    fix for dispatcher issue with to_email

--- lib/va/handlers/email.rb
+++ lib/va/handlers/email.rb
@@ -2,7 +2,18 @@ class Va::Handlers::Email < Va::RuleHandler
 
   private
     def is(evaluate_on_value)
-      evaluate_on_value && parse_email(evaluate_on_value).casecmp(value) == 0
+      matched = false
+      if evaluate_on_value && evaluate_on_value.is_a?(Hash)
+        to_email_array = evaluate_on_value[:to_emails]
+        if to_email_array && to_email_array.is_a?(Array)
+          to_email_array.each do |email|
+            matched = true if parse_email(email).casecmp(value) == 0
+          end
+        end
+      else
+       evaluate_on_value && parse_email(evaluate_on_value).casecmp(value) == 0
+      end
+      matched
     end
 
     def is_not(evaluate_on_value)
@@ -10,7 +21,18 @@ class Va::Handlers::Email < Va::RuleHandler
     end
 
     def contains(evaluate_on_value)
-      evaluate_on_value && evaluate_on_value.downcase.include?(value.downcase)
+      matched = false
+      if evaluate_on_value && evaluate_on_value.is_a?(Hash)
+        to_email_array = evaluate_on_value[:to_emails]
+        if to_email_array && to_email_array.is_a?(Array)
+          to_email_array.each do |email|
+            matched = true if email.downcase.include?(value.downcase)
+          end
+        end
+      else
+        evaluate_on_value && evaluate_on_value.downcase.include?(value.downcase)
+      end
+      matched
     end
 
     def does_not_contain(evaluate_on_value)
