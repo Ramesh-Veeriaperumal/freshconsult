732c465aa3e1f374efae8459aea6c0518518fd61
Hari Prasad <hari@freshdesk.com>
2012-07-11 16:16:10 +0530

    adding back retry changes.

--- lib/import/zen/zendesk_attachment_import.rb
+++ lib/import/zen/zendesk_attachment_import.rb
@@ -1,5 +1,11 @@
+require 'resque-retry'
+
 class Import::Zen::ZendeskAttachmentImport 
+	extend Resque::Plugins::Retry
+	
   @queue = 'ImportAttachmentWorker'
+
+  @retry_limit = 3
 
   def self.perform(note_id,url,model)
   	model = model.to_sym
