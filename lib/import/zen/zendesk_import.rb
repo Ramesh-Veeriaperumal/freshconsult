095dfe62b33ecacdf8d0df88c7145b068882a693
Hari Prasad <hari@freshdesk.com>
2012-07-12 11:20:05 +0530

    removing retry configuration for rescue based import as its not removing keys from red is server.

--- lib/import/zen/zendesk_import.rb
+++ lib/import/zen/zendesk_import.rb
@@ -1,21 +1,7 @@
-require 'resque-retry'
-
 class Import::Zen::ZendeskImport 
-	extend Resque::Plugins::Retry
-	
   @queue = 'zendeskImport'
 
-  @retry_limit = 3
-  
   def self.perform(zen_params)
-  	trap("INT") do
-     raise "Worker got Interrupted!"
-     #either raise an error here if using a tool like resque-retry or requeue the job
-    end
-    trap('TERM') do
-    	raise "Worker got Terminated!"
-    end
-    
     Import::Zen::Start.new(zen_params).perform
   end
 end
