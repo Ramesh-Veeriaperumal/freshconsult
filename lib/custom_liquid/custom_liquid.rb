a555f58abe8f279c55f5245fcd4a4ec38764da12
Parsuram Vijayasankar <parsu@freshdesk.com>
2012-07-16 14:48:35 +0530

    Custom liquid tags is being reverted now as shan will be looking into it

--- lib/custom_liquid/custom_liquid.rb
+++
@@ -1,7 +0,0 @@
-module CustomLiquid
-  #@@liquid_filters = [CoreFilters, DropFilters, UrlFilters]
-  @@liquid_tags    = "ABCD"
-  #{:translate => Liquid::Tags::Translate}
-  mattr_reader :liquid_tags
-  #, :liquid_filters
-end                                           
