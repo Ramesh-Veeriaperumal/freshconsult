47e5bd15cb365158fa7ae7a88c8fbef56842c64e
Vikram Chaitanya <vikramnet2003@gmail.com>
2012-08-13 23:30:27 +0530

    auto responder fixes and skip notification enhancements
    auto responder fixes and skip notification enhancements

--- app/models/user_notifier.rb
+++ app/models/user_notifier.rb
@@ -16,7 +16,7 @@ class UserNotifier < ActionMailer::Base
     from          reply_email || user.account.default_friendly_email
     recipients    user.email
     sent_on       Time.now
-    headers       "Reply-to" => "#{user.account.default_friendly_email}"
+    headers       "Reply-to" => "#{user.account.default_friendly_email}", "Auto-Submitted" => "auto-generated", "X-Auto-Response-Suppress" => "DR, RN, OOF, AutoReply"
     content_type  "text/html"
   end
   
