689ab7289e8b776733c8802aef039c37f1347e27
Parsuram Vijayasankar <parsu@freshdesk.com>
2012-08-27 22:52:54 +0530

    Fixed portal giving error if the template is not present

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -9,9 +9,10 @@ class SupportController < ApplicationController
   end
 
   def set_portal_page( page_type_token )
-    # @dynamic_template = current_portal.template.pages.find_by_page_type( Portal::Page::PAGE_TYPE_KEY_BY_TOKEN[ page_type_token ] ).content 
-    @dynamic_page = current_portal.template.pages.find_by_page_type( Portal::Page::PAGE_TYPE_KEY_BY_TOKEN[ page_type_token ] )
-    @dynamic_template = @dynamic_page.content unless @dynamic_page.blank?
+    unless current_portal.template.blank?
+      @dynamic_page = current_portal.template.pages.find_by_page_type( Portal::Page::PAGE_TYPE_KEY_BY_TOKEN[ page_type_token ] )
+      @dynamic_template = @dynamic_page.content unless @dynamic_page.blank?
+    end
   end
 
   private
@@ -20,9 +21,10 @@ class SupportController < ApplicationController
   	end
 
   	def set_liquid_variables
-  		@header_content ||= render_to_string :partial => "portal/header", :locals => { :dynamic_template => current_portal.template.header }
-  		@footer_content ||= render_to_string :partial => "portal/footer", :locals => { :dynamic_template => current_portal.template.footer }
-      @contact_us     ||= render_to_string :partial => "portal/contact_info", :locals => { :dynamic_template => current_portal.template.contact_info }
+      Portal::Template::TEMPLATE_MAPPING.each do |t|
+        _content = render_to_string :partial => t[1], :locals => { :dynamic_template => (current_portal.template[t[0]] unless current_portal.template.blank?) }
+        instance_variable_set "@#{t[0]}", _content
+      end      
   		@search_portal  ||= render_to_string :partial => "/search/pagesearch", :locals => { :placeholder => t('portal.search.placeholder') }
   	end
 
