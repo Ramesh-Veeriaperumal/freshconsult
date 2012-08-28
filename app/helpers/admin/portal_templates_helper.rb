c062dc10047c92ea4ecdf930a580b5321acbeb41
Parsuram Vijayasankar <parsu@freshdesk.com>
2012-08-29 02:11:56 +0530

    Moved the code-editor from ace-editor to code-mirror

--- app/helpers/admin/portal_templates_helper.rb
+++ app/helpers/admin/portal_templates_helper.rb
@@ -4,51 +4,33 @@ module Admin::PortalTemplatesHelper
 		include ActionView::Helpers::TagHelper
 		include ActionView::Helpers::FormTagHelper		
 
-		def ace_editor(method, options = {})			
+		def code_editor(method, options = {})			
 			options[:id] = field_id( method, options[:index] )
-			options[:class] = " ace-editor"
-
-			ace_editor_tag(field_name(method), @object.send(method), options)
+			code_editor_tag(field_name(method), @object.send(method), options)
 		end
 
-		def ace_editor_tag(name, content = nil, options = {})			
-          	_form_control = text_area_tag(name, content, :class => "hide")
-          	_control_id = options[:id]
-          	mode  	  = options[:mode] || 'liquid'
-          	theme 	  = options[:theme] || 'textmate' # nil => default theme
-
-          	mode_class = "#{mode}_mode".camelize
-		  	id = options[:id] = "#{( options[:id] || field_id( name ) )}_ace"
-			theme_setter = theme ? "window.ace_editors['#{id}'].setTheme('ace/theme/#{theme}');" : ""
-			_style = "height: #{options[:height] || '400px'}"
-			
-			pre_tag = content_tag(:pre, h(content), options)
-
-			_div_options = { :class => "ace-editor", 
-							 :rel => "ace-editor", 
-							 "data-form-id" => _control_id,
-							 "data-ace-id" => id,
-							 :style => _style }
-							 
-			ace_container = content_tag :div, _form_control + pre_tag, _div_options 
-		  	
-		  	output = <<HTML
-#{ace_container}
-<script type='text/javascript'>
-  jQuery(function(){
-    if (window['ace_editors'] === undefined) window.ace_editors = {};
-    window.ace_editors['#{id}'] = ace.edit('#{id}');
-    #{theme_setter}
-    var #{mode_class} = ace.require("ace/mode/#{mode}").Mode;
-    window.ace_editors['#{id}'].getSession().setMode(new #{mode_class}());
-
-    window.ace_editors['#{id}'].on("blur", function(e){
-		document.getElementById('#{_control_id}').value = window.ace_editors['#{id}'].getSession().getValue();
-	})
-  });
+		def code_editor_tag(name, content = nil, options = {})			
+			id = options[:id] = options[:id] || field_id( name )
+			output = <<HTML
+#{text_area_tag(name, content, options)}
+<script type="text/javascript">
+	if (window['code_mirrors'] === undefined) window.code_mirrors = {};
+	window.code_mirrors['#{id}'] = CodeMirror.fromTextArea(document.getElementById('#{id}'), {
+		lineNumbers: true,
+		mode: "liquid", 
+		theme: 'textmate',
+		tabMode: "indent",
+		onCursorActivity: function() {   
+		 	var editor = window.code_mirrors['#{id}'];
+			editor.setLineClass(hlLine, null, null);
+			editor.matchHighlight("CodeMirror-matchhighlight");
+			hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");  
+		}
+	});
+	var hlLine = window.code_mirrors['#{id}'].setLineClass(0, "activeline");
 </script>
 HTML
-			output.html_safe
+			output.html_safe	
 		end
 
 		def field_name(label, index = nil)
