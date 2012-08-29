3dc17a48cf97c19793418372e6cff32a528f4064
Parsuram Vijayasankar <parsu@freshdesk.com>
2012-08-29 16:37:09 +0530

    Code mirror form builder class is not moved to lib

--- app/helpers/admin/portal_templates_helper.rb
+++ app/helpers/admin/portal_templates_helper.rb
@@ -1,45 +1,4 @@
 module Admin::PortalTemplatesHelper
-	
-	class AppFormBuilder < ActionView::Helpers::FormBuilder
-		include ActionView::Helpers::TagHelper
-		include ActionView::Helpers::FormTagHelper		
-
-		def code_editor(method, options = {})			
-			options[:id] = field_id( method, options[:index] )
-			code_editor_tag(field_name(method), @object.send(method), options)
-		end
-
-		def code_editor_tag(name, content = nil, options = {})			
-			id = options[:id] = options[:id] || field_id( name )
-			output = <<HTML
-#{text_area_tag(name, content, options)}
-<script type="text/javascript">
-	if (window['code_mirrors'] === undefined) window.code_mirrors = {};
-	window.code_mirrors['#{id}'] = CodeMirror.fromTextArea(document.getElementById('#{id}'), {
-		lineNumbers: true,
-		mode: "liquid", 
-		theme: 'textmate',
-		tabMode: "indent",
-		onCursorActivity: function() {   
-		 	var editor = window.code_mirrors['#{id}'];
-			editor.setLineClass(hlLine, null, null);
-			editor.matchHighlight("CodeMirror-matchhighlight");
-			hlLine = editor.setLineClass(editor.getCursor().line, null, "activeline");  
-		}
-	});
-	var hlLine = window.code_mirrors['#{id}'].setLineClass(0, "activeline");
-</script>
-HTML
-			output.html_safe	
-		end
-
-		def field_name(label, index = nil)
-			@object_name.to_s + ( index ? "[#{index}]" : '' ) + "[#{label}]"
-		end
-
-		def field_id(label, index = nil)
-			@object_name.to_s + ( index ? "_#{index}" : '' ) + "_#{label}"
-		end
-
-	end
+	# Form builder used for including code_editor helper
+	include FormBuilders
 end
---
+++ lib/form_builders/code_mirror_builder.rb
@@ -0,0 +1,50 @@
+module FormBuilders
+  class CodeMirrorBuilder < ActionView::Helpers::FormBuilder
+    include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::FormTagHelper
+
+    CODEMIRROR_DEFAULTS = {
+      :lineNumbers => true,
+      :mode      => "liquid", 
+      :theme     => 'textmate',
+      :tabMode   => "indent",
+      :gutter    => true
+    }
+
+    def code_editor(method, options = {})     
+      options[:id] = field_id( method, options[:index] )
+      code_editor_tag(field_name(method), @object.send(method), options)
+    end
+
+    def code_editor_tag(name, content = nil, options = {})      
+      id = options[:id] = options[:id] || field_id( name )
+      if options[:height].present?
+        set_height = "window.code_mirrors['#{id}'].getScrollerElement().style.height = '#{options[:height]}'"
+      end
+
+      _javascript_options = CODEMIRROR_DEFAULTS.merge(options).to_json
+
+      output = <<HTML
+#{text_area_tag(name, content, options)}
+<script type="text/javascript">
+if (window['code_mirrors'] === undefined) window.code_mirrors = {}
+window.code_mirrors['#{id}'] = CodeMirror.fromTextArea(document.getElementById('#{id}'), #{_javascript_options})
+#{set_height || ""}
+window.code_mirrors['#{id}'].refresh()
+</script>
+HTML
+      output.html_safe  
+    end
+
+    def field_name(label, index = nil)
+      @object_name.to_s + ( index ? "[#{index}]" : '' ) + "[#{label}]"
+    end
+
+    def field_id(label, index = nil)
+      @object_name.to_s + ( index ? "_#{index}" : '' ) + "_#{label}"
+    end
+
+  end
+end
+
+ActionView::Base.default_form_builder = FormBuilders::CodeMirrorBuilder
