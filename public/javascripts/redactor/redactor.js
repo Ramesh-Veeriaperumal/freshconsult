8af5cd05501f97e63948d819b4a742dd7c4d8c6d
johnpaulh <johnpaul@freshdesk.com>
2012-09-03 17:18:20 +0530

    Redactor editor added to the canned responses too.

--- public/javascripts/redactor/redactor.js
+++ public/javascripts/redactor/redactor.js
@@ -763,7 +763,15 @@ Redactor.prototype = {
 				else if (key === 72)
 				{
 					this.shortcuts(e, 'subscript'); // Ctrl + h
-				}					
+				}	
+				else if (key === 85)
+				{
+					this.shortcuts(e, 'underline'); // Ctrl + u
+				}
+				else if(key === 37)	
+				{
+					e.preventDefault(); //Ctrl + left arrow
+				}							
 			}	
 			
 			// clear undo buffer
