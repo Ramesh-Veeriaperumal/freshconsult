0ae583a8f4d47bb28264a8dddcaec260af0680cb
Abhinav <abhinav@freshdesk.com>
2012-09-06 12:07:15 +0530

    email_notifications for password reset changed

--- db/migrate/20120823111532_update_email_notifications.rb
+++ db/migrate/20120823111532_update_email_notifications.rb
@@ -4,23 +4,23 @@ class UpdateEmailNotifications < ActiveRecord::Migration
   		    e = account.email_notifications.find_by_notification_type(EmailNotification::PASSWORD_RESET)
   		    template = '<p>A request to reset your password has been made.If you did not make this request, simply ignore this email. If you did make this request, just click the link below:<br />{{password_reset_url}}<br />If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.<br />Regards,<br />{{helpdesk_name}}</p>'  		    
 				if e.agent_template == template 
-					e.update_attributes(:agent_template => 'Hey {{agent.name}},<br />
-							A request to change your password has been made.<br />
+					e.update_attributes(:agent_template => 'Hey {{agent.name}},<br /><br />
+							A request to change your password has been made.<br /><br />
 							To reset your password, click on the link below:<br />
 							<a href="{{password_reset_url}}">Click here to reset the password.</a> <br /><br />
-							If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.<br /><br />
+							If the above URL does not work, try copying and pasting it into your browser. Please feel free to <a href="mailto:support@freshdesk.com">contact us</a> if you continue to face any problems.<br /><br />
 							Regards,<br />{{helpdesk_name}}')
 
 				end
 				
 				if e.requester_template == template 
 
-					e.update_attributes(:requester_template => 'Hey {{agent.name}},<br />
-						A request to change your password has been made.<br />
-						To reset your password, click on the link below:<br />
-						<a href="{{password_reset_url}}">Click here to reset the password.</a> <br /><br />
-						If the above URL does not work, try copying and pasting it into your browser. If you continue to have problem, please feel free to contact us.<br /><br />
-						Regards,<br />{{helpdesk_name}}' )
+					e.update_attributes(:requester_template =>'Hey {{contact.name}},<br /><br />
+							A request to change your password has been made.<br /><br />
+							To reset your password, click on the link below:<br />
+							<a href="{{password_reset_url}}">Click here to reset the password.</a> <br /><br />
+							If the above URL does not work, try copying and pasting it into your browser. Please feel free to <a href="mailto:support@freshdesk.com">contact us</a> if you continue to face any problems.<br /><br />
+							Regards,<br />{{helpdesk_name}}')
 				end
 	end
   end
