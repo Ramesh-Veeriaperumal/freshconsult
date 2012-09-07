ac5b1c71086d57525e584eb7090ada7a462f6c4b
Parsuram Vijayasankar <parsu@freshdesk.com>
2012-09-07 18:07:36 +0530

    Leaderboard pages

--- app/controllers/helpdesk/leaderboard_controller.rb
+++ app/controllers/helpdesk/leaderboard_controller.rb
@@ -1,39 +1,33 @@
 class Helpdesk::LeaderboardController < ApplicationController
     
   def mini_list
-    @mvp = mvp_scoper.first
-    @customer_champ = customer_champ_scoper.first
-    @fcr = fcr_scoper.first    
-    @speed_champ = speed_champ_scoper.first
+    generate_score_card
     render :layout => false
   end
 
   def user_list
-    @mvp = mvp_scoper.limit(10)
-    @customer_champ = customer_champ_scoper.limit(10)
-    @fcr = fcr_scoper.limit(10)
-    @speed_champ = speed_champ_scoper.limit(10)
+    generate_score_card 10
+  end
+
+  def group_list
+    generate_score_card 10, :group
   end
 
   private
-    def mvp_scoper
-      user_scoper
-    end
+    def generate_score_card( _limit = 1, type = :user )
+      c_card = (type == :user) ? user_scoper : group_scoper
 
-    def fcr_scoper
-      user_scoper.first_call
-    end
-
-    def customer_champ_scoper
-      user_scoper.customer_champion
-    end
-
-    def speed_champ_scoper
-      user_scoper.fast
+      [:mvp, :first_call, :customer_champion, :fast].each do |item|
+        instance_variable_set "@#{item}_scorecard", ((item == :mvp) ? c_card : c_card.send(item)).limit(_limit)
+      end
     end
 
     def user_scoper
       current_account.support_scores.user_score.created_at_inside(*this_month)
+    end
+
+    def group_scoper
+      current_account.support_scores.group_score.created_at_inside(*this_month)
     end
 
     def this_month
