3be042c4c353beaa4b2b04f8e678cdc6484d1c6c
Shan <shanmugamk@gmail.com>
2012-09-08 19:35:07 +0530

    Basic refactoring for New Quest form..
    Signed-off-by: Shan <shanmugamk@gmail.com>

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,4 +1,5 @@
 class Quest < ActiveRecord::Base
+  include Gamification::Quests::Constants
   
   belongs_to :account
 
@@ -6,34 +7,6 @@ class Quest < ActiveRecord::Base
   serialize :filter_data
   serialize :quest_data
   
-  GAME_TYPE = [
-    [ :quest_none,     "--- Click to Select ---",  -1],
-    [ :quest_ticket,   "Resolving Tickets",     0 ], 
-    [ :quest_solution, "Publishing Solutions",   1 ], 
-    [ :quest_forum,    "Contribute in Forums",      2 ]
-  ]
-
-  GAME_TYPE_OPTIONS = GAME_TYPE.map { |i| [i[1], i[2]] }
-  GAME_TYPE_NAMES_BY_KEY = Hash[*GAME_TYPE.map { |i| [i[2], i[1]] }.flatten] 
-  GAME_TYPE_KEYS_BY_TOKEN = Hash[*GAME_TYPE.map { |i| [i[0], i[2]] }.flatten]
-
-  BADGES_DATA = [
-    { :name => "Newbie", :desc => "Awarded for your first check in", :classname => "badges-call" },
-    { :name => "Adventurer", :desc => "Check in to 10 different venues", :classname => "badges-elephant" },
-    { :name => "Explorer", :desc => "Check in to 25 different venues", :classname => "badges-fb" },
-    { :name => "Superstar", :desc => "Check in to 50 different venues.", :classname => "badges-fcr" },
-    { :name => "Bender", :desc => "Check in 4 nights in a row.", :classname => "badges-forum" },
-    { :name => "Crunked", :desc => "Check in 4 times in a night.", :classname => "badges-love" },
-    { :name => "Local", :desc => "Check in at the same place 3x in a week.", :classname => "badges-priority" },
-    { :name => "Superuser", :desc => "Check in 30 times in a month.", :classname => "badges-settings" },
-    { :name => "Jetsetter", :desc => "Check in to 5 airports.", :classname => "badges-shooter" },
-    { :name => "Super Mayor", :desc => "Be the Mayor of 10 venues at once.", :classname => "badges-smiley" },
-    { :name => "Blah..", :desc => "Be the Mayor of 10 venues at once.", :classname => "badges-time" },
-    { :name => "Super Trophy", :desc => "Be the Mayor of 10 venues at once.", :classname => "badges-trophy" }
-  ]
-
-  BADGES_DATA_OPTIONS = BADGES_DATA.map { |i| [i[2], i[1]] }
-
   named_scope :disabled, :conditions => { :active => false }
   named_scope :enabled, :conditions => { :active => true }
 
---
+++ lib/gamification/quests/badges.rb
@@ -0,0 +1,21 @@
+module Gamification
+  module Quests
+    module Badges
+      
+      BADGES = [
+        { :name => "Newbie", :desc => "Awarded for your first check in", :classname => "badges-call" },
+        { :name => "Adventurer", :desc => "Check in to 10 different venues", :classname => "badges-elephant" },
+        { :name => "Explorer", :desc => "Check in to 25 different venues", :classname => "badges-fb" },
+        { :name => "Superstar", :desc => "Check in to 50 different venues.", :classname => "badges-fcr" },
+        { :name => "Bender", :desc => "Check in 4 nights in a row.", :classname => "badges-forum" },
+        { :name => "Crunked", :desc => "Check in 4 times in a night.", :classname => "badges-love" },
+        { :name => "Local", :desc => "Check in at the same place 3x in a week.", :classname => "badges-priority" },
+        { :name => "Superuser", :desc => "Check in 30 times in a month.", :classname => "badges-settings" },
+        { :name => "Jetsetter", :desc => "Check in to 5 airports.", :classname => "badges-shooter" },
+        { :name => "Super Mayor", :desc => "Be the Mayor of 10 venues at once.", :classname => "badges-smiley" },
+        { :name => "Blah..", :desc => "Be the Mayor of 10 venues at once.", :classname => "badges-time" },
+        { :name => "Super Trophy", :desc => "Be the Mayor of 10 venues at once.", :classname => "badges-trophy" }
+      ]
+    end
+  end
+end
