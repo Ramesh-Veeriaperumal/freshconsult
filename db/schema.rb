ea51deb05118a909d042878581691ceae27ba40d
Vikram Chaitanya <vikramnet2003@gmail.com>
2012-09-12 17:58:45 +0530

    fixed topic to ticket conversion bug
    fixed topic to ticket conversion bug

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120818070125) do
+ActiveRecord::Schema.define(:version => 20120823111532) do
 
   create_table "account_additional_settings", :force => true do |t|
     t.string   "email_cmds_delimeter"
