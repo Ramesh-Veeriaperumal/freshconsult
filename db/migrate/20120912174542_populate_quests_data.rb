54293895de101df56584f19e36c55ac0d9cb8d45
Shan <shanmugamk@gmail.com>
2012-09-12 23:34:12 +0530

    Quest seed data done for both existing and new customers.
    Signed-off-by: Shan <shanmugamk@gmail.com>

---
+++ db/migrate/20120912174542_populate_quests_data.rb
@@ -0,0 +1,17 @@
+class PopulateQuestsData < ActiveRecord::Migration
+  def self.up
+    plan_ids = SubscriptionPlan.find(:all, :conditions => { 
+      :name => ['Premium', 'Garden', 'Estate'] }).collect { |plan| plan.id }
+      
+    subscriptions = Subscription.find(:all, :conditions => { :subscription_plan_id => plan_ids }, 
+      :include => :account)
+      
+    subscriptions.each do |subscription|
+      puts "Populate Quest Data for #{subscription.account.id} - #{subscription.account.name}"
+      subscription.account.quests.create(Gamification::Quests::Seed::DEFAULT_DATA)
+    end
+  end
+
+  def self.down
+  end
+end
