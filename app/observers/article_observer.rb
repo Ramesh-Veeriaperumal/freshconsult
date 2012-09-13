928203e1f15b8163233daa121aaa82aaa6911695
Vikram Chaitanya <vikramnet2003@gmail.com>
2012-09-13 17:13:46 +0530

    added ramification feature checks in few other places
    added ramification feature checks in few other places

--- app/observers/article_observer.rb
+++ app/observers/article_observer.rb
@@ -5,14 +5,16 @@ class ArticleObserver < ActiveRecord::Observer
 	observe Solution::Article
 
 	include Gamification::Quests::ProcessSolutionQuests
+	include Gamification::GamificationUtil
 
 	SOLUTION_UPDATE_ATTRIBUTES = ["folder_id", "status", "thumbs_up"]
 
 	def after_create(article) 
-		evaluate_solution_quests(article)
+		evaluate_solution_quests(article) if gamification_feature?(article.account)
 	end
 
 	def after_update(article)
+		return unless gamification_feature?(article.account)
 		changed_filter_attributes = article.changed & SOLUTION_UPDATE_ATTRIBUTES
 		evaluate_solution_quests(article) if changed_filter_attributes.any?
 	end
