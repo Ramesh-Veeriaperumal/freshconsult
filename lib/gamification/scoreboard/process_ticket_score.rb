37fcb5ce3997e1daaf79e9ba98b98cc1eff8ef15
asrinivasa <vasu.addanki@gmail.com>
2012-09-15 23:25:26 +0530

    resolved_at can be nil use case evaluated while adding support score resque job

--- lib/gamification/scoreboard/process_ticket_score.rb
+++ lib/gamification/scoreboard/process_ticket_score.rb
@@ -7,12 +7,13 @@ module Gamification
 				args.symbolize_keys!
 				id, account_id = args[:id], args[:account_id]
 				ticket = Helpdesk::Ticket.find_by_id_and_account_id(id, account_id)
-				args[:remove_score] ? remove_score(ticket) : add_score(ticket, args[:fcr])
+				args[:remove_score] ? remove_score(ticket) : add_score(ticket, args)
 			end
 
-			def self.add_score(ticket, first_call_resolution)
-				SupportScore.add_support_score(ticket, ScoreboardRating.resolution_speed(ticket))
-    		SupportScore.add_fcr_bonus_score(ticket) if first_call_resolution
+			def self.add_score(ticket, args)
+				SupportScore.add_support_score(ticket, 
+							ScoreboardRating.resolution_speed(ticket, args[:resolved_at_time]))
+    		SupportScore.add_fcr_bonus_score(ticket) if args[:fcr]
 			end
 
 			def self.remove_score(ticket)
