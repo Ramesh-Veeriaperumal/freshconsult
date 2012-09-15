92dcb9e7fc0b4e181987df2c2a14d4f52584992b
Vivekanandan M <vivekanandan@gmail.com>
2012-09-15 16:22:34 +0530

    customer satisfaction enable check introduced to avoid listing in leaderboard

--- app/helpers/helpdesk/leaderboard_helper.rb
+++ app/helpers/helpdesk/leaderboard_helper.rb
@@ -5,6 +5,18 @@ module Helpdesk::LeaderboardHelper
 	def link_to_agent user
 		return(link_to user.name, user) if current_user && current_user.can_view_all_tickets?
 		user.name
+	end	
+
+	def leaderboard
+		scoreboard = [[ @mvp_scorecard, :mvp],
+				[ @first_call_scorecard, :sharpshooter],
+				[ @fast_scorecard, :speed]]
+		scoreboard.insert(1, [ @customer_champion_scorecard, :love]) if customer_satisfaction_enabled?
+		scoreboard
+	end
+
+	def customer_satisfaction_enabled?
+		current_account.features?(:surveys, :survey_links)
 	end
 
 end
