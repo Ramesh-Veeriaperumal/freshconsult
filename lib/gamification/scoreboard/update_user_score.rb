44eebc4f8c3ba9d63a851f08642adc72a88a5476
asrinivasa <vasu.addanki@gmail.com>
2012-09-15 18:37:42 +0530

    Memcache handled from Resque job

--- lib/gamification/scoreboard/update_user_score.rb
+++ lib/gamification/scoreboard/update_user_score.rb
@@ -8,11 +8,12 @@ module Gamification
 				id, account_id = args[:id], args[:account_id]
 				user = User.find_by_id_and_account_id(id, account_id)
 				total_score = user.support_scores.sum(:score)
-      		unless (user.agent.points.eql? total_score)
-        		user.agent.update_attribute(:points, total_score)
-      		end
-			end
+				unless (user.agent.points.eql? total_score)
+					user.agent.update_attribute(:points, total_score)
+				end
 
+				user.agent.clear_leaderboard_cache!
+			end
 		end
 	end
 end
