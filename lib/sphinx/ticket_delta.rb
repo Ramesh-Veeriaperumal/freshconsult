2c6a05d27af30730ba228c60963ad46dc3c8aeab
Kiran Darisi <kiran.darisi@gmail.com>
2012-10-20 15:35:54 +0530

    Added some prints and did some error handling

--- lib/sphinx/ticket_delta.rb
+++ lib/sphinx/ticket_delta.rb
@@ -1,16 +1,18 @@
 class Sphinx::TicketDelta < ThinkingSphinx::Deltas::DefaultDelta
 
   def index(model, instance = nil)
-  	RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Came for index -- updates Enabled #{ThinkingSphinx.updates_enabled?} --- deltas enabled #{ThinkingSphinx.deltas_enabled?}"
-  	return true unless ThinkingSphinx.updates_enabled?
-    RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Updates Enabled"
-  	return true if instance && !toggled(instance) 
-  	RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Toggled"
-  	if instance 
-  		RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Job enqued"
-  		Resque.enqueue(Sphinx::FlagAsDeleted, { :document_id => instance.sphinx_document_id,
+  	RAILS_DEFAULT_LOGGER.debug "Came for index -- updates Enabled #{ThinkingSphinx.updates_enabled?} --- deltas enabled #{ThinkingSphinx.deltas_enabled?}"
+  	begin
+  		return true unless ThinkingSphinx.updates_enabled?
+  		return true if instance && !toggled(instance) 
+  		if instance 
+  			RAILS_DEFAULT_LOGGER.debug  "Job enqued"
+  			Resque.enqueue(Sphinx::FlagAsDeleted, { :document_id => instance.sphinx_document_id,
   			:model_name => "Helpdesk::Ticket"}) 
-  	end
+  		end
+  	rescue
+  	 NewRelic::Agent.notice_error(ThinkingSphinx::SphinxError, {:custom_params => {:error => "Error in sphinx delta indexing" })
+   	end
   	ThinkingSphinx.updates_enabled = true
   end
 
