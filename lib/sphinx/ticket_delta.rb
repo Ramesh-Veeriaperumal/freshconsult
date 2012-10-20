416a3e91b3591acc3ce7745cbed2cb0d4bc3c20c
Kiran Darisi <kiran.darisi@gmail.com>
2012-10-20 13:22:27 +0530

    Debug prints added

--- lib/sphinx/ticket_delta.rb
+++ lib/sphinx/ticket_delta.rb
@@ -1,11 +1,17 @@
 class Sphinx::TicketDelta < ThinkingSphinx::Deltas::DefaultDelta
 
   def index(model, instance = nil)
+  	RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Came for index"
   	return true unless ThinkingSphinx.updates_enabled? &&
           ThinkingSphinx.deltas_enabled?
+    RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Updates Enabled"
   	return true if instance && !toggled(instance) 
-  	Resque.enqueue(Sphinx::FlagAsDeleted, { :document_id => instance.sphinx_document_id,
-  		:model_name => "Helpdesk::Ticket"}) if instance 
+  	RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Toggled"
+  	if instance 
+  		RAILS_DEFAULT_LOGGER.debug "$$$$$$$$$$$$$$$$$ Job enqued"
+  		Resque.enqueue(Sphinx::FlagAsDeleted, { :document_id => instance.sphinx_document_id,
+  			:model_name => "Helpdesk::Ticket"}) 
+  	end
   end
 
 end
