7d837e5dfb3220bb696e888a358710628810bf1f
Vikram Chaitanya <vikramnet2003@gmail.com>
2012-11-08 17:01:36 +0530

    resolved the merge conflict
    resolved the merge conflict

--- app/controllers/helpdesk/time_sheets_controller.rb
+++ app/controllers/helpdesk/time_sheets_controller.rb
@@ -4,12 +4,7 @@ class Helpdesk::TimeSheetsController < ApplicationController
   before_filter { |c| c.requires_permission :manage_tickets }  
   before_filter :load_time_entry, :only => [ :show,:edit, :update, :destroy, :toggle_timer ] 
   before_filter :load_ticket, :only => [:create, :index, :edit, :update, :toggle_timer] 
-<<<<<<< HEAD
-  before_filter :load_installed_apps, :only => [:index, :create, :edit, :update, :toggle_timer, :destroy]
-=======
-  before_filter :load_installed_apps, :only => [:index, :create, :edit, :update, :toggle_timer]
->>>>>>> origin/pre_staging
-  
+  before_filter :load_installed_apps, :only => [:index, :create, :edit, :update, :toggle_timer, :destroy]  
 
   rescue_from ActiveRecord::UnknownAttributeError , :with => :handle_error
 
