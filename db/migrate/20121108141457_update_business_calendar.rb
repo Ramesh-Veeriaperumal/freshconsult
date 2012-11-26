c4291752144cfd0bbbd62ffa65d3da2cb69dbf68
Abhinav <abhinav@freshdesk.com>
2012-11-26 18:49:43 +0530

    upgrading_business_calendars migration file bug fixed
    Signed-off-by: Abhinav <abhinav@freshdesk.com>

--- db/migrate/20121108141457_update_business_calendar.rb
+++ db/migrate/20121108141457_update_business_calendar.rb
@@ -1,10 +1,11 @@
 class UpdateBusinessCalendar < ActiveRecord::Migration
   def self.up
-    calendars  = BusinessCalendar.find_in_batches(:conditions => {:version => 1})
-  	calendars.each do |s|
+    BusinessCalendar.find_in_batches(:conditions => {:version => 1}) do |calendars|
+     	calendars.each do |s|
    		  s.version = 2
   		  s.business_time_data = s.upgraded_business_time_data
   		  s.save(false)
+      end
   	end
   end
 
