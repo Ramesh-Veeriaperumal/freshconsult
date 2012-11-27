39ff6b788a134d0bd0c025f2539730f05ce8931e
Arvinth <arvinth@freshdesk.com>
2012-11-27 19:00:05 +0530

    Contact page send activation

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -1,16 +1,29 @@
 class ActivationsController < ApplicationController
   #before_filter :require_no_user, :only => [:new, :create] #Guess we don't really need this - Shan
+
+  include HelpdeskControllerMethods
+  skip_before_filter :build_item , :only => [:new, :create]
+  
   before_filter :only => :send_invite do |c| 
     c.requires_permission :manage_users
   end
-  
+
+  before_filter :load_multiple_items, :only => :bulk_send_invite
+
   def send_invite
     user = current_account.all_users.find params[:id]
     user.deliver_activation_instructions!(current_portal, true) if user and user.has_email?
-    
-    flash[:notice] = t('users.activations.send_invite_success')
-    redirect_to(:back)
+    render :json => { :activation_sent => true }
   end
+
+  def bulk_send_invite
+    @items.each do |user|
+      user.deliver_activation_instructions!(current_portal, true) if user and user.has_email?
+    end
+    flash[:notice] = render_to_string(:inline => t("users.activations.bulk_send_invite_success", 
+      :users => params[:ids].length.to_s() ))
+    redirect_to contacts_path
+  end 
   
   def new
     @user = current_account.users.find_using_perishable_token(params[:activation_code], 1.weeks) 
@@ -35,4 +48,13 @@ class ActivationsController < ApplicationController
     end
   end
 
+  protected
+
+    def cname
+      "users"
+    end
+
+    def scoper
+      current_account.users
+    end
 end
