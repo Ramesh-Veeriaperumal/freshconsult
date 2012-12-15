7b31d03bad244b71c811ae20e8403fb6f5105a57
Sathappan Muthu <sathappan.m@gmail.com>
2012-12-15 15:40:08 +0530

    oath util controller updated with new method signature
    Signed-off-by: Sathappan Muthu <sathappan.m@gmail.com>

--- app/controllers/integrations/oauth_util_controller.rb
+++ app/controllers/integrations/oauth_util_controller.rb
@@ -7,7 +7,7 @@ before_filter :require_user
 				app_id = Integrations::Application.find(:first, :conditions => {:name => params[:provider]}).id
 				salesforce_app = Integrations::InstalledApplication.find(:first, :conditions => {:account_id => current_account, :application_id => app_id})
 				refresh_token = salesforce_app[:configs][:inputs]['refresh_token']
-				access_token = get_oauth2_access_token(refresh_token)
+				access_token = get_oauth2_access_token(params[:provider], refresh_token)
 				salesforce_app[:configs][:inputs]['oauth_token'] = access_token.token
 				salesforce_app.save!
 				render :json => {:access_token => access_token.token}
