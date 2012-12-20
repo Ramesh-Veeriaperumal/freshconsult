4cce8ca89a1ca288581782f4db1ba2cbd712c5bb
Rajeev Thiruvengadam <veejar2020@Rajeevs-MacBook-Pro.local>
2012-12-20 11:20:20 +0530

    resque bug fix- done

--- app/controllers/subscription_admin/resque/failed_controller.rb
+++ app/controllers/subscription_admin/resque/failed_controller.rb
@@ -1,7 +1,9 @@
 class SubscriptionAdmin::Resque::FailedController < ApplicationController
-	
+	include AdminControllerMethods
 	include SubscriptionAdmin::Resque::FailedHelper
-
+	
+	layout "resque_admin"
+	
 	def index
 	end
 
