9705d41c5feb807f22578643540fb97f633f76de
johnpaulh <johnpaul@freshdesk.com>
2013-01-24 18:18:36 +0530

    Dropbox feature: settings option added to get the api key from the user.

--- db/migrate/20130123072203_add_dropbox_to_applications.rb
+++ db/migrate/20130123072203_add_dropbox_to_applications.rb
@@ -7,11 +7,11 @@ class AddDropboxToApplications < ActiveRecord::Migration
 	        :display_name => "integrations.dropbox.label", 
 	        :description => "integrations.dropbox.desc", 
 	        :listing_order => 19,
-	        :options => {:no_settings => true})
+	        :options => {:app_key=>{:required=>:true,:type=>:text,:label=>"integrations.dropbox.form.app_key",:info=>"integrations.dropbox.form.app_key_info"},:keys_order=>[:app_key]})
 	end
 
   def self.down
- 	Integrations::Application.find(:first, :conditions => {:name => @app_name}).delete
     execute("DELETE installed_applications FROM installed_applications INNER JOIN applications ON applications.ID=installed_applications.application_id WHERE applications.name='#{@app_name}'")
+ 	Integrations::Application.find(:first, :conditions => {:name => @app_name}).delete
   end
 end
