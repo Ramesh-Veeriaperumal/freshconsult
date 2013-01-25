a3a02df7757c2d397be5537075324f2c1c83f63c
johnpaulh <johnpaul@freshdesk.com>
2013-01-25 13:40:41 +0530

    Dropbox feature not working for portal. Issue raised in testing. This is fixed.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -415,6 +415,12 @@ module ApplicationHelper
     return installed_app
   end
 
+  #This one checks for installed apps in account
+  def dropbox_app_key
+    app = Integrations::InstalledApplication.with_name("dropbox").find(:all,:conditions =>["installed_applications.account_id = ?",current_account])
+    app.first.configs[:inputs]['app_key']  unless (app.empty?)
+  end
+
   def get_app_widget_script(app_name, widget_name, liquid_objs) 
     installed_app = @installed_apps_hash[app_name.to_sym]
     if installed_app.blank? or installed_app.application.blank?
