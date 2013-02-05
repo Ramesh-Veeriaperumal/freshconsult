ea25164bdc1c8827adc2da957f33ce3b50bd8841
Kiran Darisi <kiran.darisi@gmail.com>
2013-02-05 16:17:15 +0530

    migration with lhm is done

--- db/migrate/20130129095650_add_time_columns_to_ticket_states.rb
+++ db/migrate/20130129095650_add_time_columns_to_ticket_states.rb
@@ -1,13 +1,21 @@
 class AddTimeColumnsToTicketStates < ActiveRecord::Migration
+  
   def self.up
-  	add_column :helpdesk_ticket_states, :first_resp_time_by_bhrs, :integer
-    add_column :helpdesk_ticket_states, :resolution_time_by_bhrs, :integer
-    add_column :helpdesk_ticket_states, :avg_response_time_by_bhrs, :float
+  	Lhm.change_table :helpdesk_ticket_states, :atomic_switch => true do |m|
+      m.add_column :first_resp_time_by_bhrs, :integer
+      m.add_column :resolution_time_by_bhrs, :integer
+      m.add_column :avg_response_time_by_bhrs, :float
+    end
   end
 
   def self.down
-  	remove_column :helpdesk_ticket_states, :avg_response_time_by_bhrs
-    remove_column :helpdesk_ticket_states, :resolution_time_by_bhrs
-    remove_column :helpdesk_ticket_states, :first_resp_time_by_bhrs
+    Lhm.change_table :helpdesk_ticket_states, :atomic_switch => true do |m|
+      m.remove_column :first_resp_time_by_bhrs
+      m.remove_column :resolution_time_by_bhrs
+      m.remove_column :avg_response_time_by_bhrs
+    end
   end
+
 end
+
+
