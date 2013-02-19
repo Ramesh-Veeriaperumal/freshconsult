ffa9fef827d46cf1113a98aa9b930268a47f4f6d
Bharath MG <mgbharath@ymail.com>
2013-02-19 13:15:01 +0530

    New Va Rule (Ticket Created during Business hours/Non-Business hours/Holidays)

--- lib/va/handlers/date_time.rb
+++ lib/va/handlers/date_time.rb
@@ -11,4 +11,21 @@ class Va::Handlers::DateTime < Va::RuleHandler
   def filter_query_greater_than
     [ "#{condition.db_column} < ?", value.to_i.hours.ago.to_s(:db) ]
   end
+
+  private
+      def during(evaluated_on_value) #possible values are business_hours, non_business_hours & holidays
+        send(value, evaluated_on_value)
+      end
+
+      def business_hours(evaluated_on_value)
+        Time.working_hours?(evaluated_on_value)
+      end
+
+      def non_business_hours(evaluated_on_value)
+        !Time.workday?(evaluated_on_value) || !Time.working_hours?(evaluated_on_value)
+      end
+
+      def holidays(evaluated_on_value)
+        !Time.workday?(evaluated_on_value)
+      end
 end
