ae6717413cb5cdf5db9b541f34b00e47292c22f6
Parsuram Vijayasankar <parsu@freshdesk.com>
2013-02-22 19:31:12 +0530

    Deleting some unused ticket helpers

--- app/helpers/support_helper.rb
+++ app/helpers/support_helper.rb
@@ -255,54 +255,6 @@ HTML
 		content_tag :div, _text, :class => "alert alert-ticket-status"
 	end
 
-	# Construct ticket form UI
-	def construct_ticket_element(object_name, field, field_label, dom_type, required, field_value = "", field_name = "", in_portal = false , is_edit = false)
-	    dom_type = (field.field_type == "nested_field") ? "nested_field" : dom_type
-	    element_class   = " #{ (required) ? 'required' : '' } #{ dom_type }"
-	    field_name      = (field_name.blank?) ? field.field_name : field_name
-	    object_name     = "#{object_name.to_s}#{ ( !field.is_default_field? ) ? '[custom_field]' : '' }"
-	    
-	    if field.editable_in_portal && field.visible_in_view_form?
-	    	label = label_tag object_name+"_"+field.field_name, field_label, :class => ((dom_type != "checkbox") ? 
-	    		("#{required ? 'required': ""} control-label") : "")
-	    else
-	    	label = content_tag :h4, field_label
-	    end
-
-	    case dom_type
-	      when "requester" then
-	      	element = render(:partial => "/support/shared/requester", 
-	      		:locals => { :label => label, :object_name => object_name, :field => field })
-	      when "email" then
-	        element = label + content_tag(:div, text_field(object_name, field_name, :class => element_class, :value => field_value), :class => "controls")
-	        element = add_cc_field_tag element ,field if (field.portal_cc_field? && !is_edit && controller_name.singularize != "feedback_widget") #dirty fix
-	        element += add_name_field unless is_edit
-	      when "text", "number" then
-	        element = label + content_tag(:div, text_field(object_name, field_name, :class => element_class + " span12", :value => field_value), :class => "controls")
-	      when "paragraph" then
-	        element = label + content_tag(:div, text_area(object_name, field_name, :class => element_class + " span12", :value => field_value, :rows => 6), :class => "controls")
-	      when "dropdown" then	        
-          	element = label + content_tag(:div, 
-          		select(object_name, field_name, field.field_type == "default_status" ? field.visible_status_choices : field.choices, 
-          			{:selected => field_value}, {:class => element_class}), :class => "controls")
-	      when "dropdown_blank" then
-	        element = label + content_tag(:div, 
-	        	select(object_name, field_name, field.choices, { :include_blank => "...", :selected => field_value }, {:class => element_class}), :class => "controls")
-	      when "nested_field" then
-	        element = label + content_tag(:div, nested_field_tag(object_name, field_name, field, {:include_blank => "...", :selected => field_value}, {:class => element_class + " custom-select"}, field_value, in_portal), :class => "controls")
-	      when "hidden" then
-	        element = hidden_field(object_name , field_name , :value => field_value)
-	      when "checkbox" then
-	        element = content_tag(:div, check_box(object_name, field_name, :class => element_class, :checked => field_value ) + label, :class => "controls")
-	      when "html_paragraph" then
-	      	_output = []
-	      	_output << %( #{ text_area(object_name, field_name, :class => element_class, :value => field_value, :rows => 6) } )
-	      	_output << %( #{ render(:partial=>"/support/shared/attachment_form") } )
-	        element = label + content_tag(:div, _output.join(" "), :class => "controls")
-	    end
-	    content_tag :div, element, :class => dom_type+" control-group"
-	end
-
 	def ticket_label object_name, field
 		required = (field[:required_in_portal] && field[:editable_in_portal])
 		element_class = " #{required ? 'required' : '' } control-label"
