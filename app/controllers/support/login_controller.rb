97488a70463984c88b06850439658ad9c0a0f1e0
Parsuram Vijayasankar <parsu@freshdesk.com>
2013-03-01 15:57:31 +0530

    Redirecting directly to the user-sessions controller

--- app/controllers/support/login_controller.rb
+++ app/controllers/support/login_controller.rb
@@ -13,32 +13,32 @@ class Support::LoginController < SupportController
 		set_portal_page :user_login
 	end
 
-	def create   
-		@user_session = current_account.user_sessions.new(params[:user_session])
-		if @user_session.save
-		  #Temporary hack due to current_user not returning proper value
-		  @current_user_session = @user_session
-		  @current_user = @user_session.record
-		  #Hack ends here
+	# def create   
+	# 	@user_session = current_account.user_sessions.new(params[:user_session])
+	# 	if @user_session.save
+	# 	  #Temporary hack due to current_user not returning proper value
+	# 	  @current_user_session = @user_session
+	# 	  @current_user = @user_session.record
+	# 	  #Hack ends here
 		  
-		  remove_old_filters if @current_user.agent?
+	# 	  remove_old_filters if @current_user.agent?
 
-		  redirect_back_or_default('/') if grant_day_pass
-		  #Unable to put 'grant_day_pass' in after_filter due to double render
-		else
-		  note_failed_login
-		  set_portal_page :user_login
-		  render :action => :new
-		end
-	end
+	# 	  redirect_back_or_default('/') if grant_day_pass 
+	# 	  #Unable to put 'grant_day_pass' in after_filter due to double render
+	# 	else
+	# 	  note_failed_login
+	# 	  set_portal_page :user_login
+	# 	  render :action => :new
+	# 	end
+	# end
 
 	private
-		def note_failed_login
-	      logger.warn "Failed login for '#{params[:user_session][:email]}' from #{request.remote_ip} at #{Time.now.utc}"
-	    end
+		# def note_failed_login
+	 #      logger.warn "Failed login for '#{params[:user_session][:email]}' from #{request.remote_ip} at #{Time.now.utc}"
+	 #    end
 
-	    def remove_old_filters
-	      remove_key(HELPDESK_TICKET_FILTERS % {:account_id => current_account.id, :user_id => current_user.id, :session_id => session.session_id})
-	    end
+	 #    def remove_old_filters
+	 #      remove_key(HELPDESK_TICKET_FILTERS % {:account_id => current_account.id, :user_id => current_user.id, :session_id => session.session_id})
+	 #    end
 
 end
