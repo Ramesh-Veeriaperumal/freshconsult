0d6a54a08364256e67787c184f8ae4b9d6637c8a
bsubramaniam <bsubramaniam@freshdesk.com>
2013-03-05 17:38:47 +0530

    SLA policy premium users escalation

--- app/helpers/helpdesk/sla_policies_helper.rb
+++ app/helpers/helpdesk/sla_policies_helper.rb
@@ -1,17 +1,17 @@
 module Helpdesk::SlaPoliciesHelper
 
   def response_time_options
-    return Helpdesk::SlaDetail::RESPONSETIME_OPTIONS if !current_account.premium
+    return Helpdesk::SlaDetail::RESPONSETIME_OPTIONS if !current_account.premium?
     Helpdesk::SlaDetail::PREMIUM_TIME_OPTIONS + Helpdesk::SlaDetail::RESPONSETIME_OPTIONS
   end
 
   def resolution_time_options
-  	return Helpdesk::SlaDetail::RESOLUTIONTIME_OPTIONS if !current_account.premium
+  	return Helpdesk::SlaDetail::RESOLUTIONTIME_OPTIONS if !current_account.premium?
     Helpdesk::SlaDetail::PREMIUM_TIME_OPTIONS+ Helpdesk::SlaDetail::RESOLUTIONTIME_OPTIONS
   end
 
   def escalation_time_options
-    (current_account.premium ? Helpdesk::SlaPolicy::ESCALATION_PREMIUM_TIME_OPTIONS :
+    (current_account.premium? ? Helpdesk::SlaPolicy::ESCALATION_PREMIUM_TIME_OPTIONS :
     	Helpdesk::SlaPolicy::ESCALATION_TIME_OPTIONS)
   end
 
