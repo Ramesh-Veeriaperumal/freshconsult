a98399570fd2549f930765cdca0fb94f8317355e
sunil <sunil@freshdesk.com>
2013-04-15 15:50:00 +0530

    Revert "Auth config stored temporarily in KeyValuePair db table is moved to Redis"
    This reverts commit f0bbdf15740b0b3f4f5c11429d30aa9882f61b2e.
    
    Signed-off-by: sunil <sunil@freshdesk.com>

--- app/controllers/integrations/user_credentials_controller.rb
+++ app/controllers/integrations/user_credentials_controller.rb
@@ -1,13 +1,12 @@
 class Integrations::UserCredentialsController < ApplicationController
   include Integrations::AppsUtil
-  
+
   def oauth_install
     begin
-      key_options = { :account_id => current_account.id, :user_id => current_user.id, :provider => params['id']}
-      kv_store = Redis::KeyValueStore.new Redis::KeySpec.new(RedisKeys::AUTH_REDIRECT_OAUTH, key_options)
-      app_config = kv_store.get
-      unless app_config.blank?
-        config_hash = JSON.parse(app_config)
+    	app_config = KeyValuePair.find_by_account_id_and_key(current_account.id, "#{params['id']}_oauth_config")
+  		unless app_config.blank?
+        config_hash = JSON.parse(app_config.value)
+  			
         app_name = config_hash["app_name"]
         config_hash.delete("app_name")	    
         
@@ -21,7 +20,7 @@ class Integrations::UserCredentialsController < ApplicationController
         Integrations::UserCredential.add_or_update(installed_application, current_user.id, config_hash)	    
         flash[:notice] = t(:'flash.application.install.success') if installed_application and 
                           request.cookies.fetch('return_uri', '').blank?
-  	    kv_store.remove
+  	    app_config.delete
       end	
     rescue Exception => msg
       puts "Something went wrong while configuring an installed application ( #{msg})"
@@ -30,5 +29,5 @@ class Integrations::UserCredentialsController < ApplicationController
 
       redirect_back_using_cookie(request, current_user.admin? ? integrations_applications_path : root_path )
   end
-  
+
 end
