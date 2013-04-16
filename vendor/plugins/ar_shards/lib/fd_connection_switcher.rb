ba025eec08ad508ccb2f1ac878a79415eda485c1
Kiran Darisi <kiran.darisi@gmail.com>
2013-04-16 15:49:46 +0530

    Facebook comments modified wrt shards

--- vendor/plugins/ar_shards/lib/fd_connection_switcher.rb
+++ vendor/plugins/ar_shards/lib/fd_connection_switcher.rb
@@ -3,7 +3,6 @@ ActiveRecord::Base.class_eval do
   class << self
 
   def on_shard(shard, &block)
-  	puts "My connection switcher 2"
     old_options = current_shard_selection.options
     switch_connection(:shard => shard) if supports_sharding?
       yield
@@ -12,7 +11,6 @@ ActiveRecord::Base.class_eval do
   end
   
   def on_all_shards(&block)
-  	puts "My connection switcher 1"
     old_options = current_shard_selection.options
     if supports_sharding?
     	shard_names.map do |shard|
