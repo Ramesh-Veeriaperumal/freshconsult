dae61cbe28b065f9017c5352e2a1a17e693e86df
venkatesanmurali <venkatesan.murali1985@gmail.com>
2013-04-19 18:25:34 +0530

    Ajax loader and new text for toggle round robin availability
    Ajax loader and new text for toggle round robin availability

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -27,24 +27,7 @@ function catchException(fn, message) {
   }
 }
 
-function processAgent(user_id) {
-  var value = ( jQuery("#available_icon").attr("class") == "header-icons-autoAssign-on" )
- 
-  new Ajax.Request('/agents/toggle_availability', 
-                   { parameters: {value: !value, id: user_id},
-                      onLoading: function() {
-                        
-                      },
-                      onSuccess: function(response) {
-                       //change the icon class.
-                      if (jQuery('#available_icon').hasClass('header-icons-autoAssign-on'))
-                        jQuery('#available_icon').removeClass('header-icons-autoAssign-on').addClass('header-icons-autoAssign-off')
-                      else
-                        jQuery('#available_icon').removeClass('header-icons-autoAssign-off').addClass('header-icons-autoAssign-on')
 
-
-                     } });
-}
 
 function freshdate(str) {
   var month_names = ['January','February','March','April','May','June','July','August','September','October','November','December'];
