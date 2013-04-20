ca065615f9213dd34e8ce4b6c959be83f4e15278
pratheep <pratheep.velicherla@gmail.com>
2013-04-20 12:50:52 +0530

    LHM for round robin features migration ..
    LHM for round robin features migration ..

--- db/migrate/20130227090250_add_ticket_assign_type_to_groups.rb
+++ db/migrate/20130227090250_add_ticket_assign_type_to_groups.rb
@@ -1,9 +1,15 @@
 class AddTicketAssignTypeToGroups < ActiveRecord::Migration
   def self.up
-    add_column :groups, :ticket_assign_type, :integer, :default => 0
+    # add_column :groups, :ticket_assign_type, :integer, :default => 0
+    Lhm.change_table :groups, :atomic_switch => true do |m|
+       m.ddl("ALTER TABLE  %s add COLUMN ticket_assign_type integer(11) DEFAULT 0" % m.name)
+    end
   end
 
   def self.down
-    remove_column :groups, :ticket_assign_type
+    # remove_column :groups, :ticket_assign_type
+    Lhm.change_table :groups, :atomic_switch => true do |m|
+       m.ddl("ALTER TABLE %s DROP COLUMN ticket_assign_type" % m.name)
+    end
   end
 end
--- db/migrate/20130320103422_add_availablity_column_to_agent.rb
+++ db/migrate/20130320103422_add_availablity_column_to_agent.rb
@@ -1,9 +1,15 @@
 class AddAvailablityColumnToAgent < ActiveRecord::Migration
   def self.up
-    add_column :agents, :available, :boolean, :default => true
+    # add_column :agents, :available, :boolean, :default => true
+    Lhm.change_table :agents, :atomic_switch => true do |m|
+       m.ddl("ALTER TABLE %s ADD COLUMN available tinyint(1) DEFAULT 1;" % m.name)
+    end
   end
 
   def self.down
-    remove_column :agents, :available
+    # remove_column :agents, :available
+    Lhm.change_table :agents, :atomic_switch => true do |m|
+       m.ddl("ALTER TABLE %s DROP COLUMN available" % m.name)
+    end
   end
 end
