db1671511586629e64ae88734718d9ac9a367ac6
pratheep <pratheep.velicherla@gmail.com>
2013-05-02 19:41:16 +0530

    CDN production related yml and initializer changes
    CDN production related yml and initializer changes

--- config/initializers/site_cloudfront_asset_host.rb
+++ config/initializers/site_cloudfront_asset_host.rb
@@ -5,8 +5,8 @@ end
 
 CloudfrontAssetHost.configure do |config|
   config.cname = Proc.new { |source, request|
-    cloudfront_cname = "static%d.freshpo.com"
-    cloudfront_distribution = "d3thwd261m1b0m.cloudfront.net"
+    cloudfront_cname = (Rails.env.production? ? "static%d.freshdesk.com" : "static%d.freshpo.com")
+    cloudfront_distribution = (Rails.env.production? ? "d3o14s01j0qbic.cloudfront.net" : "d3thwd261m1b0m.cloudfront.net")
     params = request.parameters
     if params['format'] == 'widget'
       asset_hostname = asset_hostname_ssl = Rails.env.production? ? "asset.freshdesk.com" : "localhost:3000"
@@ -20,12 +20,12 @@ CloudfrontAssetHost.configure do |config|
     end
     asset_host_url
   }
-  config.bucket           = "fd-static"
+  config.bucket           = ( Rails.env.production? ? "freshdeskstatic" : "fd-static" )
   config.key_prefix       = "#{Rails.env}"
   config.plain_prefix     = "plain"
   config.image_extensions = %w(jpg jpeg gif png ico)
   config.asset_dirs       = %w(packages)
-  config.exclude_pattern  = /psd|images\/sprite-images/
+  config.exclude_pattern  = /psd|images\/sprite-images|images\/cdn-ignored/
   config.s3_config        = "#{RAILS_ROOT}/config/s3_static_files.yml"
   config.s3_logging       = true
   config.gzip             = true  
