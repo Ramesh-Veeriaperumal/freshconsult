0b6e66ce2783ca20280fc8e21133521626cc7316
Vikram Chaitanya <vikramnet2003@gmail.com>
2013-05-16 00:05:17 +0530

    added more REDIS methods to newrelic tracing
    added more REDIS methods to newrelic tracing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -7,9 +7,22 @@ $redis_secondary = Redis.new(:host => config["host"], :port => config["port"])
 
 Redis.class_eval {add_method_tracer :set}
 Redis.class_eval {add_method_tracer :get}
+Redis.class_eval {add_method_tracer :del}
 Redis.class_eval {add_method_tracer :sadd}
-Redis.class_eval {add_method_tracer :lpop}
+Redis.class_eval {add_method_tracer :srem}
+Redis.class_eval {add_method_tracer :smembers}
 Redis.class_eval {add_method_tracer :rpush}
-Redis::Connection.class_eval {add_method_tracer :read}
-Redis::Connection.class_eval {add_method_tracer :write}
-Redis::Connection.class_eval {add_method_tracer :connect}
+Redis.class_eval {add_method_tracer :lpush}
+Redis.class_eval {add_method_tracer :rpull}
+Redis.class_eval {add_method_tracer :lpull}
+Redis.class_eval {add_method_tracer :lrange}
+Redis.class_eval {add_method_tracer :llen}
+Redis.class_eval {add_method_tracer :keys}
+Redis.class_eval {add_method_tracer :hset}
+Redis.class_eval {add_method_tracer :hget}
+Redis.class_eval {add_method_tracer :exists}
+Redis.class_eval {add_method_tracer :ttl}
+Redis.class_eval {add_method_tracer :lpop}
+Redis::Client.class_eval {add_method_tracer :read}
+Redis::Client.class_eval {add_method_tracer :process}
+Redis::Client.class_eval {add_method_tracer :connect}
