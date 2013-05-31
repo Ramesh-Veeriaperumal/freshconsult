c0205edb133892ca023b0f2ff4283c829d8fcdbb
Arvinth <arvinth@freshdesk.com>
2013-05-31 20:31:05 +0530

    Manual_dueby update validation commented out

--- app/controllers/helpdesk/tickets_controller.rb
+++ app/controllers/helpdesk/tickets_controller.rb
@@ -27,8 +27,7 @@ class Helpdesk::TicketsController < ApplicationController
   before_filter :disable_notification, :if => :notification_not_required?
   after_filter  :enable_notification, :if => :notification_not_required?
   before_filter :set_selected_tab
-  before_filter :validate_manual_dueby, :only => :update
-
+  
   layout :choose_layout 
   
   before_filter :load_multiple_items, :only => [ :destroy, :restore, :spam, :unspam, :assign, 
@@ -42,6 +41,7 @@ class Helpdesk::TicketsController < ApplicationController
   before_filter :set_date_filter ,    :only => [:export_csv]
   before_filter :csv_date_range_in_days , :only => [:export_csv]
   before_filter :check_ticket_status, :only => [:update]
+  # before_filter :validate_manual_dueby, :only => :update
   before_filter :set_default_filter , :only => [:custom_search, :export_csv]
 
   before_filter :load_email_params, :only => [:show, :reply_to_conv, :forward_conv]
@@ -900,27 +900,29 @@ class Helpdesk::TicketsController < ApplicationController
     @selected_tab = :tickets
   end
 
-  def validate_manual_dueby
-    if params[nscname][:manual_dueby]
-      unless validate_date(params[nscname][:due_by]) && validate_date(params[nscname][:frDueBy])
-        respond_to do |format|
-          format.json { 
-            render :json => { :update_failure => true, :errors => I18n.t('date_invalid') }.to_json and return
-          }
-          format.xml {
-            render :xml => { :update_failure => true, :errors => I18n.t('date_invalid') }.to_xml and return
-          }
-          format.html { render :text => I18n.t('date_invalid') and return }
-        end
-      end
-    end
-  end
+  # def validate_manual_dueby
+  #   if(@item.manual_dueby && params[nscname].key?(:due_by) && params[nscname].key?(:frDueBy))
+  #     unless validate_date(params[nscname][:due_by]) && validate_date(params[nscname][:frDueBy])
+  #       respond_to do |format|
+  #         format.json { 
+  #           render :json => { :update_failure => true, :errors => I18n.t('date_invalid') }.to_json and return
+  #         }
+  #         format.xml {
+  #           render :xml => { :update_failure => true, :errors => I18n.t('date_invalid') }.to_xml and return
+  #         }
+  #         format.html { render :text => I18n.t('date_invalid') and return }
+  #       end
+  #     end
+  #   else
+  #     params[nscname].except!(:due_by, :frDueBy)
+  #   end
+  # end
 
-  def validate_date(date_string)
-    begin
-      date = Date.parse(date_string)
-    rescue
-      return false
-    end
-  end
+  # def validate_date(date_string)
+  #   begin
+  #     date = Date.parse(date_string)
+  #   rescue
+  #     return false
+  #   end
+  # end
 end
