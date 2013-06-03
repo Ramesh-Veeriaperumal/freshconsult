9248df76e62f94d7d7616ca31a54078d559b1d63
Kiran Darisi <kiran.darisi@gmail.com>
2013-06-03 22:46:52 +0530

    added threshold so no need to record for every dumb transaction

--- lib/monitoring/record_metrics.rb
+++ lib/monitoring/record_metrics.rb
@@ -1,12 +1,12 @@
 class Monitoring::RecordMetrics
   
-  THRESHOLD_IN_MS = 1
+  THRESHOLD_IN_SECS = 180 
 
   def self.performance_data(metrics_data)
-  	#if metrics_data[:time_spent] > THRESHOLD_IN_MS
+  	if metrics_data[:time_spent] > THRESHOLD_IN_SECS
   		metrics_data[:time_spent] = metrics_data[:time_spent]/1.minutes
   		Resque.enqueue(Monitoring::SplunkStorm, metrics_data) 
-  	#end
+  	end
   end
   
   def self.register(metrics_data)
