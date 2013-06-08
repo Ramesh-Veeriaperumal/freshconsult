c50cf04135c2cd3b27ad24b896897fd66196752e
Vikram Chaitanya <vikramnet2003@gmail.com>
2013-06-08 23:37:21 +0530

    cache busting the avatar to push the https fix
    cache busting the avatar to push the https fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -391,7 +391,7 @@ module ApplicationHelper
       img_tag_options[:width] = options.fetch(:width)
       img_tag_options[:height] = options.fetch(:height)
     end 
-    avatar_content = MemcacheKeys.fetch(["v3","avatar",profile_size,user],30.days.to_i) do
+    avatar_content = MemcacheKeys.fetch(["v4","avatar",profile_size,user],30.days.to_i) do
       content_tag( :div, (image_tag (user.avatar) ? user.avatar.expiring_url(profile_size,30.days.to_i) : is_user_social(user, profile_size), img_tag_options ), :class => profile_class, :size_type => profile_size )
     end
     avatar_content
