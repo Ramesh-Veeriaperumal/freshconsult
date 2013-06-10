6c61a9c24c90eafe0d2a9cda7a968ba6a1251154
Gautham Shankar <gautham@freshdesk.com>
2013-06-10 18:43:08 +0530

    removed freshdesk_authority branch migrations

--- db/migrate/20121023100500_add_privileges_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddPrivilegesToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :privileges, :string
-  end
-
-  def self.down
-    remove_column :users, :privileges
-  end
-end
