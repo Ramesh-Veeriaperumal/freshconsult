2f0bffd88d42f63dae4c6d3b732ad56a2e2230c0
johnpaulh <johnpaul@freshdesk.com>
2013-06-12 15:48:02 +0530

    API to pull the topics monitored by the user return only the topics created and monitored by self. This is fixed.

--- app/controllers/support/discussions_controller.rb
+++ app/controllers/support/discussions_controller.rb
@@ -14,11 +14,10 @@ class Support::DiscussionsController < SupportController
 	end	
 
     def user_monitored
-	    # @monitorships  = Monitorship.active_monitors.find(:all,:conditions=>["user_id = ?",params[:user_id]])
-	    @monitorships = current_account.portal_topics.find(:all,:conditions=>["user_id = ?",params[:user_id]])
+	    @topics = current_account.portal_topics.find(:all,:joins=>"inner join #{Monitorship.table_name} on #{Topic.table_name}.id = #{Monitorship.table_name}.topic_id",:conditions=>["#{Monitorship.table_name}.active=? and #{Monitorship.table_name}.user_id = ?",true,params[:user_id]])
 	    respond_to do |format|
-	      format.xml { render :xml => @monitorships.to_xml(:except=>:account_id) }
-	      format.json { render :json => @monitorships.as_json(:except=>:account_id) }
+	      format.xml { render :xml => @topics.to_xml(:except=>:account_id) }
+	      format.json { render :json => @topics.as_json(:except=>:account_id) }
 	    end
   	end
 
