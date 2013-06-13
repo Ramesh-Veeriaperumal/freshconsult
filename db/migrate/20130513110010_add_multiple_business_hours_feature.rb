53c89b9e5c223ccef97a3b55672a705e2ad80106
venkatesanmurali <venkatesan.murali1985@gmail.com>
2013-06-13 12:51:47 +0530

    missed out shard non in features migration

--- db/migrate/20130513110010_add_multiple_business_hours_feature.rb
+++ db/migrate/20130513110010_add_multiple_business_hours_feature.rb
@@ -1,4 +1,5 @@
 class AddMultipleBusinessHoursFeature < ActiveRecord::Migration
+	shard :none
   def self.up
   	execute("insert into features (type, account_id, created_at, updated_at) select 'MultipleBusinessHoursFeature',
      account_id, now(), now() from features where type='EstateFeature'")
