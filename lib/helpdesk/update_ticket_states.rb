59ae9fa2e569b28c10a3a17c40f0c81447de5880
hariji <hariharan@freshdesk.com>
2013-06-21 19:32:45 +0530

    Helpcard changes Begin Rescue in Update Ticket States worker
    Signed-off-by: hariji <hariharan@freshdesk.com>

--- lib/helpdesk/update_ticket_states.rb
+++ lib/helpdesk/update_ticket_states.rb
@@ -6,12 +6,17 @@ module Helpdesk
 		@queue = "update_ticket_states_queue"
        
 		def self.perform(args)
-			account = Account.current
-			User.current = account.users.find_by_id args[:current_user_id]
-
-			note = Helpdesk::Note.find args[:id]
-			note.save_response_time unless note.private?
-			note.trigger_observer(args[:model_changes])
+			begin
+				account = Account.current
+				User.current = account.users.find_by_id args[:current_user_id]
+				note = Helpdesk::Note.find args[:id]
+				note.save_response_time unless note.private?
+			rescue Exception => e
+				puts e.inspect, args.inspect
+				NewRelic::Agent.notice_error(e, {:args => args})
+			ensure
+				note.trigger_observer(args[:model_changes])
+			end
 		end
 
 	end
