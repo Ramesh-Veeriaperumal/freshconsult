b63618ace3c67b68bd587b509dfb10d314687c92
venkatesanmurali <venkatesan.murali1985@gmail.com>
2013-06-21 18:15:52 +0530

    migration for garden plan

--- db/migrate/20130513110010_add_multiple_business_hours_feature.rb
+++ db/migrate/20130513110010_add_multiple_business_hours_feature.rb
@@ -1,7 +1,13 @@
 class AddMultipleBusinessHoursFeature < ActiveRecord::Migration
 	shard :none
   def self.up
-  	execute("insert into features (type, account_id, created_at, updated_at) select 'MultipleBusinessHoursFeature',account_id, now(), now() from features where type in ('EstateFeature','GardenFeature','EstateClassicFeature')")
+  	execute <<-SQL
+      INSERT INTO features 
+        (account_id, type, created_at, updated_at) 
+        SELECT account_id, 'MultipleBusinessHoursFeature', now(), now() FROM subscriptions inner join subscription_plans 
+        on subscriptions.subscription_plan_id = subscription_plans.id where subscription_plans.name in 
+        ('Garden','Estate','Estate Classic')
+    SQL
   end
 
   def self.down
