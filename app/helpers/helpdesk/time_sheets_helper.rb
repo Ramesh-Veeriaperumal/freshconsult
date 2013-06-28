fd404d97f5e66a619e9e6bfe8d9b52e475464641
Sathappan Muthu <sathappan.m@gmail.com>
2013-06-28 16:18:32 +0530

    timesheet integrations fixed for update and delete actions and twitter version upgraded in strategy
    Signed-off-by: Sathappan Muthu <sathappan.m@gmail.com>

--- app/helpers/helpdesk/time_sheets_helper.rb
+++ app/helpers/helpdesk/time_sheets_helper.rb
@@ -32,8 +32,7 @@ module Helpdesk::TimeSheetsHelper
         page << "try{"
         page << "if (jQuery('##{app[0]}-timeentry-enabled').is(':checked')) {"
         page << "#{app[2]}.updateNotesAndTimeSpent(#{timeentry.note.to_json}, #{timeentry.time_spent == 0? "0.01" : timeentry.hours}, #{timeentry.billable}, #{timeentry.executed_at.to_json});"
-        page << "#{app[2]}.logTimeEntry();"
-        page << "#{app[2]}.set_timesheet_entry_id(#{timeentry.id});" # This is not needed for update.  But no harm in calling.
+        page << "#{app[2]}.logTimeEntry(#{timeentry.id});"
         page << "}"
         page << "}catch(e){ log(e)}"
       end
@@ -48,8 +47,7 @@ module Helpdesk::TimeSheetsHelper
         integrated_app = timeentry.integrated_resources.find_by_installed_application_id(app_detail)
         unless integrated_app.blank?
           page << "#{app[2]}.updateNotesAndTimeSpent(#{timeentry.note.to_json}, #{timeentry.time_spent == 0? "0.01" : timeentry.hours}, #{timeentry.billable}, #{timeentry.executed_at.to_json});"
-          page << "#{app[2]}.logTimeEntry();"
-          page << "#{app[2]}.set_timesheet_entry_id(#{timeentry.id});" # This is not needed for update.  But no harm in calling.
+          page << "#{app[2]}.logTimeEntry(#{timeentry.id});"
         end
       end
     end
