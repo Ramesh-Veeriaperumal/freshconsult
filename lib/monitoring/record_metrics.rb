7cbfb8a0577cecfdba0f460d16feba6942f8efe3
Pratheep Velicherla <pratheepv@Pratheeps-MacBook-Pro.local>
2013-07-18 16:16:45 +0530

    splunk monitoring for queue wait time and rescue load monitoring script
    splunk monitoring for queue wait time and rescue load monitoring script

--- lib/monitoring/record_metrics.rb
+++ lib/monitoring/record_metrics.rb
@@ -1,10 +1,12 @@
 class Monitoring::RecordMetrics
   
-  THRESHOLD_IN_SECS = 180 
+  THRESHOLD_IN_SECS = 180
+  JOB_WAITING_THRESHOLD_IN_SECS = 1800 
 
   def self.performance_data(metrics_data)
-  	if metrics_data[:time_spent] > THRESHOLD_IN_SECS
+  	if metrics_data[:time_spent] > THRESHOLD_IN_SECS || metrics_data[:job_waiting_time] > JOB_WAITING_THRESHOLD_IN_SECS
   		metrics_data[:time_spent] = metrics_data[:time_spent]/1.minutes
+      metrics_data[:job_waiting_time] = metrics_data[:job_waiting_time]/1.minutes
   		Resque.enqueue(Monitoring::SplunkStorm, metrics_data) 
   	end
   end
