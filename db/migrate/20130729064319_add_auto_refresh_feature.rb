c31474f9d463a54333a035b95b4a4013085898ab
Shridar S <Shridar@Shridar.local>
2013-07-30 16:19:26 +0530

    Auto Update - Feature Migration Query Change
    Auto Update - Feature Migration Query Change

--- db/migrate/20130729064319_add_auto_refresh_feature.rb
+++ db/migrate/20130729064319_add_auto_refresh_feature.rb
@@ -5,8 +5,9 @@ class AddAutoRefreshFeature < ActiveRecord::Migration
       INSERT INTO features 
         (account_id, type, created_at, updated_at) 
         SELECT account_id, 'AutoRefreshFeature', now(), now() FROM subscriptions inner join subscription_plans 
-        on subscriptions.subscription_plan_id = subscription_plans.id and subscriptions.amount > 0 
-        where subscription_plans.name in ('Premium','Garden','Garden Classic','Estate','Estate Classic')
+        on subscriptions.subscription_plan_id = subscription_plans.id and subscriptions.amount > 0 and 
+        subscriptions.state = "active" where subscription_plans.name in 
+        ('Premium','Garden','Garden Classic','Estate','Estate Classic')
     SQL
   end
 
