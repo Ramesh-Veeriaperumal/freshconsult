c99fdfee74a59fdec9d6cb4840cb7d6d0f250712
venkatesanmurali <venkatesan.murali1985@gmail.com>
2013-08-20 15:27:03 +0530

    customer satisfaction survey feature persists after downgrading issue - Fixed
    Signed-off-by: venkatesanmurali <venkatesan.murali1985@gmail.com>

--- lib/support_ticket_controller_methods.rb
+++ lib/support_ticket_controller_methods.rb
@@ -40,7 +40,7 @@ module SupportTicketControllerMethods
   end
 
   def customer_survey_required?
-    can_access_support_ticket? && current_account && current_account.features?(:survey_links) && @ticket.closed?
+    can_access_support_ticket? && current_account && current_account.features?(:survey_links, :surveys) && @ticket.closed?
   end
 
   def check_email
