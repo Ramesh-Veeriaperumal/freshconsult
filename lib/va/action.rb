fb515d7a8c13029e18860b92ded3a9ae5da73302
suman kumar dey <bappi.5476@gmail.com>
2013-08-27 18:48:42 +0530

    fixing dispatcher and supervisor

--- lib/va/action.rb
+++ lib/va/action.rb
@@ -226,7 +226,7 @@ class Va::Action
 
     def substitute_placeholders act_on, content_key     
       content = act_hash[content_key].to_s
-      content = RedCloth.new(content).to_html unless content_key == :email_subject
+      content = RedCloth.new(h(content)).to_html unless content_key == :email_subject
       Liquid::Template.parse(content).render(
                 'ticket' => act_on, 'helpdesk_name' => act_on.account.portal_name, 'comment' => act_on.notes.last)
     end
