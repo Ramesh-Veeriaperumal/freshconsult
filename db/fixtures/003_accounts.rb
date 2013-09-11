59b3a1a0d34eabf92c20f733d891b7b463867841
Prashanth Kumar <prashanthkumar374@gmail.com>
2013-09-11 16:13:42 +0530

    redis key addition for new accounts
    Signed-off-by: Prashanth Kumar <prashanthkumar374@gmail.com>

--- db/fixtures/003_accounts.rb
+++ db/fixtures/003_accounts.rb
@@ -15,6 +15,7 @@ unless Account.current
     )
     signup.save
     signup.account.make_current
+    $redis_others.sadd('user_email_migrated', signup.account.id)
   else
     Account.first.make_current
   end
