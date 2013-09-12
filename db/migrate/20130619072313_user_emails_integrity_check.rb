1749d1ba658074ddf7f4735123cd55889a509253
Prashanth Kumar <prashanthkumar374@gmail.com>
2013-09-12 18:50:48 +0530

    Account removal change and integrity code fix
    Signed-off-by: Prashanth Kumar <prashanthkumar374@gmail.com>

--- db/migrate/20130619072313_user_emails_integrity_check.rb
+++ db/migrate/20130619072313_user_emails_integrity_check.rb
@@ -9,12 +9,16 @@ class UserEmailsIntegrityCheck < ActiveRecord::Migration
     			account_id = #{account.id} AND email IS NOT NULL))
     		user_email_count = UserEmail.count_by_sql(%(SELECT count(id) FROM user_emails WHERE 
     			account_id = #{account.id}))
+        user_email_missing = select_all(%(SELECT id FROM users WHERE id NOT IN 
+          (SELECT user_id FROM user_emails WHERE account_id=#{account.id}) 
+          AND account_id = #{account.id})).collect{|x| x["id"]};
     		user_mismatch = select_all(%(SELECT users.id FROM users LEFT JOIN user_emails ON 
     			users.id = user_emails.user_id AND users.account_id = user_emails.account_id WHERE 
     			users.account_id = #{account.id} AND users.email IS NOT NULL 
           AND users.email <> user_emails.email)).collect{|x| x["id"]}
         @error_accounts << account.id unless (user_count == user_email_count and user_mismatch.empty?)
-        @mismatched << user_mismatch
+        @mismatched += user_mismatch
+        @mismatched += user_email_missing
     	end
     end
 
