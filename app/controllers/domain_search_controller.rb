9200d74fa1bac0719e4d2366722f071a729dbb06
vijayaraj <vijayaraj@freshdesk.com>
2013-09-18 19:47:25 +0530

    Sharding changes in Domain Search controller
    Signed-off-by: vijayaraj <vijayaraj@freshdesk.com>

--- app/controllers/domain_search_controller.rb
+++ app/controllers/domain_search_controller.rb
@@ -4,15 +4,18 @@ class DomainSearchController < ApplicationController
   before_filter :unset_current_account
 
   def locate_domain
+    agents = urls = []
+    
     Sharding.execute_on_all_shards do
       agents = User.technicians.find_all_by_email(params[:user_email])
-      unless agents.empty?
-        urls = agents.collect{ |agent| agent.account.host }
-        UserNotifier.deliver_helpdesk_url_reminder(params[:user_email], urls)
+      agents.each do |agent|
+        account = agent.account
+        urls.push(account.host)
       end
-      
-      render :json => { :available => agents.present? }, :callback => params[:callback]
     end
+        
+    UserNotifier.deliver_helpdesk_url_reminder(params[:user_email], urls)
+    render :json => { :available => agents.present? }, :callback => params[:callback]
   end
 
 end
