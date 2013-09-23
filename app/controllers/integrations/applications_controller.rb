6468449b742031f953fac2e489a021e5b303fa6d
johnpaulh <johnpaul@freshdesk.com>
2013-09-23 15:18:02 +0530

    RE-CHECKING Salesforce custom fields in Staging environment. Handled Invalid session on oauth2 now. If testing failed this needs to be reverted.

--- app/controllers/integrations/applications_controller.rb
+++ app/controllers/integrations/applications_controller.rb
@@ -14,14 +14,32 @@ class Integrations::ApplicationsController < Admin::AdminController
     kv_store = Redis::KeyValueStore.new(Redis::KeySpec.new(Redis::RedisKeys::APPS_AUTH_REDIRECT_OAUTH, key_options))
     kv_store.group = :integration
     app_config = kv_store.get_key
+    if app_config.blank?
+      Rails.logger.debug "Refresh Access token as this is Edit ::#{params['id']}"
+      installed_app = Integrations::InstalledApplication.find(:first, :include=>:application, :conditions => ["#{Integrations::Application.table_name}.name = ? and  #{Integrations::InstalledApplication.table_name}.account_id = ?",params['id'],current_account])
+       #added for edit option as there won't be any redis store while editing.
+      access_token = get_oauth2_access_token(installed_app.application.oauth_provider, installed_app.configs[:inputs]['refresh_token'],params['id']) 
+      oauth_token= access_token.token
+      installed_app[:configs][:inputs]['oauth_token'] = access_token.token
+      installed_app.save #updating the installed app with new token
+      app_config = installed_app.configs[:inputs].to_json if installed_app.application.options[:pre_install]
+    end
   	begin
   		unless app_config.blank?
   		  config_hash = JSON.parse(app_config)
   			app_name = config_hash["app_name"]
   			config_hash.delete("app_name")	    
         if params['id'] == 'salesforce' 
-          config_hash['contact_fields'] = fetch_sf_contact_fields(config_hash['oauth_token'], config_hash['instance_url']) 
-          config_hash['lead_fields'] = fetch_sf_lead_fields(config_hash['oauth_token'], config_hash['instance_url']) 
+          app_name = installed_app.application.name if app_name.blank?
+          if params[:install].blank?
+            @salesforce_config=Hash.new
+            @salesforce_config['contact_fields'] = fetch_sf_contact_fields(config_hash['oauth_token'], config_hash['instance_url']) 
+            @salesforce_config['lead_fields'] = fetch_sf_lead_fields(config_hash['oauth_token'], config_hash['instance_url']) 
+            render :partial => "integrations/applications/salesforce_fields",:layout=>"application" and return
+          else
+            config_hash['contact_fields'] = params[:contacts].join(",") unless params[:contacts].nil?
+            config_hash['lead_fields'] = params[:leads].join(",") unless params[:leads].nil?
+          end
         end
 		    installed_application = Integrations::Application.install_or_update(app_name, current_account.id, config_hash)
 		    flash[:notice] = t(:'flash.application.install.success') if installed_application
@@ -32,7 +50,8 @@ class Integrations::ApplicationsController < Admin::AdminController
         end
 	    end	
   	rescue Exception => msg
-  		puts "Something went wrong while configuring an installed application ( #{msg})"
+      Rails.logger.debug "Something went wrong while configuring an installed application ( #{msg})"
+      Rails.logger.debug msg.backtrace.join("\n") #added trace as we need logs on exception!!!
       flash[:error] = t(:'flash.application.install.error')
   	end
     redirect_to :controller=> 'applications', :action => 'index'
