653af755abecc25d779a83642de8e4afe83de2af
Kaushik <kaushik@freshdesk.com>
2013-09-24 17:39:34 +0530

    Sharding support for Integrations

--- app/models/integrations/application.rb
+++ app/models/integrations/application.rb
@@ -23,11 +23,9 @@ class Integrations::Application < ActiveRecord::Base
     JSON.parse(self.to_json)["application"]
   end
 
-  def oauth_url(account_id)
+  def oauth_url(acc_portal_id)
     AppConfig['integrations_url'][Rails.env] + 
-      Liquid::Template.parse(options[:oauth_url]).render({ 
-        "portal_id" => account_id, "account_id" => account_id  
-      })
+      Liquid::Template.parse(options[:oauth_url]).render({  'account_id' => acc_portal_id, 'portal_id'  => acc_portal_id})
   end
 
   def widget
