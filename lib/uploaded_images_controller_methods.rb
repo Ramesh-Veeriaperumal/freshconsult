413657763733e7abed41d7f367a4b2c5cd1cd42a
Arvinth <arvinth@freshdesk.com>
2013-10-01 12:18:03 +0530

    Revert "Inline images changed to private scope - testing in environment"
    This reverts commit 1ff941d3b22bf9d252399cfb8f5b36c9e29e61b3.

--- lib/uploaded_images_controller_methods.rb
+++ lib/uploaded_images_controller_methods.rb
@@ -6,7 +6,7 @@ module UploadedImagesControllerMethods
 
   def create    
     @image = current_account.attachments.build({
-      :description => public_image? ? "public" : nil,
+      :description => "public",
       :content => params[:image][:uploaded_data],
       :attachable_type => "#{cname} Upload"
     })
@@ -25,18 +25,10 @@ module UploadedImagesControllerMethods
     end
 
     def success_response
-      { :filelink => get_image_url, :fileid => @image.id, :uniquekey => params["_uniquekey"] }
+      { :filelink => @image.content.url, :fileid => @image.id, :uniquekey => params["_uniquekey"] }
     end
 
     def error_response
      { :error => @image.errors.blank? ? [] : @image.errors.full_messages.to_sentence, :uniquekey => params["_uniquekey"] }
-    end
-
-    def public_image?
-      cname != "Tickets Image"
-    end
-
-    def get_image_url
-      public_image? ? @image.content.url : helpdesk_attachment_path(@image)
     end
 end
