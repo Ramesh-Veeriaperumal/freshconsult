788b091ae7b522958086adbbfdba3c91584a45cb
brbharathkumar <bharath.kumar@freshdesk.com>
2013-10-09 18:14:59 +0530

    Applied sharding in the middleware

--- lib/middleware/trusted_ip.rb
+++ lib/middleware/trusted_ip.rb
@@ -1,36 +1,36 @@
 class Middleware::TrustedIp
 
-  include FetchAccount
-
   def initialize(app)
     @app = app
   end
 
   def call(env)
-    Thread.current[:account] = current_account(env["HTTP_HOST"])
-    unless env['rack.session']['user_credentials_id'].nil?
-      if trusted_ips_enabled?
-        unless valid_ip(env['REMOTE_ADDR'], env['rack.session']['user_credentials_id'])
-          @status, @headers, @response = [302, {"Location" => "/unauthorized.html"}, 
-                                      'Your IPAddress is bocked by the administrator']
-          return [@status, @headers, @response]
+    shard = ShardMapping.lookup_with_domain(env["HTTP_HOST"])
+    raise DomainNotReady unless shard || Rails.env.development?
+    account_id =  Rails.env.development? ? Account.first.id : shard.account_id
+    Sharding.select_shard_of(account_id) do
+      @current_account = Account.find(account_id)
+      unless env['rack.session']['user_credentials_id'].nil?
+        if trusted_ips_enabled?
+          unless valid_ip(env['REMOTE_ADDR'], env['rack.session']['user_credentials_id'])
+            @status, @headers, @response = [302, {"Location" => "/unauthorized.html"}, 
+                                        'Your IPAddress is bocked by the administrator']
+            return [@status, @headers, @response]
+          end
         end
       end
     end
     @status, @headers, @response = @app.call(env)
-    ensure
-      #thread current account will be resetted when the call goes to application controller. 
-      @current_account = nil
   end
 
   def trusted_ips_enabled?
-    current_account.features_included?(:whitelisted_ips) && 
-          (current_account.whitelisted_ip_from_cache || {})[:enabled] 
+    @current_account.features_included?(:whitelisted_ips) && 
+          (@current_account.whitelisted_ip_from_cache || {})[:enabled] 
   end
 
   def valid_ip(current_ip, current_user_id)
     unless applies_to_agent?(current_user_id)
-      current_account.whitelisted_ip_from_cache.ip_ranges.each do |ip|
+      @current_account.whitelisted_ip_from_cache.ip_ranges.each do |ip|
         return true if ip_is_in_range?(IPAddress(ip[:start_ip]),IPAddress(ip[:end_ip]),IPAddress(current_ip))
       end
     else
@@ -40,8 +40,8 @@ class Middleware::TrustedIp
   end
 
   def applies_to_agent?(current_user_id)
-    current_account.whitelisted_ip_from_cache.applies_only_to_agents && 
-           !current_account.users.find_by_id(current_user_id).agent? 
+    @current_account.whitelisted_ip_from_cache.applies_only_to_agents && 
+           !@current_account.users.find_by_id(current_user_id).agent? 
   end
 
   def ip_is_in_range?(start_ip, end_ip, current_ip)
