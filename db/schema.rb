88c927efe0331800cbf086796e792a7a812a9f76
venkatesanmurali <venkatesan.murali1985@gmail.com>
2013-10-14 17:32:52 +0530

    Renaming schema migrations timestamp issue.
    Signed-off-by: venkatesanmurali <venkatesan.murali1985@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131381493575) do
+ActiveRecord::Schema.define(:version => 20131014120021) do
 
   create_table "account_additional_settings", :force => true do |t|
     t.string   "email_cmds_delimeter"
