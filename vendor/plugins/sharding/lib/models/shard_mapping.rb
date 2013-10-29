f8580051800093071ea62dd816cd6b6391cb19e4
Kiran Darisi <kiran.darisi@gmail.com>
2013-10-29 15:03:54 +0530

    reverted

--- vendor/plugins/sharding/lib/models/shard_mapping.rb
+++ vendor/plugins/sharding/lib/models/shard_mapping.rb
@@ -40,7 +40,7 @@ class ShardMapping < ActiveRecord::Base
  end
 
  def self.latest_shard
- 	"shard_2" #probably fetch it from Redis or config 
+ 	"shard_1" #probably fetch it from Redis or config 
  end
 
  def ok?
