b0662b3c6fe676a6a2edd35bdcec66d3b6d3bf25
pratheep velicherla <pratheep.velicherla@gmail.com>
2013-11-06 16:20:23 +0530

    debug statements added..

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -5,7 +5,7 @@ run "sudo chmod 755 #{config.release_path}/script/runner"
 # Added a compilation of core css files used under public/src/app
 # All files in public/stylesheets/app will be ignored and cannot be checked in
 on_app_servers do
-	
+	puts "config.current_role:::::::::#{config.current_role}"
 	if config.current_role == "app_master" || config.current_role == "solo"
 		run "RAILS_ENV=#{config.node[:environment][:framework_env]} bundle exec rake cloudfront_assets:upload"
 	else
