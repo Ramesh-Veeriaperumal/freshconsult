52ed27999ef58ec27d88e71d1210144486edd03f
Saranyaa <saranyaa@Saranyaas-MacBook-Pro.local>
2013-11-11 13:12:45 +0530

    Export Tickets - Excel file export included.

--- lib/helpdesk/tickets_export.rb
+++ lib/helpdesk/tickets_export.rb
@@ -20,53 +20,77 @@ class Helpdesk::TicketsExport
       end
       #####
       index_filter =  Account.current.ticket_filters.new(Helpdesk::Filters::CustomTicketFilter::MODEL_NAME).deserialize_from_params(export_params)
-      sql_conditions = index_filter.sql_conditions
-      sql_conditions[0].concat(%(and helpdesk_ticket_states.#{export_params[:ticket_state_filter]} 
+      @sql_conditions = index_filter.sql_conditions
+      @sql_conditions[0].concat(%(and helpdesk_ticket_states.#{export_params[:ticket_state_filter]} 
                                  between '#{export_params[:start_date]}' and '#{export_params[:end_date]}'
                                 )
                               )
 
-      sql_conditions[0].concat(%(and helpdesk_tickets.status in (#{RESOLVED}, #{CLOSED}))
+      @sql_conditions[0].concat(%(and helpdesk_tickets.status in (#{RESOLVED}, #{CLOSED}))
                               ) if export_params[:ticket_state_filter].eql?("resolved_at")
-      sql_conditions[0].concat(%(and helpdesk_tickets.status in (#{CLOSED}))
+      @sql_conditions[0].concat(%(and helpdesk_tickets.status in (#{CLOSED}))
                               ) if export_params[:ticket_state_filter].eql?("closed_at")
 
-      all_joins = index_filter.get_joins(sql_conditions)
-      all_joins[0].concat(%( INNER JOIN helpdesk_ticket_states ON 
+      @all_joins = index_filter.get_joins(@sql_conditions)
+      @all_joins[0].concat(%( INNER JOIN helpdesk_ticket_states ON 
                      helpdesk_ticket_states.ticket_id = helpdesk_tickets.id AND 
                      helpdesk_tickets.account_id = helpdesk_ticket_states.account_id))
-      csv_hash = export_params[:export_fields]
-      headers = csv_hash.keys.sort
-      select = "helpdesk_tickets.* "
-      select = "DISTINCT(helpdesk_tickets.id) as 'unique_id' , #{select}" if sql_conditions[0].include?("helpdesk_tags.name")
-      csv_string = CSVBridge.generate(:col_sep => "\t") do |csv|
-        csv_headers = headers.collect {|header| csv_hash[header]}
-        csv << csv_headers
-        Account.current.tickets.find_in_batches(:select => select,
-                                        :conditions => sql_conditions, 
-                                        :include => [:ticket_states, :ticket_status, :flexifield,
-                                                     :responder, :requester],
-                                        :joins => all_joins
-                                       ) do |items|
-          items.each do |record|
-            csv_data = []
-            headers.each do |val|
-              data = record.send(val)
-              csv_data << ((data.blank? || (data.is_a? Integer)) ? data : (CGI::unescapeHTML(data.to_s))).to_s.gsub(/\s+/, " ")
-            end
-            csv << csv_data
+      @select = "helpdesk_tickets.* "
+      @select = "DISTINCT(helpdesk_tickets.id) as 'unique_id' , #{@select}" if @sql_conditions[0].include?("helpdesk_tags.name")
+      export_params[:format] == 'csv' ?  csv_export(export_params) : xls_export(export_params)
+    end
+  end
+
+  def self.csv_export export_params
+    csv_hash = export_params[:export_fields]
+    record_headers = csv_hash.keys.sort
+    csv_string = CSVBridge.generate(:col_sep => "\t") do |csv|
+      csv_headers = record_headers.collect {|header| csv_hash[header]}
+      csv << csv_headers
+      Account.current.tickets.find_in_batches(:select => @select,
+                                      :conditions => @sql_conditions, 
+                                      :include => [:ticket_states, :ticket_status, :flexifield,
+                                                   :responder, :requester],
+                                      :joins => @all_joins
+                                     ) do |items|
+        items.each do |record|
+          csv_data = []
+          record_headers.each do |val|
+            data = record.send(val)
+            csv_data << ((data.blank? || (data.is_a? Integer)) ? data : (CGI::unescapeHTML(data.to_s))).to_s.gsub(/\s+/, " ")
           end
+          csv << csv_data
         end
       end
-      # csv_string = BOM + RubyBridge.convert_string_encoding("utf-16le", "utf-8", csv_string)
-      bom = BOM
-      bom = RubyBridge.convert_string_encoding("utf-16le", "ASCII-8BIT", bom) if csv_string.respond_to?(:force_encoding)
-      csv_string = bom + RubyBridge.convert_string_encoding("utf-16le", "utf-8", csv_string)
-      csv_string = RubyBridge.force_binary_encoding(csv_string)
+    end 
+    Rails.logger.info "<--- Triggering export tickets csv mail. User Email Id: #{User.current.email} --->"
+    Rails.logger.info "<--- Params #{export_params[:ticket_state_filter]}, #{export_params[:start_date]}, #{export_params[:end_date]} --->"
+    Helpdesk::TicketNotifier.deliver_export(export_params, csv_string, User.current)
+  end
 
-      Rails.logger.info "<--- Triggering export tickets csv mail. User Email Id: #{User.current.email} --->"
-      Rails.logger.info "<--- Params #{export_params[:ticket_state_filter]}, #{export_params[:start_date]}, #{export_params[:end_date]} --->"
-      Helpdesk::TicketNotifier.deliver_export(export_params, csv_string, User.current)
+  def self.xls_export export_params
+    require 'erb'
+    @xls_hash = export_params[:export_fields] 
+    @headers = @xls_hash.keys.sort
+    @records = []
+    Account.current.tickets.find_in_batches(:select => @select,
+                                    :conditions => @sql_conditions, 
+                                      :include => [:ticket_states, :ticket_status, :flexifield,
+                                                   :responder, :requester],
+                                      :joins => @all_joins
+                                     ) do |items|
+        items.each do |item|
+        record = Hash.new
+        @headers.each do |val|
+          data = item.send(val)
+          record[val] = ((data.blank? || (data.is_a? Integer)) ? data : (CGI::unescapeHTML(data.to_s)))
+        end
+        @records.push(record)
+      end
     end
+    path =  "#{RAILS_ROOT}/app/views/support/tickets/export_csv.xls.erb"
+    renderer = ERB.new(File.read(path))
+    xls_string = renderer.result(binding)
+    Helpdesk::TicketNotifier.deliver_export_xls(export_params, xls_string, User.current)
   end
 end
