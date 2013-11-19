1b965b1d3faba710d2823f8b3b71dde83f869a28
venkatesanmurali <venkatesan.murali1985@gmail.com>
2013-11-19 16:37:16 +0530

    Removing flash message for successful sso login. spoke to parsu on this.
    Signed-off-by: venkatesanmurali <venkatesan.murali1985@gmail.com>

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -60,7 +60,6 @@ include Redis::TicketsRedis
       @user_session = @current_user.account.user_sessions.new(@current_user)
       if @user_session.save
         remove_old_filters  if @current_user.agent?
-        flash[:notice] = t(:'flash.login.success')
         redirect_back_or_default(params[:redirect_to] || '/')  if grant_day_pass  
       else
         flash[:notice] = t(:'flash.login.failed')
