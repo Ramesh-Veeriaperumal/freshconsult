a624fab1483bf9996bf14048a8291dcc95d7693f
Vamsi Krishna <vamsi@VAMSIs-MacBook-Pro.local>
2013-12-03 19:52:04 +0530

    QA bugfix #1562 - for facebook and twitter tickets the cc_email will be nil, so before save dup of cc_email fails
    QA bugfix #1562 - for facebook and twitter tickets the cc_email will be nil, so before save dup of cc_email fails

--- app/models/helpdesk/note.rb
+++ app/models/helpdesk/note.rb
@@ -325,6 +325,7 @@ class Helpdesk::Note < ActiveRecord::Base
     end
 
     def notable_cc_email_updated?(old_cc, new_cc)
+      return !old_cc.eql?(new_cc) if old_cc.nil?
       [:cc_emails, :fwd_emails].any? { |f| 
                                        !(old_cc[f].uniq.sort.eql?(new_cc[f].uniq.sort))
                                      }
