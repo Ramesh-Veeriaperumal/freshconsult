5d59629777f4eff1f0409ae0e6ce614f0dcc46f7
venkatesanmurali <venkatesan.murali1985@gmail.com>
2014-01-07 18:51:22 +0530

    Modifying migrations for shards..
    Signed-off-by: venkatesanmurali <venkatesan.murali1985@gmail.com>

--- db/migrate/20131226093632_cleanup_corrupted_users.rb
+++ db/migrate/20131226093632_cleanup_corrupted_users.rb
@@ -6,14 +6,16 @@ class CleanupCorruptedUsers < ActiveRecord::Migration
   		:conditions => ['helpdesk_agent = ? and active = ?', true,true]) do |users|
   		count = count + 1
   		users.each do |user|
-  			user.helpdesk_agent = false if user.agent.nil?
-  			if user.has_no_credentials?
-  				user.account.make_current
-  				puts "Id: #{user.id}, Email: #{user.email}, account: #{user.account_id}"
-				user.active = false
-			end
-			user.save if user.changed?
-			Account.reset_current_account
+        Sharding.select_shard_of(user.account_id) do
+    			user.helpdesk_agent = false if user.agent.nil?
+    			if user.has_no_credentials?
+    				user.account.make_current
+    				puts "Id: #{user.id}, Email: #{user.email}, account: #{user.account_id}"
+  				  user.active = false
+          end
+          user.save if user.changed?
+        end
+        Account.reset_current_account
   		end
   		puts "Batch #{count} done"
   	end
@@ -24,8 +26,12 @@ class CleanupCorruptedUsers < ActiveRecord::Migration
   		:conditions => ["users.helpdesk_agent = ? ", false]) do |agents|
   		count = count + 1
   		agents.each do |agent|
-  			puts "Id: #{agent.user_id}, Email: #{agent.user.email}, account: #{agent.account_id}"
-  			agent.destroy
+        Sharding.select_shard_of(agent.account_id) do
+          account = Account.find(agent.account_id)
+          account.make_current
+          puts "Id: #{agent.user_id}, Email: #{agent.user.email}, account: #{agent.account_id}"
+          agent.destroy
+        end
   		end
   		puts "Batch #{count} done"
   	end
