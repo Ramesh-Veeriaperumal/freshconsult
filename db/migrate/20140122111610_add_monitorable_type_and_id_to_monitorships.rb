410220c4a7590d42844b127fe9780913abec5dd6
Prashanth Kumar <prashanthkumar374@gmail.com>
2014-02-10 09:53:51 +0530

    Migration file change in Monitorships
    Signed-off-by: Prashanth Kumar <prashanthkumar374@gmail.com>

--- db/migrate/20140122111610_add_monitorable_type_and_id_to_monitorships.rb
+++ db/migrate/20140122111610_add_monitorable_type_and_id_to_monitorships.rb
@@ -1,20 +1,16 @@
 class AddMonitorableTypeAndIdToMonitorships < ActiveRecord::Migration
   shard :all
   def self.up
-    Lhm.change_table :monitorships, :atomic_switch => true do |m|
-      m.add_column :monitorable_type, "varchar(255) DEFAULT NULL"
-      m.ddl("ALTER TABLE %s CHANGE topic_id monitorable_id bigint unsigned" % m.name)
-      m.ddl("ALTER TABLE %s ADD INDEX `complete_monitor_index` (`account_id`, `user_id`, `monitorable_id`, `monitorable_type`)" % m.name)
-    end
+    add_column :monitorships, :monitorable_type, :string
+    execute("ALTER TABLE monitorships CHANGE topic_id monitorable_id bigint(20) DEFAULT NULL")
+    execute("ALTER TABLE monitorships ADD INDEX `complete_monitor_index` (`account_id`, `user_id`, `monitorable_id`, `monitorable_type`)")
     execute("UPDATE monitorships SET monitorable_type = 'Topic'")
   end
 
   def self.down
     execute("DELETE FROM monitorships where monitorable_type='Forum'")
-    Lhm.change_table :monitorships, :atomic_switch => true do |m|
-      m.ddl("ALTER TABLE %s DROP INDEX `complete_monitor_index`" % m.name)
-      m.ddl("ALTER TABLE %s CHANGE monitorable_id topic_id bigint unsigned" % m.name)
-      m.remove_column :monitorable_type
-    end
+    execute("ALTER TABLE monitorships DROP INDEX `complete_monitor_index`")
+    execute("ALTER TABLE monitorships CHANGE monitorable_id topic_id bigint(20) DEFAULT NULL")
+    remove_column :monitorships, :monitorable_type
   end
 end
