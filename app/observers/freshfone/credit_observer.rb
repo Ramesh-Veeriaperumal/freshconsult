d91b8a1ed40e6ba595d09aa57c8dc559a6012fd1
Sathappan Muthu <sathappan.m@gmail.com>
2014-02-11 18:41:53 +0530

    Added promotional freshfone credit for new active account and fixed low balance notifications
    Signed-off-by: Sathappan Muthu <sathappan.m@gmail.com>

--- app/observers/freshfone/credit_observer.rb
+++ app/observers/freshfone/credit_observer.rb
@@ -3,17 +3,13 @@ class Freshfone::CreditObserver < ActiveRecord::Observer
 
 	include Freshfone::NodeEvents
 
-	def after_save(freshfone_credit)
-		account = freshfone_credit.account
-		notify_low_balance(freshfone_credit, account) if freshfone_credit.available_credit_changed?
-		update_freshfone_widget(freshfone_credit, account) if freshfone_credit.available_credit_changed?
-		update_freshfone_account_state(freshfone_credit, account) if freshfone_credit.available_credit_changed?
-		trigger_auto_recharge(freshfone_credit) if credit_threshold_reached?(freshfone_credit)
-	end
-
 	def after_update(freshfone_credit)
 		account = freshfone_credit.account
+		update_freshfone_widget(freshfone_credit, account) if freshfone_credit.available_credit_changed?
 		restore_freshfone_account_state(account) if freshfone_credit.freshfone_suspended?
+		update_freshfone_account_state(freshfone_credit, account) if freshfone_credit.available_credit_changed?
+		notify_low_balance(freshfone_credit, account) if freshfone_credit.available_credit_changed?
+		trigger_auto_recharge(freshfone_credit) if credit_threshold_reached?(freshfone_credit)
 	end
 
 	private
@@ -48,7 +44,10 @@ class Freshfone::CreditObserver < ActiveRecord::Observer
     end
 
 		def credit_limit_on_disabled_auto_recharge?(freshfone_credit)
-			!freshfone_credit.auto_recharge? and freshfone_credit.recharge_alert?
+			!freshfone_credit.auto_recharge? and 
+			!already_notified?(freshfone_credit) and 
+			!freshfone_credit.freshfone_suspended? and
+			freshfone_credit.recharge_alert?
 		end
 
 		def credit_threshold_reached?(freshfone_credit)
@@ -81,6 +80,10 @@ class Freshfone::CreditObserver < ActiveRecord::Observer
 
 		def previously_low?(credit)
 			credit.available_credit_was <= Freshfone::Credit::CREDIT_LIMIT[:calling_threshold]
+		end
+
+		def already_notified?(credit)
+			credit.available_credit_was <= Freshfone::Credit::CREDIT_LIMIT[:recharge_alert]
 		end
 
 end
