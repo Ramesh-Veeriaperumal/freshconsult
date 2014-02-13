352bae830581f783acd56183ca8a6b1793758696
Venkatesh <venkatesh@freshdesk.com>
2014-02-13 12:56:09 +0530

    one more check added for shortcut enabled?
    check current_user is agent?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -921,7 +921,7 @@ module ApplicationHelper
   end
 
   def shortcuts_enabled?
-    logged_in? and current_user.agent.shortcuts_enabled?
+    logged_in? and current_user.agent? and current_user.agent.shortcuts_enabled?
   end
 
   def current_platform
