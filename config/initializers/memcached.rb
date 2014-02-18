0dc5b78a0431e472f54d0b536dccf7067fa1f1cd
Pradeep <pradeep.t@freshdesk.com>
2014-02-17 22:52:29 -0800

    Pushing dalli changes
    Pushing dalli changes

--- config/initializers/memcached.rb
+++ config/initializers/memcached.rb
@@ -1,7 +1,9 @@
 require "memcache"
-config = YAML::load_file(File.join(RAILS_ROOT, 'config', 'memcached.yml'))[RAILS_ENV].symbolize_keys!
+config = YAML::load_file(File.join(Rails.root, 'config', 'memcached.yml'))[Rails.env].symbolize_keys!
 servers = config.delete(:servers)
-$memcache = MemCache.new servers, { :check_size =>  false, :timeout => 5, :no_reply => true}
+#$memcache = MemCache.new servers, { :check_size =>  false, :timeout => 5, :no_reply => true}
+options = { :namespace => config[:namespace], :compress => true}
+$memcache = Dalli::Client.new(servers, options)
 ActionController::Base.cache_store = :mem_cache_store, servers, config
 #ActionController::Base.cache_store = $memcache
 
