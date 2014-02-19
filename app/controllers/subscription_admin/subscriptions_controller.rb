e0aae4e04e9693b80ddb02cd391ff6ba49080f0c
vijayaraj <vijayaraj@freshdesk.com>
2014-02-19 12:14:39 +0530

    Revert "Fixing admin account search result repetition."
    This reverts commit 90af85e5da887ec5c7051a02d783ba5c61e887e0.

--- app/controllers/subscription_admin/subscriptions_controller.rb
+++ app/controllers/subscription_admin/subscriptions_controller.rb
@@ -104,8 +104,7 @@ class SubscriptionAdmin::SubscriptionsController < ApplicationController
   
    def search(search)
     results = []
-    domain_mappings = DomainMapping.find(:all, 
-      :conditions => ['domain LIKE ? and portal_id = ?', "%#{search}%", nil], :limit => 30)
+    domain_mappings = DomainMapping.find(:all, :conditions => ['domain LIKE ? and portal IS ?', "%#{search}%", nil])
     
     unless search.blank?
       domain_mappings.each do |domain|
