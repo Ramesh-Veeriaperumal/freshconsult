741b8260cc2dea03bdc77eaa03db623f10d6c418
Prashanth Kumar <prashanthkumar374@gmail.com>
2014-02-20 14:47:40 +0530

    User Emails Regex change
    Signed-off-by: Prashanth Kumar <prashanthkumar374@gmail.com>

--- app/models/user_email.rb
+++ app/models/user_email.rb
@@ -4,7 +4,7 @@ class UserEmail < ActiveRecord::Base
   # include Users::Activator
   # include ActionController::UrlWriter
 
-  EMAIL_REGEX = /(\A[-A-Z0-9.'’_%=+]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,4}|museum|travel)\z)/i
+  EMAIL_REGEX = /(\A[-A-Z0-9.'’_&%=+]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,10})\z)/i
 
   belongs_to :user
   belongs_to_account
