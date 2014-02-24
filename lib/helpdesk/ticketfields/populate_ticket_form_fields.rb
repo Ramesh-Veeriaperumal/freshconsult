edab6b8eb3a5fb4654a0f53202c9fadf09d4f896
Hari Prasad <hari@freshdesk.com>
2014-02-24 13:18:52 +0530

    removed execute on run_on_slave check and added begin rescue check.

--- lib/helpdesk/ticketfields/populate_ticket_form_fields.rb
+++ lib/helpdesk/ticketfields/populate_ticket_form_fields.rb
@@ -4,13 +4,11 @@ module Helpdesk::Ticketfields::PopulateTicketFormFields
 
 	def form_fields_data(starting_account_id=0)
 		Sharding.execute_on_all_shards do
-			Sharding.run_on_slave do
-				Account.find_in_batches(:batch_size => 500,
-					:conditions => "id >=#{starting_account_id}") do |accounts|
-					accounts.each do |account|
-						# Resque.enqueue(Workers::PopulateTicketFormFieldsWorker, {:account_id => account.id})
-						populate_data(account)
-					end
+			Account.find_in_batches(:batch_size => 500,
+				:conditions => "id >=#{starting_account_id}") do |accounts|
+				accounts.each do |account|
+					# Resque.enqueue(Workers::PopulateTicketFormFieldsWorker, {:account_id => account.id})
+					populate_data(account)
 				end
 			end
 		end
@@ -19,15 +17,19 @@ module Helpdesk::Ticketfields::PopulateTicketFormFields
 	def populate_data(account)
 		puts "===account==#{account.id}===#{account.name}"
 		tkt_fields = account.ticket_fields
-		tkt_fields.each do |tkt_field|
-			save_form_field(tkt_field)
-			puts "==updated field==#{tkt_field.label}"
-			if(tkt_field.field_type == 'nested_field')
-				tkt_field.nested_ticket_fields.each do |nested_field|
-					save_form_nested_field(nested_field)
-					puts "==updated nested field==#{nested_field.label}"
+		begin
+			tkt_fields.each do |tkt_field|
+				save_form_field(tkt_field)
+				puts "==updated field==#{tkt_field.label}"
+				if(tkt_field.field_type == 'nested_field')
+					tkt_field.nested_ticket_fields.each do |nested_field|
+						save_form_nested_field(nested_field)
+						puts "==updated nested field==#{nested_field.label}"
+					end
 				end
 			end
+		rescue => e 
+			puts "######### Error while populating for Account=#{account.id}==#{account.name}"
 		end
 	end
 	
