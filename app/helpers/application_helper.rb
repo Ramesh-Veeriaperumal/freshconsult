ea503023fc4d8eab1d11d383bb2352f9b85fefe7
Muthu <kumar13.ece@gmail.com>
2014-03-10 21:14:20 +0530

    Chat - portal implementation
    Signed-off-by: Muthu <kumar13.ece@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
   include Juixe::Acts::Voteable
   include ActionView::Helpers::TextHelper
   include Gamification::GamificationUtil
+  include ChatHelper
 
   include MemcacheKeys
 
---
+++ db/migrate/20140310074821_add_portal_enable_option_to_chat_settings.rb
@@ -0,0 +1,16 @@
+class AddPortalEnableOptionToChatSettings < ActiveRecord::Migration
+  shard :all
+  def self.up
+    Lhm.change_table :chat_settings, :atomic_switch => true do |m|
+      m.add_column :show_on_portal, "tinyint(1) DEFAULT '1'"
+      m.add_column :portal_login_required,"tinyint(1) DEFAULT '0'"
+    end
+  end
+
+  def self.down
+    Lhm.change_table :chat_settings, :atomic_switch => true do |m|
+      m.remove_column :show_on_portal
+      m.remove_column :portal_login_required
+    end
+  end
+end
