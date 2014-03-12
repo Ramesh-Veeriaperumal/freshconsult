a41f6cc35964f13a1d37ea511642736b1dd5446e
Sathappan Muthu <sathappan.m@gmail.com>
2014-03-12 11:38:43 +0530

    freshfone credits added to subscriptions admin
    Signed-off-by: Sathappan Muthu <sathappan.m@gmail.com>

---
+++ app/controllers/subscription_admin/freshfone_subscriptions_controller.rb
@@ -0,0 +1,33 @@
+class SubscriptionAdmin::FreshfoneSubscriptionsController < ApplicationController
+  include AdminControllerMethods
+
+  def index
+    fetch_credits_by_month
+  end
+
+  private
+    def fetch_credits_by_month
+      credits = Sharding.run_on_all_slaves { ActiveRecord::Base.connection.execute(freshfone_payments_query) }
+      @credits_by_month = cumulative_credits(credits)
+    end
+
+    def freshfone_payments_query
+      "SELECT DATE_FORMAT(freshfone_payments.created_at, '%b, %Y') AS credit_month, 
+        SUM(freshfone_payments.purchased_credit) AS credits FROM freshfone_payments 
+        JOIN accounts ON accounts.id = freshfone_payments.account_id 
+        JOIN subscriptions ON accounts.id = subscriptions.account_id 
+        WHERE subscriptions.state = 'active' AND freshfone_payments.status_message IS NULL 
+        GROUP BY DATE_FORMAT(freshfone_payments.created_at, '%b, %Y') 
+        ORDER BY credit_month DESC"
+    end
+
+    def cumulative_credits(credits, results={})
+      credits.each do |credit|
+        credit.each do |date, payment|
+          credit = results.fetch(date,0)
+          results.store(date,(credit+payment))
+        end
+      end
+      results
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -257,6 +257,7 @@
         resque.failed_show '/failed/:queue_name/show', :controller => 'failed', :action => 'show'
         resque.resources :failed, :member => { :destroy => :delete , :requeue => :put }, :collection => { :destroy_all => :delete, :requeue_all => :put }
       end
+      admin.resources :freshfone_subscriptions, :as => 'freshfone_admin'
       # admin.resources :analytics 
       admin.resources :spam_watch, :only => :index
       admin.spam_details ':shard_name/spam_watch/:user_id/:type', :controller => :spam_watch, :action => :spam_details
