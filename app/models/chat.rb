e6a0de66099250655f8c1b80a5003d8aa3022b3e
Muthu <kumar13.ece@gmail.com>
2014-03-13 17:14:14 +0530

    Revert "Chat - portal implementation"
    This reverts commit ea503023fc4d8eab1d11d383bb2352f9b85fefe7.
    
    Signed-off-by: Muthu <kumar13.ece@gmail.com>

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -10,7 +10,7 @@ class Chat < ActiveRecord::Base
 
 	attr_accessible :account_id, :title_min, :title_max, :welcome_msg, :thank_msg, :onhold_msg, 
 					:prechat_msg, :prechat_phone, :prechat_mail, :greet_time, :prechat_form, 
-					:proactive_chat, :preferences, :display_id, :is_typing, :show_on_portal, :portal_login_reguired
+					:proactive_chat, :preferences, :display_id, :is_typing
 
 	SECRET_KEY = "3f1fd135e84c2a13c212c11ff2f4b205725faf706345716f4b6996f9f8f2e6472f5784076c4fe102f4c6eae50da0fa59a9cc8cf79fb07ecc1eef62e9d370227f"
 		
