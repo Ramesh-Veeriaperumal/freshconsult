f7a532300e74deb7c59198933f7aa65b5a2a4f22
Anto <anto.arun@freshdesk.com>
2014-03-26 18:33:15 +0530

    Twilio recording deletion cron job
    Signed-off-by: Anto <anto.arun@freshdesk.com>

---
+++ lib/freshfone/cron/call_recording_attachment_delete.rb
@@ -0,0 +1,17 @@
+class Freshfone::Cron::CallRecordingAttachmentDelete
+  def self.delete_twilio_recordings(freshfone_account)
+    account = freshfone_account.account
+    subaccount = account.freshfone_subaccount
+    account.freshfone_calls.find_each(:batch_size => 1000,
+    :conditions => ["recording_url IS NOT NULL AND updated_at BETWEEN ? AND ?", 1.month.ago, 1.day.ago]) do |call|
+     next if call.recording_audio.blank?
+      begin
+        recording_sid = File.basename(call.recording_url)
+        subaccount.recordings.get(recording_sid).delete
+      rescue Exception => e
+        NewRelic::Agent.notice_error(e, {:description => "Freshfone CallRecordingAttachmentDelete job 
+          failed  account => #{call.account.id} :: call_id => #{call.id} :: call_sid => #{call.call_sid} :: recording_sid => #{recording_sid}"})
+      end
+    end 
+  end
+end
