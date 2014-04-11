ab698e3579d53cdd5a4e725303b8f0475f0b2811
Rajeev <rajeev@freshdesk.com>
2014-04-11 19:10:45 +0530

    user activation bug fix(activation mail is now received before the new ticket created notification. user signup fix - on behalf of prashanth(don).

--- app/models/user.rb
+++ app/models/user.rb
@@ -229,7 +229,7 @@ class User < ActiveRecord::Base
     self.deleted = true if (user_emails.present? && user_emails.first.email =~ /MAILER-DAEMON@(.+)/i)
     self.created_from_email = params[:user][:created_from_email] 
     return false unless save_without_session_maintenance
-    if (!deleted and !self.user_emails.empty?)
+    if (!deleted and !email.blank?)
       if self.language.nil?
         args = [ portal,false, params[:email_config]]
         Delayed::Job.enqueue(Delayed::PerformableMethod.new(self, :deliver_activation_instructions!, args), 
