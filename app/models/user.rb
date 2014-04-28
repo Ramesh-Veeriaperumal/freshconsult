743b39f36f2d385f334620a940603a6f44c9b129
venkatesanmurali <venkatesan.murali1985@gmail.com>
2014-04-28 16:11:23 +0530

    removing user email association on api
    Signed-off-by: venkatesanmurali <venkatesan.murali1985@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -93,7 +93,7 @@ class User < ActiveRecord::Base
       begin
         paginate :per_page => per_page, :page => page,
              :conditions => filter_condition(state, letter) ,
-             :order => order_by, :include => :primary_email
+             :order => order_by
       rescue Exception =>exp
         raise "Invalid fetch request for contacts"
       end
@@ -479,7 +479,7 @@ class User < ActiveRecord::Base
      options[:indent] ||= 2
       xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
       xml.instruct! unless options[:skip_instruct]
-      super(:builder => xml,:root=>options[:root], :skip_instruct => true, :include => :user_emails, :only => [:id,:name,:email,:created_at,:updated_at,:active,:customer_id,:job_title,
+      super(:builder => xml,:root=>options[:root], :skip_instruct => true, :only => [:id,:name,:email,:created_at,:updated_at,:active,:customer_id,:job_title,
                                                               :phone,:mobile,:twitter_id,:description,:time_zone,:deleted,
                                                               :helpdesk_agent,:fb_profile_id,:external_id,:language,:address]) 
   end
