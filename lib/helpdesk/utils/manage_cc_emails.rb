309a632eb8774dd94f815925ad4c065531dd870b
Hariharan <hariharan.r@freshdesk.com>
2014-05-10 12:18:22 +0530

    To emails inclusion in replies
    To emails being included into Cc emails. Earlier to emails not included in further conversations.
    
    Signed-off-by: Hariharan <hariharan.r@freshdesk.com>

---
+++ lib/helpdesk/utils/manage_cc_emails.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+module Helpdesk::Utils::ManageCcEmails
+
+	include ParserUtil
+
+	def filter_cc_emails account, emails_array, requester_email=""
+		configured_emails = []
+		account.all_email_configs.each { |ec| configured_emails << ec.to_email }
+		cc_email_val = emails_array.delete_if { |cc_email| (configured_emails.include? parse_email_text(cc_email)[:email]) || (parse_email_text(cc_email)[:email] == requester_email) }
+		cc_email_val.uniq
+	end
+
+end
