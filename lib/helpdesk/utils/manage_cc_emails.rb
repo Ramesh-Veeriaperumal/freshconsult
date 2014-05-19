558882465a275a6c3b5bd45b05f35cf3d2c55574
Hariharan <hariharan.r@freshdesk.com>
2014-05-19 13:06:50 +0530

    Stripping support emails from To emails fix
    Configured support emails weren't being stripped.
    
    Signed-off-by: Hariharan <hariharan.r@freshdesk.com>

--- lib/helpdesk/utils/manage_cc_emails.rb
+++ lib/helpdesk/utils/manage_cc_emails.rb
@@ -7,7 +7,7 @@ module Helpdesk::Utils::ManageCcEmails
 	def filter_cc_emails account, emails_array, requester_email=""
 		emails_array = fetch_valid_emails(emails_array)
 		configured_emails = []
-		account.all_email_configs.each { |ec| configured_emails << ec.to_email }
+		account.all_email_configs.each { |ec| configured_emails << ec.reply_email }
 		cc_email_val = emails_array.delete_if { |cc_email| (configured_emails.include? parse_email_text(cc_email)[:email]) || (parse_email_text(cc_email)[:email] == requester_email) }
 		cc_email_val.uniq
 	end
