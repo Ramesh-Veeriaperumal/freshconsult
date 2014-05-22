fa5a098b9a66d7837e485d769bd7aa76dd5df69c
Hrishikesh Premkumar <hrishikesh@freshdesk.com>
2014-05-22 16:32:28 +0530

    Remove Mobihelp Admin Link
    Remove Mobihelp from Admin page to prevent access for users.

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -177,7 +177,7 @@ module Admin::HomeHelper
   ######### Admin groups & Associated admin items Constant ########
 
     ADMIN_GROUP = {
-      :"support-channels"       =>    ["email", "freshchat", "freshfone", "twitter-setting", "facebook-setting", "feedback", "mobihelp"],
+      :"support-channels"       =>    ["email", "freshchat", "freshfone", "twitter-setting", "facebook-setting", "feedback"],
       :"general-settings"       =>    ["rebranding", "ticket-fields", "customer-portal", "agent", "group", "role", "security", "sla", 
                                           "business-hours", "multi-product", "tags"],
       :"helpdesk-productivity"  =>    ["dispatcher", "supervisor", "observer", "scenario", "email-notifications", "canned-response", 
