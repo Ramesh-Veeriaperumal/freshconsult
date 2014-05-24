81de59a1ef6f13d54da673aeae22f7bd07f236a5
Arvind Ravindran <arvind@freshdesk.com>
2014-05-25 02:40:04 +0530

    Spec : Bug fixes in notes_controller_spec
    Use display_id instead of id

--- spec/controllers/helpdesk/notes_controller_spec.rb
+++ spec/controllers/helpdesk/notes_controller_spec.rb
@@ -11,69 +11,71 @@ describe Helpdesk::NotesController do
 
 
   it "should create a note and go to index page(show activities)" do
-    test_ticket = create_ticket({:requester_id => @user.id, :status => 2 })
-    
-    post :create, :helpdesk_note => { :note_body_attributes => {:body_html => "<p>New note shown on index</p>"} },
+    test_ticket = create_ticket({:status => 2 })
+    body = "New note shown on index"
+    post :create, :helpdesk_note => { :note_body_attributes => {:body_html => "<p>#{body}</p>"} },
                   :ticket_id => test_ticket.display_id
 
-    get :index , :v => 2, :ticket_id => test_ticket.id
+    test_ticket.reload
+    test_ticket.notes.freshest(@account)[0].body.should =~ /#{body}/
+
+    get :index, :v => 2, :ticket_id => test_ticket.display_id, :format => "json"
     response.body.should =~ /New note shown on index/
 
   end
 
   it "should create a note and go to index page(show activities) with xhr request" do
-    test_ticket = create_ticket({:requester_id => @user.id, :status => 2 })
-    
-    post :create, :helpdesk_note => { :note_body_attributes => {:body_html => "<p>New note shown on index</p>"} },
+    test_ticket = create_ticket({:status => 2 })
+    body = "New note shown on index"
+
+    post :create, :helpdesk_note => { :note_body_attributes => {:body_html => "<p>#{body}</p>"} },
                   :ticket_id => test_ticket.display_id
 
-    xhr :get, :index , :v => 2, :ticket_id => test_ticket.id
-    response.should render_template "helpdesk/tickets/show/_conversations.html.erb"
+    test_ticket.notes.freshest(@account)[0].body.should =~ /#{body}/
+    xhr :get, :index, :v => 2, :ticket_id => test_ticket.display_id
     response.body.should =~ /New note shown on index/
-
+    response.should render_template "helpdesk/tickets/show/_conversations.html.erb"
   end
 
   it "should edit a note " do
-    test_ticket = create_ticket({:requester_id => @user.id, :status => 2 })
+    test_ticket = create_ticket({:status => 2 })
     body = Faker::Lorem.paragraph
     ticket_note = create_note({:source => test_ticket.source,
                                :ticket_id => test_ticket.id,
-                               :created_at => Time.now,
                                :body => body,
-                               :requester_id => @user.id})
-    get :edit, :ticket_id => test_ticket.id, :id => ticket_note.id
+                               :user_id => @user.id})
+    get :edit, :ticket_id => test_ticket.display_id, :id => ticket_note.id
+    response.body.should =~ /#{body}/
     response.should render_template "helpdesk/notes/_edit_note.html.erb"
   end
 
   it "should update a note " do
-    test_ticket = create_ticket({:requester_id => @user.id, :status => 2 })
+    test_ticket = create_ticket({:status => 2 })
     body = Faker::Lorem.paragraph
     ticket_note = create_note({:source => test_ticket.source,
                                :ticket_id => test_ticket.id,
-                               :created_at => Time.now,
                                :body => body,
-                               :requester_id => @user.id})
+                               :user_id => @user.id})
 
     updated_note_body = "Edited Note - #{Faker::Lorem.paragraph}"
     post :update, :helpdesk_note => {:source => test_ticket.source,
                                     :note_body_attributes => { :body_html => updated_note_body,
                                                                :id => ticket_note.id}},
                                     :id => ticket_note.id,
-                                    :ticket_id => test_ticket.id
+                                    :ticket_id => test_ticket.display_id
     flash[:notice].should be_eql("The note has been updated.")
     test_ticket.notes.last.body.should be_eql(updated_note_body)
   end
 
   it "should destroy a note " do
-    test_ticket = create_ticket({:requester_id => @user.id, :status => 2 })
+    test_ticket = create_ticket({:status => 2 })
     body = Faker::Lorem.paragraph
     ticket_note = create_note({:source => test_ticket.source,
                                :ticket_id => test_ticket.id,
-                               :created_at => Time.now,
                                :body => body,
-                               :requester_id => @user.id})
+                             :user_id => @user.id})
     test_ticket.notes.last.body.should be_eql(body)
-    post :destroy, :id => ticket_note.id, :ticket_id => test_ticket.id
+    post :destroy, :id => ticket_note.id, :ticket_id => test_ticket.display_id
     test_ticket.notes.last.deleted.should be_eql(true)
   end
 end
--- spec/support/note_helper.rb
+++ spec/support/note_helper.rb
@@ -3,11 +3,10 @@ require File.expand_path("#{File.dirname(__FILE__)}/../spec_helper")
 module NoteHelper
 
   def create_note(params = {})
-    
     test_note = Factory.build(:helpdesk_note, :source => params[:source],
                                          :notable_id => params[:ticket_id],
                                          :created_at => params[:created_at],
-                                         :user_id => params[:requester_id],
+                                         :user_id => params[:user_id],
                                          :account_id => @account.id,
                                          :notable_type => 'Helpdesk::Ticket')
     test_note.build_note_body(:body => params[:body], :body_html => params[:body])
