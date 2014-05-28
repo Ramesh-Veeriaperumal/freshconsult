f89c32fa7bf92075aee8c3ebbcf68cb2ea8d8f7c
Hrishikesh Premkumar <hrishikesh@freshdesk.com>
2014-05-28 20:15:10 +0530

    Fix missing Info Variables for App & Debug Info
    Added Install time and update time
    Added SDK version
    
    Signed-off-by: Hrishikesh Premkumar <hrishikesh@freshdesk.com>

--- app/models/mobihelp/ticket_info.rb
+++ app/models/mobihelp/ticket_info.rb
@@ -14,9 +14,9 @@ class Mobihelp::TicketInfo < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :mobihelp_device
 
-  APP_INFO_VARS = %w{app_name app_identifier app_version app_locale os os_version os_locale device_make device_model 
+  APP_INFO_VARS = %w{app_name app_identifier app_version app_locale app_install_time app_update_time os os_version os_locale device_make device_model 
                       device_sw_build active_network_type mobile_network_type mobile_network_operator_name 
                       mobile_network_country_code battery_level battery_status memory_level screen_orientation
-                      storage_space_internal storage_space_sd}
+                      storage_space_internal storage_space_sd mobihelp_sdk_version}
 
 end
