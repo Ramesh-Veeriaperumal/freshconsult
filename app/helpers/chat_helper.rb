bb0322b648b5d74836ed8c231dfe63ea7c32ce22
Nisha <nisha@freshdesk.com>
2014-05-30 17:15:02 +0530

    Revert "Passing environment information to chat and visitor js, Removed unused code on load of chat js"
    This reverts commit f04c48dfe492789f266e48406b4d81453f0fa1ee.

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -100,8 +100,7 @@ module ChatHelper
       :position => window_position,
       :ticket_link_option => ticket_link_option, 
       :expiry =>  chat_trial_expiry,
-      :custom_link_url => custom_link_url,
-      :environment => Rails.env
+      :custom_link_url => custom_link_url
     }
     return freshchat_setting.to_json.html_safe
   end
