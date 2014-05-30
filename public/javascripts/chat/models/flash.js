2b5534e746487389f81611b21dcc442f3a30a045
shakthi <shakthisree@freshdesk.com>
2014-05-30 17:21:14 +0530

    Chat: Tab overflow and ticket_search modules moved, unwanted files removed.

--- public/javascripts/chat/models/flash.js
+++
@@ -1,9 +0,0 @@
-define([], 
-	function(){
-		var FlashModel = Backbone.Model.extend({
-			constructor:function(){
-				this.id = "flash_"+(new Date()).getTime();		
-			}
-	});
-	return FlashModel;
-});
