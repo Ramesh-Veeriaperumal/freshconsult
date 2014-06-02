372866ee8280e468fe2d4ff1dc86cc8954375f75
Shridar S <shridar@freshdesk.com>
2014-06-02 17:41:05 +0530

    Specs for Customer side param filtering

--- spec/controllers/negative/support/profiles_controller_spec.rb
+++ spec/controllers/negative/support/profiles_controller_spec.rb
@@ -5,20 +5,29 @@ describe Support::ProfilesController do
   setup :activate_authlogic
   self.use_transactional_fixtures = false
 
-  # Commenting out this test for now, since this test will fail, as there are  no checks in place.
-  
-  # it "should not allow to edit an existing contact email" do
-  #   contact = Factory.build(:user, :account => @acc, :email => Faker::Internet.email,
-  #                             :user_role => 3)
-  #   contact.save
-  #   log_in(contact)
-  #   updated_email = Faker::Internet.email
-  #   put :update, :id => contact.id, :user => {:email => updated_email,
-  #                                           :job_title => "Developer",
-  #                                           :phone => Faker::PhoneNumber.phone_number,
-  #                                           :time_zone => "Arizona", 
-  #                                           :language => "fr" }
+  it "should not allow to edit an existing contact email" do
+    email = Faker::Internet.email
+    contact = Factory.build(:user, :account => @acc, :email => email,
+                              :user_role => 3)
+    contact.save
+    company = Factory.build(:company)
+    company.save
+    log_in(contact)
+    updated_email = Faker::Internet.email
+    put :update, :id => contact.id, :user => {:email => updated_email,
+                                            :customer_id => company.id,
+                                            :client_manager => true,
+                                            :helpdesk_agent => true,
+                                            :role_ids => ["1"],
+                                            :job_title => "Developer",
+                                            :phone => Faker::PhoneNumber.phone_number,
+                                            :time_zone => "Arizona", 
+                                            :language => "fr" }
 
-  #   @account.users.find_by_email(updated_email).should be_nil
-  # end
+    @account.users.find_by_email(updated_email).should be_nil
+    user = @account.users.find_by_email(email)
+    user.is_client_manager?.should be_false
+    user.helpdesk_agent.should be_false
+    user.user_role.should be_eql(3)
+  end
 end
