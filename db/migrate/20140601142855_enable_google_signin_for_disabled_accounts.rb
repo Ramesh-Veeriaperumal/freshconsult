9e74653e8988d5a7b672cd05e2e4c394cc898ba4
Swathi Thangavel <swathit@freshdesk.com>
2014-06-03 13:41:59 +0530

    Google Oauth - included disabled account_id & Google chrome web store verification files

--- db/migrate/20140601142855_enable_google_signin_for_disabled_accounts.rb
+++ db/migrate/20140601142855_enable_google_signin_for_disabled_accounts.rb
@@ -3,16 +3,16 @@ class EnableGoogleSigninForDisabledAccounts < ActiveRecord::Migration
 
   def self.up
   	count = 0
-		Sharding.run_on_all_shards do
+		Sharding.run_on_shard("shard_2") do
 			puts "Entering a Shard"
-			Subscription.find_in_batches(:batch_size => 300, :conditions => ["account_id > ?", 1]) do |subscriptions|
+			Account.find_in_batches(:batch_size => 300, :conditions => ['id > ?',123353]) do |accounts|
 				count = count + 1
-				subscriptions.each do |s|
-					account = Account.find(s.account_id)
-					account.make_current
-					if !account.features?(:google_signin)
-						account.features.google_signin.create
-						puts "Google signin feature enabled: #{account.id}"
+				accounts.each do |account|
+					acc = Account.find(account.id)
+					acc.make_current
+					if !acc.features?(:google_signin)
+						acc.features.google_signin.create
+						puts "Google signin feature enabled: #{acc.id}"
 					end
 				end
 				Account.reset_current_account
