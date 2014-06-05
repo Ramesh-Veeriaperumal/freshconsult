e30d0b8df8cd1ca6bde23a8e56f6075e93189e19
Hariharan <hariharan.r@freshdesk.com>
2014-06-05 15:16:08 +0530

    & turning to &amp; in Portal name
    In Title
    
    Signed-off-by: Hariharan <hariharan.r@freshdesk.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -132,7 +132,7 @@ module ApplicationHelper
   end
 
   def page_title    
-    portal_name = " : #{h(current_portal.portal_name)}" if current_portal.portal_name.present?
+    portal_name = " : #{h(current_portal.portal_name.html_safe)}" if current_portal.portal_name.present?
     "#{(@page_title || t('helpdesk_title'))}#{portal_name}"
   end 
   
