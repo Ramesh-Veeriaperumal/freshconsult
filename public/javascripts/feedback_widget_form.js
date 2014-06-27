ab6e0a967ab132402e50fb1ec5e45edf541a1d85
venkatesanmurali <venkatesan.murali1985@gmail.com>
2014-06-27 17:29:16 +0530

    Removing unwanted css on feedback widget
    http://bugs.freshdesk.com/helpdesk/tickets/3294
    Signed-off-by: venkatesanmurali <venkatesan.murali1985@gmail.com>

--- public/javascripts/feedback_widget_form.js
+++ public/javascripts/feedback_widget_form.js
@@ -2,8 +2,6 @@
 
 	$(function () {
 
-		if(jQuery.browser.msie) jQuery("#feedback-widget-container").addClass("ie")
-
 		$("select").select2();
 
 		var $ticket_desc = $("#helpdesk_ticket_ticket_body_attributes_description_html"),
