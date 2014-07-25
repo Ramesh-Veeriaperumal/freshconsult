863c5aec5fbb0cf0257542f897336bce017cfc8d
Anto Arun Dominic <anto.arun@freshdesk.com>
2014-07-25 14:00:21 +0530

    [Ticket #3658] Freshfone failed call's status update crone job fixes - twilio api call fix and log message added
    Signed-off-by: Anto Arun Dominic <anto.arun@freshdesk.com>

--- lib/freshfone/cron/intermediate_call_status_update.rb
+++ lib/freshfone/cron/intermediate_call_status_update.rb
@@ -4,18 +4,19 @@ class Freshfone::Cron::IntermediateCallStatusUpdate
     sid = call.is_root? ? call.call_sid : call.dial_call_sid
     return if (sid.blank? && !Freshfone::Call::INTERMEDIATE_CALL_STATUS.include?(call.call_status))
     
-    call_status = twilio_call_status(sid) || final_call_status(call)
+    call_status = twilio_call_status(account, sid) || final_call_status(call)
     if call_status.present? && call_status != call.call_status 
       call.call_status = call_status
       call.save
     end
   end
 
-  def self.twilio_call_status(sid)
+  def self.twilio_call_status(account, sid)
     begin
       status = account.freshfone_subaccount.calls.get(sid).status
       Freshfone::Call::CALL_STATUS_STR_HASH[status]
     rescue => e
+      Rails.logger.debug "Twilio api request error in IntermediateCallStatusUpdate => #{e}"
     end
   end
 
