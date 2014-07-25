ff5a90880ba768babf78a5923ff7f99907ea6ee7
A J Sudharshan <sudharshan@freshdesk.com>
2014-07-25 14:04:28 +0530

    migration file name change and index change reverted

--- db/migrate/20140725083159_add_mobile_token_refreshed_at_to_freshfone_users.rb
+++ db/migrate/20140725083159_add_mobile_token_refreshed_at_to_freshfone_users.rb
@@ -5,16 +5,12 @@ class AddMobileTokenRefreshedAtToFreshfoneUsers < ActiveRecord::Migration
   def self.up
 		Lhm.change_table :freshfone_users, :atomic_switch => true do |m|
 	    m.add_column :mobile_token_refreshed_at, "datetime"
-			m.remove_index [:account_id, :presence]
-			m.add_index [:account_id, :presence, :mobile_token_refreshed_at], :index_on_account_id_and_presence_and_mobile_token_refreshed_at
 		end
   end
 
   def self.down
 		Lhm.change_table :freshfone_users, :atomic_switch => true do |m|
-			m.remove_index [:account_id, :presence, :mobile_token_refreshed_at], :index_on_account_id_and_presence_and_mobile_token_refreshed_at
     	m.remove_column :mobile_token_refreshed_at
-			m.add_index [:account_id, :presence]
 		end
   end
 end
