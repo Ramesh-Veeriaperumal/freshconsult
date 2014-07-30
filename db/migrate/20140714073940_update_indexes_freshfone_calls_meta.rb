85544754f30e61600492f1522e8ed77144438143
johnpaulh <johnpaul@freshdesk.com>
2014-07-30 16:37:28 +0530

    Synching with staging: Bug/Feature Id: 3627 : QA Reported. Lhm failing for freshfone_calls_meta index removal.
    - Index name is custom added and required the name to be specified in the remove index. This is fixed.
    
    Signed-off-by: johnpaulh <johnpaul@freshdesk.com>

--- db/migrate/20140714073940_update_indexes_freshfone_calls_meta.rb
+++ db/migrate/20140714073940_update_indexes_freshfone_calls_meta.rb
@@ -3,8 +3,8 @@ class UpdateIndexesFreshfoneCallsMeta < ActiveRecord::Migration
 
   def self.up
     Lhm.change_table :freshfone_calls_meta, :atomic_switch => true do |m|
-      m.remove_index [:account_id, :call_id]
-      m.remove_index [:account_id, :group_id]
+      m.remove_index [:account_id, :call_id], 'index_ff_meta_data_on_account_id_and_call_id'
+      m.remove_index [:account_id, :group_id], 'index_ff_meta_data_on_account_id_and_group_id'
       m.add_index [:account_id, :call_id, :group_id], 'index_ff_calls_on_account_id_call_id_group_id'
     end
   end
@@ -12,8 +12,8 @@ class UpdateIndexesFreshfoneCallsMeta < ActiveRecord::Migration
   def self.down
     Lhm.change_table :freshfone_calls_meta, :atomic_switch => true do |m|
       m.remove_index  [:account_id, :call_id, :group_id], 'index_ff_calls_on_account_id_call_id_group_id'
-      m.add_index [:account_id, :group_id]
-      m.add_index [:account_id, :call_id]
+      m.add_index [:account_id, :group_id], 'index_ff_meta_data_on_account_id_and_group_id'
+      m.add_index [:account_id, :call_id], 'index_ff_meta_data_on_account_id_and_call_id'
     end
   end
 end
