4600d26e09b4f91bfe4c7eb43157197e61a52f04
parsuram <parsu@freshdesk.com>
2014-08-01 20:36:07 +0530

    Portal render_to_string was coming along with Layout
    - Removed the layout to render fully from portal view handle

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -123,9 +123,10 @@ class SupportController < ApplicationController
       partial = Portal::Page::PAGE_FILE_BY_TOKEN[ page_token ]
       dynamic_template = nil
       dynamic_template = page_data(page_token) if feature?(:layout_customization)
-      _content = render_to_string :file => partial,
+      _content = render_to_string :file => partial, :layout => false,
                   :locals => { :dynamic_template => dynamic_template } if dynamic_template.nil? || !dynamic_template.blank?
-
+                  
+                  
       @page_yield = @content_for_layout = _content
     end
 
@@ -157,7 +158,7 @@ class SupportController < ApplicationController
     def process_template_liquid
       Portal::Template::TEMPLATE_MAPPING.each do |t|
         dynamic_template = template_data(t[0]) if feature?(:layout_customization)
-        _content = render_to_string :partial => t[1], 
+        _content = render_to_string :partial => t[1], :layout => false,
                     :locals => { :dynamic_template => dynamic_template } if dynamic_template.nil? || !dynamic_template.blank?
         instance_variable_set "@#{t[0]}", _content
       end
