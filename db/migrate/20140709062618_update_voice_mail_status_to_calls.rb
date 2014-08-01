6038c4227583113b5072913946068ed0fb4c0d9c
johnpaulh <johnpaul@freshdesk.com>
2014-08-01 14:26:41 +0530

    Bug/Feature Id: 3627 : Freshfone summary reports. Migration query updated to ignore  direct dial calls from voicemails.
    Signed-off-by: johnpaulh <johnpaul@freshdesk.com>

--- db/migrate/20140709062618_update_voice_mail_status_to_calls.rb
+++ db/migrate/20140709062618_update_voice_mail_status_to_calls.rb
@@ -1,7 +1,7 @@
 class UpdateVoiceMailStatusToCalls < ActiveRecord::Migration
   shard :all
   def self.up
-    execute "update freshfone_calls set call_status = 10 where call_type = 1 and call_status = 3 and recording_url IS NOT NULL"
+    execute "update freshfone_calls set call_status = 10 where call_type = 1 and call_status = 3 and recording_url IS NOT NULL and user_id is NULL and direct_dial_number is NULL"
   end
 
   def self.down
