48fa6b32ac306540ff8ffdf4ea9be46a370cf456
Muthu <muthu@freshdesk.com>
2014-08-07 15:41:53 +0530

    #3820 - Chat: Downgraded account (sprout/blossom) has chat feature issue fix
    Signed-off-by: Muthu <muthu@freshdesk.com>

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -10,7 +10,8 @@ module Admin::HomeHelper
       },
       :freshchat                       =>   {
         :url                           =>   "/admin/chat_setting",
-        :privilege                     =>   privilege?(:admin_tasks) && (is_chat_plan? || current_account.features?(:chat))
+        :privilege                     =>   privilege?(:admin_tasks) && (current_account.subscription.is_chat_plan? || 
+                                                                                    current_account.features?(:chat))
       },
       :freshfone                       =>   {
         :url                           =>   "/admin/freshfone",
