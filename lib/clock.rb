262bd0834fdca25cc3b4541ab2e9c5ae08bc61bc
Hari Prasad <hari@freshdesk.com>
2014-08-18 11:13:12 +0530

    Squashed commit of the following:
    commit 56b989dade1f70e0df1dd20ae3337d3cc4f48dc0
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Mon Aug 18 11:10:26 2014 +0530
    
        Corrected max error for loading data to redshift.
    
    commit 50b3b104b0e1d8d769a3a18f7365fe42777611f4
    Merge: 41dbae4 bb1f66f
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Tue Jul 29 18:53:31 2014 +0530
    
        Merge remote-tracking branch 'origin/master' into reports_regenerate
    
        Conflicts:
        	Gemfile
        	Gemfile.lock
        	app/models/account/constants.rb
        	deploy/before_migrate.rb
        	lib/workers/load_data_to_redshift.rb
        	lib/workers/regenerate_archive_data.rb
    
    commit 41dbae4a9a58884434b62756e98c70fdef8d4b17
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Tue Jul 29 17:37:54 2014 +0530
    
        need to reverse merge with master.
    
    commit c956fc0b05ff342cdae1f9769157205e881fab57
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Tue Jul 29 16:55:21 2014 +0530
    
        ruby version problem fixed.
    
    commit 2a39964bdef94d2b0ac94d2a016d38985bf65061
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Fri Jul 11 18:01:05 2014 +0530
    
        added the rows affected data to the notification.
    
    commit c45426f93649a3a9b3171867beb7a303647c0250
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Thu Jul 3 22:37:17 2014 +0530
    
        code refactor....
    
    commit c56924f51369a54c791874dcae3d9bc41512f532
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Tue Jul 1 18:53:36 2014 +0530
    
        missed out file
    
    commit 7a759f587c6eef4ebcd5e9b6d120d12298d0cbcb
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Tue Jul 1 18:50:19 2014 +0530
    
        removed unwanted check-ins and some code refactoring.
    
    commit cc6663d0a438c9d8e12c27f5ece291a7ff60dac7
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Thu Jun 26 13:00:49 2014 +0530
    
        Proper message and data validation done
    
    commit ea1e26029acad34ce5ce7600ae07fb86739ac97d
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Wed Jun 25 13:28:22 2014 +0530
    
        moved the staging table creation to different module.
    
    commit 9c73e4db303007286b50511e6b7f6fb13104c4c2
    Author: Hari Prasad <hari@freshdesk.com>
    Date:   Mon Jun 23 23:09:47 2014 +0530
    
        added the code for regenerate report data using staging table method.

--- lib/clock.rb
+++ lib/clock.rb
@@ -8,12 +8,23 @@ Clockwork.every(1.hour, "ArchiveData_By_TimeZone", :at => "**:05", :tz => 'UTC')
 	utc_time = Time.zone.now.utc
 	time_zones = TIMEZONES_BY_UTC_TIME[utc_time.hour.to_s]
 	yesterday_date_in_tz = utc_time.in_time_zone(time_zones[0]).to_date.yesterday
-	Resque.enqueue(Workers::ArchiveDataByTimeZone, {:date => utc_time.strftime('%Y_%m_%d'), :hour => utc_time.hour.to_s, 
+	Resque.enqueue(Reports::Workers::ArchiveDataByTimeZone, {:date => utc_time.strftime('%Y_%m_%d'), :hour => utc_time.hour.to_s, 
 			:yesterday_date => yesterday_date_in_tz.to_s}) }
 
 Clockwork.every(1.hour, "Load_ArchiveData_To_Redshift", :at => "**:35", :tz => 'UTC') { 
 	utc_time = Time.zone.now.utc
-	Resque.enqueue(Workers::LoadDataToRedshift, {:date => utc_time.strftime('%Y_%m_%d'), :hour => utc_time.hour.to_s}) }
+	Resque.enqueue(Reports::Workers::LoadDataToRedshift, {:date => utc_time.strftime('%Y_%m_%d'), :hour => utc_time.hour.to_s}) }
+
+Clockwork.every(1.hour, "Load_RegenerateData_To_Redshift", :at => "**:35", :tz => 'UTC') { 
+	utc_time = Time.zone.now.utc
+	date, hour = utc_time.strftime('%Y_%m_%d'), utc_time.hour
+	if hour == 0
+		datetime = 1.hour.ago.utc # we will load the data from folder which got created one hour ago 
+		date, hour = datetime.strftime('%Y_%m_%d'), datetime.hour
+	else
+		hour = hour - 1
+	end
+	Resque.enqueue(Reports::Workers::LoadRegeneratedDataToRedshift, {:date => date, :hour => hour.to_s}) }
 
 Clockwork.every(1.day, "Run_Redshift_Table_Vacuum", :at => "15:00", :tz => 'UTC') { 
-		Resque.enqueue(Workers::RunRedshiftTableVacuum,{}) }
+		Resque.enqueue(Reports::Workers::RunRedshiftTableVacuum,{}) }
