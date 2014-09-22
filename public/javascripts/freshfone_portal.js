d5ffe5aebfccb0cae09d07504912809534707c73
Abhishek <natraj.abhishek@gmail.com>
2014-09-22 13:23:58 +0530

    Fixed freshfone js files path for portal
    JS files path fixed

--- public/javascripts/freshfone_portal.js
+++ public/javascripts/freshfone_portal.js
@@ -1,5 +1,5 @@
 //= require sound/soundmanager2-jsmin.js
-//= require soundmanager2/berniecode-animator.js
-//= require soundmanager2/360player.js
-//= require freshfone/soundmanager_config.js
+//= require plugins/soundmanager2/berniecode-animator.js
+//= require plugins/soundmanager2/360player.js
+//= require plugins/freshfone/soundmanager_config.js
 //= require report/date.js
