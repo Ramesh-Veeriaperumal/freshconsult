d54eea2f57b39e5b4b76e02abf04ea97c490ee26
Shrikrishna Holla <shrikrishna@freshdesk.com>
2014-10-21 13:42:28 +0530

    Freshfone Rspec Fixes
    Fixed a few specs that were failing because of errors in the specs
    
    Signed-off-by: Shrikrishna Holla <shrikrishna@freshdesk.com>

--- spec/controllers/freshfone/freshfone_controller_spec.rb
+++ spec/controllers/freshfone/freshfone_controller_spec.rb
@@ -35,7 +35,7 @@ describe FreshfoneController do
     set_twilio_signature('freshfone/voice', incoming_params)
     @account.features.freshfone.destroy
     post :voice, { :format => "html" }
-    response.should render_template "errors/non_covered_feature.html.erb"
+    response.body.should include('freshfone enabled validation failed')
   end
 
   it 'should redirect to login when non-twilio-aware methods are called by not logged in users' do
