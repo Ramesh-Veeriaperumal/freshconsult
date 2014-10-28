9682ccf65e2269c0fbd70128da013eef34219f06
prajwal <prajwal@freshdesk.com>
2014-10-28 19:09:10 +0530

    jura custom field support  issue id -4894
    Signed-off-by: prajwal <prajwal@freshdesk.com>

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -874,6 +874,10 @@ jQuery.fn.serializeObject = function(){
               epoch_sec = merge.getTime() + (1100*60000);
               merge = (new Date(epoch_sec)).toISOStringCustom().trim();
             }
+            else if (jQuery(self).find('[name="'+this.name+'"]').hasClass("numeric"))
+            {
+              merge = parseFloat(merge);
+            }
             while((k = keys.pop()) !== undefined){
 
                 // adjust reverse_key
@@ -897,7 +901,6 @@ jQuery.fn.serializeObject = function(){
 
             json = jQuery.extend(true, json, merge);
         });
-
         return json;
     };
 
