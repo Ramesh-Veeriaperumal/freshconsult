d024ded67cb8e0a115a5c71549ee77e528625727
Sathappan <sathappan.m@gmail.com>
2014-11-07 18:32:35 +0530

    Fixed #6006 with node session.
    Signed-off-by: Sathappan <sathappan.m@gmail.com>

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -7,7 +7,7 @@ class UserSession < Authlogic::Session::Base
   single_access_allowed_request_types :any
   after_save :set_user_time_zone, :set_node_session
   before_destroy :delete_node_session
-  # after_validation :set_missing_node_session
+  after_validation :set_missing_node_session
   generalize_credentials_error_messages true
 
   SECRET_KEY = "3f1fd135e84c2a13c212c11ff2f4b205725faf706345716f4b6996f9f8f2e6472f5784076c4fe102f4c6eae50da0fa59a9cc8cf79fb07ecc1eef62e9d370227f"
@@ -29,12 +29,12 @@ class UserSession < Authlogic::Session::Base
     controller.cookies.delete 'helpdesk_node_session'
   end
   
-  # def set_missing_node_session
-  #   if controller.cookies['helpdesk_node_session'].blank?
-  #     generated_hash = Digest::SHA512.hexdigest("#{SECRET_KEY}::#{self.attempted_record.id}")
-  #     controller.cookies['helpdesk_node_session'] = generated_hash
-  #   end
-  # end
+  def set_missing_node_session
+    if controller.cookies['helpdesk_node_session'].blank?
+      generated_hash = Digest::SHA512.hexdigest("#{SECRET_KEY}::#{self.attempted_record.id}")
+      controller.cookies['helpdesk_node_session'] = generated_hash
+    end
+  end
   
   attr_accessor :email, :password
   password_field(:password)
