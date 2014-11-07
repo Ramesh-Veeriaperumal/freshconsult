b84b82e2525c988309e9015975c8131cc4ad1d8a
ghariharan <hariharan@freshdesk.com>
2014-11-07 19:20:09 +0530

    Revert "Observer Bug Fix - symbolize_keys! on Hash (model_changes)"
    This reverts commit d42cdfbbc81b742cbe5dfa87f54e86ca4e1e356b.

--- lib/va/observer/constants.rb
+++ lib/va/observer/constants.rb
@@ -8,9 +8,9 @@ module Va::Observer::Constants
 											'SurveyResult' => :customer_id,
 											'Helpdesk::TimeSheet' => :user_id }
 
-	TICKET_EVENTS = [ :status, :priority, :ticket_type, :group_id, :responder_id, :due_by,
-										:time_sheet_action, :deleted, :spam, :reply_sent, :note_type,
-										:customer_feedback ]
+	TICKET_EVENTS = [ 'status', 'priority', 'ticket_type', 'group_id', 'responder_id', 'due_by',
+										'time_sheet_action', 'deleted', 'spam', 'reply_sent', 'note_type',
+										'customer_feedback' ]
 
 	TICKET_UPDATED = { :ticket_action => :update }
 	TICKET_DELETED = { :ticket_action => :delete }
