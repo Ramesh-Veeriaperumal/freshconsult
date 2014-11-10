410bb88a7aa78b4693fbc29beda1dc5395696b4d
Murugan <murugu2012@gmail.com>
2014-11-10 12:19:42 +0530

    Rails3: [#6037] Fix for Newrelic Exception - convert_to_contact

--- app/models/agent/associations.rb
+++ app/models/agent/associations.rb
@@ -6,12 +6,18 @@ class Agent < ActiveRecord::Base
 
   belongs_to :level, :class_name => 'ScoreboardLevel', :foreign_key => 'scoreboard_level_id'
 
-  has_many :agent_groups, :class_name => 'AgentGroup', :through => :user , 
-          :foreign_key =>'user_id', :primary_key => "user_id", :source => :agent, 
-          :dependent => :delete_all
-
-  has_many :time_sheets, :class_name => 'Helpdesk::TimeSheet' , :through => :user , 
-          :foreign_key =>'user_id', :primary_key => "user_id", :source => :agent
+  # ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection: 
+  # Cannot modify association 'Agent#agent_groups' because the source reflection class 'Agent' is 
+  # associated to 'User' via :has_one.  
+  # Changing since got problem when deleting an agent in Rails3
+  has_many :agent_groups, :class_name => 'AgentGroup', 
+          :foreign_key =>'user_id', :primary_key => "user_id", 
+          :dependent => :delete_all 
+  # ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection: Cannot modify association 
+  # 'Agent#time_sheets' because the source reflection class 'Agent' is associated to 'User' via :has_one
+  # Same issue as above for both create and delete
+  has_many :time_sheets, :class_name => 'Helpdesk::TimeSheet' ,  
+          :foreign_key =>'user_id', :primary_key => "user_id"
 
   has_many :achieved_quests, :foreign_key =>'user_id', :primary_key => "user_id",
           :dependent => :delete_all
