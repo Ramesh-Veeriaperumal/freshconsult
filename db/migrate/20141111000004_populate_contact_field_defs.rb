5a3f09aa646c1de8a5df65cda3a76564253e4b5a
ghariharan <hariharan@freshdesk.com>
2014-11-12 13:07:41 +0530

    Bug ID #6135 Contact Fields Phase 1- Removing the ff_def_suffix from migration file

--- db/migrate/20141111000004_populate_contact_field_defs.rb
+++ db/migrate/20141111000004_populate_contact_field_defs.rb
@@ -6,7 +6,7 @@ class PopulateContactFieldDefs < ActiveRecord::Migration
     Account.find_in_batches(:batch_size => 500) do |accounts|
       accounts.each do |account|
         execute(%(INSERT INTO flexifield_defs (name, account_id, module, created_at, updated_at) 
-        VALUES ("Contact_#{account.main_portal.id}", "#{account.id}", 'Contact', '#{Time.now.utc.to_s(:db)}', '#{Time.now.utc.to_s(:db)}')))
+        VALUES ("Contact", "#{account.id}", 'Contact', '#{Time.now.utc.to_s(:db)}', '#{Time.now.utc.to_s(:db)}')))
       end
     end
   end
