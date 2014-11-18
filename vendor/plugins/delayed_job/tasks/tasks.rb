22801a843d058acf9aca8821b540abb5e1a51b94
Shridar S <shridar@freshdesk.com>
2014-11-18 18:51:16 +0530

    Bug ID [#6377]
    New delayed-jobs table for custom-mailbox enabled accounts

--- vendor/plugins/delayed_job/tasks/tasks.rb
+++ vendor/plugins/delayed_job/tasks/tasks.rb
@@ -10,6 +10,19 @@ namespace :jobs do
 
   desc "Start a delayed_job worker."
   task :work => [:merb_env, :environment] do
-    Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
+    Delayed::Worker.new(::Delayed::Job, :min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
+  end
+end
+
+
+namespace :mailbox_jobs do
+  desc "Clear the mailbox_job queue."
+  task :clear => :environment do
+    Mailbox::Job.delete_all
+  end
+
+  desc "Start a delayed_job worker."
+  task :work => :environment do
+    Delayed::Worker.new(::Mailbox::Job, :min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
   end
 end
