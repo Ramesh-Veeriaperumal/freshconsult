f56cab3377d75f27fc0f12921004dd1665ec9f74
Gaurav Sachan <gaurav@freshdesk.com>
2014-11-25 14:54:37 +0530

    db migration
    adding migration table for czentrix

---
+++ db/migrate/20141125083659_populate_czentrix.rb
@@ -0,0 +1,21 @@
+class PopulateCzentrix < ActiveRecord::Migration
+  shard :all
+  @app_name = "czentrix"
+  def self.up
+  	czentrix = Integrations::Application.create(
+        :name => "czentrix",
+        :display_name => "integrations.czentrix.label",
+        :description => "integrations.czentrix.desc",
+        :listing_order => 26,
+        :options => {:direct_install => false,:keys_order => [:host_ip],
+        :host_ip => { :type => :text, :required => true, :label => "integrations.czentrix.host_name", 
+        :info => "integrations.czentrix.host_name_info"}},
+        :application_type => "CTI Integration",
+        :account_id => Integrations::Constants::SYSTEM_ACCOUNT_ID )
+    czentrix.save
+  end
+
+  def self.down
+  	execute("DELETE FROM applications WHERE name='#{@app_name}'")
+  end
+end
