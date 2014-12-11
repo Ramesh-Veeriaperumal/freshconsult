92ab9cf84f8d9681c4f33c550f8f566ff8f0d63b
Vishnu Balaji KM <vishnubalaji.km@gmail.com>
2014-12-11 20:18:13 +0530

    Zendesk import email font setting fix

--- app/models/admin/data_import_mailer.rb
+++ app/models/admin/data_import_mailer.rb
@@ -8,7 +8,7 @@ class Admin::DataImportMailer < ActionMailer::Base
     subject       "Data Import for #{options[:domain]}"
     headers       "Reply-to" => "","Auto-Submitted" => "auto-generated", "X-Auto-Response-Suppress" => "DR, RN, OOF, AutoReply"
     sent_on       Time.now
-    body          (options, :account => Account.current)
+    body          (options.merge({ :account => Account.current }))
     content_type  "text/html"
   end 
   
@@ -45,12 +45,12 @@ class Admin::DataImportMailer < ActionMailer::Base
   def google_contacts_import_email(options)
     @last_stats = options[:status]
     set_default_mail_options options[:email], "Successfully imported Google contacts for #{options[:domain]}"
-    body          (options, :account => Account.current)
+    body          (options.merge({ :account => Account.current }))
   end
 
   def google_contacts_import_error_email(options)
     set_default_mail_options options[:email], "Error in importing Google contacts for #{options[:domain]}"
-    body          (options, :account => Account.current)
+    body          (options.merge({ :account => Account.current }))
   end
 
   private
