40392aca0e3ae09e88e3f3a63b5bd6e260172f7a
Jasmine <jasmine@freshdesk.com>
2014-12-12 18:16:19 +0530

    Bug [#202267] Fix Max character limit on email domain per client

--- vendor/gems/custom_fields-0.1/lib/custom_fields/view/dom_element.rb
+++ vendor/gems/custom_fields-0.1/lib/custom_fields/view/dom_element.rb
@@ -33,15 +33,19 @@ module CustomFields
       private
         def construct_text
           regex_validity = (@field.field_options and @field.field_options['regex']) ? true : false; 
+          maxlength_validity = @field.field_type != :default_domains ? true : false # Temp fix
+
           html_options = {
-                            :class => "#{@field_class} field_maxlength " + (regex_validity ? "regex_validity" : ''), 
+                            :class => "#{@field_class}" +
+                                (regex_validity ? " regex_validity" : '') +
+                                (maxlength_validity ? ' field_maxlength' : ""), 
                             :disabled => @disabled, 
-                            :type => 'text',
-                            :maxlength => '255'
+                            :type => 'text'
                           }
-          if regex_validity 
-            html_options['data-regex-pattern'] = "/#{CGI.unescapeHTML(@field.field_options['regex']['pattern'])}/#{@field.field_options['regex']['modifier']}"
-          end
+          
+          html_options['data-regex-pattern'] = "/#{CGI.unescapeHTML(@field.field_options['regex']['pattern'])}/#{@field.field_options['regex']['modifier']}" if regex_validity
+          html_options['maxlength'] = '255' if maxlength_validity
+          
           text_field_tag("#{@object_name}[#{@field_name}]", @field_value, html_options);
         end
 
