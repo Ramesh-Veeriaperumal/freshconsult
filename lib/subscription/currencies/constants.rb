5255f449b6c5b5331625009e62fd1ee7774a1928
Bhavya-Y <bhavya@freshdesk.com>
2014-12-15 15:04:50 +0530

    [#7322]Adding brl currency

--- lib/subscription/currencies/constants.rb
+++ lib/subscription/currencies/constants.rb
@@ -1,26 +1,28 @@
 module Subscription::Currencies::Constants
-	
-	BILLING_CURRENCIES = [ "EUR", "INR", "USD", "ZAR" ]
-	DEFAULT_CURRENCY = "USD"
+
+  BILLING_CURRENCIES = [ "EUR", "INR", "USD", "ZAR", "BRL" ]
+  DEFAULT_CURRENCY = "USD"
 
   COUNTRY_MAPPING = 	{
-		"INDIA" => "INR",
-		"SOUTH AFRICA" => "ZAR"
-	}
-	
+    "INDIA" => "INR",
+    "BRAZIL" => "BRL",
+    "SOUTH AFRICA" => "ZAR"
+  }
+
   EUR_COUNTRIES = [ "AUSTRIA", "BELGIUM", "CYPRUS", "ESTONIA", "FINLAND", "FRANCE", "GERMANY", 
     "GREECE", "IRELAND", "ITALY", "LATVIA", "LUXEMBOURG", "MALTA", "NETHERLANDS", 
     "PORTUGAL", "SLOVAKIA", "SLOVENIA", "SPAIN", "ANDORRA", "KOSOVO", "MONTENEGRO", 
     "MONACO", "SAN MARINO", "THE VATICAN CITY" ]
-	
-	EUR_CURRENCY_MAPPING = Hash[ *EUR_COUNTRIES.collect { |country| [ country, "EUR" ] }.flatten ]
-	COUNTRY_MAPPING.merge!(EUR_CURRENCY_MAPPING)
 
-	CURRENCY_UNITS = { 
+  EUR_CURRENCY_MAPPING = Hash[ *EUR_COUNTRIES.collect { |country| [ country, "EUR" ] }.flatten ]
+  COUNTRY_MAPPING.merge!(EUR_CURRENCY_MAPPING)
+
+  CURRENCY_UNITS = { 
     "EUR" => "\u20AC", 
     "INR" => "\u20B9", 
     "USD" => "$",
-    "ZAR" => "R"
+    "ZAR" => "R",
+    "BRL" => "R$"
   }
 
 end
