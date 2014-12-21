c6e87d2240bedf6a5699e98773dd3e79c433f8c9
pratheepv <pratheep.velicherla@gmail.com>
2014-12-21 12:57:34 +0530

    trying IAM roles instead of keys
    trying IAM roles instead of keys

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -2,8 +2,8 @@ if node[:opsworks]
   require 'aws-sdk'
   # establishing connection with aws to run custom restart of nginx
   awscreds = {
-    :access_key_id    => node[:opsworks_access_keys][:access_key_id],
-    :secret_access_key => node[:opsworks_access_keys][:secret_access_key],
+    # :access_key_id    => node[:opsworks_access_keys][:access_key_id],
+    # :secret_access_key => node[:opsworks_access_keys][:secret_access_key],
     :region           => node[:opsworks_access_keys][:region]
   }
 
@@ -82,62 +82,4 @@ if node[:opsworks]
     end
   end
   Chef::Log.debug("************ after after restart ************* ")
-else
-  def all_instances_of(engine)
-    utility_instances = []
-    config.node['utility_instances'].each do |utility|
-      if utility['name'].include?(engine)
-        utility_instances << utility['name']
-      end
-    end
-    utility_instances << 'freshdesk_utility' if utility_instances
-    utility_instances
-  end
-
-  #To restart delayed_job workers..
-  # on_utilities(all_instances_of("freshdesk_utility")) do
-  #   run "sudo monit -g dj_helpkit restart all"
-  # end
-
-  on_utilities(all_instances_of('reports_app_')) do
-    run "sudo /etc/init.d/nginx restart" 
-  end
-
-  # on_utilities(all_instances_of('resque')) do
-  # 	run "sudo monit restart all -g helpkit_resque" 
-  # end
-
-  # on_utilities(all_instances_of('twitter_utility')) do
-  #   run "sudo monit restart all -g helpkit_twitter_realtime"
-  # end
-
-  # on_utilities(all_instances_of('facebook_utility')) do
-  #   run "sudo monit restart all -g helpkit_facebook_realtime"
-  # end
-
-
-  if config.current_role == "app_master"
-    newrelic_key = (config.framework_env == "production") ? "53e0eade912ffb2c559d6f3c045fe363609df3ee" : "7a4f2f3abfd0f8044580034278816352324a9fb7"
-    begin
-      run "curl -H \"x-license-key:#{newrelic_key}\" -d \"deployment[app_name]=#{config.environment_name} / helpkit (#{config.framework_env})\" -d \"deployment[revision]=#{config.revision}\" -d \"deployment[user]=#{config.deployed_by}\" https://rpm.newrelic.com/deployments.xml"
-    rescue  Exception => e  
-      puts "The following error occurred: #{e.message}"
-    end
-    # Deploying the opsworks...
-    run "RAILS_ENV=#{config.node[:environment][:framework_env]} bundle exec rake opswork:deploy"
-  end
-
-  if config.framework_env == "staging"
-    on_utilities(all_instances_of('workers')) do
-      run "sudo monit -g dj_helpkit restart all"
-      run "sudo monit restart all -g helpkit_resque" 
-      run "sudo monit restart all -g helpkit_twitter_realtime"
-      run "sudo monit restart all -g helpkit_facebook_realtime"
-    end
-
-    on_utilities(all_instances_of('services')) do
-      run "sudo monit restart all -g helpkit_clockwork"
-    end
-  end
-
 end
