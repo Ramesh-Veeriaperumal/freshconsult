43182857e2ec7a11acb55ad41b0be9cd13ebc78b
Sukanya V <sukanya@freshdesk.com>
2014-12-29 17:59:49 +0530

    [#7077] Split Ticket feature for facebook tickets

--- app/models/user.rb
+++ app/models/user.rb
@@ -133,7 +133,7 @@ class User < ActiveRecord::Base
       #return self.find(options[:id]) if options.key?(:id)
       return UserEmail.user_for_email(options[:email]) if options.key?(:email)
       return self.find_by_twitter_id(options[:twitter_id]) if options.key?(:twitter_id)
-      return self.find_by_fb_profile_id(options[:facebook_id]) if options.key?(:fb_profile_id)
+      return self.find_by_fb_profile_id(options[:fb_profile_id]) if options.key?(:fb_profile_id)
       return self.find_by_external_id(options[:external_id]) if options.key?(:external_id)
       return self.find_by_phone(options[:phone]) if options.key?(:phone)
     end 
