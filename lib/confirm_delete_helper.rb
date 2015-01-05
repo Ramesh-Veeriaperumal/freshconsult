53da4aa965850a5286e996fd0e1ee138bb94586c
Sony Mathew <sony@freshdesk.com>
2015-01-05 17:30:34 +0530

    [#7491] Delete Confirm Dialogue box in Solutions & Forums
    Previously in the edit pages of solution category, solution folder, solution article, forum category, forum and topic, when you click on the delete button, it will show a dialogue box asking for confirmation.
    Now a text field has been added in this pop-up(except in case of topic delete), so that the delete button will become active only if the user enter the first five characters of the title of the item he/she is going to delete.
    
    commit 04a2514eb4de9ec7c059efcd6f47bf1ab4b19c9d
    Author: Sony Mathew <sony@freshdesk.com>
    Date:   Mon Jan 5 16:20:03 2015 +0530
    
        [#7491] Delete Confirmation Message Change
    
        Changed the displayed message in the delete confirmation popup from "This action is undoable"  to "This action is irreversible".
    
        Signed-off-by: Sony Mathew <sony@freshdesk.com>
    
    commit 61477058b0f7d0dd60968a7eec498d453c82a81b
    Author: Sony Mathew <sony@freshdesk.com>
    Date:   Wed Dec 31 18:06:39 2014 +0530
    
        [7491] Cleaning up  delete_confirm.js
    
        Signed-off-by: Sony Mathew <sony@freshdesk.com>
    
    commit 3d2f19846fe8157d11fad9ff3fe7b0c776e36211
    Author: Sony Mathew <sony@freshdesk.com>
    Date:   Wed Dec 31 17:18:27 2014 +0530
    
        [#7491] Delete Confirm Dialogue box in Solutions & Forums
    
        A confirmation popup asking to type in the name of the item to be deleted in Solutions & Forums and UI revamp has been done.
    
        Signed-off-by: Sony Mathew <sony@freshdesk.com>
    
    Signed-off-by: Sony Mathew <sony@freshdesk.com>

---
+++ lib/confirm_delete_helper.rb
@@ -0,0 +1,80 @@
+module ConfirmDeleteHelper
+
+	SUPPORTED_DIALOGS = [
+		"Solution::Category", "Solution::Folder", "Solution::Article", 
+		"Forum", "ForumCategory", "Topic"
+	]
+
+	def confirm_delete(item, url, options = {})
+		return "" unless SUPPORTED_DIALOGS.include? item.class.name
+		link_to t('delete').html_safe, url, confirm_delete_defaults(item, url).merge(options)
+	end
+
+	def confirm_delete_defaults(item, url)
+		{
+			:class => 'btn',
+			:rel => 'confirmdelete',
+			
+			"data-warning-message" => "<b>#{t('delete_confirm.hard_delete')}</b>".html_safe,
+			
+			"data-details-message" => deletion_message(item).html_safe,
+			"data-destroy-url" => url,
+			"data-item-title" => item_title(item),
+			"data-dialog-id" => "deletion_#{dom_id(item)}",
+
+			"data-delete-msg" => deletion_hint(item),
+			"data-delete-title-msg" => deletion_title(item),
+
+			"data-title" => t('delete_confirm.confirm'),
+			"data-width" => '500px',
+			"data-close-label" => t('cancel'),
+			"data-submit-label" => t('delete_permanently'),
+
+			"data-confirmation" => true
+		}
+	end
+
+	def deletion_message(item)
+		respond_to?("#{item.class.name.parameterize.underscore}_delete_message") ?
+			send("#{item.class.name.parameterize.underscore}_delete_message", item) : default_deletion_message
+	end
+
+	def item_title(item)
+		item[:name] || item[:title] || ""
+	end
+
+	def deletion_hint(item)
+		t('delete_confirm.confirm_msg', :item_type => t("deletion_titles.#{item.class.name.parameterize.underscore}"))
+	end
+
+	def deletion_title(item)
+		t('delete_confirm.confirm_title', :item_type => t("deletion_titles.#{item.class.name.parameterize.underscore}"))
+	end
+
+	def solution_article_delete_message(article)
+		t('solution.info8')
+	end
+
+	def solution_folder_delete_message(folder)
+		t('folder.delete_confirm', :count => folder.articles.size)
+	end
+
+	def solution_category_delete_message(category)
+		return t('solution_category.info1') if (category.folders.size == 0) && (category.articles.size == 0)
+		t('solution_category.delete_confirm', :folders => category.folders.size, :articles => category.articles.size)
+	end
+
+	def topic_delete_message(topic)
+    t('topics.delete_confirm', :count => (topic.posts_count - 1))
+	end
+
+	def forum_delete_message(forum)
+		t('forum.delete_confirm', :count => forum.topics_count)
+	end
+
+	def forumcategory_delete_message(forum_category)
+		return t('forum_confirm_msg1').html_safe if (forum_category.forums.size == 0) && (forum_category.topics.size == 0)
+		t('forum_category.delete_confirm', :forums => forum_category.forums.size, :topics => forum_category.topics.size)
+	end
+
+end
