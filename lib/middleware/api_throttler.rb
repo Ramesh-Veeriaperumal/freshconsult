49ed532b366bc67ef400dcc8c47f76b1fa7d4b91
Hariharan <hariharan.r@freshdesk.com>
2015-01-22 17:51:37 +0530

    API throtting code revamp [SQUASH]:
    commit 08ed645da4052260254d3a7f144c5e2e64ef51ff
    Merge: 97eeaf8 1907516
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Thu Jan 22 17:49:42 2015 +0530
    
        Merge remote-tracking branch 'origin/staging' into api_throttler_revamp
    
    commit 97eeaf8bb38a410664608f7c6eb209fd2f5c6bc9
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Thu Jan 22 15:18:57 2015 +0530
    
        Resetting app requests
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    commit 01b75d899dde8e38c559dad75eaba8d27d728c2f
    Merge: cbeddc9 0c4c4d9
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Wed Jan 21 15:41:22 2015 +0530
    
        Merge remote-tracking branch 'origin/master' into api_throttler_revamp
    
    commit 0c4c4d981f7769edc6d4646cd798ee7148aac610
    Merge: ccd0330 2f48cf5
    Author: Ashwin Murali <ashwin.murali@freshdesk.com>
    Date:   Fri Jan 16 18:53:44 2015 +0530
    
        Merge remote-tracking branch 'origin/staging'
    
    commit cbeddc992a886ac4eb5fe905bf01340df2ab52a2
    Merge: 635c0ba ccd0330
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Tue Jan 13 19:41:42 2015 +0530
    
        Merge remote-tracking branch 'origin/master' into api_throttler_revamp
    
    commit ccd0330f6a6db6a8dbabfb0d7d5c151cb6035631
    Author: suman <bappi.5476@gmail.com>
    Date:   Tue Jan 13 01:43:11 2015 +0530
    
        created by meta bug fix to populate both body html and body
    
    commit 8f9a210b92caba8aa19fed51bdb5a0d20bf2577a
    Author: Divya <diviatvit@gmail.com>
    Date:   Mon Jan 12 16:36:46 2015 +0530
    
        #8099 - Handle Exception when API key is missing on mobihelp ticket creation request
    
    commit 635c0ba00cddf9eb71e2fc816c10d5f33a2e281b
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Thu Jan 8 16:25:29 2015 +0530
    
        Api throttler code [Revamped]
    
        Bugs Id: [#8009]
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    Signed-off-by: Hariharan <hariharan.r@freshdesk.com>

--- lib/middleware/api_throttler.rb
+++ lib/middleware/api_throttler.rb
@@ -6,9 +6,10 @@ class Middleware::ApiThrottler < Rack::Throttle::Hourly
   include Redis::OthersRedis
   include MemcacheKeys
   
-  SKIPPED_SUBDOMAINS = ["admin", "billing", "partner","signup", "email","login", "emailparser", "mailboxparser"] 
+  SKIPPED_SUBDOMAINS = ["admin", "billing", "partner", "signup", "freshsignup", "email","login", "emailparser", "mailboxparser"] 
   THROTTLED_TYPES = ["application/json", "application/x-javascript", "text/javascript",
                       "text/x-javascript", "text/x-json", "application/xml", "text/xml"]
+  ALLOWED_PATHS = [/\/integrations\/.*/]
   ONE_HOUR = 3600
 
   def initialize(app, options = {})
@@ -16,65 +17,103 @@ class Middleware::ApiThrottler < Rack::Throttle::Hourly
     @app = app
   end
 
-  def allowed?
-    begin
-      Sharding.select_shard_of(@account_id) do 
-        current_account = Account.find(@account_id)
-        api_key = API_LIMIT% {:account_id => @account_id}
-        api_limit = MemcacheKeys.fetch(api_key) do
-          current_account.api_limit.to_i
-        end
-        return true if by_pass_throttle?
-        remove_others_redis_key(key) if get_others_redis_key(key+"_expiry").nil?
-        @count = get_others_redis_key(key).to_i
-        return api_limit > @count
-      end
-    rescue Exception => e
-      Rails.logger.debug "Exception on api throttler ::: #{e.message}"
-      NewRelic::Agent.notice_error(e)
-      true
-    end
-  end
-
   def call(env)
+    @request = Rack::Request.new(env)
     @host = env["HTTP_HOST"]
-    @content_type = env['CONTENT-TYPE'] || env['CONTENT_TYPE']
-    @api_path = env["REQUEST_URI"]
     @sub_domain = @host.split(".")[0]
+    @api_path = env["REQUEST_URI"].to_s
+    @content_type = env['CONTENT-TYPE'] || env['CONTENT_TYPE']
+
     if SKIPPED_SUBDOMAINS.include?(@sub_domain)
       @status, @headers, @response = @app.call(env)
       return [@status, @headers, @response]
     end
-    domain = DomainMapping.find_by_domain(env["HTTP_HOST"])
-    @account_id = domain.account_id if domain
-    if allowed?
-      @status, @headers, @response = @app.call(env)
-      unless by_pass_throttle?
-        remove_others_redis_key(key) if get_others_redis_key(key+"_expiry").nil?
-        increment_others_redis(key)
-        value = get_others_redis_key(key).to_i
-        set_others_redis_key(key+"_expiry",1,ONE_HOUR) if value == 1
+
+    if throttle?
+      @count = increment_others_redis(key)
+      ### Notify dev-ops if count exceeds threshold?
+      set_others_redis_expiry(key, ONE_HOUR) if @count == 1
+      
+      if !allowed?
+        @status, @headers,@response = [403, {'Retry-After' => retry_after,'Content-Type' => 'text/html'}, 
+                                      ["You have exceeded the limit of requests per hour"]]
+      elsif !@in_app_request and !THROTTLED_TYPES.include?(@content_type)
+        @status, @headers,@response = [415, {'Content-Type' => 'text/html'}, ["Invalid Content-Type"]]
+      else
+        @status, @headers, @response = @app.call(@request.env)
       end
     else
-      @status, @headers,@response = [403, {'Retry-After' => retry_after,'Content-Type' => 'text/html'}, 
-                                      ["You have exceeded the limit of requests per hour"]]
+      @status, @headers, @response = @app.call(@request.env)
     end
     
      [@status, @headers, @response]
+    
+    ensure
+      @in_app_request = false
   end
 
-  def by_pass_throttle?
-    return true if  SKIPPED_SUBDOMAINS.include?(@sub_domain)
-    if @content_type.nil?
-      return ( !@api_path.include?(".xml") && !@api_path.include?(".json") )
+  def api_path?
+    @api_path.include?(".xml") or @api_path.include?(".json")
+  end
+
+  def throttle?
+    if app_request? or first_web_request? or is_native_mobile? or is_mobihelp? or allowed_paths_call?
+      @in_app_request = true
+      (api_path? ? true : false)
     else
-      Rails.logger.debug "Content type on API:: #{@content_type}"
-      return !THROTTLED_TYPES.include?(@content_type)
+      Rails.logger.debug "SOURCE IP :: #{@request.env["HTTP_X_REAL_IP"]}, Content type on API :: #{@content_type}"
+      return true
+    end
+  end
+
+  def app_request?
+    @request.env["HTTP_X_CSRF_TOKEN"].present? or
+    @request.cookies["_helpkit_session"].present?
+  end
+
+  def first_web_request?
+    @request.get? and @request.env["HTTP_ACCEPT"].to_s.include?('application/xhtml+xml')
+  end
+
+  def is_native_mobile?
+    @request.env["HTTP_USER_AGENT"].to_s[/#{AppConfig['app_name']}_Native/].present? and 
+    (@api_path.include?('user_session') or @request.env["HTTP_AUTHORIZATION"].to_s[/Basic .*/].present?)
+  end
+
+  def is_mobihelp?
+    @request.env["HTTP_X_FD_MOBIHELP_AUTH"].present? and 
+    (@api_path[/\/support\/mobihelp\/.*/] or @api_path[/\/mobihelp\/.*/]).present?
+  end
+
+  def allowed_paths_call?
+    @api_path.match(Regexp.union(ALLOWED_PATHS)).present?
+  end
+
+  def allowed?
+    begin
+      domain = DomainMapping.find_by_domain(@request.env["HTTP_HOST"])
+      account_id = domain.account_id if domain
+
+      Sharding.select_shard_of(account_id) do 
+        api_limit = MemcacheKeys.fetch((API_LIMIT % {:account_id => account_id})) do
+          Account.find(account_id).api_limit.to_i
+        end
+
+        return (@count <= api_limit)
+      end
+    rescue Exception => e
+      Rails.logger.debug "Exception on api throttler ::: #{e.message}"
+      NewRelic::Agent.notice_error(e,{ :custom_params => { 
+                                                            :description => "API Throttler Error", 
+                                                            :domain => domain,
+                                                            :account_id => account_id 
+                                                          }})
+      return true
     end
   end
 
   def retry_after
-    get_others_redis_expiry(key+"_expiry").to_s
+    get_others_redis_expiry(key).to_s
   end
 
   def key
@@ -82,4 +121,3 @@ class Middleware::ApiThrottler < Rack::Throttle::Hourly
   end
 
 end
-
