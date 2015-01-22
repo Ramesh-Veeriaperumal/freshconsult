04edba2374b35c31fab662935271be6481bae028
Anto Arun Dominic <anto.arun@freshdesk.com>
2015-01-22 14:59:12 +0530

    #7544: Fix for error in dequeueing calls because of an already-completed call in the forefront of the queue
    Fixes spec errors caused by static assignment of variables
    
    Signed-off-by: Shrikrishna Holla <shrikrishna@freshdesk.com>
    Signed-off-by: Anto Arun Dominic <anto.arun@freshdesk.com>

--- db/migrate/20141216134434_create_freshfone_number_addresses.rb
+++ db/migrate/20141216134434_create_freshfone_number_addresses.rb
@@ -2,19 +2,26 @@ class CreateFreshfoneNumberAddresses < ActiveRecord::Migration
   shard :all
   def self.up
     execute(
-      "CREATE TABLE `freshfone_number_groups` (
+      "CREATE TABLE `freshfone_number_address` (
       `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-      `account_id` bigint(20) unsigned DEFAULT NULL,
-      `freshfone_number_id` int(11) DEFAULT NULL,
-      `group_id` int(11) DEFAULT NULL,
+      `account_id` bigint(20) DEFAULT NULL,
+      `freshfone_account_id` bigint(20) DEFAULT NULL,
+      `address_sid` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `friendly_name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `business_name` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `address` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `city` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `state` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `postal_code` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+      `country` varchar(5) CHARACTER SET latin1 DEFAULT NULL,
       `created_at` datetime DEFAULT NULL,
       `updated_at` datetime DEFAULT NULL,
       UNIQUE KEY `index_freshfone_number_address_on_id_and_account_id` (`id`,`account_id`),
-      KEY `index_freshfone_number_address_on_account_id_country` (`account_id`,`country`)
+      KEY `index_freshfone_number_address_on_account_id_and_country` (`account_id`,`country`)
       ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
       /*!50100 PARTITION BY HASH (account_id)
-      PARTITIONS 128 */;"
-      )
+        PARTITIONS 128 */;"
+    )
   end
 
   def self.down
--- spec/controllers/freshfone/queue_controller_spec.rb
+++ spec/controllers/freshfone/queue_controller_spec.rb
@@ -31,12 +31,13 @@ describe Freshfone::QueueController do
     xml[:Response][:Say].should_not be_blank
   end
 
-  it 'should raise error whe trying to dequeue calls not in-progress' do
+  it 'should remove call sid for a call not in-progress from the queue' do
     log_in(@agent)
     controller.send(:queued_members).stubs(:list).returns(["random"])
     controller.set_key( "FRESHFONE:CALLS:QUEUE:#{@account.id}", 
                         ["CAae09f7f2de39bd201ac9276c6f1cc66a"].to_json )
-    lambda { post :bridge }.should raise_error(Twilio::REST::RequestError, /Cannot dequeue call/)
+    post :bridge
+    controller.get_key( "FRESHFONE:CALLS:QUEUE:#{@account.id}").should be_eql([].to_json)
   end
 
   it 'should success json when queue list is empty' do
@@ -57,7 +58,7 @@ describe Freshfone::QueueController do
     create_freshfone_call('CDEFAULTQUEUE')
     set_default_queue_redis_entry
     post :hangup, hangup_params
-    controller.get_key(DEFAULT_QUEUE).should be_nil
+    controller.get_key(DEFAULT_QUEUE % {account_id: @account.id}).should be_nil
   end
 
   it 'should remove all agent priority queue entries from redis on hangup' do
@@ -67,7 +68,7 @@ describe Freshfone::QueueController do
     set_agent_queue_redis_entry
     post :hangup, 
       hangup_params.merge({:hunt_type => "agent", :hunt_id => @agent.id, "CallSid" => "CAGENTQUEUE"})
-    controller.get_key(AGENT_QUEUE).should be_nil
+    controller.get_key(AGENT_QUEUE % {account_id: @account.id}).should be_nil
   end
 
   it 'should render dequeue twiml on queue to voicemail' do
