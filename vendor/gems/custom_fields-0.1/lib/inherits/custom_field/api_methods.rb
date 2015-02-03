75a9bd629194ef2ae39c344eab35e103afc137ab
RashmiRam <rashmi@freshdesk.com>
2015-02-03 15:19:25 +0530

    [#8371] - API for listing Custom fields in Contacts & Companies
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- vendor/gems/custom_fields-0.1/lib/inherits/custom_field/api_methods.rb
+++ vendor/gems/custom_fields-0.1/lib/inherits/custom_field/api_methods.rb
@@ -6,7 +6,7 @@ module Inherits
         options[:indent] ||= 2
         xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
         xml.instruct! unless options[:skip_instruct]
-        super(:builder => xml, :skip_instruct => true, :except => [:account_id]) do |xml|
+        super(:builder => xml, :skip_instruct => true, :except => [:account_id, :column_name, self.class::CUSTOM_FORM_ID_COLUMN]) do |xml|
           xml.choices do
             choices.each do |choice|  
               xml.option do
@@ -20,7 +20,7 @@ module Inherits
       
       #Use as_json instead of to_json for future support Rails3 refer:(http://jonathanjulian.com/2010/04/rails-to_json-or-as_json/)
       def as_json(options={})
-        options[:except] ||= [:account_id]
+        options[:except] ||= [:account_id, :column_name, self.class::CUSTOM_FORM_ID_COLUMN]
         options[:methods]||= [:choices]
         super(options)
       end
