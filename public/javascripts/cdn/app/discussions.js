cec4d30ec88139576737180764c086e9a0cc5ebd
Thanashyam Raj <thanashyam@gmail.com>
2015-02-04 10:09:02 +0530

    [Rails3][#8347] Fixed the asset file for Discussions.js
    Signed-off-by: Thanashyam Raj <thanashyam@gmail.com>

--- public/javascripts/cdn/app/discussions.js
+++ public/javascripts/cdn/app/discussions.js
@@ -1,6 +1,7 @@
 //= require app/discussions/discussions.js
 //= require app/discussions/sidebar.js
 //= require app/discussions/reorder.js
+//= require app/discussions/category.js
 //= require app/discussions/topic_list.js
 //= require app/discussions/topic.js
 //= require app/discussions/moderation.js
