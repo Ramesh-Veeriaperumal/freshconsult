dd3b040d03e60e5afdbb6016c628f111062e1c39
RashmiRam <rashmi@freshdesk.com>
2015-02-10 16:41:45 +0530

    API Spec fixes
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- spec/controllers/api/xml/company_fields_api_spec.rb
+++ spec/controllers/api/xml/company_fields_api_spec.rb
@@ -12,8 +12,7 @@ describe Admin::CompanyFieldsController do
 	it "should list all the company_fields" do
         get :index, :format => 'xml'
         result = parse_xml(response)
-        debugger
-        expected = (response.status == "200 OK") &&(compare(result["company_fields"].first.keys,APIHelper::COMPANY_FIELDS_ATTRIBS,{}).empty?) 
+        expected = (response.status == 200) &&(compare(result["company_fields"].first.keys,APIHelper::COMPANY_FIELDS_ATTRIBS,{}).empty?) 
         expected.should be(true)
 	end
 
