6a424baaec89f8c5961826637673146865489937
RashmiRam <rashmi@freshdesk.com>
2015-02-11 14:10:05 +0530

    [#8564] API | XML | Contact & company fields listing not working as expected | Error
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- vendor/gems/custom_fields-0.1/lib/inherits/custom_field/api_methods.rb
+++ vendor/gems/custom_fields-0.1/lib/inherits/custom_field/api_methods.rb
@@ -2,11 +2,14 @@ module Inherits
   module CustomField
     module ApiMethods
 
+      #including field_type as attribute in to_xml is breaking in rails3. So including as method and excluding from attributes.
+    #enforcing the same in as_json for consistency in responses.
+
       def to_xml(options = {})
         options[:indent] ||= 2
         xml = options[:builder] ||= Builder::XmlMarkup.new(:indent => options[:indent])
         xml.instruct! unless options[:skip_instruct]
-        super(:builder => xml, :skip_instruct => true, :except => [:account_id, :column_name, self.class::CUSTOM_FORM_ID_COLUMN]) do |xml|
+        super(:builder => xml, :skip_instruct => true, :except => [:account_id, :column_name, self.class::CUSTOM_FORM_ID_COLUMN, :field_type], :methods => [:field_type]) do |xml|
           xml.choices do
             choices.each do |choice|  
               xml.option do
@@ -20,8 +23,8 @@ module Inherits
       
       #Use as_json instead of to_json for future support Rails3 refer:(http://jonathanjulian.com/2010/04/rails-to_json-or-as_json/)
       def as_json(options={})
-        options[:except] ||= [:account_id, :column_name, self.class::CUSTOM_FORM_ID_COLUMN]
-        options[:methods]||= [:choices]
+        options[:except] ||= [:account_id, :column_name, self.class::CUSTOM_FORM_ID_COLUMN, :field_type]
+        options[:methods]||= [:field_type, :choices]
         super(options)
       end
       
