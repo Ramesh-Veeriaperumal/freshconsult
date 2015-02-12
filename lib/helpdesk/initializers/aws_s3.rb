ae84e324c7f857bb02088ef2f3d945e5130a1de4
Sudhir Cirra <sudhir@freshdesk.com>
2015-02-12 15:46:21 +0530

    Region support is being added for AWS config. Deafult region is 'us-east-1'.

--- lib/helpdesk/initializers/aws_s3.rb
+++ lib/helpdesk/initializers/aws_s3.rb
@@ -3,5 +3,6 @@ config = YAML::load(ERB.new(File.read("#{Rails.root}/config/s3.yml")).result)
 S3_CONFIG = (config[Rails.env] || config).symbolize_keys
 
 AWS.config(
-		:access_key_id => S3_CONFIG[:access_key_id],
-		:secret_access_key => S3_CONFIG[:secret_access_key])
+    :access_key_id => S3_CONFIG[:access_key_id],
+    :secret_access_key => S3_CONFIG[:secret_access_key],
+    :region => S3_CONFIG[:region] ? S3_CONFIG[:region] : 'us-east-1')
