7401e5a97d3242039797fcec35559eb7185ded96
RashmiRam <rashmi@freshdesk.com>
2015-02-19 15:47:43 +0530

    API Spec fixes
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- spec/controllers/api/json/contact_fields_api_spec.rb
+++ spec/controllers/api/json/contact_fields_api_spec.rb
@@ -12,7 +12,8 @@ describe Admin::ContactFieldsController do
 	it "should list all the contact_fields" do
         get :index, :format => 'json'
         result = parse_json(response)
-        expected = (response.status == 200) && (compare(result.first["contact_field"].keys,APIHelper::CONTACT_FIELDS_ATTRIBS,{}).empty?) 
+        attributes = (APIHelper::CONTACT_FIELDS_ATTRIBS - ['field_type']).insert(APIHelper::CONTACT_FIELDS_ATTRIBS.index('field_options')+1, 'field_type')
+        expected = (response.status == 200) && (compare(result.first["contact_field"].keys, attributes, {}).empty?) 
         expected.should be(true)
 	end
 
