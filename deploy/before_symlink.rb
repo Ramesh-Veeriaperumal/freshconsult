3a0283b51bdc9f88067062a88c6119d1ee606efb
pratheepv <pratheep.velicherla@gmail.com>
2015-03-04 15:10:55 +0530

    asset pipeline host name added for timebeing
    asset pipeline host name added for timebeing

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,5 +1,6 @@
 if node[:opsworks]
 	master_node = node[:opsworks][:layers][:application][:instances].keys.sort.first  if node[:opsworks][:layers] && node[:opsworks][:layers][:application] && node[:opsworks][:layers][:application][:instances]
+  master_node = node[:rails3][:asset_pipeline_host] if node[:rails3] && node[:rails3][:asset_pipeline_host]
   if master_node && (node[:opsworks][:instance][:hostname] == master_node) && ::File.exists?("#{release_path}/config/database.yml")
     if node[:custom_db_migrate]
     	run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake db:migrate"
