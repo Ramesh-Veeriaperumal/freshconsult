24a2192d9829989b7ac26909a006d88e0834a2b9
pratheepv <pratheep.velicherla@gmail.com>
2015-03-04 15:43:40 +0530

    asset pipeline more generic wayt
    asset pipeline more generic wayt

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,7 @@
 if node[:opsworks]
 	master_node = node[:opsworks][:layers][:application][:instances].keys.sort.first  if node[:opsworks][:layers] && node[:opsworks][:layers][:application] && node[:opsworks][:layers][:application][:instances]
-  master_node = node[:rails3][:asset_pipeline_host] if node[:rails3] && node[:rails3][:asset_pipeline_host]
+  asset_pipeline_host = node[:rails3][:asset_pipeline_host] if node[:rails3] && node[:rails3][:asset_pipeline_host]
+  current_host = node[:opsworks][:instance][:hostname] 
   if master_node && (node[:opsworks][:instance][:hostname] == master_node) && ::File.exists?("#{release_path}/config/database.yml")
     if node[:custom_db_migrate]
     	run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake db:migrate"
@@ -11,5 +12,9 @@ if node[:opsworks]
   elsif node[:opsworks][:instance][:hostname].include?("-app-") && ::File.exists?("#{release_path}/config/database.yml")
     run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake assets:clean_expired"
     run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake assets:precompile:primary"
+  elsif asset_pipeline_host && asset_pipeline_host == current_host && ::File.exists?("#{release_path}/config/database.yml")
+    run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake assets:clean_expired"
+    run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake assets:precompile:primary"
+    run "cd #{release_path} && RAILS_ENV=#{node[:opsworks][:environment]} bundle exec rake assets:sync"
   end
 end
