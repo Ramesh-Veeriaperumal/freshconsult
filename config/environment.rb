d51fce98e00fe4ca263041f0fb80bed28c58440e
suman <bappi.5476@gmail.com>
2015-03-07 10:05:32 +0530

    session sharing between rails3 and rails2 #murugans commits

--- config/environment.rb
+++ config/environment.rb
@@ -80,7 +80,7 @@ Rails::Initializer.run do |config|
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_helpkit_session',
-    :secret      => '3f1fd135e84c2a13c212c11ff2f4b205725faf706345716efb6686f9f8f2e6472f5784076c4fe102f4c6eae50da0fa59a9cc8cf79fb07ecc1eef62e9d370227f'
+    :secret      => '3f1fd34135e84c4245a13c212c11ff2f4b205725faf706345716efb6686f9f8f2e6472f5784076c4fe102f4c6eae50da0fa59a9cc8cf79fb07ecc1eef62e9d370227f'
   }
   
   ENV['RECAPTCHA_PUBLIC_KEY']  = '6LfNCb8SAAAAACxs6HxOshDa4nso_gyk0sxKcwAI'
