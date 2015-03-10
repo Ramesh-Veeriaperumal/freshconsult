59d731523c9e8b251548452555d68897b6625a0b
arunkumarvelu <arunkumar.v@freshdesk.com>
2015-03-10 13:43:31 +0530

    [#8747] Agent notifications during chat
    [#8747] Agent notifications during chat. Additional changes for the agent side notifications and Visitor side notifications for chat transfer

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -123,7 +123,8 @@ module ChatHelper
       :default_agent_left_msg => t("freshchat.agent_left_msg"),
       :default_thank_message => t("freshchat.thank_message"),
       :default_non_availability_message => t("freshchat.non_availability_message"),
-      :default_prechat_message => t("freshchat.prechat_message")
+      :default_prechat_message => t("freshchat.prechat_message"),
+      :agent_transfered_msg => t("freshchat.agent_transfered_msg")
     }
     return freshchat_setting.to_json.html_safe
   end
@@ -169,6 +170,18 @@ module ChatHelper
             :select_location => t("freshchat.select_location"),
             :new_visitors => t("freshchat.new_visitors"),
             :visitor_disconnect_msg => t("freshchat.visitor_disconnect_msg"),
+            :visitor_left_chat_msg => t("freshchat.visitor_left_chat_msg"),
+            :agent_transfer_accept_msg => t("freshchat.agent_transfer_accept_msg"),
+            :agent_transfer_reject_msg => t("freshchat.agent_transfer_reject_msg"),
+            :agent_transfer_missed_msg => t("freshchat.agent_transfer_missed_msg"),
+            :chat_inactive_for_10_min => t("freshchat.chat_inactive_for_10_min"),
+            :agent_transfer_waiting_msg => t("freshchat.agent_transfer_waiting_msg"),
+            :agent_chat_accept_msg => t("freshchat.agent_chat_accept_msg"),
+            :close_chat_tooltip => t("freshchat.close_chat_tooltip"),
+            :minimise_chat_tooltip => t("freshchat.minimise_chat_tooltip"),
+            :settings_chat_tooltip => t("freshchat.settings_chat_tooltip"),
+            :visitor_details_tooltip => t("freshchat.visitor_details_tooltip"),
+            :agent_chat_placeholder => t("freshchat.agent_chat_placeholder"),
             :last_message => t("freshchat.last_message"),
             :typing_message => t("freshchat.typing_message"),
             :is_typing_header => t("freshchat.is_typing_header"),
@@ -247,6 +260,7 @@ module ChatHelper
             :date_range => t('freshchat.date_range'),
             :agent_joined_msg => t('freshchat.agent_joined_msg'),
             :agent_left_msg => t('freshchat.agent_left_msg'),
+            :agent_transfered_msg => t('freshchat.agent_transfered_msg'),
             :connecting_msg => t('freshchat.connecting_msg'),
             :non_availability_message => t('freshchat.non_availability_message'),
             :me => t('freshchat.me'),
