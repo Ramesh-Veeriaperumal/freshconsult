ba446981c844fbd788262f21f489dba27891ccc8
Vishnu Balaji KM <vishnubalaji.km@gmail.com>
2015-03-25 21:53:34 +0530

    Bud ID[#9174]

--- public/javascripts/cdn/app/freshfonecallhistory.js
+++ public/javascripts/cdn/app/freshfonecallhistory.js
@@ -1,3 +1,4 @@
+//= require report/daterangepicker.jQuery.js
 //= require freshfone/call_history/call_history.js
 //= require freshfone/call_history/call_logs.js
 //= require freshfone/call_history/call_block.js
