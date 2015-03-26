5d147798fc3b572bec6b8b14e46d0e21bf6fc3d3
Hariharan <hariharan.r@freshdesk.com>
2015-03-27 01:03:16 +0530

    [#9215] Detect user language - Deleted User
    mailer-daemon emails were getting nil language set.
    
    Signed-off-by: Hariharan <hariharan.r@freshdesk.com>

--- lib/workers/detect_user_language.rb
+++ lib/workers/detect_user_language.rb
@@ -3,8 +3,8 @@ class Workers::DetectUserLanguage
   @queue = 'detect_user_language'
 
   def self.perform(args)
-    user = Account.current.users.find(args[:user_id])
-    Helpdesk::DetectUserLanguage.set_user_language!(user, args[:text])
+    user = Account.current.all_users.find(args[:user_id])
+    Helpdesk::DetectUserLanguage.set_user_language!(user, args[:text]) if user
   end
 
 end
