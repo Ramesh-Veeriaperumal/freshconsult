4f842ded94d071b271454c00e6172a34a5163249
Bala Kumar <bala@freshdesk.com>
2015-04-18 17:04:05 +0530

    [#9428] Parent bug (Google contacts OAuth1 migration). Squashed commit of the following:
    commit 8ffd4b428eb5d353e8415d2db5048fed3bdd001a
    Author: Bala Kumar <bala@freshdesk.com>
    Date:   Sat Apr 18 15:49:21 2015 +0530
    
        Addressing current account clean up and suggested comments as notes.
    
    commit 343f755a2822f5ba3fa50c741a93cd3f14f60943
    Author: Bala Kumar <bala@freshdesk.com>
    Date:   Sat Apr 18 11:57:43 2015 +0530
    
        [#9454][#9455]: Google Contacts - header fix for fetch detail and delete multiple entry in config hash fix.
    
    commit c5dc88064734c10ba1dba58f2ce6437cdac9bbab
    Author: Bala Kumar <bala@freshdesk.com>
    Date:   Fri Apr 17 14:43:17 2015 +0530
    
        [#9447]: Google Contacts reenable same account fix.
    
    commit c7d26eb752c826bf349cc06c26358dfb0c913d74
    Author: Bala Kumar <bala@freshdesk.com>
    Date:   Thu Apr 16 19:22:50 2015 +0530
    
        Google Contact - on delete - url concat error fix
    
    commit c65e249ebbb82c136d322602de03b8f9c1e4fa1f
    Author: Bala Kumar <bala@freshdesk.com>
    Date:   Thu Apr 16 16:30:44 2015 +0530
    
        [#9428]: Google Contacts OAuth2 migration.

--- app/controllers/integrations/applications_controller.rb
+++ app/controllers/integrations/applications_controller.rb
@@ -6,6 +6,7 @@ class Integrations::ApplicationsController < Admin::AdminController
   
   before_filter :load_object, :only => [:show]
   before_filter :load_installed_application, :only => [:edit, :update, :destroy]
+  before_filter :handle_google_contacts, :only => [:oauth_install]
   def index
     @applications = Integrations::Application.available_apps(current_account)
     @installed_applications = get_installed_apps
@@ -106,6 +107,45 @@ class Integrations::ApplicationsController < Admin::AdminController
   private
     def load_object
      @installing_application = Integrations::Application.available_apps(current_account).find(params[:id])
+    end
+
+    def handle_google_contacts
+      if params['id'] == "google_contacts"
+        key_options = { :account_id => @current_account.id, :provider => params['id']}
+        kv_store = Redis::KeyValueStore.new(Redis::KeySpec.new(Redis::RedisKeys::APPS_AUTH_REDIRECT_OAUTH, key_options))
+        kv_store.group = :integration
+        app_config = kv_store.get_key
+        config_hash = JSON.parse(app_config)
+        user_info = config_hash['info']
+        unless user_info.blank?
+          if config_hash['origin'].blank? || config_hash['origin'].include?("integrations") 
+            Rails.logger.error "The session variable to omniauth is not preserved or not set properly."
+            @omniauth_origin = "install"
+          end
+          @google_account = Integrations::GoogleAccount.new
+          @db_google_account = Integrations::GoogleAccount.find_by_account_id_and_email(@current_account, user_info["email"])
+          if !@db_google_account.blank? && @omniauth_origin == "install"
+            Rails.logger.error "As already an account has been configured can not configure one more account."
+            flash[:error] = t("integrations.google_contacts.already_exist")
+            redirect_to edit_integrations_installed_application_path(config_hash["iapp_id"]) 
+          else
+            @existing_google_accounts = Integrations::GoogleAccount.find_all_by_account_id(@current_account)
+            @google_account.account = @current_account #should it be account object or account.id ?
+            @google_account.token = config_hash['oauth_token']
+            @google_account.secret = config_hash['refresh_token']
+            @google_account.name = user_info["name"]
+            @google_account.email = user_info["email"]
+            @google_account.sync_group_name = "Freshdesk Contacts"
+            Rails.logger.debug "@google_account details #{@google_account.inspect} existing_google_accounts #{@existing_google_accounts.inspect}"
+            @google_groups = @google_account.fetch_all_google_groups(nil, use_oauth2=true)
+            # Reuse the group id, if the group with same name already exist.
+            @google_groups.each { |g_group|
+              @google_account.sync_group_id = g_group.group_id if g_group.name == @google_account.sync_group_name
+            }
+            render "integrations/google_accounts/edit"
+          end
+        end
+      end
     end
 
     def load_installed_application
