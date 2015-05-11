c929b88254b7ed5cb14f6158963624820241d75e
RashmiRam <rashmi@freshdesk.com>
2015-05-12 00:54:55 +0530

    fixing test
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- test/functional/api_discussions/posts_controller_test.rb
+++ test/functional/api_discussions/posts_controller_test.rb
@@ -4,32 +4,35 @@ module ApiDiscussions
   class PostsControllerTest < ActionController::TestCase
    
     def test_update
-      post = Post.first || create_test_post(Topic.first, current_user)
+      post = quick_create_post
       put :update, :version => "v2", :format=> :json, :id => post.id, :post => {:body_html => "test reply 2", :answer => 1}
       assert_response :success
       response.body.must_match_json_expression(post_pattern({:body_html => "test reply 2", :answer => true}, post.reload)) 
     end
 
     def test_update_invalid_answer
-      put :update, :version => "v2", :format=> :json, :id => Post.first.id, :post => {:body_html => "test reply 2", :answer => 90}
+      post = Post.first
+      put :update, :version => "v2", :format=> :json, :id => post, :post => {:body_html => "test reply 2", :answer => 90}
       assert_response :bad_request
       response.body.must_match_json_expression([bad_request_error_pattern("answer", "is not included in the list", {:list => ""})])
     end
 
     def test_update_with_user_id
-      put :update, :version => "v2", :format=> :json, :id => Post.first.id, :post => {:body_html => "test reply 2", :user_id => User.first}
+      post =  Post.first
+      put :update, :version => "v2", :format=> :json, :id => post, :post => {:body_html => "test reply 2", :user_id => User.first}
       assert_response :bad_request
       response.body.must_match_json_expression([bad_request_error_pattern("user_id", "invalid_field")])
     end
 
     def test_update_with_topic_id
-      put :update, :version => "v2", :format=> :json, :id => Post.first.id, :post => {:body_html => "test reply 2", :topic_id => Topic.first}
+      post =  Post.first
+      put :update, :version => "v2", :format=> :json, :id => post, :post => {:body_html => "test reply 2", :topic_id => Topic.first}
       assert_response :bad_request
       response.body.must_match_json_expression([bad_request_error_pattern("topic_id", "invalid_field")])
     end
 
     def test_destroy
-      post = Post.first || create_test_post(Topic.first, current_user)
+      post = quick_create_post
       delete :destroy, :version => "v2", :format => :json, :id => post.id
       assert_equal " ", @response.body
       assert_response :no_content
