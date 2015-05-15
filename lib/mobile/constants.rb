bebe152b995e9b4504edb78a6d83c919a7fe3c83
Ashwin Murali <ashwin.murali@freshdesk.com>
2015-05-15 21:03:04 +0530

    Revert "#9591 Mobile api to get full_name for cname"
    This reverts commit f1a9fe3f42301896bf6b8a5cfe36a7505a622579.
    
    Signed-off-by: Ashwin Murali <ashwin.murali@freshdesk.com>
    
    # Conflicts:
    #	app/controllers/mobile/settings_controller.rb
    #	db/schema.rb

--- lib/mobile/constants.rb
+++ lib/mobile/constants.rb
@@ -32,13 +32,4 @@ module Mobile::Constants
     :cannot_unfavorite => 20,
     :validation_failed => 21
   }
-
-  MOBILE_TYPE_ANDROID = 0
-  MOBILE_TYPE_IOS = 1
-
-  MOBILE_API_RESULT_SHA_FAIL=0
-  MOBILE_API_RESULT_SUCCESS=1
-  MOBILE_API_RESULT_UNSUPPORTED=2
-  MOBILE_API_RESULT_PARAM_FAILED=3
-
 end
