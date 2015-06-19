f6166444e6b7aa0ee87d6dd893eb33914ac71ab9
Balaji29 <balaji.rk@freshdesk.com>
2015-06-19 18:05:37 +0530

    Pivotal constant change

---
+++ lib/integrations/pivotal_tracker/constant.rb
@@ -0,0 +1,6 @@
+module Integrations::PivotalTracker::Constant
+
+	PIVOTAL_STORY = "story"
+	EXCLUDE_ARR = ["accepted_at", "updated_at","owner_ids","label_ids", "labels", "after_id", "owned_by_id", "before_id"]
+	
+end
