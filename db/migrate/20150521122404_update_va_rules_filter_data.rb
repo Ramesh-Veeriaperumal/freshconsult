6a1056194f6b87c8652dc25846d718025ed0fb09
vinodhkumarS <vinodhkumar@freshdesk.com>
2015-06-20 01:00:22 +0530

    [#10095] domain match wont work in dispatchr
    http://bugs.freshdesk.com/helpdesk/tickets/10095
    Signed-off-by: vinodhkumarS <vinodhkumar@freshdesk.com>

---
+++ db/migrate/20150521122404_update_va_rules_filter_data.rb
@@ -0,0 +1,51 @@
+class UpdateVaRulesFilterData < ActiveRecord::Migration
+shard :all
+
+def migrate(direction)
+    self.send(direction)
+end
+
+def up
+	failed_accounts = []
+  ShardMapping.find_in_batches(:batch_size => 300) do |shards|      
+    shards.each do |shard|
+      Account.reset_current_account
+      begin
+        Sharding.select_shard_of(shard.account_id) do
+          account = Account.find_by_id(shard.account_id)
+          next if account.nil?
+          account.make_current
+		  		account.all_va_rules.each do |va_rule|
+		  			to_be_saved = false
+			  		va_rule.filter_data.each do |condition|
+			  			if condition["evaluate_on"] == "company" && condition["name"] == "domain"
+			  				condition["name"] = "domains"
+			  				to_be_saved = true
+			  			end
+			  		end
+			  		va_rule.save if to_be_saved
+			  	end
+			  	account.all_observer_rules.each do |va_rule|
+			  		to_be_saved = false
+			  		va_rule.filter_data[:conditions].each do |condition|
+			  			if condition[:evaluate_on] == "company" && condition[:name] == "domain"
+			  				condition[:name] = "domains"
+			  				to_be_saved = true
+			  			end
+			  		end
+			  		va_rule.save if to_be_saved	  				
+			  	end
+  			end
+			rescue Exception => e
+        puts ":::::::::::#{e}:::::::::::::"
+        failed_accounts << shard.account_id
+			end
+		end
+	end
+	puts failed_accounts.inspect
+  failed_accounts
+end
+
+  def down
+  end
+end
