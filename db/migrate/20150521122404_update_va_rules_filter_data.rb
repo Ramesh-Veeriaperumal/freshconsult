e5cdc4786894cc758f5ffc55f0d33759296474e5
vinodhkumarS <vinodhkumar@freshdesk.com>
2015-06-20 03:23:54 +0530

    [#10095] Migration change
    Signed-off-by: vinodhkumarS <vinodhkumar@freshdesk.com>

--- db/migrate/20150521122404_update_va_rules_filter_data.rb
+++ db/migrate/20150521122404_update_va_rules_filter_data.rb
@@ -7,14 +7,13 @@ end
 
 def up
 	failed_accounts = []
-  ShardMapping.find_in_batches(:batch_size => 300) do |shards|      
-    shards.each do |shard|
-      Account.reset_current_account
-      begin
-        Sharding.select_shard_of(shard.account_id) do
-          account = Account.find_by_id(shard.account_id)
-          next if account.nil?
-          account.make_current
+	Account.find_in_batches(:batch_size => 300) do |accounts|
+  	accounts.each do |account|
+  		Account.reset_current_account
+	    begin
+	      Sharding.select_shard_of(account.id) do
+	      	next if account.nil?
+	      	account.make_current
 		  		account.all_va_rules.each do |va_rule|
 		  			to_be_saved = false
 			  		va_rule.filter_data.each do |condition|
@@ -33,12 +32,12 @@ def up
 			  				to_be_saved = true
 			  			end
 			  		end
-			  		va_rule.save if to_be_saved	  				
+			  		va_rule.save if to_be_saved
 			  	end
-  			end
+				end
 			rescue Exception => e
-        puts ":::::::::::#{e}:::::::::::::"
-        failed_accounts << shard.account_id
+	    	puts ":::::::::::#{e}:::::::::::::"
+	    	failed_accounts << account.id
 			end
 		end
 	end
