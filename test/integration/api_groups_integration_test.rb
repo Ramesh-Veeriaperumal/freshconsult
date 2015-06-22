f30e8f1033bfb17d06c6ab9234688b279f1520fb
Soorya <sooryazeal@gmail.com>
2015-06-22 15:20:35 +0530

    Removing unnecessary lines from groups test

--- test/integration/api_groups_integration_test.rb
+++ test/integration/api_groups_integration_test.rb
@@ -30,8 +30,6 @@ class ApiGroupsIntegrationTest < ActionDispatch::IntegrationTest
 
     v2[:destroy], v2[:api_destroy] = count_api_queries { delete("/api/v2/groups/#{id1}", nil, @headers) }
     v1[:destroy] = count_queries { delete("/groups/#{id2}.json", nil, @headers) }
-    p v1
-    p v2
     v1.keys.each do |key|
       api_key = "api_#{key}".to_sym
       assert v2[key] <= v1[key]
