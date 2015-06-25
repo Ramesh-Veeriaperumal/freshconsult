5dc0b7b870cfbe754dd54bc91d3c12f710527141
Hariharan <hariharan.r@freshdesk.com>
2015-06-25 22:11:49 +0530

    [#10284] Multilingual solution search for Pinnacle Sports
    commit 3098591c370340bc57199defc04edd772c1a7a3a
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Thu Jun 25 18:32:09 2015 +0530
    
        * query tampering with language search
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    commit b1d52c0fbd38a4c671f05d7797fff13e7569246a
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Thu Jun 25 15:32:54 2015 +0530
    
        Bug fix
    
        Instance variable inaccessible inside block
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    commit c17c9f4104f0d019e2706bc01b120dab97cab9e3
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Wed Jun 24 20:16:14 2015 +0530
    
        current_account to Account.current
    
        current_account not available in model. So changed to Account.current
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    commit ca058f0f4653cd312fa3c81806f6383293d807c1
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Mon Jun 22 18:22:44 2015 +0530
    
        Review changes
    
        - Renaming @lang -> @search_lang
        - Moving analyser picking to SearchUtil
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    commit e0fb49ee5549876c6d9f900511e407060ab20f4c
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Mon Jun 22 13:25:50 2015 +0530
    
        Analysers switch + Static acc_id hack added
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    commit 1cb71dd516ed3a9f48b75cf00c5285a1f0498976
    Merge: da15aa3 ae5bc25
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Mon Jun 22 13:15:47 2015 +0530
    
        Merge remote-tracking branch 'origin/rails3-phase2' into multilang_soln_search
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
        Conflicts:
        	app/models/account/constants.rb
    
    commit da15aa3bf123ef58dd9dcd6f84e0c75dedcdc88d
    Author: Hariharan <hariharan.r@freshdesk.com>
    Date:   Thu Jun 18 18:57:19 2015 +0530
    
        Multilane solution search - Initial Commit
    
        Known to-do:
        - query term & pick analyser based on feature check
        - acc id hack in static routing
    
        Signed-off-by: Hariharan <hariharan.r@freshdesk.com>
    
    Signed-off-by: Hariharan <hariharan.r@freshdesk.com>

--- lib/search/update_search_index.rb
+++ lib/search/update_search_index.rb
@@ -10,9 +10,24 @@ class Search::UpdateSearchIndex
       Search::EsIndexDefinition.es_cluster(@update_item.account_id)
       @update_item.class.index_name @update_item.search_alias_name
       @update_item.tire.update_index_es if Tire.index(@update_item.search_alias_name).exists?
+
+      handle_multiplexing if @update_item.is_a?(Solution::Article) and @update_item.account.features_included?(:es_multilang_solutions)
     end
     ensure
       key = Redis::RedisKeys::SEARCH_KEY % { :account_id => args[:account_id], :klass_name => args[:klass_name], :id => args[:id] }
       Search::Job.remove_job_key(key)
+  end
+
+  # Hack for multiplexing to old and new indices
+  # Can be removed when dynamic solutions support goes live
+  def self.handle_multiplexing
+    lang = @update_item.folder.category.portals.last.try(:language)
+    locale_alias = Search::EsIndexDefinition.searchable_aliases([Solution::Article], 
+                                                                  @update_item.account_id, 
+                                                                  { :language => lang })
+    if(locale_alias != @update_item.search_alias_name)
+      @update_item.class.index_name locale_alias
+      @update_item.tire.update_index_es if Tire.index(locale_alias).exists?
+    end
   end
 end
