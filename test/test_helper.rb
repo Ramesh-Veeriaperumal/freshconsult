1f71b5ecb2000f4c186e31b677a1218304362e10
RashmiRam <rashmi@freshdesk.com>
2015-06-30 00:33:43 +0530

    Rescued AWS dynamo db exception in test
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,12 @@ ENV['RAILS_ENV'] = 'test'
 require_relative 'helpers/test_files.rb'
 
 class ActionController::TestCase
+
+  rescue_from AWS::DynamoDB::Errors::ResourceNotFoundException do |exception|
+    Rake::Task["forum_moderation:create_tables"].invoke(Time.zone.now.year,Time.zone.now.month) if  Rails.env.test?
+    Rake::Task["forum_moderation:create_tables"].invoke(Time.zone.now.year,(Time.zone.now.month+1)) if  Rails.env.test?
+  end
+
   def setup
     activate_authlogic
     get_agent
@@ -16,6 +22,12 @@ class ActionController::TestCase
 end
 
 class ActionDispatch::IntegrationTest
+
+  rescue_from AWS::DynamoDB::Errors::ResourceNotFoundException do |exception|
+    Rake::Task["forum_moderation:create_tables"].invoke(Time.zone.now.year,Time.zone.now.month) if  Rails.env.test?
+    Rake::Task["forum_moderation:create_tables"].invoke(Time.zone.now.year,(Time.zone.now.month+1)) if  Rails.env.test?
+  end
+
   def setup
     get_agent
     set_request_headers
