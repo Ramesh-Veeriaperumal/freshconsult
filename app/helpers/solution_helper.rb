0489e20362753a7ad7c0b1735062a8af28a4b515
Sravya <sravya@freshdesk.com>
2015-07-03 13:30:56 +0530

    Using new category/folder/article everywhere instead of add
    Signed-off-by: Sravya <sravya@freshdesk.com>

--- app/helpers/solution_helper.rb
+++ app/helpers/solution_helper.rb
@@ -51,9 +51,9 @@ module SolutionHelper
 	end
 	
 	def new_solutions_button(default_btn = :article)
-		category = [t('article.add_category'), new_solution_category_path, false, new_btn_opts(:category)]
-		folder    = [t('article.add_folder'),    new_solution_folder_path(btn_default_params(:folder)), false, new_btn_opts(:folder)]
-		article    = [t("article.add_article"),    new_solution_article_path(btn_default_params(:article)), false, new_btn_opts(:article)]
+		category = [t('solution.add_category'), new_solution_category_path, false, new_btn_opts(:category)]
+		folder    = [t('solution.add_folder'),    new_solution_folder_path(btn_default_params(:folder)), false, new_btn_opts(:folder)]
+		article    = [t("solution.add_article"),    new_solution_article_path(btn_default_params(:article)), false, new_btn_opts(:article)]
 
 		if privilege?(:manage_solutions)
 			article = nil unless privilege?(:publish_solution)
