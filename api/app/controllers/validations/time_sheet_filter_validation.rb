e1142667474206d98a0006cbfc608f253e30913b
RashmiRam <rashmi@freshdesk.com>
2015-07-21 16:21:10 +0530

    Time sheet filter validation refactored.
    1. Validation constructor should not fire any db query. will be taken care via custom validator (like validate)
    2. Remove group_id filter in time sheets.
    
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- api/app/controllers/validations/time_sheet_filter_validation.rb
+++ api/app/controllers/validations/time_sheet_filter_validation.rb
@@ -1,17 +1,23 @@
 class TimeSheetFilterValidation < ApiValidation
-  attr_accessor :company_id, :user_id, :billable, :executed_after, :executed_before, :company, :user, :group_id, :group
+  attr_accessor :company_id, :user_id, :billable, :executed_after, :executed_before
 
   validates :billable, custom_inclusion: { in: ApiConstants::BOOLEAN_VALUES }, allow_blank: true
   validates :executed_after, :executed_before, date_time: { allow_nil: true }
-  validates :user_id, :company_id, :group_id, numericality: { allow_nil: true }
-  validates :company, presence: true, if: -> { company_id && errors[:company_id].blank? }
-  validates :user, presence: true, if: -> { user_id && errors[:user_id].blank? }
-  validates :group, presence: true, if: -> { group_id && errors[:group_id].blank? }
+  validates :user_id, :company_id, numericality: { allow_nil: true }
+  validate :valid_user?, if: -> { user_id && errors[:user_id].blank? }
+  validate :valid_company?, if: -> { company_id && errors[:company_id].blank? }
 
   def initialize(filter_params, item)
     super(filter_params, item)
-    @company = Account.current.companies_from_cache.find { |x| x.id == @company_id.to_i } if  @company_id && errors[:company_id].blank?
-    @user = Account.current.agents_from_cache.find { |x| x.user_id == @user_id.to_i } if  @user_id && errors[:user_id].blank?
-    @group = Account.current.groups_from_cache.find { |x| x.id == @group_id.to_i } if  @group_id && errors[:group_id].blank?
+  end
+
+  def valid_user?
+    user = Account.current.agents_from_cache.find { |x| x.user_id == @user_id.to_i }
+    errors.add(:user_id, "can't be blank") unless user
+  end
+
+  def valid_company?
+    user = Account.current.companies_from_cache.find { |x| x.id == @company_id.to_i }
+    errors.add(:company_id, "can't be blank") unless user
   end
 end
