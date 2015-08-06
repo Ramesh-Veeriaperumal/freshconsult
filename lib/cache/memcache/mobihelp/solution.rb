a90672456b392d6f518341f3bd6f15b6f62f57d2
Divya <diviatvit@gmail.com>
2015-08-06 17:08:42 +0530

    Multilingual solutions changes for mobihelp

--- lib/cache/memcache/mobihelp/solution.rb
+++ lib/cache/memcache/mobihelp/solution.rb
@@ -17,18 +17,18 @@ module Cache::Memcache::Mobihelp::Solution
     }
   end
 
-  def solutions_with_category(category_ids)
+  def solutions_with_category(categories)
     category_json_strings = []
-    category_ids.each do |category_id|
-      category_json_strings << solutions(category_id)
+    categories.each do |category|
+      category_json_strings << solutions(category)
     end
     "[#{category_json_strings.join(",")}]"
   end
 
-  def solutions_without_category(category_ids)
+  def solutions_without_category(categories)
     category_json_strings = []
-    category_ids.each do |category_id|
-      category_hash = ActiveSupport::JSON.decode(solutions(category_id))
+    categories.each do |category|
+      category_hash = ActiveSupport::JSON.decode(solutions(category))
       category_hash["category"]["public_folders"].each do |f|
         category_json_strings << {"folder" => f}.to_json
       end
@@ -36,18 +36,19 @@ module Cache::Memcache::Mobihelp::Solution
     "[#{category_json_strings.join(",")}]"
   end
 
+  def mobihelp_solutions_key
+    MOBIHELP_SOLUTIONS % { :account_id => account_id, :category_id => self.id }
+  end
+
   private
-    def mobihelp_solutions_key
-      MOBIHELP_SOLUTIONS % { :account_id => account_id, :category_id => self.id }
-    end
 
     def mobihelp_solution_updated_time_key(app_id)
       MOBIHELP_SOLUTION_UPDATED_TIME % { :account_id => account_id, :app_id => app_id }
     end
 
-    def solutions(category_id)
-      MemcacheKeys.fetch(mobihelp_solutions_key(category_id)) {
-
+    def solutions(category)
+      MemcacheKeys.fetch(category.mobihelp_solutions_key) {
+        category_id = category.id
         ### MULTILINGUAL SOLUTIONS - META READ HACK!!
         include_hash = (Account.current.launched?(:meta_read) ? 
                         {:public_folders_through_meta => {:published_articles_through_meta => [:tags]}} : 
