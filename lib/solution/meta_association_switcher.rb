275a90f92c5be6126ad473afb83943252f98b713
Arvinth <arvinth@freshdesk.com>
2015-08-12 15:54:55 +0530

    Portal save callback error during signup fixed
    Signed-off-by: Arvinth <arvinth@freshdesk.com>

--- lib/solution/meta_association_switcher.rb
+++ lib/solution/meta_association_switcher.rb
@@ -6,7 +6,8 @@ module Solution::MetaAssociationSwitcher
 	def self.included(base)
 		base::FEATURE_BASED_METHODS.each do |method|
 			define_method(%{#{method.to_s}_with_association}) do
-				if Account.current.launched?(:meta_read) && !self.new_record?
+				account = (self.class.name == "Account") ? self : self.account
+				if account.launched?(:meta_read) and !self.new_record?
 					send(%{#{method.to_s}_through_meta})
 				else
 					send(%{#{method.to_s}_without_association})
