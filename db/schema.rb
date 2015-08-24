a02461ea615dd39c4f0c5954ab444ba3829f6a84
Vijaya Raj M <vijayaraj@freshdesk.com>
2015-08-25 01:19:22 +0530

    #11372 - Pivotal Tracker changes - replacing ticket display ID with Ticket ID

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150805133724) do
+ActiveRecord::Schema.define(:version => 20150824192547) do
 
   create_table "account_additional_settings", :force => true do |t|
     t.string   "email_cmds_delimeter"
