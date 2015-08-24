df3a6df1e096288d697ec4b6172f22a8f88b2016
jayaram2701 <jayaram.r@freshdesk.com>
2015-08-24 14:00:18 +0530

    [#10910] UI enhancements
    Added subtext for dispatcher.
    cleanup in email commands helpnote and business calender.
    
    Signed-off-by: jayaram2701 <jayaram.r@freshdesk.com>

--- app/controllers/admin/email_commands_settings_controller.rb
+++ app/controllers/admin/email_commands_settings_controller.rb
@@ -6,14 +6,6 @@ class Admin::EmailCommandsSettingsController < Admin::AdminController
     @email_content_commands = '"status":"pending", "priority":"medium", "agent":"John Robert"'
     
     delimeter = current_account.email_cmds_delimeter
-
-    @email_content_custom_commands = ""
-    @email_content_custom_commands += "action: note <br>"
-
-    current_account.ticket_fields.custom_fields.each do |field|
-      @email_content_custom_commands += "#{field.label}: [Custom Field Value] <br>" unless field.field_type == "nested_field"
-    end
-
     
     content_prefix = "Hi,\n\n";
     content_suffix = "\n\nYour issue is resolved now.\n\nThanks,\nAgent Name.\n"; 
