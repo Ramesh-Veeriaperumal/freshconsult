839464afb68af2667868bcebdba1f52fae5f9eb0
Shridar S <shridar@freshdesk.com>
2015-08-24 15:57:50 +0530

    Bug ID [#11350]

--- lib/helpdesk/email/handle_ticket.rb
+++ lib/helpdesk/email/handle_ticket.rb
@@ -107,11 +107,12 @@ class Helpdesk::Email::HandleTicket
     content_id_hash = {}
     email[:attached_items].count.times do |i|
       begin
+        content_id = cid(i) && verify_inline_attachments(item, cid(i))
         att = Helpdesk::Attachment.create_for_3rd_party(account, item, 
                                                         email[:attached_items]["attachment-#{i+1}"], 
-                                                        i, cid(i), true)
+                                                        i, content_id, true)
         if att.is_a? Helpdesk::Attachment
-          if cid(i)
+          if content_id
             content_id_hash[att.content_file_name+"#{i}"] = cid(i)
             inline_attachments.push att
           else
