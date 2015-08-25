0e3b9c01c7f8adf3695cc398bb68a62b77f2fbab
venkatesanmurali <venkatesan.murali1985@gmail.com>
2015-08-25 15:51:17 +0530

    [#11385] Long running ticket export

--- lib/helpdesk/tickets_export.rb
+++ lib/helpdesk/tickets_export.rb
@@ -5,8 +5,11 @@ class Helpdesk::TicketsExport
   def self.perform(export_params)
   	#harcoding key here to avoid instance/class method conflict.
   	key = "PREMIUM_TICKET_EXPORT"
+  	long_running_key = "LONG_RUNNING_TICKET_EXPORT"
   	if $redis_others.sismember(key, Account.current.id)
   		Resque.enqueue(Helpdesk::PremiumTicketsExport, export_params)
+  	elsif $redis_others.sismember(long_running_key, Account.current.id)
+  		Resque.enqueue(Helpdesk::LongRunningTicketsExport, export_params)
   	else
   		Helpdesk::TicketsExportWorker.new(export_params).perform
   	end
