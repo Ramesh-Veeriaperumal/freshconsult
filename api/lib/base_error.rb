0607fa35cb0d55e838a28087ef829f961b2c9b8b
RashmiRam <rashmi@freshdesk.com>
2015-09-02 16:58:39 +0530

    Renamed already_exists to duplicate value in base error
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- api/lib/base_error.rb
+++ api/lib/base_error.rb
@@ -5,7 +5,7 @@ class BaseError
     missing_field: ['missing_field', 'Mandatory attribute missing', 'missing',
                     'requester_id_mandatory', 'phone_mandatory', 'required_and_numericality',
                     'required_and_inclusion', 'required_number', 'required_integer', 'required_date', 'required_format'],
-    already_exists: ['has already been taken', 'already exists in the selected category'],
+    duplicate_value: ['has already been taken', 'already exists in the selected category'],
     invalid_value: ["can't be blank", 'is not included in the list', 'invalid_user'],
     datatype_mismatch: ['is not a date', 'is not a number', 'data_type_mismatch', 'must be an integer'],
     invalid_field: ['invalid_field', "Can't update user when timer is running"],
@@ -13,12 +13,12 @@ class BaseError
   }
 
   API_HTTP_ERROR_STATUS_BY_CODE = {
-    already_exists: 409
+    duplicate_value: 409
   }
 
   # Reverse mapping, this will result in:
-  # {'has already been taken' => :already_exists,
-  # 'already exists in the selected category' => :already_exists
+  # {'has already been taken' => :duplicate_value,
+  # 'already exists in the selected category' => :duplicate_value
   # 'can't be blank' => :invalid_value
   # ...}
   API_ERROR_CODES_BY_VALUE = Hash[*API_ERROR_CODES.flat_map { |code, errors| errors.flat_map { |error| [error, code] } }]
