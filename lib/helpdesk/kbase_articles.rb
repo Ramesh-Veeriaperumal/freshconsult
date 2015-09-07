8968ba49c43ab0d58da8e405a9394836400eba10
Sravya <sravya@freshdesk.com>
2015-09-07 17:21:43 +0530

    Bug fix - Inline images issue
    Signed-off-by: Sravya <sravya@freshdesk.com>

--- lib/helpdesk/kbase_articles.rb
+++ lib/helpdesk/kbase_articles.rb
@@ -40,6 +40,7 @@ class Helpdesk::KbaseArticles
       
       unless content_ids.blank?
         article.draft.update_attributes!(:description => temp_body_html)
+        article.update_attributes!(:description => temp_body_html)
       end
     end
 
