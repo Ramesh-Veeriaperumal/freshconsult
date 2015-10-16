25d25588450745316c53083829faeeaaf20d9d79
jayaram2701 <jayaram.r@freshdesk.com>
2015-10-16 16:40:47 +0530

    Ticket#12482
    Password policy - Problem while saving SSO settings
    
    Signed-off-by: jayaram2701 <jayaram.r@freshdesk.com>

--- lib/password_policies/user_helpers.rb
+++ lib/password_policies/user_helpers.rb
@@ -3,58 +3,58 @@ module PasswordPolicies
 
     def user_policy
       if self.agent?
-        policy_account.agent_password_policy_from_cache || policy_account.build_agent_password_policy
+        policy_account.agent_password_policy_from_cache 
       else
-        policy_account.contact_password_policy_from_cache || policy_account.build_contact_password_policy
+        policy_account.contact_password_policy_from_cache
       end
     end
 
     def password_history_enabled?
-      user_policy.password_history_enabled?
+      user_policy && user_policy.password_history_enabled?
     end 
 
     def password_history_depth
-      user_policy.history_depth.to_i
+      user_policy && user_policy.history_depth.to_i
     end
 
     def password_alphanumeric_enabled?
-      user_policy.password_alphanumeric_enabled?
+      user_policy && user_policy.password_alphanumeric_enabled?
     end 
 
     def password_special_character_enabled?
-      user_policy.password_special_character_enabled?
+      user_policy && user_policy.password_special_character_enabled?
     end
 
     def password_mixed_case_enabled?
-      user_policy.password_mixed_case_enabled?
+      user_policy && user_policy.password_mixed_case_enabled?
     end 
 
     def password_contains_login_enabled?
-      user_policy.password_contains_login_enabled?
+      user_policy && user_policy.password_contains_login_enabled?
     end
 
     def periodic_login_enabled?
-      policy_account.launched?(:periodic_login_feature) ? (user_policy.periodic_login_enabled?) : false
+      user_policy && policy_account.launched?(:periodic_login_feature) ? (user_policy.periodic_login_enabled?) : false
     end    
 
     def password_expiry_enabled?
-      login_via_password? && user_policy.password_expiry_enabled?
+      user_policy && login_via_password? && user_policy.password_expiry_enabled?
     end   
 
     def password_length_enabled?
-      user_policy.password_length_enabled?
+      user_policy && user_policy.password_length_enabled?
     end 
 
     def periodic_login_duration
-      user_policy.periodic_login_duration && user_policy.periodic_login_duration.to_i.days
+      user_policy && user_policy.periodic_login_duration && user_policy.periodic_login_duration.to_i.days
     end
 
     def password_expiry_duration
-      user_policy.password_expiry_duration && user_policy.password_expiry_duration.to_i.days
+      user_policy && user_policy.password_expiry_duration && user_policy.password_expiry_duration.to_i.days
     end
 
     def minimum_password_length
-      user_policy.minimum_password_length && user_policy.minimum_password_length.to_i
+      user_policy && user_policy.minimum_password_length && user_policy.minimum_password_length.to_i
     end
 
     def login_via_password?
