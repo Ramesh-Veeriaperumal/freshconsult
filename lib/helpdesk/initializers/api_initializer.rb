2be9fdde5e688bee6043f20c5a9b5008a6b3dd03
sudhircirra <sudhir@freshdesk.com>
2015-10-27 19:01:31 +0530

    API specific infra layer changes
    - Adding back rails/all require to application.rb.
    - Renamed clean_api_middlewares to api_initializer
    - Metal Authlogic specific include is done irrespective of whether is layer is api or not
    - Infra yml load is moved out of load_initializers.

--- lib/helpdesk/initializers/api_initializer.rb
+++ lib/helpdesk/initializers/api_initializer.rb
@@ -1,3 +1,5 @@
+Infra = YAML.load_file(File.join(Rails.root, 'config', 'infra_layer.yml'))
+
 if Infra['API_LAYER']
   Helpkit::Application.configure do
 
@@ -40,7 +42,9 @@ if Infra['API_LAYER']
     config.middleware.delete HttpAcceptLanguage::Middleware
   end
 
-  ActionController::Metal.send(:include, AbstractController::Callbacks )
-  ActionController::Metal.send(:include, Authlogic::ControllerAdapters::RailsAdapter::RailsImplementation)
-
 end
+
+# Metal changes has to be included irrespective of whether the layer is API or not. 
+# This is required as in some cases, API and web requests can be served from the same box.
+ActionController::Metal.send(:include, AbstractController::Callbacks )
+ActionController::Metal.send(:include, Authlogic::ControllerAdapters::RailsAdapter::RailsImplementation)
