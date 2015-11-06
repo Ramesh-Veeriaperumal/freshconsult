1524e58fa85cad30f5c3eeb40dbe7a1f4b0ac72b
Sukanya V <sukanya@freshdesk.com>
2015-11-06 18:01:35 +0530

    [#12834] Changing twitter user name as contact name for twitter users

--- lib/social/twitter/common.rb
+++ lib/social/twitter/common.rb
@@ -1,14 +1,18 @@
 module Social::Twitter::Common
   
-  def get_twitter_user(screen_name, profile_image_url=nil)
-    account = Account.current
-    user = account.all_users.find_by_twitter_id(screen_name)
-    unless user
+  def get_twitter_user(screen_name, profile_image_url=nil, user_name=nil)
+    account   = Account.current
+    user      = account.all_users.find_by_twitter_id(screen_name)
+    user_name = screen_name if user_name.to_s.blank?
+    
+    if user && (user.name != user_name)
+      user.update_attributes({:name => user_name})
+    elsif user.nil?
       user = account.contacts.new
       user.signup!({
                      :user => {
                        :twitter_id      => screen_name,
-                       :name            => screen_name,
+                       :name            => user_name.to_s,
                        :active          => true,
                        :helpdesk_agent  => false
                      }
