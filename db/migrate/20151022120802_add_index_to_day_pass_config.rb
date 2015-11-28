21ac51acc5c0c58d69ee8bd7c7ad1bf863d92d1b
akila <akila@freshdesk.com>
2015-11-28 13:33:24 +0530

    schema version update.
    Signed-off-by: akila <akila@freshdesk.com>

--- db/migrate/20151022120802_add_index_to_day_pass_config.rb
+++ db/migrate/20151022120802_add_index_to_day_pass_config.rb
@@ -1,5 +1,10 @@
 class AddIndexToDayPassConfig < ActiveRecord::Migration
   shard :all
+
+  def migrate(direction)
+    self.send(direction)
+  end
+  
   def up
     Lhm.change_table :day_pass_configs, :atomic_switch => true do |t|
       t.add_index [:account_id]
