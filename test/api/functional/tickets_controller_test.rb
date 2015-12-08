1107870dcae57f12ec70b9b068787bc855a011f2
RashmiRam <rashmi@freshdesk.com>
2015-12-08 12:01:22 +0530

    fixing jenkins build failure.
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- test/api/functional/tickets_controller_test.rb
+++ test/api/functional/tickets_controller_test.rb
@@ -2074,7 +2074,7 @@ class TicketsControllerTest < ActionController::TestCase
     get :index, controller_params
     assert_response 200
     response = parse_response @response.body
-    assert_equal Helpdesk::Ticket.where(deleted: 0, spam: 0).count, response.size
+    assert_equal [Helpdesk::Ticket.where(deleted: 0, spam: 0).count, 30].min, response.size
 
     Agent.any_instance.stubs(:ticket_permission).returns(3)
     get :index, controller_params
