259a63c20ac8f422123ff23fb0dc2387c06a923d
sumit08 <agr.sumit08@gmail.com>
2015-12-09 16:57:20 +0530

    [#12943] - Adding migration file

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 
-ActiveRecord::Schema.define(:version => 20151127100242) do
+ActiveRecord::Schema.define(:version => 20151209110505) do
 
 
   create_table "account_additional_settings", :force => true do |t|
