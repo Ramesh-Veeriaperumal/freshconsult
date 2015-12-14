e7e711cbb44ff723e3c3ab6890ac1647f7c43139
suman <bappi.5476@gmail.com>
2015-12-14 23:38:29 +0530

    adding spam check in contact import

--- lib/workers/import/contact.rb
+++ lib/workers/import/contact.rb
@@ -4,6 +4,8 @@ class Workers::Import::Contact
   @queue = "contact_import"
 
   def self.perform(args)
+  	acc = Account.find(args[:account_id])
+  	raise "Spam Account" if acc.subscription.trial? and acc.tickets.count < 10 and !$spam_watcher.get(acc.id)
     Import::Customers::Contact.new(args).import
   end
   
