c924175d1f74f3b11524e3cee2e96efb98eb3baa
RashmiRam <rashmi@freshdesk.com>
2015-12-23 20:45:07 +0530

    Fixing tests related to agent as requester feature.
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- test/api/functional/ticket_concern_test.rb
+++ test/api/functional/ticket_concern_test.rb
@@ -87,13 +87,16 @@ class FakeControllerTest < ActionController::TestCase
   end
 
   def test_verify_ticket_permission_with_requester_ticket_permission_valid
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
     User.any_instance.stubs(:can_view_all_tickets?).returns(false).at_most_once
     User.any_instance.stubs(:group_ticket_permission).returns(false).at_most_once
     Helpdesk::Ticket.any_instance.stubs(:requester_id).returns(@agent.id)
     actual = @controller.send(:verify_ticket_permission, @agent,  Helpdesk::Ticket.first)
     User.any_instance.unstub(:can_view_all_tickets, :group_ticket_permission)
     Helpdesk::Ticket.unstub(:assigned_tickets_permission, :requester_id)
-    assert actual
+    assert_equal 403, response.status
+    assert_equal request_error_pattern(:access_denied).to_json, response.body
   end
 
   def test_verify_ticket_permission_with_group_ticket_permission_valid
