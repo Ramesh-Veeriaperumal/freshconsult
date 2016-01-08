26a3f30ec0036f36fe146f9a957f50e6c8c5b12d
Hariharan R <hariharan.r@freshdesk.com>
2016-01-08 17:29:44 +0530

    Symbolizing hosts
    Signed-off-by: Hariharan R <hariharan.r@freshdesk.com>

--- lib/helpdesk/initializers/search/v2.rb
+++ lib/helpdesk/initializers/search/v2.rb
@@ -2,4 +2,4 @@
 
 ES_V2_SUPPORTED_TYPES = YAML.load_file(File.join(Rails.root, 'config/search/supported_types.yml'))
 ES_V2_BOOST_VALUES    = YAML::load_file(File.join(Rails.root, 'config/search', 'boost_values.yml'))
-ES_V2_CLUSTERS        = YAML::load_file(File.join(Rails.root, 'config/search', 'esv2_hosts.yml'))[Rails.env]
+ES_V2_CLUSTERS        = YAML::load_file(File.join(Rails.root, 'config/search', 'esv2_hosts.yml'))[Rails.env].symbolize_keys
