b21939da67370218b5c5d359ef64e6601cb1bde0
RashmiRam <rashmi@freshdesk.com>
2016-01-27 19:57:04 +0530

    Authentication improved for APIs
    1. Password_expiry enforced in API request via password.
    2. Consecutive failed login attempts handled in API
    
    Signed-off-by: RashmiRam <rashmi@freshdesk.com>

--- test/api/functional/api_application_controller_test.rb
+++ test/api/functional/api_application_controller_test.rb
@@ -17,12 +17,25 @@ class ApiApplicationControllerTest < ActionController::TestCase
   end
 
   def test_api_current_user_failed_login_count_on_valid_pwd
-    auth = ActionController::HttpAuthentication::Basic.encode_credentials(@agent.single_access_token, 'X')
+    auth = ActionController::HttpAuthentication::Basic.encode_credentials(@agent.email, 'test')
+    params = ActionController::Parameters.new("format" => "json")
+    controller.params = params
     @controller.request.env['HTTP_AUTHORIZATION'] = auth
     @agent.update_attribute(:failed_login_count, 1)
     @controller.send(:api_current_user)
     assert_equal 0, @agent.reload.failed_login_count
   end
+
+  def test_api_current_user_failed_login_count_on_valid_api_key
+    auth = ActionController::HttpAuthentication::Basic.encode_credentials(@agent.single_access_token, 'X')
+    params = ActionController::Parameters.new("format" => "json")
+    controller.params = params
+    @controller.request.env['HTTP_AUTHORIZATION'] = auth
+    @agent.update_attribute(:failed_login_count, 1)
+    @controller.send(:api_current_user)
+    assert_equal 1, @agent.reload.failed_login_count
+  end
+
 
   def test_invalid_field_handler_with_invalid_multi_part
     response = ActionDispatch::TestResponse.new
