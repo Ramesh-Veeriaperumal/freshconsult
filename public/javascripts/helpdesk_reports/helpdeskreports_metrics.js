aee0fa4706c2776b89aef591037da381501e1b86
Shanmugapriya11 <mshanmugapriya11@gmail.com>
2016-02-05 11:48:31 +0530

    Reports squashed commit of the following:
    14072			Enhancement	Implementing caching layer for Report Service
    13958			Enhancement	Reports : Kissmetrics
    11646			Bug Fix	New Reports: Agent/Group Summary: Average 1st response time mismatches because Some First responses are not considered #11646
    14269			Bug Fix	New Reports : Filter are not getting applied for Glance PDF
    14098			Enhancement	Reports Filter performance improvement

---
+++ public/javascripts/helpdesk_reports/helpdeskreports_metrics.js
@@ -0,0 +1,19 @@
+window.App = window.App || {};
+window.App.Report = window.App.Report || {};
+(function ($) {
+    "use strict";
+    App.Report.Metrics = {
+		recordIdentity: function(){
+			if(typeof (_kmq) != 'undefined' ){
+				_kmq.push(['identify', full_domain]);
+			}
+		},
+		push_event: function (event,property) {
+			if(typeof (_kmq) != 'undefined' ){
+				this.recordIdentity();
+    			_kmq.push(['record',event,property]);	
+			}
+			
+		}
+	};
+}(jQuery));
