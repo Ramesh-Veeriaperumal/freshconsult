acfe77d0078e6f70c724d4950a88c5b8e6e94322
Sravya <sravya@freshdesk.com>
2016-03-04 15:08:26 +0530

    Review changes [language_settings.js]
    public/javascripts/app/admin/language_settings.js
    
    Signed-off-by: Sravya <sravya@freshdesk.com>

--- public/javascripts/app/admin/language_settings.js
+++ public/javascripts/app/admin/language_settings.js
@@ -1,5 +1,5 @@
 /*jslint browser: true */
-/*global  App */
+/*global  App, alert */
 
 window.App = window.App || {};
 window.App.Admin = window.App.Admin || {};
@@ -13,43 +13,35 @@ window.App.Admin = window.App.Admin || {};
     },
     
     bindHandlers: function () {
-      var $this = this;
-      $( document ).ready(function() {
-        $("body").on('submit.language_settings', '#manage_languages_form', $this.validateLanguage);
-        $this.portalLanguageConfig();
-      });
+      $("body").on('submit.language_settings', '#manage_languages_form', this.validateLanguage);
+      this.portalLanguageConfig();
     },
 
     validateLanguage: function (ev) {
-      var language_flag = $.inArray($("#account_main_portal_attributes_language").val(),
-                          $("#account_account_additional_settings_attributes_supported_languages").val());
-      if(language_flag != -1 ) {
-        ev.preventDefault(); 
+      if (($.inArray($("#account_main_portal_attributes_language").val(), $("#account_account_additional_settings_attributes_supported_languages").val())) !== -1) {
+        ev.preventDefault();
         alert(App.Admin.LanguageSettings.INVALID_LANGUAGE_ALERT);
       }
     },
 
     portalLanguageConfig: function () {
       var $this = this;
-      $('#account_supported_languages').select2();
-      $('#account_supported_languages').on('change.language_settings', function() {
-          var newOptions = $this.supportedPortalLanguages();
-          $('#portal_supported_languages').select2({
-            multiple: true,
-            data: newOptions
-          });
-      });
-      $('#account_supported_languages').trigger('change');
-      $('#portal_supported_languages').select2('val', $('#portal_supported_languages').data('portalLanguages'))
-                                      .trigger('change');
+      $('#account_supported_languages').on('change.language_settings', function () {
+				$('#portal_supported_languages').select2({
+					multiple: true,
+					data: $this.supportedPortalLanguages()
+				});
+      }).trigger('change');
+      $('#portal_supported_languages').select2('val', $('#portal_supported_languages').data('portalLanguages')).trigger('change');
     },
 
-    supportedPortalLanguages: function() {
-      var supportedLanguages = $('#account_supported_languages').select2('data');
-      var portalLanguages = [];
-      for(var language of supportedLanguages) {  
-        portalLanguages.push ({id: language.id, text: language.text});
-      }
+    supportedPortalLanguages: function () {
+      var supportedLanguages = $('#account_supported_languages :selected'),
+				portalLanguages = [],
+				language;
+      $.each(supportedLanguages, function (i, language) {
+        portalLanguages.push({ id: language.value, text: language.text });
+      });
       return portalLanguages;
     },
 
