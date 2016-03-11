11dfd63f3b655a9fb51ebc7653a6484efa81a478
Revathi <revathi@freshdesk.com>
2016-03-11 12:14:54 +0530

    [#15291] Knocking off classic reports

--- app/models/account.rb
+++ app/models/account.rb
@@ -151,8 +151,8 @@ class Account < ActiveRecord::Base
     ismember?(TAG_BASED_ARTICLE_SEARCH, self.id)
   end
 
-  def classic_reports_disabled?
-    !ismember?(CLASSIC_REPORTS_ENABLED, self.id)
+  def classic_reports_enabled?
+    ismember?(CLASSIC_REPORTS_ENABLED, self.id)
   end
 
   #Temporary feature check methods - using redis keys - ends here
