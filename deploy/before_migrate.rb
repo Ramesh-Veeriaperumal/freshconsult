293227268973714aa835cf6352b72a554a5c32db
Hariharan R <hariharan.r@freshdesk.com>
2016-04-13 15:35:33 +0530

    Recipes added so enabling symlinks
    Signed-off-by: Hariharan R <hariharan.r@freshdesk.com>

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -25,7 +25,7 @@ run "ln -nfs #{shared_path}/config/braintree.yml #{node[:rel_path]}/config/brain
 run "ln -nfs #{shared_path}/config/rabbitmq.yml #{node[:rel_path]}/config/rabbitmq.yml"
 run "ln -nfs #{shared_path}/config/sidekiq.yml #{node[:rel_path]}/config/sidekiq.yml"
 run "ln -nfs #{shared_path}/config/s3.yml #{node[:rel_path]}/config/s3.yml"
-# run "ln -nfs #{shared_path}/config/aws_v2.yml #{node[:rel_path]}/config/aws_v2.yml"
+run "ln -nfs #{shared_path}/config/aws_v2.yml #{node[:rel_path]}/config/aws_v2.yml"
 run "ln -nfs #{shared_path}/config/s3_static_files.yml #{node[:rel_path]}/config/s3_static_files.yml"
 run "ln -nfs #{shared_path}/config/dalli.yml #{node[:rel_path]}/config/dalli.yml"
 run "ln -nfs #{shared_path}/config/dalli_api.yml #{node[:rel_path]}/config/dalli_api.yml"
@@ -53,7 +53,7 @@ run "ln -nfs #{shared_path}/config/pod_dns_config.yml #{node[:rel_path]}/config/
 run "ln -nfs #{shared_path}/config/search/boost_values.yml #{node[:rel_path]}/config/search/boost_values.yml"
 run "ln -nfs #{shared_path}/config/search/supported_types.yml #{node[:rel_path]}/config/search/supported_types.yml"
 run "ln -nfs #{shared_path}/config/search/esv2_hosts.yml #{node[:rel_path]}/config/search/esv2_hosts.yml"
-# run "ln -nfs #{shared_path}/config/search/dynamo_tables.yml #{node[:rel_path]}/config/search/dynamo_tables.yml"
+run "ln -nfs #{shared_path}/config/search/dynamo_tables.yml #{node[:rel_path]}/config/search/dynamo_tables.yml"
 
 # Xero Cert files start
 run "ln -nfs #{shared_path}/config/cert/integrations/xero/entrust-cert.pem #{node[:rel_path]}/config/cert/integrations/xero/entrust-cert.pem"
