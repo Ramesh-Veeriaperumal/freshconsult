c050341228babf42cb50eff3b3317f28cde84a7f
Shanmugapriya11 <mshanmugapriya11@gmail.com>
2016-05-03 14:50:51 +0530

    [#15668] - After commit changes

--- app/models/helpdesk/scheduled_task.rb
+++ app/models/helpdesk/scheduled_task.rb
@@ -17,7 +17,7 @@ class Helpdesk::ScheduledTask < ActiveRecord::Base
   before_save :calculate_next_run_at, :update_last_run_at
   #callbacks commit/update - save inside leads to infinte loop
   
-  after_commit :trigger, if: Proc.new { :persisted? && :available? } 
+  after_commit :trigger, if: Proc.new { persisted? && available? } 
   
   scope :by_schedulable_type, lambda{ |schedulable_type| {:conditions => ['schedulable_type = ?', schedulable_type]}}
 
