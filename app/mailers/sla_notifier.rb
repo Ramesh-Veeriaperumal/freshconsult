8d843789f169aa0e77cc96daf09cf48c7b766bb7
Athi Ruban <athi@freshdesk.com>
2016-05-12 15:06:50 +0530

    Bug 16624 - sla notification changes
    Currently some SLA notification emails are not going via the configured "out-going email category" assigned to the customer for the particular domain.
    
    To fix this we plan to append the category id in the email header to avoid sidekiq multi-threading issues.
    
    Signed-off-by: Athi Ruban <athi@freshdesk.com>

--- app/mailers/sla_notifier.rb
+++ app/mailers/sla_notifier.rb
@@ -10,7 +10,8 @@ class SlaNotifier < ActionMailer::Base
 			:sent_on                   => Time.now,
 			"Reply-to"                 => "#{ticket.account.default_friendly_email}",
 			"Auto-Submitted"           => "auto-generated", 
-			"X-Auto-Response-Suppress" => "DR, RN, OOF, AutoReply"
+			"X-Auto-Response-Suppress" => "DR, RN, OOF, AutoReply",
+			"X-FD-Email-Category"      => ticket.reply_email_config.category
 		}
 		mail(headers) do |part|
 			part.text do
