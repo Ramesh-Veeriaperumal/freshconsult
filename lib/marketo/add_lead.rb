6ce431423c767ba27289cfedceca031016c7ea5d
Bhagirath <bhagi.goud@gmail.com>
2016-05-20 13:38:37 +0530

    [#16664] - Freshsales Workflow changes regarding Account Cancellation, Account Trial Extension and Signup

--- lib/marketo/add_lead.rb
+++ lib/marketo/add_lead.rb
@@ -8,6 +8,6 @@ class Marketo::AddLead
 	  account = Account.current
 	  crm.add_signup_data(account, {:signup_id => args[:signup_id] })
   ensure
-    Resque.enqueue(CRM::Freshsales::Signup, { account_id: Account.current.id })
+    Resque.enqueue(CRM::Freshsales::Signup, { account_id: Account.current.id, fs_cookie: args[:fs_cookie] })
 	end 
 end
