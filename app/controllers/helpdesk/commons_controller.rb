b0104165733e74446dac60403814b630dbfa70af
Shridar S <shridar@freshdesk.com>
2016-06-21 14:18:00 +0530

    Bug ID [#17847] - Phone contacts issue

--- app/controllers/helpdesk/commons_controller.rb
+++ app/controllers/helpdesk/commons_controller.rb
@@ -3,6 +3,8 @@ class Helpdesk::CommonsController < ApplicationController
   before_filter :set_mobile, :only => [:group_agents]
   skip_before_filter :check_privilege, :verify_authenticity_token
 
+  PHONE_REGEX = /.+\((.+)\)/
+
   include AccountConstants
 
   def group_agents
@@ -31,12 +33,16 @@ class Helpdesk::CommonsController < ApplicationController
   def user_companies
     to_ret = false
     if current_user && (current_user.agent? || current_user.contractor?)
+      user = nil
       if params[:email] =~ EMAIL_REGEX
         email = $1
         user_email = current_account.user_emails.find_by_email(email)
         user = user_email.user if user_email
-        to_ret = user.companies.sorted.collect { |c| [c.name, c.id] } if (user && user.companies.present?)
+      elsif params[:email] =~ PHONE_REGEX
+        phone = $1
+        user = current_account.users.where(["phone = ? or mobile = ?", phone, phone]).first
       end
+      to_ret = user.companies.sorted.collect { |c| [c.name, c.id] } if (user && user.companies.present?)
     end
     render :json => to_ret
   end
