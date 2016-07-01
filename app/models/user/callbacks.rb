b7b91a1ef83bb2309d0fdd0e241b57b3858dc9c0
Shridar S <shridar@freshdesk.com>
2016-07-01 19:29:15 +0530

    Bug ID [#18133]

--- app/models/user/callbacks.rb
+++ app/models/user/callbacks.rb
@@ -172,7 +172,7 @@ class User < ActiveRecord::Base
   end  
 
   def backup_customer_id
-    if self.user_companies.present?
+    if self.user_companies.length > 1
       user_comp = self.user_companies.find{ |uc| uc.default }
       self.customer_id = user_comp.present? ? user_comp.company_id : nil
     elsif self.default_user_company.present?
