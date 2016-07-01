8738c00e4ff89d53c3ca431a1d7fdce0da57ac20
iswaryar <iswarya.r@freshdesk.com>
2016-07-01 11:20:05 +0530

    Fix for "While merging two secondary contacts with same work phone to a primary contact without work phone, work phone contact detail is deleted from only one secondary contact."
    1. sidekiq worker checks for only the first child contact that has value while moving values to the parent. altered it to fetch all child contacts(secondary contacts) with values, moved it to the primary contact and removing it from both the secondary contact..
    
    2. while merging phone numbers, same phone numbers should be allowed to be merged. Had done the same for twitter also.. but it is not necessary since anyway two contacts can't have the same twitter_id. So removed that part
    
    Signed-off-by: iswaryar <iswarya.r@freshdesk.com>

--- app/controllers/contact_merge_controller.rb
+++ app/controllers/contact_merge_controller.rb
@@ -39,7 +39,6 @@ class ContactMergeController < ApplicationController
         end
         target.mobile = "" if @source_user.mobile.eql? target.mobile
         target.phone = "" if @source_user.phone.eql? target.phone
-        target.twitter_id = "" if @source_user.twitter_id.eql? target.twitter_id
         target.deleted = true
         target.user_emails.update_all_with_publish({
                                                       :user_id => @source_user.id,
@@ -99,7 +98,6 @@ class ContactMergeController < ApplicationController
           :company => i.company_name,
           :user_emails => i.emails.join(","),
           :twitter => i.twitter_id.present?,
-          :twitter_id => i.twitter_id,
           :facebook => i.fb_profile_id.present?,
           :phone => i.phone.present?, 
           :phone_num => i.phone,
