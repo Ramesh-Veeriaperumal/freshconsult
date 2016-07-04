80cce638521b78de63c530cb9be96383e202169f
Shanmugapriya11 <mshanmugapriya11@gmail.com>
2016-07-04 17:41:47 +0530

    [#18159] - Old report problem create pdf typo fix

--- app/controllers/reports/agents_analysis_controller.rb
+++ app/controllers/reports/agents_analysis_controller.rb
@@ -51,7 +51,7 @@ class Reports::AgentsAnalysisController < ApplicationController
     render :pdf => @report_title,
       :layout => 'report/agent_analysis_pdf.html.erb', # uses views/layouts/pdf.haml
       :show_as_html => params[:debug].present?, # renders html version if you set debug=true in URL
-      :template => 'sections/generate_report_pdf'
+      :template => 'sections/generate_report_pdf',
       :page_size => "A3",                           
       :handlers => [:erb],
       :formats => [:html]
