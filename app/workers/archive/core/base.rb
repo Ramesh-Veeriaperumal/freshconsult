d7fcc8da65c46531495734ed6e8ce3e82423d3bc
venkatesanmurali <venkatesan.murali1985@gmail.com>
2016-07-06 16:25:34 +0530

    [#18240] manual publish to count cluster on archive.
    typo

--- app/workers/archive/core/base.rb
+++ app/workers/archive/core/base.rb
@@ -179,7 +179,7 @@ module Archive
           key = RabbitMq::Constants::RMQ_REPORTS_TICKET_KEY
         end
         ticket.manual_publish_to_rmq("update", key, {:manual_publish => true})
-        ticket.count_es_manual_publish("delete") #for count es, its a delete action and we ll remove document from count cluster.
+        ticket.count_es_manual_publish("destroy") #for count es, its a delete action and we ll remove document from count cluster.
         if archive_ticket_destroy(ticket)
           Helpdesk::ArchiveTicket.where(:id => archive_ticket.id, :account_id => archive_ticket.account_id, :progress => true).update_all(:progress => false)
           archive_ticket.sqs_manual_publish
