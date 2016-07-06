97d4abe0b0b50ad8ccc2d8aeafcf603b43888048
Balaji29 <balaji.rk@freshdesk.com>
2016-07-06 20:06:17 +0530

    review comment changes
    deleting element instance through sidekiq queue, removed unnecessary instance variable, changed portal_url to full_domain

--- lib/integrations/cloud_elements/crm/constant.rb
+++ lib/integrations/cloud_elements/crm/constant.rb
@@ -3,5 +3,44 @@ module Integrations::CloudElements
     MAPPING_ELEMENTS = [:salesforce_crm_sync]
     OAUTH_ELEMENTS = ["salesforce_crm_sync"]
     OAUTH_ERROR = "OAuth Token is nil"
+    SALESFORCE_CRM_SYNC = Integrations::CloudElements::Crm::SalesforceCrmSync::Constant::SALESFORCE_CRM_SYNC
+    SALESFORCE_CRM_SYNC_JSON = Integrations::CloudElements::Crm::SalesforceCrmSync::Constant::SALESFORCE_CRM_SYNC_JSON
+
+    FRESHDESK_JSON = {
+			"element" => { "key" => "freshdeskv2" },
+			"configuration" => {
+				"username" => "%{api_key}",
+				"subdomain" => "%{subdomain}",
+				"event.notification.enabled" => "false"
+			},
+			"tags" => [],
+			"name" => "%{fd_instance_name}"
+		}
+
+		INSTANCE_TRANSFORMATION_JSON = {
+	    "level" => "instance",
+	    "vendorName" => "%{object_name}",
+	    "fields" => [],
+	    "configuration" => [
+	      {
+	        "type" => "passThrough",
+	        "properties" => {
+	          "fromVendor" => true,
+	          "toVendor" => true
+	        }
+	     	}
+	    ]
+  	}
+
+  	FORMULA_INSTANCE_JSON = {
+	    "formula" => {"active" => true},
+	    "name" => "%{formula_instance}",
+	    "active" => "%{active}",
+	    "configuration" => {
+	        "element.source" => "%{source}", 
+	        "element.target" => "%{target}"
+	      }
+		}.to_json
+
   end
 end
