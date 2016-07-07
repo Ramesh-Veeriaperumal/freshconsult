34c13cf064dc01668cce64a3b10236ebeefbd254
Balaji29 <balaji.rk@freshdesk.com>
2016-07-07 16:26:50 +0530

    renaming methods dynamics

--- lib/integrations/cloud_elements/crm/dynamics_crm_sync/constant.rb
+++ lib/integrations/cloud_elements/crm/dynamics_crm_sync/constant.rb
@@ -4,7 +4,7 @@ module Integrations::CloudElements::Crm
 		DYNAMICS_CRM_SYNC = {
 			"objects"=>{"contact"=>"contact", "account"=>"account"}, 
 			"keys"=>["username", "password", "site_url"], 
-			"fd_delete_fields"=>["cf_dycontactid", "cf_dyaccountid"],
+			"fd_hide_fields"=>["cf_dycontactid", "cf_dyaccountid"],
 			"delete_fields"=>{"contact_fields"=>"attributes.new_fdcontact", "contact_fields_types"=>"FDCONTACT", "account_fields"=>"attributes.new_fdaccount", "account_fields_types"=>"FDACCOUNT"}, 
 			"existing_companies"=>[{"fd_field"=>"name", "sf_field"=>"attributes.name"}], 
 			"existing_contacts"=>[{"fd_field"=>"name", "sf_field"=>"attributes.fullname"}, {"fd_field"=>"email", "sf_field"=>"attributes.emailaddress1"}, {"fd_field"=>"mobile", "sf_field"=>"attributes.mobilephone"}, {"fd_field"=>"phone", "sf_field"=>"attributes.telephone1"}], 
