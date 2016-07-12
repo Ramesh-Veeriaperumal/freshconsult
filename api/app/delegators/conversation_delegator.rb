76453d8917598a968e47e89fb424d2e5372f61b5
Rashmi <rashmi@freshdesk.com>
2016-07-12 17:04:11 +0530

    Accept from_email in reply API
    Summary:
    Ref:  https://support.freshdesk.com/helpdesk/tickets/677528
    Signed-off-by: Rashmi <rashmi@freshdesk.com>
    
    Reviewers: sudhir, ganesh.r, seetharam.r
    
    Reviewed By: seetharam.r
    
    Differential Revision: http://wall.freshdesk.com/D825

--- api/app/delegators/conversation_delegator.rb
+++ api/app/delegators/conversation_delegator.rb
@@ -1,11 +1,25 @@
 class ConversationDelegator < BaseDelegator
-  validate :validate_agent_emails, if: -> { note? && attr_changed?('to_emails', schema_less_note) && to_emails.present? }
+
+  attr_accessor :email_config_id
+
+  validate :validate_agent_emails, if: -> { note? && to_emails.present? && attr_changed?('to_emails', schema_less_note)}
+
+  validate :validate_from_email, if: -> { email_conversation? && from_email.present? && attr_changed?('from_email', schema_less_note)}
 
   def validate_agent_emails
     invalid_emails = to_emails - Account.current.agents_from_cache.map { |x| x.user.email }
     unless invalid_emails.empty?
       errors[:notify_emails] << :invalid_agent_emails
       (self.error_options ||= {}).merge!(notify_emails: { invalid_emails: "#{invalid_emails.join(', ')}" })
+    end
+  end
+
+  def validate_from_email
+  	email_config = Account.current.email_configs.where(reply_email: from_email).first
+    if email_config
+      self.email_config_id = email_config.id
+    else
+      errors[:from_email] << :"can't be blank"
     end
   end
 end
