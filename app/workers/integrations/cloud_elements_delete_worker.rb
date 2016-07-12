5171b150a0185be13594dfef2f0df515e4469c00
Balaji29 <balaji.rk@freshdesk.com>
2016-07-12 11:44:48 +0530

    adding FreshdeskErrorMailers

--- app/workers/integrations/cloud_elements_delete_worker.rb
+++ app/workers/integrations/cloud_elements_delete_worker.rb
@@ -1,7 +1,6 @@
 module Integrations
   class CloudElementsDeleteWorker < ::BaseWorker
     include Sidekiq::Worker
-
     sidekiq_options :queue => :cloud_elements_delete, :retry => 0, :backtrace => true, :failures => :exhausted
     def perform options
       begin
@@ -12,14 +11,20 @@ module Integrations
         obj = IntegrationServices::Services::CloudElementsService.new(installed_app, {}, {:element_instance_id => options[:element_id]})
         obj.receive(:delete_element_instance)
       rescue IntegrationServices::Errors::TimeoutError => timeouterr
-        timeout_error = "Timeout error on CE elements delete, delete #{options[:element_id]} manually - error: #{timeouterr}"
+        timeout_error = "Timeout error on CE elements delete, delete element id #{options[:element_id]} manually - error: #{timeouterr}"
         Rails.logger.debug timeout_error
         NewRelic::Agent.notice_error(timeout_error)
+        FreshdeskErrorsMailer.error_email(nil, nil, timeouterr, {
+          :subject => timeout_error, :recipients => "integrations@freshesk.com"
+        })
         raise timeouterr
       rescue Exception => error
-        error_log = "error on CE elements delete, delete #{options[:element_id]} manually - error: #{error}"
+        error_log = "error on CE elements delete, delete element id #{options[:element_id]} manually - error: #{error}"
         Rails.logger.debug error_log
         NewRelic::Agent.notice_error(error_log)
+        FreshdeskErrorsMailer.error_email(nil, nil, error, {
+          :subject => error_log, :recipients => "integrations@freshesk.com"
+        })
         raise error
       end
     end
