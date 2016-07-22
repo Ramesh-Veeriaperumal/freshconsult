cce6a0b304098e56ed65ce845e9ad998194983fb
Sukanya V <sukanya@freshdesk.com>
2016-07-22 15:55:29 +0530

    [#18617] Facebook Realtime Messaging - Phase 1

--- vendor/gems/facebook/lib/facebook/oauth/constants.rb
+++ vendor/gems/facebook/lib/facebook/oauth/constants.rb
@@ -17,7 +17,9 @@ module Facebook
         "email"
       ]
 
-      FB_AUTH_DIALOG_URL   = "https://www.facebook.com/dialog/oauth"
+      FB_API_VERSION       = "v2.6"      
+      
+      FB_AUTH_DIALOG_URL   = "https://www.facebook.com/#{FB_API_VERSION}/dialog/oauth"
       
       PAGE_TAB_URL         = "https://www.facebook.com/dialog/pagetab"
 
