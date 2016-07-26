299bc34ac736915e69a3e112d93c2586aaf4fe02
Bhavya-Y <bhavya@freshdesk.com>
2016-07-26 09:07:01 +0530

    [#18703] changing webhook db size limit

--- db/migrate/20160711075200_create_account_webhook_keys.rb
+++ db/migrate/20160711075200_create_account_webhook_keys.rb
@@ -5,7 +5,7 @@ class CreateAccountWebhookKeys < ActiveRecord::Migration
   def self.up
   	create_table :account_webhook_keys do |t|
       t.integer :account_id, :limit => 8, :null => false
-      t.string  :webhook_key, :limit => 15
+      t.string  :webhook_key, :limit => 35
       t.integer :vendor_id, :limit => 11
       t.integer :status, :limit => 1
     end 
