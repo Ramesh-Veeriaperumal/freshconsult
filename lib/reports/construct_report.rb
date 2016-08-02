6299eec18e050d7fe96c41720a1e8fd6da5bb389
Shanmugapriya11 <mshanmugapriya11@gmail.com>
2016-08-02 12:32:30 +0530

    [18443] - Knocking Off old reports

--- lib/reports/construct_report.rb
+++ lib/reports/construct_report.rb
@@ -2,8 +2,8 @@ module Reports::ConstructReport
   
   def build_tkts_hash(val,params)
     @val = val
-    data = (parse_start_date == parse_end_date) ? 
-                     fetch_tkts_and_data : fetch_tkts_and_data_from_redshift
+    @from_db = (parse_start_date == parse_end_date)
+    data = @from_db ? fetch_tkts_and_data : fetch_tkts_and_data_from_redshift
     merge_hash(data)
   end
 
@@ -11,7 +11,11 @@ module Reports::ConstructReport
     data = {}
     data_hash.each do |rec|
       responder_hash = {}
-      responder = rec["#{@val}_id"].blank? ? "Unassigned" : rec["#{@val}_id"].to_i
+      responder = if @from_db
+                    rec["#{@val}_id"].blank? ? "Unassigned" : rec["#{@val}_id"].to_i
+                  else
+                    rec["#{@val == "group" ? "group_id" : "agent_id"}"].to_i
+                  end
       responder_hash.store(:tot_tkts,rec['resolved_tickets'])
       responder_hash.store(:average_first_response_time, rec['avgfirstresptime'])
       responder_hash.store(:fcr, rec['fcr_tickets'])
@@ -24,10 +28,34 @@ module Reports::ConstructReport
   end
 
   def fetch_tkts_and_data_from_redshift
-    r_db = Reports::RedshiftQueries.new({:start_time => start_date, :end_time => end_date})
-    options = {:select_cols => "#{r_db.summary_report_metrics}, #{@val}_id", 
-      :conditions => r_db.conditions ,:group_by => "#{@val}_id"}
-    r_db.execute(options)
+    s_date = Date.parse(start_date).strftime("%e %b, %Y")
+    e_date = Date.parse(end_date).strftime("%e %b, %Y")
+
+    params = {
+      model:      "TICKET",
+      metric:     "OLD_REPORT_GROUPBY",
+      group_by:   [@val == "group" ? "group_id" : "agent_id"],
+      date_range: "#{s_date} - #{e_date}",
+      filter:     [],
+      account_id: 1010003081,#Account.current.id,
+      account_domain: Account.current.full_domain,
+      time_zone: (Account.current.time_zone || "Pacific Time (US & Canada)")
+    }
+
+    req_params = [ { 'req_params' => params}] 
+    
+    begin
+      url = ReportsAppConfig::TICKET_REPORTS_URL
+      response = RestClient.post url, req_params.to_json, :content_type => :json, :accept => :json
+      res = JSON.parse(response.body)
+      res.first["result"]
+    rescue => e
+      [{"errors" => e.inspect}]
+    end
+    # r_db = Reports::RedshiftQueries.new({:start_time => start_date, :end_time => end_date})
+    # options = {:select_cols => "#{r_db.summary_report_metrics}, #{@val}_id", 
+    #   :conditions => r_db.conditions ,:group_by => "#{@val}_id"}
+    # r_db.execute(options)
   end
 
 
