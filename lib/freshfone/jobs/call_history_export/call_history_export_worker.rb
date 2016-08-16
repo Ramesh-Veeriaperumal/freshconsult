71a4d96d567cf46d75bd32a5dad36dbd9b02b3a1
lavenya <alavenya@freshdesk.com>
2016-08-16 13:03:29 +0530

    [#19222], [#19224], [#19225] - New notifications warm transfer cancel fix, call history export meta fix

--- lib/freshfone/jobs/call_history_export/call_history_export_worker.rb
+++ lib/freshfone/jobs/call_history_export/call_history_export_worker.rb
@@ -97,10 +97,9 @@ module Freshfone::Jobs
         def with_warm_transfer_hash activerecord_relation
           count = 0
           activerecord_relation.map { |phone_call|
-              unless phone_call.meta.warm_transfer_revert?
-                count += 1
-                field_hash.call(phone_call, count) 
-              end
+            next if phone_call.meta.present? && phone_call.meta.warm_transfer_revert?
+            count += 1
+            field_hash.call(phone_call, count)
           }
         end
 
