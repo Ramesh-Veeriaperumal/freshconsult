3c6140f4fb7727895100b532daa3f8f0a9b08fc7
Shreyas <shreyas.ns@freshdesk.com>
2016-08-23 12:02:03 +0530

    [#19400] phone number dropdown default value empty when number is deleted.
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>

--- public/javascripts/cti/screen_pop_form.js
+++ public/javascripts/cti/screen_pop_form.js
@@ -338,6 +338,9 @@ var ScreenPop;
           if(data.length > 0) {
             $("#cti_phone_numbers").select2("val", self.ctiCallNumber || self.ctiCallOldNumber || data[0].id);
           }
+          if (!jQuery("#cti_phone_numbers").select2("val")) {    //This is a fallback when a number gets deleted and redis entry is not removed.
+            $("#cti_phone_numbers").select2("val", data[0].id);
+          }
           if (!!data.length && !self.ctiCallNumber) {
             self.showPop();
           }
