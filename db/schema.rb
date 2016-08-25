0c610c952dc4bf6766f062a8473f0ec389fe65ba
Divya <divya@freshdesk.com>
2016-08-25 15:45:50 +0530

    Rename Migration file
    Signed-off-by: Divya <divya@freshdesk.com>

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 
-ActiveRecord::Schema.define(:version => 20160729085738) do
+ActiveRecord::Schema.define(:version => 20160825085738) do
   
   create_table "account_additional_settings", :force => true do |t|
     t.string   "email_cmds_delimeter"
