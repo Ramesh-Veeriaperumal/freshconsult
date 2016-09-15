2e4e85b7b7b53ad9d3f9f5ce3b3f9b344ac58da7
Satheesh-freshdesk <satheeshn@freshdesk.com>
2016-09-15 18:57:51 +0530

    [#19902] -Day view on last 30 days - scheduled ticket volume report

--- lib/helpdesk_reports/param_constructor/performance_distribution.rb
+++ lib/helpdesk_reports/param_constructor/performance_distribution.rb
@@ -1,17 +1,24 @@
 class HelpdeskReports::ParamConstructor::PerformanceDistribution < HelpdeskReports::ParamConstructor::Base
 
   METRICS_WITH_BUCKETS = {
-      "AVG_RESPONSE_TIME" => ["response_time"],
-      "AVG_FIRST_RESPONSE_TIME" => ["first_response_time"],
-      "AVG_RESOLUTION_TIME" => ["resolution_time"],
-    }
+    "AVG_RESPONSE_TIME" => ["response_time"],
+    "AVG_FIRST_RESPONSE_TIME" => ["first_response_time"],
+    "AVG_RESOLUTION_TIME" => ["resolution_time"],
+  }
 
   DAY_TOGGLE =['this_week','previous_week','last_7','last_30','this_month','previous_month']
-  
+
+  TREND_OPTION = { :doy => 'doy',
+                   :w   => 'w',
+                   :mon => 'mon',
+                   :qtr => 'qtr',
+                   :y   => 'y'
+                   }
+
   def initialize options
     @report_filter_params = options
     @report_type = :performance_distribution
-    @trend_conditions = ["doy", "w", "mon", "qtr", "y"]
+    @trend_conditions = TREND_OPTION.values
     super options
   end
 
@@ -22,21 +29,22 @@ class HelpdeskReports::ParamConstructor::PerformanceDistribution < HelpdeskRepor
   def query_params
     dr = @date_range.split("-")
     single_day_date_range = (dr.size == 1) || (dr[0].strip == dr[1].strip) #for one day time range, only bucket query is needed.
-    
-    unless single_day_date_range
-      if basic_param_structure[:scheduled_report]  
-        conditions = @report_filter_params[:date]["period"] || @report_filter_params[:date]["date_range"]
-        @trend_conditions =   case conditions
-                              when  *DAY_TOGGLE, 6, 29
-                                options[:resolution_trend] = options[:response_trend] = options[:trend] = "doy"
-                                ["doy","y"]
-                              else 
-                                options[:resolution_trend] = options[:response_trend] = options[:trend] = "mon"
-                                ["mon","y"]
-                              end  
-      end 
-    end
 
+    unless single_day_date_range
+      if basic_param_structure[:scheduled_report]
+        conditions = @report_filter_params[:date]["period"] || @report_filter_params[:date]["date_range"]
+
+        if ( DAY_TOGGLE.include?(conditions) || (1..31).include?(conditions) )
+          options[:resolution_trend] = options[:response_trend] = options[:trend] = TREND_OPTION[:doy]
+          @trend_conditions = [TREND_OPTION[:doy], TREND_OPTION[:y]]
+        else
+          options[:resolution_trend] = options[:response_trend] = options[:trend] = TREND_OPTION[:mon]
+          @trend_conditions = [TREND_OPTION[:mon], TREND_OPTION[:y]]
+        end
+
+      end
+    end
+    
     METRICS_WITH_BUCKETS.keys.inject([]) do |params, metric|
       query = basic_param_structure
       unless single_day_date_range
