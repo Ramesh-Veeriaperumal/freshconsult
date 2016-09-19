bd5b0e847bd75b19a1e8b7fad9e6255dc3e20ff9
Rohit Agarwal <rohitagarwal@freshdesk.com>
2016-09-19 13:58:51 +0530

    [#20134] Tooltips for archive tkts cust pages

--- app/helpers/contacts_companies_helper.rb
+++ app/helpers/contacts_companies_helper.rb
@@ -19,7 +19,7 @@ module ContactsCompaniesHelper
                 :ticket_url => ticket_url(ticket),
                 :ticket_subject => h("#{ticket.subject} ##{ticket.display_id}"),
                 :'data-pjax' => "#body-container",
-                :ticket_desc => get_ticket_desc(ticket)).html_safe
+                :ticket_desc => h(truncate(ticket_desc(ticket), { :length => 350, :omission => "..." }))).html_safe
     text_wrapper = content_tag(:p, text, :class => "break-word timeline-head #{grey_class}")
 
     sentence_type = user_page ? "user_ticket_timeinfo" : "company_ticket_timeinfo"
@@ -34,8 +34,14 @@ module ContactsCompaniesHelper
     (icon_wrapper + text_wrapper + time_div)
   end
 
-  def get_ticket_desc(ticket)
-    ticket.class.eql?(Helpdesk::Ticket)?h(truncate(ticket.ticket_body.description, { :length => 350, :omission => "..." })):""
+  def ticket_desc(ticket)
+    if ticket.class.eql?(Helpdesk::Ticket)
+      ticket.ticket_body.description
+    elsif ticket.class.eql?(Helpdesk::ArchiveTicket)
+      ticket.description
+    else
+      ""
+    end
   end
 
   def ticket_url(ticket)
