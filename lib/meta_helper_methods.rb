86263e8202d776252fa7af276e9b9cdb411a3bfa
Aniket <anikets@freshdesk.com>
2016-10-11 12:11:23 +0530

    [#20496] - Squashed commit of the following:
    commit 372ed9d0fea873698c7702b59de56d178daa939d
    Merge: 863b363 a1187c0
    Author: Aniket <anikets@freshdesk.com>
    Date:   Tue Oct 11 12:10:31 2016 +0530
    
        Merge remote-tracking branch 'origin/prestaging' into swat-meta-chat-ticket
    
    commit 863b363ab94d83584cd9966bd2ce1652d242d611
    Author: Aniket <anikets@freshdesk.com>
    Date:   Tue Oct 11 12:07:35 2016 +0530
    
        Updated sprite icons
    
    commit 723dcad46699804bb24f8badf9a673246035d578
    Author: Aniket <anikets@freshdesk.com>
    Date:   Thu Sep 22 15:29:00 2016 +0530
    
        Add meta data to chat tickets, fix osx icon for meta, add new icons for meta
    
        Reviewers: parsu, rajasekar.m, arvind, mehul, rohitagarwal, girish.k
    
        Differential Revision: http://wall.freshdesk.com/D1648

--- lib/meta_helper_methods.rb
+++ lib/meta_helper_methods.rb
@@ -14,7 +14,10 @@ module MetaHelperMethods
     content = []
     content << "<div class='meta-info'>"
     content << "#{user_agent_browser(meta['user_agent'])} #{user_agent_os(meta['user_agent'])}" if meta['user_agent'].present?
+    content << meta_visitor_os_element(meta['visitor_os']) if meta['visitor_os'].present? && ticket.chat?
     content << meta_referrer_element(meta['referrer']) if meta['referrer'].present?
+    content << meta_ip_address_element(meta['ip_address']) if meta['ip_address'].present? && ticket.chat?
+    content << meta_location_element(meta['location']) if meta['location'].present? && ticket.chat?
     content << meta.body[1..-3].gsub('"',' ').gsub(",","<br /> ") if ticket.mobihelp?
     content << "</div>"
     content_tag(:span, t("helpdesk.tickets.show.meta"), 
@@ -59,8 +62,8 @@ module MetaHelperMethods
   end
 
   def os_shorthand(os)
-    ['windows', 'mac', 'linux', 'android' ].each do |v|
-      return v if os.downcase.include?(v)
+    ['windows', 'mac', 'linux', 'android', 'osx' ].each do |v|
+      return v if os.downcase.gsub!(/\s+/, '').include?(v)
     end
   end
   
@@ -84,6 +87,36 @@ module MetaHelperMethods
     rescue
       truncate(url, :length => 20)
     end
+  end
+
+  def meta_visitor_os_element(visitor_os)
+    %(<div>
+        <span class="meta-os os-#{os_shorthand(visitor_os)}" title="OS">OS</span>
+        <label>
+          #{visitor_os}
+        </label>
+      </div>
+    )
+  end
+
+  def meta_ip_address_element(ip_address)
+    %(<div>
+        <span class="meta-ip-address tooltip" title="IP Address">IP Address</span>
+        <label>
+          #{ip_address}
+        </label>
+      </div>
+    )
+  end
+
+  def meta_location_element(location)
+    %(<div>
+        <span class="meta-location tooltip" title="Location">Location</span>
+        <label>
+          #{location}
+        </label>
+      </div>
+    )
   end
 
 
