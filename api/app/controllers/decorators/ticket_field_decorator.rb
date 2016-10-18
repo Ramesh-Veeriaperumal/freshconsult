b3ae6ebd42d34def8b291a3cfa7428dae4810bfe
Thanashyam Raj <thanashyam@gmail.com>
2016-10-18 20:37:04 +0530

    Structural change in choices for TicketFields API
    Showing them as  label and value instead of id, value
    
    Signed-off-by: Thanashyam Raj <thanashyam@gmail.com>

--- api/app/controllers/decorators/ticket_field_decorator.rb
+++ api/app/controllers/decorators/ticket_field_decorator.rb
@@ -3,6 +3,7 @@ class TicketFieldDecorator < ApiDecorator
            :field_type, :required, :required_in_portal, :label_in_portal, :editable_in_portal, :visible_in_portal,
            :level, :ticket_field_id, :picklist_values, to: :record
 
+  DEFAULT_FIELDS = %w(default_agent default_priority default_source default_status default_ticket_type default_group default_product).freeze
   def portal_cc
     record.field_options.try(:[], 'portalcc')
   end
@@ -67,25 +68,15 @@ class TicketFieldDecorator < ApiDecorator
   end
 
   def ticket_field_choices_by_id
+    # Many of these are fetched from cache, which will not work properly in production
+    # TODO-EMBER Fix caching issue
     @choices_by_id ||= case record.field_type
       when 'custom_dropdown'
         choices_by_id(Hash[record.picklist_values.map { |pv| [pv.id, pv.value] }])
-      when 'default_priority'
-        choices_by_id(TicketConstants.priority_list)
-      when 'default_source'
-        choices_by_id(TicketConstants.source_list)
       when 'nested_field'
         nested_field_choices_by_id(record.picklist_values)
-      when 'default_status'
-        Helpdesk::TicketStatus.statuses_list(Account.current)
-      when 'default_ticket_type'
-        choices_by_id(Hash[Account.current.ticket_types_from_cache.map { |tt| [tt.id, tt.value] }])
-      when 'default_agent'
-        choices_by_id(Hash[Account.current.agents_details_from_cache.map { |a| [a.id, a.name] }])
-      when 'default_group'
-        choices_by_id(Hash[Account.current.groups_from_cache.map { |g| [g.id, CGI.escapeHTML(g.name)] }])
-      when 'default_product'
-        choices_by_id(Hash[Account.current.products_from_cache.map { |p| [p.id, CGI.escapeHTML(p.name)] }])
+      when *DEFAULT_FIELDS
+        send(:"#{record.field_type}_choices")
       else
         []
     end
@@ -96,20 +87,76 @@ class TicketFieldDecorator < ApiDecorator
     def nested_field_choices_by_id(pvs)
       pvs.collect { |c| 
         {
-          :id => c.id,
-          :value => c.value,
-          :choices => nested_field_choices_by_id(c.sub_picklist_values)
+          label: c.value,
+          value: c.value,
+          choices: nested_field_choices_by_id(c.sub_picklist_values)
         }
       }
     end
 
     def choices_by_id(list)
-      list.map { |k, v|
+      list.map do |k, v|
         {
-          :id => k,
-          :value => v
+          label: v,
+          value: v
         }
-      }
+      end
+    end
+    
+    def choices_by_name_id(list)
+      list.map do |item|
+        {
+          label: item.name,
+          value: item.id
+        }
+      end
+    end
+    
+    def default_priority_choices
+      TicketConstants.priority_list.map do |k, v|
+        {
+          label: v,
+          value: k
+        }
+      end
+    end
+    
+    def default_source_choices
+      TicketConstants.source_names.map do |k, v|
+        {
+          label: k,
+          value: v
+        }
+      end
+    end
+    
+    def default_status_choices
+      # TODO-EMBER This is a cached method. Not expected work properly in production
+      Helpdesk::TicketStatus.statuses_list(Account.current).map do |status|
+        status.slice(:customer_display_name, :stop_sla_timer, :deleted).merge({
+          label: status[:name],
+          value: status[:status_id]
+          })
+      end
+    end
+    
+    def default_agent_choices
+      choices_by_name_id Account.current.agents_details_from_cache
+    end
+    
+    def default_ticket_type_choices
+      Account.current.ticket_types_from_cache.map do |type|
+        {
+          label: type.value,
+          value: type.value
+        }
+      end
+    end
+    
+    [:group, :product].each do |field_name|
+      define_method "default_#{field_name}_choices" do
+        choices_by_name_id Account.current.send(:"#{field_name.to_s.pluralize}_from_cache")
+      end
     end
 
 end
