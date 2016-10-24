7d37bfa2cc0de3132e664d7c0a510c84082df789
siwalik <siwalik@freshdesk.com>
2016-10-24 14:48:53 +0530

    [#20910] [#20930] Datepicker Issue Fix
    Fix for datepicker translation bug

--- lib/account_constants.rb
+++ lib/account_constants.rb
@@ -10,12 +10,14 @@ module AccountConstants
     :us => {
       :short_day => "%b %-d %Y",
       :short_day_separated => "%b %-d, %Y",
+      :date_iso_format => "%G-%m-%d",
       :short_day_with_week => "%a, %b %-d, %Y",
       :short_day_with_time => "%a, %b %-d, %Y at %l:%M %p",
     },
     :non_us => {
       :short_day => "%-d %b %Y",
       :short_day_separated => "%-d %b, %Y",
+      :date_iso_format => "%G-%m-%d",
       :short_day_with_week => "%a, %-d %b, %Y",
       :short_day_with_time => "%a, %-d %b, %Y at %l:%M %p",
     }
