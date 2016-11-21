17415033b4541052142945fd4cbf3d42237f216f
SaravanaKumar <saravana@freshdesk.com>
2016-11-21 18:56:52 +0530

    Revert "[21544] Emails sent via inappropriate email config while mailbox jobs moved to sidekiq"
    This reverts commit 6ab6ce445eaaabdc018783a453ddbb0eef62cec7.

--- app/models/draft_mailer.rb
+++ app/models/draft_mailer.rb
@@ -12,37 +12,34 @@ class DraftMailer < ActionMailer::Base
 
   def discard_email(draft, article, current_author, current_user, portal)
     mail_config = portal.primary_email_config || current_user.account.primary_email_config
-    begin
-      configure_email_config mail_config
+    self.class.set_email_config mail_config
 
-      headers = {
-        :to        => current_author.email,
-        :from      => current_user.email,
-        :subject   => "[Draft Discarded] #{draft[:title]}",
-        :sent_on   => Time.now
-      }
+    headers = {
+      :to        => current_author.email,
+      :from      => current_user.email,
+      :subject   => "[Draft Discarded] #{draft[:title]}",
+      :sent_on   => Time.now
+    }
 
-      headers.merge!(make_header(nil, nil, current_user.account_id, "Discard Email"))
-      @draft = draft
-      @article = article
-      @current_author = current_author
-      @user = current_user
-      @host = portal.host
-      @body_html = generate_body_html(draft[:description])
+    headers.merge!(make_header(nil, nil, current_user.account_id, "Discard Email"))
+    @draft = draft
+    @article = article
+    @current_author = current_author
+    @user = current_user
+    @host = portal.host
+    @body_html = generate_body_html(draft[:description])
 
-      mail(headers) do |part|
-        part.text { render "mailer/draft/draft_discard_email.text.plain.erb" }
-        part.html do 
-          Premailer.new(
-            render("mailer/draft/draft_discard_email.text.html.erb"), 
-            :with_html_string => true, 
-            :input_encoding => 'UTF-8'
-          ).to_inline_css 
-        end
-      end.deliver
-    ensure
-      remove_email_config
-    end
+    mail(headers) do |part|
+      part.text { render "mailer/draft/draft_discard_email.text.plain.erb" }
+      part.html do 
+        Premailer.new(
+          render("mailer/draft/draft_discard_email.text.html.erb"), 
+          :with_html_string => true, 
+          :input_encoding => 'UTF-8'
+        ).to_inline_css 
+      end
+    end.deliver
+
   end
 
 end
