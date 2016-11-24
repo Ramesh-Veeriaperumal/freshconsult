17b0cf8677a782fef0658384241a52f83e1fc92b
venkatesanmurali <venkatesan.murali1985@gmail.com>
2016-11-24 10:33:21 +0530

    [#21730]disable UTF-8 memcache enforcement
    this has no effect until environment variable is set.
    On behalf of Suresh from devops.
    
    No testing required if env variable isnt set

--- lib/helpdesk/initializers/memcached.rb
+++ lib/helpdesk/initializers/memcached.rb
@@ -1,7 +1,31 @@
+# In lib/helpdesk/initializers/ruby193_monkey_patches.rb, we are monkey
+# patching Marshal.load() method to enforce utf8 encoding for string objects
+# (even when they are already utf8). That was needed during ruby 1.8 to 1.9
+# transition and is likely not needed any more. Leaving that imposes huge
+# perf penalty for all memcache calls (2x slowdown), so we undo the monkey
+# patch here.
+if ENV['DISABLE_MEMCACHE_UTF8_ENFORCEMENT'] == 'true'
+  module MarshalOrg
+    include Marshal
+    class << self
+      def load(obj, other_proc=nil)
+        Marshal.load_without_utf8_enforcement(obj, other_proc)
+      end
+
+      def dump(obj)
+        Marshal.dump(obj)
+      end
+    end
+  end
+  memcacheserializer = MarshalOrg
+else
+  memcacheserializer = Marshal
+end
+
 # The below config is used for caching the app data. (Groups, Agent, TicketFields)
 config = YAML::load_file(File.join(Rails.root, 'config', 'dalli.yml'))[Rails.env].symbolize_keys!
 servers = config.delete(:servers)
-options = { :namespace => config[:namespace], :compress => config[:compress], :socket_max_failures => config[:socket_max_failures], :socket_timeout => config[:socket_timeout] }
+options = { :namespace => config[:namespace], :compress => config[:compress], :socket_max_failures => config[:socket_max_failures], :socket_timeout => config[:socket_timeout], :serializer => memcacheserializer }
 $memcache = Dalli::Client.new(servers, options)
 
 # The below config is used for controller caching(page, action, fragment) offered by rails
