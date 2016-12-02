1fb92379fc36712fd5656493dd391b9f8abf2598
Kiruba <kiruba@freshdesk.com>
2016-12-02 15:07:29 +0530

    #[20356] Quoted-Printable transfer encoding attachment fix

--- lib/helpdesk/email_parser/processed_attachment.rb
+++ lib/helpdesk/email_parser/processed_attachment.rb
@@ -37,6 +37,8 @@ class Helpdesk::EmailParser::ProcessedAttachment
       		lines = /begin \d\d\d (.*)\r\nend/m.match(part.raw_source)[1].split /[\r\n]+/
       		encoded = lines[1..-2].join("\n")
       		attachment = Helpdesk::EmailParser::MailAttachment.new(encoded.unpack("u")[0])
+      	elsif(part.content_transfer_encoding.upcase == "QUOTED-PRINTABLE")
+      		attachment = Helpdesk::EmailParser::MailAttachment.new(part.decoded.unpack("M")[0])
     	else
       		attachment = Helpdesk::EmailParser::MailAttachment.new(part.decoded)
     	end
