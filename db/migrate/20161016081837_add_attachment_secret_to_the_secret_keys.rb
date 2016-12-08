5301854bb2e46896ad46a492c00ddc53180c46e4
Sravya <sravya@freshdesk.com>
2016-12-08 13:11:25 +0530

    [#20668] Change in migration file
    Signed-off-by: Sravya <sravya@freshdesk.com>

--- db/migrate/20161016081837_add_attachment_secret_to_the_secret_keys.rb
+++ db/migrate/20161016081837_add_attachment_secret_to_the_secret_keys.rb
@@ -4,17 +4,18 @@ class AddAttachmentSecretToTheSecretKeys < ActiveRecord::Migration
   def self.up
     failed_accounts = []
     Sharding.run_on_all_shards do
-      AccountAdditionalSettings.find_each(:batch_size => 300) do |account_additional_settings|
-        account_additional_settings.account.make_current
+      Account.find_each(:batch_size => 300) do |account|
+        account.make_current
+        account_additional_settings = account.account_additional_settings
         begin
-          if account_additional_settings.secret_keys.nil?
+          if account_additional_settings.secret_keys.blank?
             account_additional_settings.secret_keys = { :attachment_secret => SecureToken.generate }
           else
             account_additional_settings.secret_keys[:attachment_secret] = SecureToken.generate
           end
           account_additional_settings.save!
         rescue => e
-          failed_accounts << "#{account_additional_settings.account_id} => #{e.message}"
+          failed_accounts << "#{account.id} => #{e.message}"
         end
       end
     end
