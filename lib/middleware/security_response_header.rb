03ef569ada41bceb722c5c7e9d932831d2a59e47
krishnanandnb <krishnanand@freshdesk.com>
2016-12-21 12:41:44 +0530

    12655 -adding missing security headers

--- lib/middleware/security_response_header.rb
+++ lib/middleware/security_response_header.rb
@@ -1,22 +1,17 @@
-
-require 'rack/request'
-
 class Middleware::SecurityResponseHeader
-  def initialize(app)
-    @app = app
-  end
+ def initialize(app)
+   @app = app
+ end
 
-  def call(env)
-    request = Rack::Request.new(env)
-    resource = env['PATH_INFO']
-    status, headers, body      = @app.call(request.env)
-     headers["X-XSS-Protection"] = "1; mode=block"
-     headers["X-Content-Type-Options"]  = "nosniff"
-     headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
-
-    if resource.include? 'login'
-       headers["Content-Security-Policy"] = "frame-ancestors 'none'"
-    end
-    [status, headers, body]
-  end
+ def call(env)
+   req_path = env['PATH_INFO']
+   status, headers, response    = @app.call(env)
+   headers["X-XSS-Protection"] = "1; mode=block"
+   headers["X-Content-Type-Options"]  = "nosniff"
+   headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+   if req_path.include? 'login'
+        headers["Content-Security-Policy"] = "frame-ancestors 'none'"
+   end
+  [status, headers, response]
+ end
 end
