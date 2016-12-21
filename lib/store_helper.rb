7fa598391a8f26af33aa7600f1e47ab54ef410ed
ragav0102 <ragavendren@freshdesk.com>
2016-12-21 15:41:11 +0530

    [#22171] The agent name is not searched properly , when 2 charecters are typed in the agents field inside add note.
     The agent name is not searched properly , when 2 charecters are typed in the agents field inside add note.

--- lib/store_helper.rb
+++ lib/store_helper.rb
@@ -1,8 +1,8 @@
 module StoreHelper # Datas to be exposed to clientside
   
   def get_store_data
-    @agents_list ||= current_account.agents_details_from_cache
-    @agents_list.inject([]) do |res,agent|
+    agentslist = current_account.agents_details_from_cache
+    @agents_list ||= agentslist.inject([]) do |res,agent|
       res << {:id => agent.id, :name => agent.name, :is_account_admin => agent.email == current_account.admin_email}
     end
 
@@ -16,6 +16,6 @@ module StoreHelper # Datas to be exposed to clientside
 
     @features_list ||= current_account.features_list
 
-    {:current_user => current_user, :agent => @agents_list, :group => @groups_list, :product => @products_list, :features_list => @features_list, :agents_count => @agents_list.count}.to_json
+    {:current_user => current_user, :agent => @agents_list, :group => @groups_list, :product => @products_list, :features_list => @features_list, :agents_count => agentslist.count}.to_json
   end
 end
