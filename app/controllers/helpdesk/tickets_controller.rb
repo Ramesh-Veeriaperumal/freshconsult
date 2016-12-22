0582d7b107df5f303981e646ce9d6bd6eb74d445
shreeranjni <shreeranjni@freshdesk.com>
2016-12-22 18:37:13 +0530

    Bug ID [#22223] - Sentiment Performance Change

--- app/controllers/helpdesk/tickets_controller.rb
+++ app/controllers/helpdesk/tickets_controller.rb
@@ -207,8 +207,8 @@ class Helpdesk::TicketsController < ApplicationController
       ticket_ids =  @items.map(&:id)
 
       sentiment_sql_array = ["select notable_id,int_nc04 from helpdesk_notes n inner join helpdesk_schema_less_notes sn
-                    on n.id=sn.note_id where n.account_id = %s and n.notable_id in (%s) 
-                    and sn.int_nc04 is not null 
+                    on n.id=sn.note_id and n.account_id=sn.account_id 
+                    where n.account_id = %s and n.notable_id in (%s) and sn.int_nc04 is not null 
                     order by n.created_at;",
                     Account.current.id, ticket_ids.join(',')]
       
