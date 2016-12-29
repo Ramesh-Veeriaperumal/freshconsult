e7d458d7eca95b2339b6d5da08f0e49f653968c8
Tharakai <tharakai@freshdesk.com>
2016-12-29 21:18:21 +0530

    [#22152]_fix_for_resqueue_failure
    Reverted the code for the bug 22152 as it caused resqueue failure in production

--- app/observers/sitemap_observer.rb
+++ app/observers/sitemap_observer.rb
@@ -1,7 +1,7 @@
 class SitemapObserver < ActiveRecord::Observer
 
   observe ForumCategory, Forum, Topic, Post, Solution::CategoryMeta, Solution::Category, Solution::FolderMeta, 
-  Solution::Folder, Solution::ArticleMeta, Solution::Article, PortalSolutionCategory, PortalForumCategory, Features::Feature
+  Solution::Folder, Solution::ArticleMeta, Solution::Article, PortalSolutionCategory, PortalForumCategory
 
   include Redis::RedisKeys
   include Redis::PortalRedis
@@ -15,15 +15,9 @@ class SitemapObserver < ActiveRecord::Observer
   end
 
   def set_sitemap_key(item)
-    return unless (item.account.features_included?(:sitemap) && sitemap_trigger?(item))
+    return unless item.account.features_included?(:sitemap)
     key = SITEMAP_OUTDATED % {:account_id => item.account.id}
     set_portal_redis_key(key, 1)
-  end
- 
-  def sitemap_trigger?(item)
-    return true unless item.class.name.include?("Feature") #Needed to check only for Feature class
-    sitemap_triggers = [:open_solutions, :open_forums, :hide_portal_forums].map { |f| "#{f.to_s.camelize}Feature" }
-    sitemap_triggers.include?(item.type)
   end
 
 end
