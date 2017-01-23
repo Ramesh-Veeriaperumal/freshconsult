64e017d95ce4f21feb39d1dd1330353c35051309
Abinaya K S <abinaya@freshdesk.com>
2017-01-23 15:20:05 +0530

    Changes from review - Bootstrap and agent hash modifications

--- api/app/controllers/ember/agents_controller.rb
+++ api/app/controllers/ember/agents_controller.rb
@@ -1,5 +1,10 @@
 module Ember
   class AgentsController < ApiAgentsController
     decorate_views(decorate_object: [:show, :me], decorate_objects: [:index])
+
+    def index
+      super
+      response.api_meta = { count: @items_count }
+    end
   end
 end
