298c0748c63dc2f839c7e91b6f1e80f60097081c
vishnubalajikm <vishnubalaji.km@gmail.com>
2017-01-27 15:16:58 +0530

    [FDCORE-727] Table background issue

--- lib/sanitize/config/image_relaxed.rb
+++ lib/sanitize/config/image_relaxed.rb
@@ -1,16 +1,16 @@
 #--
 # Copyright (c) 2011 Ryan Grove <ryan@wonko.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -41,9 +41,11 @@ class Sanitize
         'ins'        => ['cite', 'datetime'],
         'ol'         => ['start', 'reversed', 'type'],
         'q'          => ['cite'],
-        'table'      => ['summary', 'width', 'border', 'cellspacing', 'cellpadding'],
-        'td'         => ['abbr', 'axis', 'colspan', 'rowspan', 'width'],
-        'th'         => ['abbr', 'axis', 'colspan', 'rowspan', 'scope', 'width'],
+        'table'      => ['summary', 'width', 'border', 'cellspacing', 'cellpadding', 'bgcolor'],
+        'td'         => ['abbr', 'axis', 'colspan', 'rowspan', 'width', 'bgcolor'],
+        'th'         => ['abbr', 'axis', 'colspan', 'rowspan', 'scope', 'width', 'bgcolor'],
+        'tr'         => ['bgcolor'],
+        'tbody'      => ['bgcolor'],
         'time'       => ['datetime', 'pubdate'],
         'ul'         => ['type'],
         'div'        => ['class'],
