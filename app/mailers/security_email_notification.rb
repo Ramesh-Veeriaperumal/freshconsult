efe370944dafaa2f3387a1f8fcf55c5ac9cbf1a8
krishnanandnb <krishnanand@freshdesk.com>
2017-02-21 17:57:12 +0530

    FDCORE-1121 Security: When an agent's primary email address is changed
    Adding additional changes . If an agent changes his own email that will be send as an alert him

--- app/mailers/security_email_notification.rb
+++ app/mailers/security_email_notification.rb
@@ -38,14 +38,14 @@ class SecurityEmailNotification < ActionMailer::Base
     end
 
   end
-  def agent_email_change(model,to, subject, changed_attributes,doer)
+  def agent_email_change(model,to, subject, changed_attributes,doer,mail_template)
     begin
       # sending this email via account's primary email config so that if the customer wants this emails
       # to be sent via custom mail server, simply switching the primary email config will do
       email_config = Account.current.primary_email_config
       configure_email_config email_config
       Time.zone = model.time_zone
-      agent_headers = {
+      headers = {
         :to    => to,
         :from  => Account.current.default_friendly_email,
         :subject => subject,
@@ -54,34 +54,17 @@ class SecurityEmailNotification < ActionMailer::Base
         "Auto-Submitted" => "auto-generated",
         "X-Auto-Response-Suppress" => "DR, RN, OOF, AutoReply"
       }
-      admin_headers = {
-        :to    => doer.email,
-        :from  => Account.current.default_friendly_email,
-        :subject => subject,
-        :sent_on => Time.now,
-        "Reply-to" => "",
-        "Auto-Submitted" => "auto-generated",
-        "X-Auto-Response-Suppress" => "DR, RN, OOF, AutoReply"
-      }
-
-      account_id = -1
-      account_id = Account.current.id if Account.current
-      agent_headers.merge!(make_header(nil, nil, account_id, "Agent Email Change"))
-      agent_headers.merge!({"X-FD-Email-Category" => email_config.category}) if email_config.category.present?
-      admin_headers.merge!(make_header(nil, nil, account_id, "Agent Email Change"))
-      admin_headers.merge!({"X-FD-Email-Category" => email_config.category}) if email_config.category.present?
+      account_id = Account.current.present? ? Account.current.id : -1
+      headers.merge!(make_header(nil, nil, account_id, "Agent Email Change"))
+      headers.merge!({"X-FD-Email-Category" => email_config.category}) if email_config.category.present?
       @changes = changed_attributes
       @time = Time.zone.now.strftime('%B %e at %l:%M %p %Z')
       @model = model
       @account = Account.current
       @doer =doer
-      mail(admin_headers) do | part|
-        part.text { render "admin_alert_email_change.text.html.erb" }
-        part.html { render "admin_alert_email_change.text.html.erb" }
-      end.deliver
-      mail(agent_headers) do | part|
-        part.text { render "agent_email_change.text.plain.erb" }
-        part.html { render "agent_email_change.text.html.erb" }
+      mail(headers) do | part|
+        part.text { render "#{mail_template}.text.plain.erb" }
+        part.html { render "#{mail_template}.text.html.erb" }
       end.deliver
     ensure
       remove_email_config
