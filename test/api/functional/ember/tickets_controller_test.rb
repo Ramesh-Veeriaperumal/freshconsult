59e9d16317e38ca726cafdb201a4cc950f2863c0
K Sashi Kumar <ksashikumark93@gmail.com>
2017-02-28 17:27:57 +0530

    FDCORE-1273 - Fix monitored_by filter in tickets index API (#85)

--- test/api/functional/ember/tickets_controller_test.rb
+++ test/api/functional/ember/tickets_controller_test.rb
@@ -114,7 +114,7 @@ module Ember
     def test_index_with_invalid_filter_names
       get :index, controller_params(version: 'private', filter: Faker::Lorem.word)
       assert_response 400
-      valid_filters = ["spam", "deleted", "overdue", "pending", "open", "due_today", "new", "new_and_my_open", "all_tickets", "unresolved", "article_feedback", "my_article_feedback", "watching", "on_hold", "raised_by_me"]
+      valid_filters = ["spam", "deleted", "overdue", "pending", "open", "due_today", "new", "monitored_by", "new_and_my_open", "all_tickets", "unresolved", "article_feedback", "my_article_feedback", "watching", "on_hold", "raised_by_me"]
       match_json([bad_request_error_pattern(:filter, :not_included, list: valid_filters.join(', '))])
     end
 
