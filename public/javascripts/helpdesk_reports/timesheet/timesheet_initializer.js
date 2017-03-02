6ce4553cd1c77edbbe56f7c74691a488ec637fda
fdsedhu <sedhuraja@freshdesk.com>
2017-03-02 15:00:23 +0530

    [22839] Timesheet Deleted Customer Fix

--- public/javascripts/helpdesk_reports/timesheet/timesheet_initializer.js
+++ public/javascripts/helpdesk_reports/timesheet/timesheet_initializer.js
@@ -81,9 +81,13 @@ Helpkit.TimesheetInitializer = (function () {
 									      	row['status_name'] = el["status_name"];
 									      	row['group_by_day_criteria'] = (new moment(el["executed_at"])).format("ddd, Do MMM,YYYY");
 									      	row['group_name'] = el["group_name"] != null ? el["group_name"] : '-';
+									      	row['group_id'] = el["group_id"] !=null ? el["group_id"] : -1;
+									      	row["user_id"] = el["user_id"] != null ? el["user_id"] : -1;
 									      	row['hours'] = self.hour_markup(row);
 									      	row['ticket'] = el['subject'];
-									      	row['product_name'] = el['product_id'] != null ? el['product']['name'] : '-';
+									      	row['product_id'] = el['product_id'] !=null ? el["product_id"] : -1;;
+									      	row['customer_id'] = el['customer_id'] != null ? el['customer_id'] : -1;
+									      	row['product_name'] = el['product_id'] != null ? el['product_name'] : '-';
 									      	var note = el['note'];
 									      	if(note != null && note.length > 73) {
 									      		note = note.substr(0,73) + '...'
@@ -115,36 +119,20 @@ Helpkit.TimesheetInitializer = (function () {
 			 			var current_group_by = Helpkit.locals.current_group_by == undefined ? "customer_name" : Helpkit.locals.current_group_by;
 			 			var group_count = Helpkit.locals.pagination['group_count'];
 			 			var group_names  = Helpkit.locals.pagination['group_names'];
+			            api.column(self.group_columns[current_group_by], {page:'current'} ).nodes().each( function ( td, i ) {
+			            	
+			            	var group_id,group_name;
+			            	var group_name = jQuery(td).html();
+			            	var row = jQuery(rows).eq(i);
 
-			            api.column(self.group_columns[current_group_by], {page:'current'} ).data().each( function ( group, i ) {
+			            	if(group_name == '-') {
+			                	group_id = 0;
+		                	} else {
+		                		group_id = row.attr('data-groupby-id')
+		                	}
 
-			                if ( last !== group ) {
-			                	var group_id;
-			                	var row = jQuery(rows).eq(i);
-
-			                	if(group == '-') {
-			                		group_id = 0;
-			                	} else {
-			                		jQuery.each(_.keys(group_names),function(idx,el) {
-					            		// For date and ticket title, we format before putting it to table
-					            		// So we apply same formatting and validate
-					            		if(current_group_by == "group_by_day_criteria") {
-					            			 var group_name  = el;
-					            			 //converting to date format
-					            			 var is_same = moment(group_name).isSame(row.attr('data-executed-at'),'day');
-					            			 if(is_same){
-					            			 	group_id = group_name;
-					            			 }
-					            		} else if( current_group_by == "workable") {
-					            			//ticket id will be fetched from dom
-					            		} else {
-					            			if(group_names[el] == group){
-						            			group_id = el;
-						            		}
-					            		}
-				            		});
-			                	}
-
+			                if ( last !== group_id ) {
+			                	
 			                	if(current_group_by == "workable") {
 			                		var fr_group_count = group_count[row.attr('data-workable-id')];
 			                		var mkup = '<tr class="group" data-group="' + row.attr('data-workable-desc') +'"><td colspan="' + (Helpkit.locals.colspan + 1) +'" >'+'</td><td class="hours"><strong>'+ fr_group_count +'</strong></td></tr>';
@@ -152,22 +140,34 @@ Helpkit.TimesheetInitializer = (function () {
 			                	} else {
 			                		var fr_group_count = group_count[group_id];
 			                		jQuery(rows).eq( i ).before(
-			                      	  '<tr class="group" data-group="' + group +'"><td colspan="' + (Helpkit.locals.colspan) +'" >'+'</td><td class="hours"><strong>'+ fr_group_count +'</strong></td></tr>'
+			                      	  '<tr class="group" data-group="' + group_name +'"><td colspan="' + (Helpkit.locals.colspan) +'" >'+'</td><td class="hours"><strong>'+ fr_group_count +'</strong></td></tr>'
 			                    	);
 			                	}
 
-			                    last = group;
+			                    last = group_id;
 			                }
 			            });
 			            fixedColumn.init({},"#timesheet_table");
 			        },
 			        "createdRow": function ( row, data, index ) {
+
+			        	if(current_group_by == "group_by_day_criteria") {
+	            			group_id = data['executed_at'];
+	            		} else if( current_group_by == "workable") {
+	            			group_id = data['workable_id'];
+	            		} else if( current_group_by == "agent_name") {
+	            			group_id = data['user_id'];
+	            		} else if( current_group_by == "customer_name") {
+	            			 group_id = data['customer_id'];
+	            		} else if( current_group_by == "group_name") {
+	            			 group_id = data['group_id'];
+	            		} else if( current_group_by == "product") {
+	            			 group_id = data['product_id'];
+	            		}
+
 			        	jQuery(row).attr({
-		        			'data-workable-id': data['workable_id'],
 		        			'data-workable-desc': data['workable_desc'],
-		        			'data-group-id' : data['group_id'],
-		        			'data-user-id' : data['user_id'],
-		        			'data-executed-at' : data['executed_at']
+		        			'data-groupby-id' : group_id
 		        		});
 			        }
 			 };
@@ -184,14 +184,14 @@ Helpkit.TimesheetInitializer = (function () {
 				config.aoColumnDefs.push(row);
 			});
 
-			var oTable = $table_container.dataTable(config);
+			 oTable = $table_container.dataTable(config);
 	        if($table_container.length == 1) {
-
+	        	/*
 				if (jQuery.browser.safari) {
 					setTimeout(function(){
 					 oTable.columns.adjust().draw();
 					}, 0 );
-	 			}
+	 			} */
 	        }
 		},
 		hour_markup : function(row) {
