53a58f35b1aec9b239701fb2bd9fc8059df4b144
Roshini <roshini@freshdesk.com>
2017-03-09 08:58:41 +0530

    corrected the earlier commit

--- api/app/controllers/ember/integrated_resources_controller.rb
+++ api/app/controllers/ember/integrated_resources_controller.rb
@@ -28,7 +28,7 @@ class IntegratedResourcesController < ApiApplicationController
   end
 
   def destroy
-    resource = { :integrated_resource => { "remote_integratable_id" => @item.remote_integratable_id, :account => @current_account}}
+    resource = { :integrated_resource => { "remote_integratable_id" => @item.id, :account => @current_account}}
     Integrations::IntegratedResource.delete_resource_by_remote_integratable_id(resource)
     head 204
   end
