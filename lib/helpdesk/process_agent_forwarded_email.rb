0a98f0ca434ebd29a99405f765c4b7bdabab585d
Bhavya-Y <bhavya@freshdesk.com>
2017-03-21 14:54:37 +0530

    [#EMAIL-38] Incorrect requester set from an email forwarded by an Agent, during regex parsing

--- lib/helpdesk/process_agent_forwarded_email.rb
+++ lib/helpdesk/process_agent_forwarded_email.rb
@@ -4,22 +4,23 @@ module Helpdesk
     include AccountConstants
 
     def identify_original_requestor(content)
-      from_name = from_email = from_index = nil
       if content
         t_content = decode_brackets_in_text(content.gsub("\r\n", "\n"))
-        from_index = (t_content =~ /^\s*\*?(?:From\s?:|De\s?:|Desde\s?:|Von\s?:|Van\s?:)\*?\s(.*)\s+\[mailto:(.*)\]/ or
-                      t_content =~ /^>*\s*\*?(?:From\s?:|De\s?:|Desde\s?:|Von\s?:|Van\s?:)\*?\s*(.*)\s+<(.*)>$/ or
-                      t_content =~ /^>>>+\s(.*)\s+<(.*)>$/) 
-                      
-       
-                      
-        if from_index
-          from_name, from_email = $1, $2
+        regex_arr = [
+          Regexp.new(/^\s*\*?(?:From\s?:|De\s?:|Desde\s?:|Von\s?:|Van\s?:)\*?\s(.*)\s+\[mailto:(.*)\]/), # From: Sample <mailto:sample@example.com>
+          Regexp.new(/^>*\s*\*?(?:From\s?:|De\s?:|Desde\s?:|Von\s?:|Van\s?:)\*?\s*(.*)\s+<(.*)>$/), # From: Sample <sample@example.com>
+          Regexp.new(/^>>>+\s(.*)\s+<(.*)>$/), # >>> From:  sample <sample@example.com>
+          Regexp.new(/From\s?:\s?(.*)/) #For cases with only email address- From: sample@example.com
+        ]
+
+        from_index, from_email, from_name = parse_requester_info(t_content, regex_arr)
+                              
+        if (from_index and from_index != t_content.length)
           begin
             cc_emails  = parse_cc_in_forward_text(t_content, from_index)
           rescue Exception => e
             error_msg = "Exception occurred while parsing email content to identify Cc emails if any"
-            Rails.logger.debug "#{error_msg}, #{e}, {e.message}, #{e.backtrace}"
+            Rails.logger.debug "#{error_msg}, #{e}, #{e.message}, #{e.backtrace}"
             NewRelic::Agent.notice_error(e, {:description => error_msg})
           end
           cc_emails ||= []
@@ -33,18 +34,23 @@ module Helpdesk
 
     private  
       def parse_cc_in_forward_text(t_content, from_index = 0)
-        offset = t_content[from_index..-1] =~ /\n\n/
-        parsed_header_content = t_content[from_index..(from_index + offset + 1)]
-        cc_text = "" 
-        to_text = ""
-        parsed_header_content.sub!(/^(\*?Cc:\*?)/, "Cc:")
-        parsed_header_content.sub!(/^(\*?(?:To:|Pour:|Para:|Zu:|Aan:)\*?)/, "To:")
+        # Checks new lines and lines starting with '>' or '>>>'
+        # > To: sample@example.com
+        # >>> To:sample@example.com <mailto:sample@example.com>
+        offset = (t_content[from_index..-1] =~ /\n\n/ or t_content[from_index..-1] =~ /\n>+\s?\n>+/)
+        if offset
+          parsed_header_content = t_content[from_index..(from_index + offset + 1)]
+          cc_text = "" 
+          to_text = ""
+          parsed_header_content.sub!(/^((>\s)?\*?Cc:\*?)/, "Cc:") # Cc: Sample <sample@example.com> or > Cc: Sample <sample@example.com>
+          parsed_header_content.sub!(/^((>\s)?\*?(?:To:|Pour:|Para:|Zu:|Aan:)\*?)/, "To:")
 
-        parsed_header_content.split("\n").each do |line| 
-          if (line.start_with?("Cc")) 
-            cc_text << line 
-          elsif (line.start_with?("To"))
-            to_text << line 
+          parsed_header_content.split("\n").each do |line|
+            if (line.start_with?("Cc")) 
+              cc_text << line 
+            elsif (line.start_with?("To"))
+              to_text << line 
+            end
           end
         end
         merge_to_and_cc_emails(cc_text, to_text)
@@ -61,5 +67,21 @@ module Helpdesk
       def decode_brackets_in_text(content)
         content.gsub(/\\[Uu]003[Cc]/, "<").gsub(/\\[Uu]003[Ee]/, ">")
       end
+
+      def parse_requester_info(t_content, regex_arr)
+        from_index = ret_value = nil
+        tl = t_content.length
+
+        from_index = regex_arr.inject(tl) do |min, regex|
+          cur_index = t_content.index(regex)
+          if (cur_index.present? && cur_index < min)
+            ret_value = ((regex == regex_arr.last ) ? $1 : [$2, $1])
+            min = cur_index
+          end
+          min
+        end
+        return [from_index, ret_value].flatten
+      end
+
   end
 end
