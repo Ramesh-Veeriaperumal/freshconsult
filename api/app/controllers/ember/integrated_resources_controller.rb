7bce28b1ea44241f72ee45ff7333589a7695336c
Roshini <roshini@freshdesk.com>
2017-03-22 10:45:28 +0530

    create integrated resource handled for timesheet apps

--- api/app/controllers/ember/integrated_resources_controller.rb
+++ api/app/controllers/ember/integrated_resources_controller.rb
@@ -8,28 +8,32 @@ class IntegratedResourcesController < ApiApplicationController
   def create
     return unless validate_body_params
       params.with_indifferent_access
-      ticketId = params[:integrated_resource][:local_integratable_id]
-      ticket = fetch_ticket_using_display_id(ticketId)
-      if ticket.nil?
-        render_custom_errors
-      else
-        params[:integrated_resource].delete(:local_integratable_id)
-        applicationId = params[:application_id].to_i
-        params[:integrated_resource].merge!({:account => @current_account,:local_integratable_id => ticket.id})
-        params.delete(:application_id)
-        params.merge!("application_id" => applicationId)
-        newIntegratedResource = Integrations::IntegratedResource.createResource(params)
-        if newIntegratedResource.blank?
+      if (params[:integrated_resource][:local_integratable_type] === "Helpdesk::Ticket")
+        ticketId = params[:integrated_resource][:local_integratable_id]
+        ticket = fetch_ticket_using_display_id(ticketId)
+        if ticket.nil?
           render_custom_errors
         else
-          render '/ember/integrated_resources/show'
+          params[:integrated_resource].delete(:local_integratable_id)
+          params[:integrated_resource].merge!({:local_integratable_id => ticket.id})
         end
+      end
+
+      applicationId = params[:application_id].to_i
+      params[:integrated_resource].merge!({:account => @current_account})
+      params.delete(:application_id)
+      params.merge!({"application_id" => applicationId})
+      @item = Integrations::IntegratedResource.createResource(params)
+      if @item.blank?
+        render_custom_errors
+      else
+        render '/ember/integrated_resources/show'
       end      
   end
 
   def destroy
     resource = { :integrated_resource => { "remote_integratable_id" => @item.id, :account => @current_account}}
-    Integrations::IntegratedResource.delete_resource_by_remote_integratable_id(resource)
+    Integrations::IntegratedResource.deleteResource(resource)
     head 204
   end
 
