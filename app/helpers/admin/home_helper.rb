808e0d2ef3f8348c99215dce48e86a525ebb006c
Selvakumar <selvakumarn@freshdesk.com>
2017-03-25 12:54:37 +0530

    FDCORE-2247 Squashed commit for zendesk import app
    commit 7abae57ca871fd8d851ebfefebe31f650bf6c45d
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Sat Mar 25 12:09:10 2017 +0530
    
        add removed line from gemfile.lock
    
    commit c24d21fa21efd61513738115bd87620eef1c3b9c
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Fri Mar 24 20:34:35 2017 +0530
    
        Add redis key for zendesk import app
    
    commit dc0b0a56260ee2327481f90662729db20adf3f06
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Fri Mar 24 20:07:28 2017 +0530
    
        check redis key for zendesk import app
    
    commit 4a60dad3467c172e0bc30e070407b63a469c6547
    Merge: da0ac3c 9cfd186
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Fri Mar 24 20:05:07 2017 +0530
    
        Merge branch 'rails3-phase2' of github.com:freshdesk/helpkit into admin_zenimport
    
        # Conflicts:
        #	app/models/account/constants.rb
        #	app/models/account/features.rb
        #	config/helpdesk_initializers.yml
    
    commit da0ac3cb143af712b017125dd275047839d031b7
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Fri Feb 10 13:24:43 2017 +0530
    
        Use Dynamic feature check
    
    commit 51fce4d8bed20a2cfea37fe847edb2af5b4d5566
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Fri Feb 10 12:52:53 2017 +0530
    
        Code cleanup
    
    commit 64f3567817a920e656c4de505400191350e91460
    Merge: e1f3d2e 753cbcd
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Thu Feb 9 12:37:27 2017 +0530
    
        Merge branch 'rails3-phase2' of github.com:freshdesk/helpkit into admin_zenimport
    
        # Conflicts:
        #	deploy/before_migrate.rb
    
    commit e1f3d2ee24b8c3e5b262efc4753c026c1193a8aa
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Thu Feb 9 12:25:07 2017 +0530
    
        Add environment config for Zendesk import app
    
    commit 4695581c6ee638eafd2428773fd8e463115ef5f7
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Thu Jan 19 15:21:57 2017 +0530
    
        Add app id as constant for zen import
    
    commit 94c14c0b31a569a9100ed4c13ce1b3e4bc53431a
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Thu Jan 12 18:50:00 2017 +0530
    
        Add launchparty check for zendesk import app
    
    commit c2f85c9fd8753c3c3fb8ea82fe749c5a6661b31e
    Merge: ccb3c8d 76b3de7
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Thu Jan 12 18:47:27 2017 +0530
    
        Merge branch 'admin_zenimport' of github.com:freshdesk/helpkit into admin_zenimport
    
    commit ccb3c8df42bec0b46b04ad63025b94302c21ca8e
    Merge: bae8d9d c527f30
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Thu Jan 12 18:37:24 2017 +0530
    
        Merge branch 'rails3-phase2' of github.com:freshdesk/helpkit into admin_zenimport
    
    commit 76b3de78211fbccbc116dbe25be6e17bed5af6e1
    Author: Selvakumar <selvakumarn@freshdesk.com>
    Date:   Wed Jan 11 12:39:32 2017 +0530
    
        Change zendesk import URL in admin

--- app/helpers/admin/home_helper.rb
+++ app/helpers/admin/home_helper.rb
@@ -1,4 +1,5 @@
  module Admin::HomeHelper
+  include Redis::RedisKeys
 
   ######### Admin Items ########
 
@@ -141,7 +142,9 @@
         :privilege                     =>   privilege?(:manage_account)
       },
       :import                          =>   {
-        :url                           =>   "/admin/zen_import/",
+        :url                           =>   redis_key_exists?(ZENDESK_IMPORT_APP_KEY) ?
+                                            "/integrations/applications##{ZEN_APP_ID}":
+                                            "/admin/zen_import",
         :privilege                     =>   privilege?(:manage_account)
       },
       :day_pass                        =>   {
@@ -199,6 +202,8 @@
       }
     }
   end
+
+  ZEN_APP_ID = ZendeskAppConfig::APP_ID
 
   ######### Admin groups & Associated admin items Constant ########
 
@@ -334,6 +339,7 @@
       :"day_pass"                             =>    [:occasional_agent],
       :custom_mailbox                         =>    [:custom_mailbox_meta]
     }
+
 
   ######### Constructing Admin Page ########
 
