71d0b56aad5e390b362f3cb45c7213d417bea9c3
Manu <manuollakkode@gmail.com>
2017-03-30 11:42:30 +0530

    bug fix - failed content in agent notification
    Signed-off-by: Manu <manuollakkode@gmail.com>

--- lib/helpdesk/email/failed_email_msg.rb
+++ lib/helpdesk/email/failed_email_msg.rb
@@ -7,22 +7,22 @@ class Helpdesk::Email::FailedEmailMsg
   end
 
   def save!
-    note        = Account.current.notes.where(id:@note_id).first
-    @note_owner = note.user
+    @note        = Account.current.notes.where(id:@note_id).first
+    @note_owner = @note.user
     @recipient  = @note_owner.email
-    @ticket     = note.notable
-    note.dynamodb_range_key = @dynamodb_key
-    if note.failure_count.present?
-      note.failure_count += 1
+    @ticket     = @note.notable
+    @note.dynamodb_range_key = @dynamodb_key
+    if @note.failure_count.present?
+      @note.failure_count += 1
     else
-      note.failure_count  = 1
+      @note.failure_count  = 1
     end
-    note.schema_less_note.save!
-    Rails.logger.info "FailedEmailPoller: Saved! account_id: #{note.account_id}, ticket_id: #{@ticket.id}, note_id: #{@note_id}, failed_email: #{@failed_email}, dynamo: #{@dynamodb_key}"
+    @note.schema_less_note.save!
+    Rails.logger.info "FailedEmailPoller: Saved! account_id: #{@note.account_id}, ticket_id: #{@ticket.id}, note_id: #{@note_id}, failed_email: #{@failed_email}, dynamo: #{@dynamodb_key}"
   end
 
   def notify
-    failed_content = @ticket.description
+    failed_content = @note.body
     ticket_url = "#{Account.current.url_protocol}://#{Account.current.full_domain}/helpdesk/tickets/#{@ticket.display_id}"
     type    = Helpdesk::Email::Constants::FAILURE_CATEGORY[@failure_category]
     error   = I18n.t("email_failure.#{type}.summary_error_text")
