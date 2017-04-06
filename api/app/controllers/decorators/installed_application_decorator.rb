09c5bbc148490cc06bcf574400ff8f9d416e961c
Roshini <roshini@freshdesk.com>
2017-04-06 17:44:11 +0530

    review changes done

--- api/app/controllers/decorators/installed_application_decorator.rb
+++ api/app/controllers/decorators/installed_application_decorator.rb
@@ -1,23 +1,21 @@
 class InstalledApplicationDecorator < ApiDecorator
 
-delegate :id, :application_id, :configs, :app_name, :configs,  to: :record
+delegate :id, :application_id, :configs, :application, :configs,  to: :record
 
 def to_hash 
-  
-  inst_app_hash = {
-    id: record.id,
-    application_id: record.application_id,
+  {
+    id: id,
+    application_id: application_id,
     app_name: record.application.name,
     configs: configs_hash,
-    app_display_name: record.application.display_name,
+    app_display_name: application.display_name,
     display_option: Integrations::Constants::APPS_DISPLAY_MAPPING[record.application.name]
   }
-  inst_app_hash
 end
 
 def configs_hash
-  return {} unless record.configs[:inputs].present?
-  configsHash = record.configs[:inputs]
+  return {} unless configs[:inputs].present?
+  configsHash = configs[:inputs]
   configsHash.symbolize_keys.except(*Integrations::Constants::EXCLUDE_FROM_APP_CONFIGS_HASH) 
 end
 
--- api/app/controllers/decorators/integrated_resource_decorator.rb
+++ api/app/controllers/decorators/integrated_resource_decorator.rb
@@ -2,14 +2,12 @@ class IntegratedResourceDecorator < ApiDecorator
 delegate :id, :installed_application_id, :remote_integratable_id, :local_integratable_id, :remote_integratable_type, to: :record
 
 def to_hash
-	
-  res_hash = {
-  	id: record.id,
-  	installed_application_id: record.installed_application_id,
-  	remote_integratable_id: record.remote_integratable_id,
-  	remote_integratable_type: record.remote_integratable_type,
-    local_integratable_id: record.local_integratable_id
+  {
+    id: id,
+    installed_application_id: installed_application_id,
+    remote_integratable_id: remote_integratable_id,
+    remote_integratable_type: remote_integratable_type,
+    local_integratable_id: local_integratable_id
   }
-  res_hash
 end
 end
