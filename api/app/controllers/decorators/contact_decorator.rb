c104f1076128926461e57db9c069d1652af469c1
Ellanki Hari Prasad <hari@freshdesk.com>
2017-04-07 12:13:52 +0530

    Addeding contact id to restricted requester hash
    Addeding contact id to restricted requester hash

--- api/app/controllers/decorators/contact_decorator.rb
+++ api/app/controllers/decorators/contact_decorator.rb
@@ -68,6 +68,7 @@ class ContactDecorator < ApiDecorator
   def restricted_requester_hash
     req_hash = construct_hash(requester_widget_contact_fields, record)
     req_hash[:company] = construct_hash(requester_widget_company_fields, record.company) if record.company.present?
+    req_hash[:id] = record.id
     req_hash
   end
 
