2f4fc27e22a1a1106ef27ca712efd56fd7b91c82
ragav0102 <ragavendren@freshdesk.com>
2017-04-10 15:14:03 +0530

    Revert "Revert "#FDCORE-737 Send and set race condition resolved""
    This reverts commit b23dc770cb0ca39915e94ac93d3881396034d6b6.

---
+++ app/workers/tickets/send_and_set_worker.rb
@@ -0,0 +1,31 @@
+class Tickets::SendAndSetWorker < BaseWorker
+
+  include Helpdesk::TicketNotifications
+
+  sidekiq_options :queue => :send_and_set_observer, :retry => 0, :backtrace => true, :failures => :exhausted
+
+  def perform args
+    begin
+      args.symbolize_keys!
+      args[:ticket_changes].symbolize_keys!
+      account = Account.current
+      evaluate_on = account.tickets.find_by_id args[:ticket_changes][:ticket_id]      
+      Tickets::ObserverWorker.new.perform(args[:ticket_changes])
+      Tickets::UpdateTicketStatesWorker.new.perform(args[:note_changes])
+      status = evaluate_on.status
+      if (status == RESOLVED)
+        notify_by_email(EmailNotification::TICKET_RESOLVED) 
+        notify_watchers("resolved")
+      end
+      if (status == CLOSED)
+        notify_by_email(EmailNotification::TICKET_CLOSED)
+        notify_watchers("closed")
+      end
+      puts "Send and Set Observer run for Account id:: #{Account.current.id}, Ticket id:: #{args[:ticket_changes][:ticket_id]}, params: #{args} "
+    rescue => e
+      puts "Something is wrong in Send and Set Observer : Account id:: #{Account.current.id}, Ticket id:: #{args[:ticket_changes][:ticket_id]}, #{e.message}"
+      NewRelic::Agent.notice_error(e, {:custom_params => {:args => args }})
+      raise e
+    end
+  end
+end
