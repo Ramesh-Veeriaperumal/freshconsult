2268cb8aeac8be7dabb8df4fe994862cab9d74da
Ramkumar R <ramkumar@freshdesk.com>
2017-04-11 15:56:56 +0530

    FDCORE-1263
    JS file organizing

--- public/javascripts/cdn/app/ticketschedule.js
+++ public/javascripts/cdn/app/ticketschedule.js
@@ -1,4 +1,4 @@
 //= require plugins/clipboard.js
-//= require app/ticketschedule.js
-//= require app/ticketscheduleindex.js
-//= require app/ticketactivity.js
+//= require app/scheduled_exports/schedule.js
+//= require app/scheduled_exports/index.js
+//= require app/scheduled_exports/activity.js
