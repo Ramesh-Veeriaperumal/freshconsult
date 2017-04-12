82a900e5e60b1f929cdd85894df8e6cdeb5fc2f5
krishnanand <krishnanand@freshdesk.com>
2017-04-12 18:49:28 +0530

    FDCORE-1121 : adding test cases

---
+++ test/core/functional/helpdesk/agents_controller_tests.rb
@@ -0,0 +1,38 @@
+require_relative '../../test_helper'
+
+class AgentsControllerTest < ActionController::TestCase
+  include UsersTestHelper
+  include ControllerTestHelper
+
+  def setup
+    super
+    login_admin
+  end
+
+  def test_email_trigger_valid
+    count_of_delayed_jobs_before = Delayed::Job.count
+    agent = add_test_agent
+    agent.update_attributes(:email => Faker::Internet.email)
+    assert_equal count_of_delayed_jobs_before+3, Delayed::Job.count
+
+  end
+  def test_email_trigger_same_email
+    count_of_delayed_jobs_before = Delayed::Job.count
+    agent = add_test_agent
+    agent.update_attributes(:email => agent.email)
+    assert_equal count_of_delayed_jobs_before, Delayed::Job.count
+  end
+  def test_email_trigger_capscase
+    count_of_delayed_jobs_before = Delayed::Job.count
+    agent = add_test_agent
+    agent.update_attributes(:email => agent.email.capitalize)
+    assert_equal count_of_delayed_jobs_before+1, Delayed::Job.count
+  end 
+  def test_email_trigger_nil_new_email
+    count_of_delayed_jobs_before = Delayed::Job.count
+    agent = add_test_agent
+    agent.update_attributes(:email => nil)
+    assert_equal count_of_delayed_jobs_before, Delayed::Job.count
+  end 
+
+end
