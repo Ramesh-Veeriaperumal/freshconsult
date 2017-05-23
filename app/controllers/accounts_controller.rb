ddfc37f5b67615e01afa427d6b93d55c91094334
srikanth-brahmasani <srikanth@freshdesk.com>
2017-05-23 17:59:40 +0530

    FDCORE-3782 - Adding same account twice in freshsales bug fix"

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -119,7 +119,6 @@ class AccountsController < ApplicationController
   def update_domain
     if current_account.update_default_domain_and_email_config(params["company_domain"],params["support_email"])
       current_user.reset_perishable_token!
-      add_to_crm(current_account.id)
       render json: {  :success => true, 
                       :url => signup_complete_url(:token => current_user.perishable_token, :host => current_account.full_domain)
                     }
@@ -443,9 +442,9 @@ class AccountsController < ApplicationController
 
     def add_to_crm(account_id)
       if (Rails.env.production? or Rails.env.staging?)
-        Resque.enqueue_at(3.minute.from_now, Marketo::AddLead, { :account_id => account_id,
+        Resque.enqueue_at(15.minute.from_now, Marketo::AddLead, { :account_id => account_id,
           :signup_id => params[:signup_id]})
-        Resque.enqueue_at(5.minute.from_now, CRM::Freshsales::Signup, { account_id: account_id,
+        Resque.enqueue_at(15.minute.from_now, CRM::Freshsales::Signup, { account_id: account_id,
          fs_cookie: params[:fs_cookie] })
       end  
     end  
