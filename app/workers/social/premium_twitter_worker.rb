4313998495cd28108c8102887e377fb9b252ed22
ImAjayS <ajay@freshdesk.com>
2017-05-25 17:01:26 +0530

    FDCORE-3871 Adding observer logs on skip, and facebook cron logs

--- app/workers/social/premium_twitter_worker.rb
+++ app/workers/social/premium_twitter_worker.rb
@@ -4,6 +4,7 @@ module Social
     sidekiq_options :queue => :premium_twitter, :retry => 0, :backtrace => true, :failures => :exhausted
     
     def perform(args)
+      Rails.logger.debug "Premium Twitter Worker starting at #{Time.now} for account #{args['account_id']}"
       Sharding.select_shard_of(args['account_id']) do
         Account.find(args['account_id']).make_current
         super({})
