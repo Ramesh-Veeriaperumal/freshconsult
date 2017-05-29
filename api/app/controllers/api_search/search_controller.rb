a78d20a34420751f7065f6008dab985cbef52029
rseetharamaguptha <rseetharamaguptha@gmail.com>
2017-05-29 12:47:28 +0530

    [SEARCH-113]
    Signed-off-by: rseetharamaguptha <rseetharamaguptha@gmail.com>

--- api/app/controllers/api_search/search_controller.rb
+++ api/app/controllers/api_search/search_controller.rb
@@ -68,7 +68,7 @@ module ApiSearch
             current_page: page,
             offset:       ApiSearchConstants::DEFAULT_PER_PAGE,
             types:        type,
-            es_params:    { search_terms: search_terms.to_json, offset: (page - 1) * ApiSearchConstants::DEFAULT_PER_PAGE, size: ApiSearchConstants::DEFAULT_PER_PAGE, account_id: current_account.id }
+            es_params:    construct_es_params(search_terms, type, page)
           }).query_results
         rescue Exception => e
           Rails.logger.error "Searchv2 exception - #{e.message} - #{e.backtrace.first}"
@@ -76,6 +76,27 @@ module ApiSearch
           @records = []
         end
         @records
+      end
+
+      def construct_es_params(search_terms, type, page)
+        Hash.new.tap do |es_params|
+          es_params[:search_terms] = search_terms.to_json
+          es_params[:account_id] = current_account.id
+          es_params[:offset] = (page - 1) * ApiSearchConstants::DEFAULT_PER_PAGE
+          es_params[:size] = ApiSearchConstants::DEFAULT_PER_PAGE
+
+          if type.include?('ticket')
+            if current_user.restricted?
+              es_params[:restricted_responder_id] = current_user.id.to_i
+              es_params[:restricted_group_id]     = current_user.agent_groups.map(&:group_id) if current_user.group_ticket_permission
+
+              if current_account.shared_ownership_enabled?
+                es_params[:restricted_internal_agent_id] = current_user.id.to_i
+                es_params[:restricted_internal_group_id] = current_user.agent_groups.map(&:group_id) if current_user.group_ticket_permission
+              end
+            end
+          end
+        end
       end
   end
 end
