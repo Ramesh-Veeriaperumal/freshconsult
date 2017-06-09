68ab650aaf3a9f2a00bfb48351e7ebabe661608d
rseetharamaguptha <rseetharamaguptha@gmail.com>
2017-06-09 19:05:06 +0530

    Adding feature check for search service signups
    Signed-off-by: rseetharamaguptha <rseetharamaguptha@gmail.com>

--- lib/redis/redis_keys.rb
+++ lib/redis/redis_keys.rb
@@ -248,6 +248,9 @@ module Redis::RedisKeys
 
   ACCOUNT_ADMIN_ACTIVATION_JOB_ID = "ACCOUNT_ADMIN_ACTIVATION_JOB_ID:%{account_id}"
 
+  # Search Service Keys
+  SEARCH_SERVICE_SIGNUP = "SEARCH_SERVICE_SIGNUP"
+
   #Following are the dead keys. Need to remove them from code and any references
   GROUP_AGENT_TICKET_ASSIGNMENT = "GROUP_AGENT_TICKET_ASSIGNMENT:%{account_id}:%{group_id}"
   HELPDESK_GAME_NOTIFICATIONS = "HELPDESK_GAME_NOTIFICATIONS:%{account_id}:%{user_id}"
