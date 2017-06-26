1e4f15ef1f25dedfb85c148b818dd82b882cec5c
venkatesanmurali <venkatesan.murali1985@gmail.com>
2017-06-26 11:12:16 +0530

    FDCORE-4237 XSS issue in Reply/Forward templates in ticket detail page. (#296)
    * Xss enable for specific accounts only
    
    * Xss code changes
    
    * Xss in reply
    
    * Code refactor - metaprogram, user drop
    
    * Xss bug fix - Canned response.

--- app/helpers/helpdesk/tickets_helper.rb
+++ app/helpers/helpdesk/tickets_helper.rb
@@ -284,7 +284,8 @@ module Helpdesk::TicketsHelper
     else
           default_reply_forward = (signature.blank?)? "<p/><p/><br/>": "<p/><p><br></br></p><p></p><p></p><div>#{signature}</div>"
     end
-
+    
+    ticket.escape_liquid_attributes = current_account.launched?(:escape_liquid_for_reply)
     quoted_text = ""
     if quoted
       quoted_text = quoted_text(item, forward)
