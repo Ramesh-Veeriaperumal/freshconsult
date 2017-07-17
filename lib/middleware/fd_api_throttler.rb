c93b888e076fa9c10dfb55198928b5470e2ff6ff
venkatesanmurali <venkatesan.murali1985@gmail.com>
2017-07-17 17:07:35 +0530

    FDCORE-: FDCORE-4539 Private api throttler squash

--- lib/middleware/fd_api_throttler.rb
+++ lib/middleware/fd_api_throttler.rb
@@ -4,14 +4,14 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
   include Redis::RedisKeys
   include Redis::RateLimitRedis
 
-  FRESHDESK_DOMAIN = 'freshdesk'
+  FRESHDESK_DOMAIN = 'freshdesk'.freeze
   SKIPPED_SUBDOMAINS =  %w(admin billing partner signup freshsignup email login emailparser mailboxparser freshops) + FreshopsSubdomains
   THROTTLE_PERIOD    =  1.hour
   API_LIMIT = 3000
   DEFAULT_USED_LIMIT = 1
-  API_CURRENT_VERSION = 'v2'
-  NOT_FOUND_RESPONSE = [404, { 'Content-Type' => 'application/json' }, [' ']]
-  LIMIT_EXCEEDED_MESSAGE = [{ message: 'You have exceeded the limit of requests per hour' }.to_json]
+  API_CURRENT_VERSION = 'v2'.freeze
+  NOT_FOUND_RESPONSE = [404, { 'Content-Type' => 'application/json' }, [' ']].freeze
+  LIMIT_EXCEEDED_MESSAGE = [{ message: 'You have exceeded the limit of requests per hour' }.to_json].freeze
 
   def initialize(app, options = {})
     super(app, options)
@@ -23,39 +23,18 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
     @host         = env['HTTP_HOST']
     @shard        = fetch_shard(env)
 
-    if @shard.try(:not_found?)
+    if shard_not_found?
       Rails.logger.debug "Domain Not Found while throttling :: Host: #{@host}"
       @status, @headers, @response = NOT_FOUND_RESPONSE
     elsif throttle?
-      @api_limit = api_limit
-      increment_redis_key(DEFAULT_USED_LIMIT)
-      expiry_set = set_redis_expiry(key, THROTTLE_PERIOD) if @count <= used_limit # Setting expiry for first time.
-
-      if allowed?
-        @status, @headers, @response = @app.call(@request.env)
-        # In order to avoid a 'get' and an 'incrby' call to redis for every request, 'incrby' is being called twice conditionally.
-        unless extra_credits == 0
-          increment_redis_key(extra_credits)
-          # Expiry should be set immediately after increment, as the expiry condition depends on the incremented value.
-          # expiry_set will be true when it is already set in the current request.
-          # if expiry happens during @app.call, @count will be less than used_limit. Hence checking '<=' instead of '=='
-          set_redis_expiry(key, THROTTLE_PERIOD) if !expiry_set && (@count <= used_limit)
-        end
-        set_rate_limit_headers if @count
-        Rails.logger.debug("Throttled :: Host: #{@host}, Time: #{Time.now}, Count: (#{@count})")
-      else
-        retry_value = handle_expiry_not_set
-        @status, @headers, @response = [429, { 'Retry-After' => retry_value.to_s, 'Content-Type' => 'application/json' },
-                                        LIMIT_EXCEEDED_MESSAGE]
-        Rails.logger.error("429 Error :: Host: #{@host}, Time: #{Time.now}, Count: #{@count}}")
-      end
+      throttle_and_proceed
     else
       @status, @headers, @response = @app.call(@request.env)
     end
-
     set_version_headers unless @status == 404 # Version will not be present if status is 404
     [@status, @headers, @response]
   ensure
+    @api_limit = nil
     unset_current_account
     unset_shard_in_env(env)
   end
@@ -65,15 +44,52 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
     def handle_expiry_not_set
       retry_value = retry_after.to_i
       if retry_value < 0
-        Rails.logger.error("Expiry not set properly :: Host: #{@host}, Retry Value: #{retry_value}, Time: #{Time.now}, Count: #{@count}}")
+        Rails.logger.error("Expiry not set properly :: Host: #{@host}, Retry Value: #{retry_value}, Time: #{Time.zone.now}, Count: #{@count}}")
         retry_value = 1
         set_redis_expiry(key, retry_value)
       end
       retry_value
     end
 
+    def shard_not_found?
+      @shard.try(:not_found?)
+    end
+
+    def throttle_and_proceed
+      increment_redis_key(DEFAULT_USED_LIMIT)
+      expiry_set = set_redis_expiry(key, api_expiry) if @count <= used_limit # Setting expiry for first time.
+
+      if allowed?
+        increment_key_and_proceed(expiry_set)
+      else
+        retry_value = handle_expiry_not_set
+        @status = 429
+        @headers = { 'Retry-After' => retry_value.to_s, 'Content-Type' => 'application/json' }
+        @response = LIMIT_EXCEEDED_MESSAGE
+        log_data("429 Error :: Host: #{@host}, Time: #{Time.zone.now}, Count: #{@count}}", 'error')
+      end
+    end
+
+    def increment_key_and_proceed(expiry_set)
+      @status, @headers, @response = @app.call(@request.env)
+      # In order to avoid a 'get' and an 'incrby' call to redis for every request, 'incrby' is being called twice conditionally.
+      unless extra_credits.zero?
+        increment_redis_key(extra_credits)
+        # Expiry should be set immediately after increment, as the expiry condition depends on the incremented value.
+        # expiry_set will be true when it is already set in the current request.
+        # if expiry happens during @app.call, @count will be less than used_limit. Hence checking '<=' instead of '=='
+        set_redis_expiry(key, api_expiry) if !expiry_set && (@count <= used_limit)
+      end
+      set_rate_limit_headers if @count
+      log_data("Throttled :: Host: #{@host}, Time: #{Time.zone.now}, Count: (#{@count})", 'debug')
+    end
+
     def account_id
       @shard.try(:account_id)
+    end
+
+    def api_expiry
+      THROTTLE_PERIOD
     end
 
     def skipped_domain?
@@ -93,11 +109,11 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
     end
 
     def allowed?
-      @shard.try(:ok?) ? @count <= @api_limit : true
+      @shard.try(:ok?) ? @count <= api_limit : true
     end
 
     def retry_after
-      handle_exception { return $rate_limit.perform_redis_op("ttl", key) }
+      handle_exception { return $rate_limit.perform_redis_op('ttl', key) }
     end
 
     def extra_credits
@@ -105,8 +121,8 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
     end
 
     def set_rate_limit_headers # Rate Limit headers are not set when status is 429
-      remaining = [(@api_limit - @count), 0].max.to_s
-      @headers = @headers.merge('X-RateLimit-Total' => @api_limit.to_s,
+      remaining = [(api_limit - @count), 0].max.to_s
+      @headers = @headers.merge('X-RateLimit-Total' => api_limit.to_s,
                                 'X-RateLimit-Remaining' => remaining,
                                 'X-RateLimit-Used-CurrentRequest' => used_limit.to_s)
     end
@@ -125,14 +141,15 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
     end
 
     def api_limit
-      api_limits = get_multiple_redis_keys(account_api_limit_key, plan_api_limit_key, default_api_limit_key) || []
-      (api_limits[0] || api_limits[1] || api_limits[2] || API_LIMIT).to_i
+      @api_limit ||= begin
+        api_limits = get_multiple_redis_keys(account_api_limit_key, plan_api_limit_key, default_api_limit_key) || []
+        (api_limits[0] || api_limits[1] || api_limits[2] || API_LIMIT).to_i
+      end
     end
 
     def plan_api_limit_key
-      if plan_id = fetch_plan_id
-        PLAN_API_LIMIT % { plan_id: plan_id }
-      end
+      plan_id = fetch_plan_id
+      format(PLAN_API_LIMIT, plan_id: plan_id) if plan_id.present?
     end
 
     def fetch_plan_id
@@ -140,9 +157,13 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
         Subscription.fetch_by_account_id(account_id).try(:plan_id)
       end
     rescue Exception => e
-      Rails.logger.error "Exception while fetching subscription_plan_id :: Host: #{@host}, Message: #{e.message}, Time: #{Time.now}"
+      Rails.logger.error "Exception while fetching subscription_plan_id :: Host: #{@host}, Message: #{e.message}, Time: #{Time.zone.now}"
       NewRelic::Agent.notice_error(e, custom_params: { description: 'Freshdesk API Throttler Error',
                                                        domain: @host, account_id: account_id })
+    end
+
+    def log_data(content, log_type)
+      Rails.logger.send(log_type, content)
     end
 
     def key
@@ -161,7 +182,8 @@ class Middleware::FdApiThrottler < Rack::Throttle::Hourly
     # So setting env & thread variable here and unsetting it in ensure block. Only CorsEnabler is present above this in the chain.
     # If any middleware is going to be at the top of the chain with account based logic, all this should move there.
     def fetch_shard(env)
-      shard = ShardMapping.lookup_with_domain(env["HTTP_HOST"])
+      shard = ShardMapping.lookup_with_domain(env['HTTP_HOST'])
+      # shard = ShardMapping.lookup_with_domain("localhost.freshdesk-dev.com")
       if shard
         env['SHARD'] = shard
         # Finding account & setting it in the thread variable also here. So trusted ip & controller will make use of it.
