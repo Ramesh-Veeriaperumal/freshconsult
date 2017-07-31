9162d4c82d05668e66b5517e0bf214cc54615548
rseetharamaguptha <rseetharamaguptha@gmail.com>
2017-07-31 19:35:20 +0530

    [SEARCH-258]
    If any custom field have the name same as that of default field, the validations and conditions of the default fields should be applied.
    
    Signed-off-by: rseetharamaguptha <rseetharamaguptha@gmail.com>

--- test/api/helpers/ticket_fields_test_helper.rb
+++ test/api/helpers/ticket_fields_test_helper.rb
@@ -3,10 +3,14 @@ module TicketFieldsTestHelper
 
   FIELD_MAPPING = { 'number' => 'int', 'checkbox' => 'boolean', 'paragraph' => 'text', 'decimal' => 'decimal', 'date' => 'date' }
 
-  def create_custom_field(name, type)
+  def create_custom_field(name, type, field_name = nil)
     ticket_field_exists = @account.ticket_fields.find_by_name("#{name}_#{@account.id}")
     return ticket_field_exists if ticket_field_exists
-    flexifield_mapping = type == 'text' ? 'ffs_13' : "ff_#{FIELD_MAPPING[type]}05"
+    if field_name
+      flexifield_mapping = type == 'text' ? "ffs_#{field_name}" : "ff_#{FIELD_MAPPING[type]}#{field_name}"
+    else
+      flexifield_mapping = type == 'text' ? 'ffs_13' : "ff_#{FIELD_MAPPING[type]}05"
+    end
     flexifield_def_entry = FactoryGirl.build(:flexifield_def_entry,
                                              flexifield_def_id: @account.flexi_field_defs.find_by_module('Ticket').id,
                                              flexifield_alias: "#{name.downcase}_#{@account.id}",
