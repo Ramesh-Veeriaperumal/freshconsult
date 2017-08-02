35d5d089e5378bc86c1a4e19e3baddecd08c9909
Kalyanasundaram <Kalyanasundaram@FDLMC1390.local>
2017-08-02 17:36:18 +0530

    FDCORE-5651 bug fix

--- lib/export/payload_enricher/base.rb
+++ lib/export/payload_enricher/base.rb
@@ -4,22 +4,36 @@ class Export::PayloadEnricher::Base
   ACTION = "action"
   ID = "id"
 
+  def initialize(sqs_msg, enricher_config)
+    @sqs_msg         = sqs_msg
+    @enricher_config = enricher_config
+  end
+
   def collect_properties(fields_to_export)
-    fields_to_export.inject({}) do |properties_hash, field_name|
-      @from_object ||= fetch_object
-      properties_hash[field_name] = field_value(@from_object, field_name)
-      properties_hash
+    from_object = fetch_object
+    response_map = {}
+    if from_object
+      fields_to_export.inject(response_map) do |properties_map, field_name|
+        properties_map[field_name] = field_value(from_object, field_name)
+        properties_map
+      end
     end
+    return response_map
   end
 
   def field_value(object, name)
     object.send(name)
   rescue Exception => e
     Rails.logger.error "[Export::PayloadEnricher::Base] Exception occured while 
-        trying to get value for property: #{name} on object class: 
-        #{object.class.name}, id: #{object.id}, acc_id: #{object.account_id}, 
+        trying to get value for property: #{name} on object: #{object.inspect},
         Exception => #{e.message} - #{e.backtrace.inspect}"
     return nil
+  end
+
+  def enrich
+    @sqs_msg.tap do |sqs_msg|
+      sqs_msg[property_key].merge!(properties) if sqs_msg[ACTION] != DESTROY
+    end
   end
 
 end
