aa27a6c139357fa02431a3e7ce6f2a7b200f9303
logeshr <logeshr@freshdesk.com>
2017-08-03 12:49:46 +0530

    FRESHCALL-734 - Call logs api fix
    Squashed commit of the following:
    
    commit b4e9d5c7523bc9290605c38c04e118b1ffbee7e3
    Merge: 0dc7e30041 249a06630e
    Author: logeshr <logeshr@freshdesk.com>
    Date:   Thu Aug 3 12:46:21 2017 +0530
    
        Merge remote-tracking branch 'origin/prestaging' into ff-dev-call-logs-export-api-734-hot-fix
    
    commit 0dc7e30041a8346df2a02d00c2d45214505c236c
    Author: sarathprasath <sarath@freshdesk.com>
    Date:   Wed Aug 2 19:58:16 2017 +0530
    
        [FRESHCALL-533] For accompanying ruby downgrade recipe change
    
        Signed-off-by: sarathprasath <sarath@freshdesk.com>
    
    commit 10d5851fb1312ae72398717c9742b21b43d433d2
    Author: logeshr <logeshr@freshdesk.com>
    Date:   Wed Aug 2 15:55:31 2017 +0530
    
        FRESHCALL-876
    
        Fixing the json style hash to rocket hash to be compatible with ruby 2.1.9

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -20,6 +20,7 @@ run "ln -nfs #{shared_path}/config/mailgun.yml #{node[:rel_path]}/config/mailgun
 run "ln -nfs #{shared_path}/config/mailbox.yml #{node[:rel_path]}/config/mailbox.yml"
 run "ln -nfs #{shared_path}/config/freshfone.yml #{node[:rel_path]}/config/freshfone.yml"
 run "ln -nfs #{shared_path}/config/rate_limit.yml #{node[:rel_path]}/config/rate_limit.yml"
+run "ln -nfs #{shared_path}/config/re_route_mapping.yml #{node[:rel_path]}/config/re_route_mapping.yml"
 run "ln -nfs #{shared_path}/config/akismet.yml #{node[:rel_path]}/config/akismet.yml"
 run "ln -nfs #{shared_path}/config/braintree.yml #{node[:rel_path]}/config/braintree.yml"
 run "ln -nfs #{shared_path}/config/rabbitmq.yml #{node[:rel_path]}/config/rabbitmq.yml"
