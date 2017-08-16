f553c3dedc37711f5c3c602a18f4ad2adf2806cb
Shreyas <shreyas.ns@freshdesk.com>
2017-08-16 12:15:00 +0530

    [FRESHPIPE-1737] add billing info api to freshdesk (#520)
    * [FRESHPIPE-1737] add billing info api to freshdesk
    
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>
    
    * review comments.
    
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>
    
    * review comment- change api route to /account/:account_id/info
    
    Signed-off-by: Shreyas <shreyas.ns@freshdesk.com>

---
+++ test/api/functional/pipe/account_info_controller_test.rb
@@ -0,0 +1,27 @@
+require_relative '../../test_helper'
+
+module Pipe
+  class AccountInfoControllerTest < ActionController::TestCase
+
+    def test_info_correct_account_id
+      get :index, controller_params(version: 'private', account_id: @account.id)
+      assert_response 200
+      match_json(account_info(@account))
+    end
+
+    def test_info_non_existent_account
+      get :index, controller_params(version: 'private', account_id: 23423425235)
+      assert_response 500
+    end
+
+    private
+
+    def account_info(account)
+      {
+        status: account.subscription.state,
+        plan: account.plan_name,
+        features: (account.feature_from_cache + account.features_list).uniq
+      }
+    end
+  end
+end
