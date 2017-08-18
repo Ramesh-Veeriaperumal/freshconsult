62d446634214350670646ea7869c614d0592a9ba
kalyanasundaram <vkalyanasundaram.mca@gmail.com>
2017-08-18 14:28:06 +0530

    Fdcore 5767 windows browser issue (#541)
    * FDCORE-: FDCORE-5767
    Import function not happens on windows chrome browser
    
    * FDCORE-: Fdcore 5767 review changes

--- api/app/controllers/validations/customer_import_validation.rb
+++ api/app/controllers/validations/customer_import_validation.rb
@@ -2,15 +2,17 @@ class CustomerImportValidation < ApiValidation
   include CustomerImportConstants
 
   attr_accessor :type, :file, :fields
-  
-  validates :type, required: true, custom_inclusion: { 
-              in: CustomerImportConstants::CUSTOMER_IMPORT_TYPES, allow_nil: false }
-  validates :fields, data_type: { rules: Hash, allow_nil: false }, 
-              required: true
+
+  validates :type, required: true, custom_inclusion: {
+    in: CustomerImportConstants::CUSTOMER_IMPORT_TYPES, allow_nil: false
+  }
+  validates :fields, data_type: { rules: Hash, allow_nil: false },
+                     required: true
   validates :file, required: true
   validates_each :file, on: :create do |record, attr, value|
-  	record.errors.add(attr, 
-      ErrorConstants::ERROR_MESSAGES[:invalid_format]%ACCEPTED_FILE_TYPE) if value && value.content_type != CSV_CONTENT_TYPE
+    if value && !CSV_FILE_EXTENSION_REGEX.match(value.original_filename)
+      record.errors.add(attr,
+          ErrorConstants::ERROR_MESSAGES[:invalid_format] % ACCEPTED_FILE_TYPE)
+    end
   end
-  
 end
