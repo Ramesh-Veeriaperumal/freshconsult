c9fa527d5b182ee803114ec04e58a03a9a922c05
saravana <saravana@freshdesk.com>
2017-08-24 20:20:13 +0530

    Revert "[EMAIL-196 Removal of require statements]"
    This reverts commit 66e3593f79f8ac65b0a31339e83daced9315b5cb.

--- lib/helpdesk/initializers/action_mailer_callbacks.rb
+++ lib/helpdesk/initializers/action_mailer_callbacks.rb
@@ -11,7 +11,6 @@ module ActionMailerCallbacks
   module ClassMethods
   include ParserUtil
   include EmailHelper
-  include Email::EmailService::IpPoolHelper
 
     def set_smtp_settings(mail)
       account_id_field = mail.header["X-FD-Account-Id"]
