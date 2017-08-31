9ffa3a008dc1a1d370f7260c50c501d0beee43ba
venkatesanmurali <venkatesan.murali1985@gmail.com>
2017-08-31 18:06:55 +0530

    FDCORE-5878 redis key set on some models change
    this will be used for falcon consumption

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,9 @@ class Survey < ActiveRecord::Base
   
   self.primary_key = :id
   include Reports::ActivityReport
+  include DataVersioning::Model
   
+  VERSION_MEMBER_KEY = 'SURVEY'.freeze
   ANY_EMAIL_RESPONSE = 1
   RESOLVED_NOTIFICATION = 2
   CLOSED_NOTIFICATION = 3
