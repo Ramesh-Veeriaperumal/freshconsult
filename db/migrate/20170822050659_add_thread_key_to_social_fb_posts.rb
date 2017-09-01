1c55632e4a3e0560c0af9db41da04c2072e0f90b
ImAjayS <ajay@freshdesk.com>
2017-09-01 18:53:18 +0530

    [FDCORE-6321]  Change index name

--- db/migrate/20170822050659_add_thread_key_to_social_fb_posts.rb
+++ db/migrate/20170822050659_add_thread_key_to_social_fb_posts.rb
@@ -7,7 +7,7 @@ class AddThreadKeyToSocialFbPosts < ActiveRecord::Migration
   def up
     Lhm.change_table :social_fb_posts, :atomic_switch => true do |m|
       m.add_column :thread_key, "varchar(255)"
-      m.add_index [:account_id, :thread_key, :postable_type, :facebook_page_id], "index_social_fb_posts_on_account_id_thread_key_and_postable_type_and_facebook_page_id"
+      m.add_index [:account_id, :thread_key, :postable_type, :facebook_page_id], "index_on_thread_key"
     end
   end
 
