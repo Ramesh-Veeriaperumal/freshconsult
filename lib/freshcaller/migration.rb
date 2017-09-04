bae96158148cc3c40295d0842451cd09bafaa8c3
Lavenya <alavenya@freshdesk.com>
2017-09-04 12:06:31 +0530

    FDCORE-6304 Bucket owner permission and skipping account creation

--- lib/freshcaller/migration.rb
+++ lib/freshcaller/migration.rb
@@ -11,12 +11,12 @@ module Freshcaller
 
     def migrate_account
       FileUtils.mkdir_p account_migration_location
-      response = create_freshcaller_account(current_account.id)
+      response = create_freshcaller_account(current_account.id) if params[:account_creation]
       save_freshcaller_account(response)
       disable_freshfone_feature
       enable_freshcaller_feature
       retrieve_account_details
-      upload_to_s3(response[:freshcaller_account_id])
+      upload_to_s3(params[:freshcaller_account_id] || response[:freshcaller_account_id])
       initiate_migration(response)
     end
 
@@ -24,8 +24,8 @@ module Freshcaller
       Rails.logger.info "Save Freshcaller account :: Account :: #{current_account.id}"
       Freshcaller::Account.create(
         account_id: current_account.id,
-        freshcaller_account_id: response[:freshcaller_account_id],
-        domain:  response[:freshcaller_account_domain]
+        freshcaller_account_id: params[:freshcaller_account_id] || response[:freshcaller_account_id],
+        domain:  params[:freshcaller_account_domain] || response[:freshcaller_account_domain]
       )
     end
 
@@ -54,21 +54,27 @@ module Freshcaller
     end
 
     def upload_to_s3(freshcaller_account_id)
+      Rails.logger.info "S3 Upload :: Account :: #{current_account.id}"
       Dir.foreach(account_migration_location) do |file|
         next if ['.', '..'].include?(file)
         data = File.read(account_migration_location + file)
-        AwsWrapper::S3Object.store(S3_LOCATION + "/#{freshcaller_account_id}/#{file}", data, FreshcallerConfig['s3_bucket'], content_type: 'json')
+        AwsWrapper::S3Object.store(S3_LOCATION + "/#{freshcaller_account_id}/#{file}", 
+                                   data, FreshcallerConfig['s3_bucket'],
+                                   { content_type: 'json',
+                                    acl: 'bucket-owner-full-control' })
       end
       FileUtils.rm_rf(account_migration_location)
     end
 
     def initiate_migration(account_detail)
+      Rails.logger.info "Freshcaller request to migrate for Account :: #{current_account.id}"
       protocol = Rails.env.development? ? 'http://' : 'https://'
       freshcaller_request({ helpkit_account: current_account.id,
                             requestor: params[:email],
                             helpkit_domain: current_account.full_domain,
-                            account_id: account_detail[:freshcaller_account_id] },
-                            "#{protocol}#{FreshcallerConfig['domain_prefix']}#{account_detail[:freshcaller_account_domain]}/migrate",
+                            account_id: params[:freshcaller_account_id] || account_detail[:freshcaller_account_id] },
+                            #"#{protocol}#{FreshcallerConfig['domain_prefix']}#{params[:freshcaller_account_domain] || account_detail[:freshcaller_account_domain]}/migrate"
+                            "#{protocol}#{FreshcallerConfig['domain_prefix']}d2badcff.ngrok.io:3000/migrate",
                             :post)
     end
   end
