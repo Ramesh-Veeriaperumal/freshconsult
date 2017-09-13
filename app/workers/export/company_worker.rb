c2457d092bdd5438f99d3ca52060f1a0b1da7c2c
selvakumar784 <selvakumarn@freshdesk.com>
2017-09-13 17:55:34 +0530

    FDCORE-6732: Moving company export to sidekiq
    This commit contains the changes that are needed to move company export from Resque to Sidekiq.

---
+++ app/workers/export/company_worker.rb
@@ -0,0 +1,13 @@
+class Export::CompanyWorker
+  include Sidekiq::Worker
+
+  sidekiq_options :queue => :company_export, :retry => 0, :backtrace => true,
+                  :failures => :exhausted
+
+  def perform(args)
+    args.symbolize_keys!
+
+    customer_export = Export::Customer.new(args[:csv_hash], args[:portal_url], "company")
+    customer_export.export_data
+  end
+end 
