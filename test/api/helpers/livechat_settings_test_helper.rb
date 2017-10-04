c43341e070590082e1d9635ad0bf65c5fc3d37ac
vigneswaranm <vigneswaran.murugesan@freshworks.com>
2017-10-04 17:16:35 +0530

    Falcon livechat backend (#736)
    * Livechat falcon backend implementaion
    
    * refactor as per shyam's suggestions
    
    * tests for verifying that we are sending data for initializing chat in Ember
    
    * thansyham code review - we got rid of the function
    
    We are managing the data for agent groups directly from the ember store.
    Data for group options is not required
    
    * fixed as per review comments
    
    removed AGENT_GROUP_OPTIONS and AGENTS_IN_GROUPS from test case
    now that it's not being transmitted int he chat settings decorator
    
    * reverting assert compilation as it is we added this code to our brach to get work

---
+++ test/api/helpers/livechat_settings_test_helper.rb
@@ -0,0 +1,46 @@
+
+module LivechatSettingsTestHelper
+
+  #test_setup
+  # Account.current.chat_setting = ""
+
+
+
+  def chat_settings_pattern
+      {
+          CURRENT_ACCOUNT: {
+              id: Account.current.id,
+              name: Account.current.name ,
+              product_id: Account.current.portals.first.product ? Account.current.portals.first.product.id : "",
+              domain: Account.current.full_domain,
+              chat_enabled: Account.current.chat_setting.enabled,
+              # current_chat_widget.widget_id - note - this is defined in main_chat_widget
+              widget_id: Account.current.main_chat_widget.widget_id,
+              chat_routing: Account.current.has_feature?(:chat_routing),
+              cobrowsing: Account.current.has_feature?(:cobrowsing)
+          },
+          CURRENT_USER: {
+              id: @agent.id,
+              username: @agent.name,
+              isAdmin: @agent.privilege?(:admin_tasks),
+              email: @agent.email,
+              groups: @agent.agent_groups.collect{|g| g.group_id},
+              language: @agent.language,
+              time_zone_offset: Time.now.in_time_zone(@agent.time_zone).utc_offset,
+              falcon_enabled: @agent.is_falcon_pref?
+          },
+          CHAT_DEBUG: ChatConfig['chat_debug'],
+          LIVECHAT_APP_ID: ChatConfig['app_id'],
+          SITE_ID: Account.current.chat_setting.site_id,
+          CHAT_CONSTANTS:  ChatSetting::CHAT_CONSTANTS_BY_KEY,
+          # LIVECHAT_TOKEN: livechat_token(Account.current.chat_setting.site_id, @agent.id),
+          LIVECHAT_TOKEN: String,
+          ASSET_URL: ChatConfig['asset_url'],
+          CS_URL: ChatConfig['communication_url'],
+          CHAT_ENV: Rails.env,
+          FC_HTTP_ONLY: ChatConfig['http_only'] == 1
+      }
+      
+    
+  end
+end
