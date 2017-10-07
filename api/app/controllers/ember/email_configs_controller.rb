4e7f47219d9d411ad5627d87cc4fee972ce17924
Abinaya <abinaya.sarathi@freshworks.com>
2017-10-07 15:17:48 +0530

    [FDCORE-7766] Email config bug fix (#773)
    Default limit extended to 100
    Active filter applied

--- api/app/controllers/ember/email_configs_controller.rb
+++ api/app/controllers/ember/email_configs_controller.rb
@@ -4,5 +4,15 @@ module Ember
       super
       response.api_meta = { count: @items_count }
     end
+
+    private
+      def scoper
+	  	super.where(active: true)
+	  end
+
+	  def per_page
+	  	# Temporary hack to make sure more than 30 email configs are fetched
+	  	(params[:per_page] || ApiConstants::DEFAULT_PAGINATE_OPTIONS[:max_per_page]).to_i
+	  end
   end
 end
