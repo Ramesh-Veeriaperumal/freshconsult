723cab0ad4622a78e820d53c64fdca0f1d52f7db
Tharakai Natesan <tharakai.natesan@freshworks.com>
2017-10-26 12:48:21 +0530

    FDCORE-6422 Sub-domain takeover of deleted/closed accounts with new signup (#839)
    Validate the domain ownership before adding any custom domain by using a TXT record

--- lib/custom_domain/cname_validator.rb
+++ lib/custom_domain/cname_validator.rb
@@ -3,27 +3,36 @@ module CustomDomain
     include Dnsruby
 
     DNS_QUERY_TIMEOUT_SECS = 2
+    FDKEY_PREFIX = 'fdkey.'
 
-    def initialize(domain, valid_fd_domains)
+    def initialize(domain, valid_fd_domains, verification_hash)
       @domain = domain
       @valid_fd_domains = valid_fd_domains
+      @verification_hash = verification_hash
     end
 
-    def allow?
-      cname_mapping_present
+    def mapping_error
+      return 'flash.portal.update.invalid_cname' unless cname_mapping_present
+      return 'flash.portal.update.invalid_hash_mapping' unless hash_mapping_present
     end
 
     private
 
       def cname_mapping_present()
-        dns_response = retrieve_dns_records(@domain)
+        dns_response = retrieve_dns_records(@domain, Types.CNAME)
         return false unless dns_response.present?
         dns_response.any? { |dns_rec| @valid_fd_domains.include?(dns_rec.cname.to_s.chomp('.')) }
       end
 
-      def retrieve_dns_records(domain)
+      def hash_mapping_present
+        dns_response = retrieve_dns_records(FDKEY_PREFIX + @domain, Types.TXT)
+        return false unless dns_response.present?
+        dns_response.any? { |dns_rec| dns_rec.rdata.to_s.include?(@verification_hash) }
+      end
+
+      def retrieve_dns_records(domain, type)
         dns = Dnsruby::Resolver.new(do_caching: false, query_timeout: DNS_QUERY_TIMEOUT_SECS)
-        dns.query(domain, Types.CNAME).answer
+        dns.query(domain, type).answer
       rescue Dnsruby::NXDomain
         Rails.logger.debug("Domain [#{domain}] does not exist")
         []
