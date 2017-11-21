5bf1091bc7cdc4858420b02045ad1b8ada8c7d7a
Rajeshguna <rajesh.gunasekaran@freshworks.com>
2017-11-21 15:37:05 +0530

    FD-49, FD-1277, FD-1129: Falcon squad 15 11 (#964)
    * Falcon requester add company (#823)
    
    * FDCORE-6631: Add company option in requester widget
    
    - validation added
    - ticket and contact association with new company
    
    
    * FDCORE-: 49: Add company option in requester widget
    
    new company
    
    * FD-49: Add company option in requester widget
    
    -test case
    
    * FD-1129: Requester widget - tag change (#952)
    
    * FD-1129: Requester widget - tag change
    
    - rename tag_names to tags in requester restricted hash
    
    * FD-1129: Requester widget - tag change
    
    - test case fix

--- test/api/functional/ember/tickets/requester_controller_test.rb
+++ test/api/functional/ember/tickets/requester_controller_test.rb
@@ -13,6 +13,7 @@ module Ember
         create_company_field(company_params(type: 'text', field_type: 'custom_text', label: 'Place', name: 'cf_place', field_options: { 'widget_position' => 7 }))
         create_contact_field(cf_params(type: 'text', field_type: 'custom_text', label: 'Position', name: 'cf_position', required_for_agent: 'true', editable_in_signup: 'true', field_options: { 'widget_position' => 8 }))
         Account.current.stubs(:enabled_features_list).returns([:requester_widget])
+        @account.reload
       end
 
       def teardown
@@ -58,7 +59,7 @@ module Ember
         params_hash = { contact: { email: Faker::Internet.email, custom_fields: {} }, company: { domain: Faker::Lorem.word, custom_fields: {} } }
         put :update, construct_params({ version: 'private', id: @ticket.display_id }, params_hash)
         assert_response 400
-        match_json([bad_request_error_pattern(:email, :invalid_field)])
+        match_json([bad_request_error_pattern(:domain, :invalid_field)])
       end
 
       def test_update_requester_with_invalid_datatype_params
@@ -86,6 +87,53 @@ module Ember
         assert_equal params_hash[:contact][:name], @ticket.requester.name
         assert_equal params_hash[:contact][:custom_fields][:position], @ticket.requester.custom_field['cf_position']
         assert_equal params_hash[:company][:custom_fields][:place], @ticket.company.custom_field['cf_place']
+      end
+
+      def test_update_requester_with_associated_company_name
+        #ticket update with associtated company name
+        @account.reload
+        params_hash = { contact: { name: Faker::Lorem.word, custom_fields: { position: Faker::Lorem.word } }, company: { name: Faker::Lorem.word,   custom_fields: { place: Faker::Lorem.word } } }
+        put :update, construct_params({ version: 'private', id: @ticket.display_id }, params_hash)
+        assert_response 400
+        match_json([bad_request_error_pattern(:name, :invalid_field)])
+      end
+
+      def test_update_requester_with_agent_as_requester
+        agent = add_test_agent(@account)
+        ticket = create_ticket(ticket_params_hash.merge(requester_id: agent.id))
+        params_hash = { contact: { name: Faker::Lorem.word, custom_fields: { position: Faker::Lorem.word } }, company: { custom_fields: { place: Faker::Lorem.word } } }
+        put :update, construct_params({ version: 'private', id: ticket.display_id }, params_hash)
+        assert_response 403
+        match_json(request_error_pattern(:action_restricted,  action: :requester_update, reason: 'requester is agent'))
+      end
+
+      def test_update_requester_with_new_company
+        #contact and ticket without company
+        user  = add_new_user(@account)
+        ticket = create_ticket(ticket_params_hash.merge(requester_id: user.id))
+        params_hash = { contact: { name: Faker::Lorem.word, custom_fields: { position: Faker::Lorem.word } }, company: { name: Faker::Lorem.word,   custom_fields: { place: Faker::Lorem.word } } }
+        put :update, construct_params({ version: 'private', id: ticket.display_id }, params_hash)
+        assert_response 200
+        response = parse_response @response.body
+        user.reload
+        ticket.reload
+        assert_equal user.customer_id, response['company']['id']
+        assert_equal ticket.owner_id, response['company']['id']
+      end
+
+      def test_update_requester_with_new_company_2
+        #ticket with deleted company
+        company = create_company
+        user  = add_new_user(@account, customer_id: company.id)
+        ticket = create_ticket(ticket_params_hash.merge(requester_id: user.id))
+        company.destroy
+        @account.reload
+        params_hash = { contact: { name: Faker::Lorem.word, custom_fields: { position: Faker::Lorem.word } }, company: { name: Faker::Lorem.word,   custom_fields: { place: Faker::Lorem.word } } }
+        put :update, construct_params({ version: 'private', id: ticket.display_id }, params_hash)
+        assert_response 200
+        response = parse_response @response.body
+        assert_equal params_hash[:contact][:name], response['contact']['name']
+        assert_equal nil, response['company']
       end
     end
   end
