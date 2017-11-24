86239c204feab9bf8079a09035dd3d00688d5fc1
Keerthana Raghavan <keerthana.raghavan@freshworks.com>
2017-11-24 12:29:51 +0530

    Fix for test failures
    Made changes to api_agents_dependency_test and auth_helper_test

--- test/api/unit/auth_helper_test.rb
+++ test/api/unit/auth_helper_test.rb
@@ -2,10 +2,15 @@ require_relative '../unit_test_helper'
 
 class AuthHelperTest < ActionView::TestCase
   def test_get_email_user_deleted
+    account = mock
+    account.stubs(:features_included?).returns(false)
+    account.stubs(:id).returns(1)
+    Account.stubs(:current).returns(account)
     user = mock
     user.stubs(:deleted).returns(true)
     User.expects(:find_by_user_emails).with('sample@freshdesk.com').returns(user).once
     assert_nil AuthHelper.get_email_user('sample@freshdesk.com', 'test', nil)
+    Account.unstub(:current)
   end
 
   def test_get_email_user_invalid
@@ -18,12 +23,17 @@ class AuthHelperTest < ActionView::TestCase
   end
 
   def test_get_email_user_invalid_password
+    account = mock
+    account.stubs(:features_included?).returns(false)
+    account.stubs(:id).returns(1)
+    Account.stubs(:current).returns(account)
     user = mock
     user.stubs(:deleted).returns(false)
     user.stubs(:valid_password?).returns(false)
     user.expects(:update_failed_login_count).with(false, 'sample@freshdesk.com', nil).returns(nil)
     User.expects(:find_by_user_emails).with('sample@freshdesk.com').returns(user).once
     assert_nil AuthHelper.get_email_user('sample@freshdesk.com', 'test', nil)
+    Account.unstub(:current)
   end
 
   def test_get_token_user_invalid
@@ -36,9 +46,14 @@ class AuthHelperTest < ActionView::TestCase
   end
 
   def test_get_token_user_deleted
+    account = mock
+    account.stubs(:features_included?).returns(false)
+    account.stubs(:id).returns(1)
+    Account.stubs(:current).returns(account)
     user = mock
     user.stubs(:deleted).returns(true)
     User.expects(:where).with(single_access_token: 'wwwtoken').returns([user]).once
     assert_nil AuthHelper.get_token_user('wwwtoken')
+    Account.unstub(:current)
   end
 end
