bef6bc127f2b0e2b89e4b7b0dba2b958b083c511
akila <akila@freshdesk.com>
2017-12-01 13:30:40 +0530

    FD-1518 - reviwe comments

--- lib/falcon_helper_methods.rb
+++ lib/falcon_helper_methods.rb
@@ -1,4 +1,6 @@
 module FalconHelperMethods
+  include Cache::LocalCache
+
   def falcon_redirect_check(root_path)
     if current_account && current_account.falcon_ui_enabled?(current_user)
       "parent.location.href='#{root_path}'"
@@ -7,22 +9,22 @@ module FalconHelperMethods
     end
   end
 
-  def mint_supported_languages
-    @mint_langs ||= $redis_others.perform_redis_op("smembers", "FALCON_ENABLED_LANGUAGES")
+  def available_mint_locales
+    @mint_langs ||= fetch_lcached_set(FALCON_ENABLED_LANGUAGES, 5.minutes)
+  end
+
+  def mint_locale_names
+    @mint_langs_name ||= I18n.available_locales_with_name.select {|loc| available_mint_locales.include?(loc[1].to_s)}
   end
 
   def account_and_user_in_mint_langs?
-    mint_langs = mint_supported_languages
+    mint_langs = available_mint_locales
     mint_langs.include?(current_account.language) && current_user && mint_langs.include?(current_user.language)
   end
 
-  def mint_languages_hash(alert=false)
-    @mint_languages ||= I18n.available_locales_with_name.select {|loc| mint_supported_languages.include?(loc[1].to_s)}
-    if alert
-      @mint_languages.concat([["-----", {:disabled => true}], 
-              ["Coming soon in your language", {:disabled => true}]])
-    end
-    @mint_languages
+  def mint_locales_with_separator
+    mint_locale_names + [["----------", {:disabled => true}], 
+            ["Coming soon in your language", {:disabled => true}]]
   end
 
 end
