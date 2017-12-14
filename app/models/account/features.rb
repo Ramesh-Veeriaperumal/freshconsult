2aaeb3596951cf21f3ab8bb4acf32811b5df2b69
Sathishkumar CR <sathishkumar.ravi@freshworks.com>
2017-12-14 19:35:08 +0530

    Resolving merge conflict in feature.rb for archive ghost test failure

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -2,7 +2,7 @@ class Account < ActiveRecord::Base
 
   LP_FEATURES   = [:link_tickets, :select_all, :round_robin_capping, :suggest_tickets, :customer_sentiment_ui,
                    :dkim, :bulk_security, :scheduled_ticket_export, :ticket_contact_export,
-                   :email_failures, :disable_emails, :skip_one_hop, :falcon_portal_theme, :freshid, :freshchat_integration, :smart_filter]
+                   :email_failures, :disable_emails, :skip_one_hop, :falcon_portal_theme, :freshid, :freshchat_integration, :smart_filter, :archive_ghost]
   DB_FEATURES   = [:shared_ownership, :custom_survey, :requester_widget, :archive_tickets, :sitemap]
   BITMAP_FEATURES = [
     :split_tickets, :add_watcher, :traffic_cop, :custom_ticket_views, :supervisor, :create_observer, :sla_management,
