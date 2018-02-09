94f296870a9e9d31977fdde0a7b188e1dd852338
Satish ASK <satish.kalaiselvan@freshworks.com>
2018-02-09 17:19:06 +0530

    fixes for companies api

--- api/app/controllers/constants/company_constants.rb
+++ api/app/controllers/constants/company_constants.rb
@@ -11,8 +11,6 @@ module CompanyConstants
   ACTIVITIES_FIELDS = %w(type).freeze
   BULK_ACTION_METHODS = [:bulk_delete].freeze
 
-  FIELDS = %w(name description note health_score
-              account_tier renewal_date industry).freeze | ARRAY_FIELDS | HASH_FIELDS
   FIELD_MAPPINGS = { :"company_domains.base" => :domains, :"company_domains.domain" => :domains }.freeze
   DEFAULT_DROPDOWN_FIELDS = %i(health_score account_tier industry)
   VALIDATABLE_DELEGATOR_ATTRIBUTES = %w(custom_field health_score account_tier industry).freeze
