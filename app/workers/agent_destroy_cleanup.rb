c943f9600416173881882c2e7afc084d0ba40e1e
Sathishkumar CR <sathishkumarc@freshdesk.com>
2018-03-01 11:22:19 +0530

    FD-3644 agent destroy cleanup for leaderboard (#1346)
    Signed-off-by: Sathishkumar CR <sathishkumar.ravi@freshworks.com>

--- app/workers/agent_destroy_cleanup.rb
+++ app/workers/agent_destroy_cleanup.rb
@@ -2,6 +2,7 @@ class AgentDestroyCleanup < BaseWorker
 
   include Redis::RedisKeys
   include Redis::SortedSetRedis
+  include MemcacheKeys
 
   sidekiq_options :queue => :agent_destroy_cleanup, :retry => 0, :backtrace => true, :failures => :exhausted
 
@@ -18,6 +19,7 @@ class AgentDestroyCleanup < BaseWorker
         destroy_agents_personal_items
         delete_user_associated
         delete_user_from_leaderboard
+        clear_leaderboard_response_cache
       end
     rescue Exception => e
       puts e.inspect, args.inspect
@@ -41,12 +43,18 @@ class AgentDestroyCleanup < BaseWorker
     end
 
     def delete_user_from_leaderboard
+      user = @account.users.find_by_id(args[:user_id])
+      group_ids = user.agent_groups.pluck(:group_id)
       (0..3).each do |months_counter|
         delete_month = months_counter.month.ago(Time.now.in_time_zone(@account.time_zone)).month
         [:mvp, :love, :sharpshooter, :speed].each do |category|
           key = agents_leaderboard_key @account.id, category, delete_month
-
           delete_member_sorted_set_redis key, args[:user_id]
+
+          group_ids.each do |group_id|
+            group_agents_key = group_agents_leaderboard_key @account.id, category, delete_month, group_id
+            delete_member_sorted_set_redis group_agents_key, args[:user_id]
+          end
         end
       end
     end
@@ -54,5 +62,26 @@ class AgentDestroyCleanup < BaseWorker
     def agents_leaderboard_key account_id, category, month
       GAMIFICATION_AGENTS_LEADERBOARD % { :account_id => account_id, :category => category, :month => month }
     end
-end
 
+    def group_agents_leaderboard_key account_id, category, month, group_ids
+      GAMIFICATION_GROUP_AGENTS_LEADERBOARD % { :account_id => account_id, :category => category, :month => month, :group_id => group_id }
+    end
+
+    def clear_leaderboard_response_cache
+      users = @account.technicians.includes(:agent_groups).find(:all)
+      users.each do |user|
+        MemcacheKeys.delete_from_cache(account_leaderboard_widget_cache_key(user.id))
+        user.agent_groups.each do |agent_group|
+          MemcacheKeys.delete_from_cache(group_agents_leaderboard_widget_cache_key(user.id, agent_group.group_id))
+        end
+      end
+    end
+
+    def group_agents_leaderboard_widget_cache_key(user_id, group_id)
+      GROUP_AGENTS_LEADERBOARD_MINILIST_REALTIME_FALCON % { account_id: @account.id, user_id: user_id, group_id: group_id }
+    end
+
+    def account_leaderboard_widget_cache_key(user_id)
+      LEADERBOARD_MINILIST_REALTIME_FALCON % { account_id: @account.id, user_id: user_id }
+    end
+end
