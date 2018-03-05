fa77e5a9d5203bf37511da34c06891dcf5bfdf53
Muthu1391 <muthu.kumar@freshworks.com>
2018-03-05 15:48:25 +0530

    FD-3660-email-DKIM-changes
    Sendgrid has introduced a limit of 1 DKIM configuration/sec.
    Create two set of whitelables with two different user api keys
    Putting a lock in order to avoid multiple configuration calls to sendgrid per sec and if any lock acquired by call it will retry for 10 times with linear increment delays.

--- lib/dkim/configure_dkim_record.rb
+++ lib/dkim/configure_dkim_record.rb
@@ -21,17 +21,31 @@ class Dkim::ConfigureDkimRecord
 
   private
     def add_whitelabel_to_domain
-      response_1 = make_api(SG_URLS[:create_domain][:request], SG_URLS[:create_domain][:url], build_dkim_record(domain_category.email_domain))
-      response_2 = make_api(SG_URLS[:create_domain][:request], SG_URLS[:create_domain][:url], build_dkim_record_1(domain_category.email_domain))
-      save_sg_response(response_1, response_2)
+      response = request_configure
+      save_sg_response(response[:record_1], response[:record_2])
       filter_dkim_records
+    end
+
+    def request_configure
+      #lock in order to avoid multiple configuration calls to sendgrid/1 sec
+      lock_dkim_configuration_in_progress
+      response_1 = make_api(SG_URLS[:create_domain][:request], SG_URLS[:create_domain][:url], build_dkim_record(domain_category.email_domain), SENDGRID_CREDENTIALS[:dkim_key][:user1])
+      response_2 = make_api(SG_URLS[:create_domain][:request], SG_URLS[:create_domain][:url], build_dkim_record_1(domain_category.email_domain), SENDGRID_CREDENTIALS[:dkim_key][:user2])
+      return {:record_1 => response_1, :record_2 => response_2}
     end
 
     def save_sg_response(response_1, response_2)
       Rails.logger.debug("save_sg_response ....... #{response_1.inspect} #{response_2.inspect}")
-      if ([response_1[0], response_2[0]] - VALID_RESPONSE_CODE).empty?
+      response_codes = [response_1[0], response_2[0]]
+      if response_codes.uniq.count == 1 && response_codes.first == SENDGRID_RESPONSE_CODE[:created]
         create_dkim_records(response_1[1], RECORD_TYPES[:res_1], domain_category.category)
         create_dkim_records(response_2[1], RECORD_TYPES[:res_2])
+      else
+        if SENDGRID_RESPONSE_CODE[:success].in?(response_codes)
+          succeed_result = response_1[0] == SENDGRID_RESPONSE_CODE[:success] ? response_1[1] : response_2[1]
+          make_api(SG_URLS[:delete_domain][:request], SG_URLS[:delete_domain][:url] + succeed_result[:id].to_s)
+        end
+        raise "DKIM configuration failed" if SENDGRID_RESPONSE_CODE[:too_many_requests].in?(response_codes)
       end
     end
 
