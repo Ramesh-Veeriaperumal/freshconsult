b975c7b49d88f33f5ad914627da8874a331d49aa
arvinthj <arvinth.jagannathan@freshworks.com>
2018-03-21 16:38:44 +0530

    [FD-4834] set_current_account for edit_domain (#1533)
    1. set_current_account done for edit_domain, validate_domain and update_domain
    2. Duplicate key removed in en and de locale files
    3. Added onboarding_inlinemanual to LP_FEATURES constant. The feature can now be enabled through freshopsadmin for staging accounts.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -15,7 +15,7 @@ class AccountsController < ApplicationController
 
   skip_before_filter :set_locale, :except => [:cancel, :show, :edit, :manage_languages, :edit_domain]
   skip_before_filter :set_time_zone, :set_current_account,
-    :except => [:cancel, :edit, :update, :delete_logo, :delete_favicon, :show, :manage_languages, :update_languages]
+    :except => [:cancel, :edit, :update, :delete_logo, :delete_favicon, :show, :manage_languages, :update_languages, :edit_domain, :validate_domain, :update_domain]
   skip_before_filter :check_account_state
   skip_before_filter :redirect_to_mobile_url
   skip_before_filter :check_day_pass_usage, 
