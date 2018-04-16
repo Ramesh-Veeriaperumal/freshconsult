d48edbb3ddee959be95870587c316f3d306eaf06
Jayaseelan <jayaseelan@freshdesk.com>
2018-04-16 17:04:38 +0530

    Missed while resolving the conflicts
    Signed-off-by: Jayaseelan <jayaseelan@freshdesk.com>

--- lib/helpdesk/initializers/githooks.rb
+++ lib/helpdesk/initializers/githooks.rb
@@ -1,11 +1,4 @@
 if Rails.env.development?
-<<<<<<< HEAD
-  module GitHooksInit
-    def self.link_script(script_name)
-      `rm "#{Rails.root}/.git/hooks/#{script_name}"`
-      `ln -s "#{Rails.root}/script/githooks/#{script_name}" "#{Rails.root}/.git/hooks/#{script_name}"`
-      `chmod +x "#{Rails.root}/.git/hooks/#{script_name}"`
-=======
   # Sets up git hooks when starting up rails server or console
   module GitHooksInit
     def self.link_script(script_name)
@@ -13,7 +6,6 @@ if Rails.env.development?
       `rm "#{rails_root}/.git/hooks/#{script_name}"`
       `ln -s "#{rails_root}/script/githooks/#{script_name}" "#{rails_root}/.git/hooks/#{script_name}"`
       `chmod +x "#{rails_root}/.git/hooks/#{script_name}"`
->>>>>>> origin/staging
     end
 
     def self.add_scripts
