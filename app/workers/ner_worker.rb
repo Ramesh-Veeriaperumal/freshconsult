ff9fb2d788603b5f4861aeb154f6ca389b586b6f
Gokul S <gokul.subramanian@freshworks.com>
2018-04-19 21:48:34 +0530

    FD-4932:Exclude quoted text during NER API parsing (#1548)
    FD-4932:Exclude quoted text during NER API parsing
    1) Exclude Quoted text while sending text to NER API
    2) Add validation before NER API call -> Only for incoming customer notes.
    3) Add Timezone fallback value if the user has invalid timezone

--- app/workers/ner_worker.rb
+++ app/workers/ner_worker.rb
@@ -1,4 +1,3 @@
-
 # NER API - Name Entity Recognizer (https://duckling.wit.ai/ for more info).
 # API Params - text (data), user_id & client_id (customer & client), username (for authorization).
 
@@ -6,7 +5,7 @@
 # we try to fetch email from the object that is passed, provided there is an association with the name 'user'
 
 # This worker takes following params : obj_typ (Corresponding association name from Account/associations eg: notes, articles etc.,),
-# obj_id, user_email (for user_id), text (data) & html(optional param => To improve performance we send text data to api and will convert to html indexes from text indexes)
+# obj_id, user_email (for user_id), text (data), html(optional param => To improve performance we send text data to api and will convert to html indexes from text indexes)
 
 # As worker has customer & client information, encrypting both before passing it to API.
 # API returns datetime information which is then passed on to a hook method store_ner_data.
@@ -16,6 +15,7 @@ class NERWorker < BaseWorker
 
   sidekiq_options :queue => :ner_worker, :retry => 0, :backtrace => true, :failures => :exhausted
   MAXIMUM_LENGTH = 3000
+  UTC_TIMEZONE = "Etc/UTC"
 
   def perform(args)
     args.symbolize_keys!
@@ -25,9 +25,11 @@ class NERWorker < BaseWorker
 
     user_email = obj.user.email || fetch_pii(obj)
 
-    timezone = ActiveSupport::TimeZone.zones_map[obj.user.time_zone].tzinfo.name
+    timezone = fetch_tzinfo(obj)
 
-    req_body = {  text: args[:text].to_s.first(MAXIMUM_LENGTH), #Sending only first 3000 characters to api because the api response time is more than 4sec for the string length >3000
+    text =  exclude_quoted_text?(obj) ? exclude_quoted_text(args[:html]) : args[:text]
+
+    req_body = {  text: text.to_s.first(MAXIMUM_LENGTH), #Sending only first 3000 characters to api because the api response time is more than 4sec for the string length >3000
                   user_id: encrypt_pii(user_email),
                   client_id: encrypt_pii(obj.account.full_domain),
                   username: NER_API_TOKENS['username'],
@@ -42,7 +44,7 @@ class NERWorker < BaseWorker
 
     unless ner_data['datetimes'].empty?
       ner_data = NER::HtmlIndexTransformer.new( :ner_data => ner_data, 
-        :text => args[:text], :html => args[:html]).perform if args[:html]
+        :text => text, :html => args[:html]).perform if args[:html]
       obj.store_ner_data(ner_data)
     end
   end
@@ -57,5 +59,27 @@ class NERWorker < BaseWorker
 
   def encrypt_pii(key)
     Encryptor.encrypt(value: key, key: NER_API_TOKENS['secret_key'], iv: NER_API_TOKENS['iv'])
+  end
+
+  # If it's a note & body & full_text length are same, it is possible that body will have quoted_text in it. 
+  def exclude_quoted_text?(obj)
+    (obj.class.name == "Helpdesk::Note") && (obj.body_html.length == obj.full_text_html.length)
+  end
+
+  def exclude_quoted_text(html_text)
+    body_text = Nokogiri::HTML(html_text)
+    body_text.search('.freshdesk_quote').remove
+    text = Helpdesk::HTMLSanitizer.plain(body_text.to_html)
+  end
+
+  def fetch_tzinfo(obj)
+    user_timezone = get_timezonemap(obj.user.time_zone)
+    user_timezone ? user_timezone.tzinfo.name : get_timezonemap(obj.account.time_zone).tzinfo.name
+    rescue
+      UTC_TIMEZONE 
+  end
+
+  def get_timezonemap(tz)
+    ActiveSupport::TimeZone.zones_map[tz]
   end
 end
