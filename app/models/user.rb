f2a1b9760c6e142b094d8606ef06cfc53c96ae75
keerthi-priya <keerthi.priya@freshworks.com>
2018-05-03 16:08:55 +0530

    FD-3480-search company users falcon - updating ES (#1714)
    Search to find a contact (linked to multiple companies) fails when searched by another contact, but works fine if an agent searches the contact
    
    FD-7288 update ES when contact’s company is updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -140,7 +140,8 @@ class User < ActiveRecord::Base
   end
 
   attr_accessor :import, :highlight_name, :highlight_job_title, :created_from_email, :sbrr_fresh_user,
-                :primary_email_attributes, :tags_updated, :keep_user_active, :escape_liquid_attributes, :role_ids_changed, :detect_language # (This role_ids_changed used to forcefully call user callbacks only when role_ids are there.
+                :primary_email_attributes, :tags_updated, :keep_user_active, :escape_liquid_attributes,
+                :role_ids_changed, :detect_language, :user_companies_updated  # (This role_ids_changed used to forcefully call user callbacks only when role_ids are there.
   # As role_ids are not part of user_model(it is an association_reader), agent.update_attributes won't trigger user callbacks since user doesn't have any change.
   # Hence user.safe_send(:attribute_will_change!, :role_ids_changed) is being called in api_agents_controller.)
 
