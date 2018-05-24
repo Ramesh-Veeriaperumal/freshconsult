f11caded884b2d53b3d412415ef7f29ebf3d2940
Ramkumar S <ramkumar.shanmugam@freshworks.com>
2018-05-24 22:49:29 +0530

    FD-9817 Bug fix for resque to sidekiq: Add agent to round robin
    Bug fix for resque to sidekiq: Add agent to round robin
    
    Signed-off-by: Ramkumar S <ramkumar.shanmugam@freshworks.com>

--- app/workers/groups/add_agent_to_round_robin.rb
+++ app/workers/groups/add_agent_to_round_robin.rb
@@ -5,15 +5,15 @@ class Groups::AddAgentToRoundRobin < BaseWorker
                   failures: :exhausted
 
   def perform(args)
-    args.symbolize_keys!
+    params = args.deep_symbolize_keys
     account = Account.current
-    agent = account.agents.find_by_user_id(args[:user_id])
-    group = account.groups.round_robin_groups.find_by_id(args[:group_id])
-    group.add_or_remove_agent(args[:user_id]) if agent.available?
+    agent = account.agents.find_by_user_id(params[:user_id])
+    group = account.groups.round_robin_groups.find_by_id(params[:group_id])
+    group.add_or_remove_agent(params[:user_id]) if agent.available?
   rescue => e
     Rails.logger.error "Error while adding agent via round robin 
       \n#{e.message}\n#{e.backtrace.join("\n\t")}"
-    NewRelic::Agent.notice_error(e, {:args => args})
+    NewRelic::Agent.notice_error(e, {:args => params})
   end
 
 end
