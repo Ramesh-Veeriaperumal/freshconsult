96a3c2c6243e6d83169d8cd52d1806eb32ef5479
Premchandar S <premchandar@freshdesk.com>
2018-05-24 20:06:29 +0530

    FD-7550, FD-7551: Helpkit emoji issue (#1604)
    * FD-5498 emoji issue fix
    
    * error message changed
    
    * FD-5498 review chagnes for emoji fix.Rename file and methods
    
    FD-5498 review chagnes for emoji fix.Rename file and methods
    
    *  FD-7554: Helpkit fix for supporting emojis in FB posts/DMs (#1935)
    
    * FD-7554: Handling emojis in tickets/note on direct messages
    
    * FD-7554: Handling emojis in tickets/note on realtime direct messages
    
    * FD-7554: Handling emojis in tickets/note on posts
    
    * FD-7552: Handling emojis in ticket subject and ticket/note desctiption (#1928)

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -6,7 +6,7 @@ class Account < ActiveRecord::Base
                    :year_in_review_2017, :facebook_page_redirect, :announcements_tab, :ticket_central_publish,
                    :solutions_central_publish, :launch_smart_filter, :outgoing_attachment_limit_25,
                    :incoming_attachment_limit_25, :whitelist_sso_login, :apigee, :admin_only_mint, :customer_notes_s3,
-                   :imap_error_status_check, :va_any_field_without_none, :auto_complete_off, :sanbox_lp]
+                   :imap_error_status_check, :va_any_field_without_none, :auto_complete_off, :sanbox_lp, :encode_emoji]
 
   DB_FEATURES   = [:custom_survey, :requester_widget, :archive_tickets, :sitemap, :freshfone]
   BITMAP_FEATURES = [
