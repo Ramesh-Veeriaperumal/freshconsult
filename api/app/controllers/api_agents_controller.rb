a64d0a739e591cd24724422d816521431954755d
Shridar S <shridar.suryamoorthy@freshworks.com>
2018-05-28 14:58:49 +0530

    FD-9969 - Mint agent availability change bug fix
    FD-9969 - Mint agent availability change bug fix

--- api/app/controllers/api_agents_controller.rb
+++ api/app/controllers/api_agents_controller.rb
@@ -14,8 +14,10 @@ class ApiAgentsController < ApiApplicationController
       end
       @item.user_changes = @item.user.agent.user_changes || {}
       @item.user_changes.merge!(@item.user.changes)
-      group_ids = params[cname].delete(:group_ids)
-      @item.build_agent_groups_attributes(group_ids)
+      if params[cname].key?(:group_ids)
+        group_ids = params[cname].delete(:group_ids)
+        @item.build_agent_groups_attributes(group_ids)
+      end
       return if @item.update_attributes(params[cname].except(:user_attributes))
       render_custom_errors
     end
