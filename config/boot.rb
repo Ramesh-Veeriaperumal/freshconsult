be739b6ee4a3b0199f037b88dfe6904c569f9409
Bala Kumar <sribalakumar@users.noreply.github.com>
2018-05-29 19:26:17 +0530

    [FD-7230] Bootsnap inclusion (#1690)
    To begin with it will work only in Dev, later when we ensure the change to be stable recipe will be changed to include ruby env so that it works in all environments.

--- config/boot.rb
+++ config/boot.rb
@@ -4,3 +4,23 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+env = ENV['RAILS_ENV'] || "development"
+
+# First cut, enable Bootsnap in dev alone and monitor.
+# If no issues then include ENV['ENABLE_BOOTSNAP'] in ruby-with-env nginx config for staging/prod.
+
+ENV['ENABLE_BOOTSNAP'] = "true" if env == 'development'
+
+if ENV['ENABLE_BOOTSNAP'] == "true"
+  require 'bootsnap'
+  Bootsnap.setup(
+    cache_dir:            'tmp/cache',
+    development_mode:     env == 'development',
+    load_path_cache:      true,  # Optimize the LOAD_PATH with a cache
+    autoload_paths_cache: false, # Optimize ActiveSupport autoloads with cache
+    disable_trace:        true,  # (Alpha) Set `RubyVM::InstructionSequence.compile_option = { trace_instruction: false }`
+    compile_cache_iseq:   false,  # Compile Ruby code into ISeq cache, breaks coverage reporting.
+    compile_cache_yaml:   false   # Compile YAML into a cache
+  )
+end
