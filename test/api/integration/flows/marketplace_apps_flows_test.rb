74f31865cd975242e70c02ac638bcd4619c3c086
Monisha Victor <monisha.victor@freshworks.com>
2018-07-18 13:39:26 +0530

    FD-12115 Reducing network calls for marketplace apps (#2308)
    * MP-2178 show api for attachments and supporting attach to response for other ticket attachments
    
    * MP-2178 test cases
    
    * MP-2178 updating api route
    
    * MP-2178 checking permission only for other ticket attachments
    
    * MP-2178 review comments
    
    * FD-5478 API to add attachment to response - Fixing tests
    
    Signed-off-by: Mounika Mudda <mounika.mudda@freshworks.com>
    
    * Fixing spec
    
    * minor issue fix
    
    * Fixing update of note
    
    * Adding comment
    
    * Adding comments
    
    * [MP-2231] Request API to support greater timeouts
    
    * Revert "[MP-2231] Request API to support greater timeouts"
    
    This reverts commit 8ee2cfb042350b4c936fbe4adc74c5e698452d88.
    
    * [#MP-2691] - Adding Tenant ID
    
    * MP-1640 Etag changes for Marketplace API (#2032)
    
    * MP-1640 Etag changes for Marketplace API
    
    * Fixing rubocop errors

---
+++ test/api/integration/flows/marketplace_apps_flows_test.rb
@@ -0,0 +1,60 @@
+require_relative '../../test_helper'
+
+class MarketplaceAppsFlowsTest < ActionDispatch::IntegrationTest
+  include Redis::RedisKeys
+  include Redis::OthersRedis
+  include MarketplaceTestHelper
+
+  CURRENT_VERSION = 'private-v1'.freeze
+
+  @@before_all = false
+
+  def setup
+    super
+    stub_marketplace_response
+    before_all
+  end
+
+  def stub_marketplace_response
+    Ember::MarketplaceAppsController.any_instance.stubs(:installed_extensions).returns(installed_extensions_v2)
+    Ember::MarketplaceAppsController.any_instance.stubs(:extension_details_v2).returns(extension_details_v2)
+  end
+
+  def before_all
+    return if @@before_all
+    @@before_all = true
+    @account.add_feature(:falcon)
+  end
+
+  def test_index_without_timestamp
+    get '/api/_/marketplace_apps', nil, @headers
+    assert_response 200
+  end
+
+  def test_index_with_same_etag
+    redis_timestamp = 1.day.ago.to_i
+    $redis_others.hset(version_redis_key, 'MARKETPLACE_APPS_LIST', redis_timestamp)
+    @write_headers = @headers.merge('If-None-Match' => EtagGenerator.generate_etag(redis_timestamp, CURRENT_VERSION))
+    get '/api/_/marketplace_apps', nil, @write_headers
+    assert_response 304
+  end
+
+  def test_index_with_different_etag
+    $redis_others.hset(version_redis_key, 'MARKETPLACE_APPS_LIST', Time.zone.now.to_i)
+    @write_headers = @headers.merge('If-None-Match' => EtagGenerator.generate_etag(1.hour.ago.to_i, CURRENT_VERSION))
+    get '/api/_/marketplace_apps', nil, @write_headers
+    assert_response 200
+  end
+
+  def test_index_without_updating_version_timestamp
+    redis_timestamp = Time.zone.now.to_i
+    $redis_others.hset(version_redis_key, 'MARKETPLACE_APPS_LIST', redis_timestamp)
+    get '/api/_/marketplace_apps', nil, @headers.merge('If-None-Match' => EtagGenerator.generate_etag(1.hour.ago.to_i, CURRENT_VERSION))
+    assert_response 200
+    assert_not_equal get_others_redis_hash_value(version_redis_key, 'MARKETPLACE_APPS_LIST'), redis_timestamp
+  end
+
+  def version_redis_key
+    format(DATA_VERSIONING_SET, account_id: @account.id)
+  end
+end
