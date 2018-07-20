a57c8aee23f46a4084ad30d9de7d7ae3da9a9d3b
Monisha Victor <monisha.victor@freshworks.com>
2018-07-20 14:34:47 +0530

    FD-12445 App configurations(iParams) are not getting updated (#2388)

--- app/controllers/admin/marketplace/installed_extensions_controller.rb
+++ app/controllers/admin/marketplace/installed_extensions_controller.rb
@@ -9,7 +9,7 @@ class Admin::Marketplace::InstalledExtensionsController <  Admin::AdminControlle
   before_filter :extension, :only => [:install, :reinstall, :uninstall, :oauth_callback, :new_configs, :edit_configs, :new_oauth_iparams, :edit_oauth_iparams]
   before_filter :extension_has_config?, :only => [:new_configs, :edit_configs]
   before_filter :verify_billing_info, :only => [:install, :reinstall], :if => :paid_app?
-  after_filter  :update_timestamp, only: [:install, :reinstall, :uninstall, :enable, :disable]
+  after_filter  :update_timestamp, only: [:install, :reinstall, :uninstall, :enable, :disable, :update_config]
 
   rescue_from Exception, :with => :mkp_exception
 
