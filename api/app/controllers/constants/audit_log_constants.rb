a671a167619427ce72627321eac571df7686fc04
Shridar S <shridar.suryamoorthy@freshworks.com>
2018-07-23 15:40:06 +0530

    FD-12393 - Audit Log - Pushing readable names to Central (#2380)
    FD-12393 - Audit Log - Pushing readable names to Central (#2380)

--- api/app/controllers/constants/audit_log_constants.rb
+++ api/app/controllers/constants/audit_log_constants.rb
@@ -14,13 +14,6 @@ module AuditLogConstants
   AUDIT_LOG_PARAMS = [:before, :since].freeze
   FEATURE_NAME = [:audit_logs_central_publish].freeze
 
-  BILLING_CYCLE = {
-      1 => 'subscription_plan.billing_cycle.monthly',
-      3 => 'subscription_plan.billing_cycle.quarterly',
-      6 => 'subscription_plan.billing_cycle.sixmonth',
-      12 => 'subscription_plan.billing_cycle.annual'
-  }.freeze
-
   MODEL_TRANSLATION_PATH = {
       subscription: 'admin.audit_log.subscription.',
       agent: 'admin.audit_log.agent.',
@@ -29,20 +22,9 @@ module AuditLogConstants
       automation_rule: 'admin.audit_log.automation_rule.'
   }.freeze
 
-  AGENT_TICKET_SCOPE = {
-      1 => 'admin.audit_log.agent.global',
-      2 => 'admin.audit_log.agent.group',
-      3 => 'admin.audit_log.agent.individual'
-  }.freeze
-
-  AGENT_AVAILABILITY = {
-      0 => 'admin.audit_log.agent.is_available',
-      1 => 'admin.audit_log.agent.not_available'
-  }.freeze
-
-  AUTOMATION_RULE_TOGGLE = {
-      0 => 'admin.audit_log.automation_rule.rule_off',
-      1 => 'admin.audit_log.automation_rule.rule_on'
+  TOGGLE_ACTIONS = {
+      true => I18n.t('admin.audit_log.toggle_on'),
+      false => I18n.t('admin.audit_log.toggle_off')
   }.freeze
 
   FILTER_PARAMS = [:agent, :time, :type, :observer_id, :dispatcher_id, 
