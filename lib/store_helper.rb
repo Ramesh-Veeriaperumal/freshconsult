a1fdaeb1523f59fd6930ed76f7177182efda2b76
Tharakai Natesan <tharakai.natesan@freshworks.com>
2018-07-26 15:54:16 +0530

    FD-11423 Sensitive Data Exposure - Due to Fragment Cache Enabled
    FD-11423 Sensitive Data Exposure - Due to Fragment Cache Enabled

--- lib/store_helper.rb
+++ lib/store_helper.rb
@@ -1,14 +1,8 @@
-module StoreHelper # Datas to be exposed to clientside
+module StoreHelper
   
   def get_store_data
-    agentslist = current_account.agents_details_from_cache
-    @agents_list ||= agentslist.inject([]) do |res,agent|
-      res << {:id => agent.id, :name => agent.name, :is_account_admin => agent.email == current_account.admin_email}
-    end
-
-    @groups_list ||= current_account.groups_from_cache.inject([]) do |res,group|
-      res << {:id => group.id, :name => group.name}
-    end
+    get_agent_list
+    get_group_list
 
     @products_list ||= current_account.products_from_cache.inject([]) do |res,product|
       res << {:id => product.id, :name => product.name}
@@ -16,6 +10,30 @@ module StoreHelper # Datas to be exposed to clientside
 
     @features_list ||= current_account.features_list
 
-    {:current_user => current_user, :agent => @agents_list, :group => @groups_list, :product => @products_list, :features_list => @features_list, :agents_count => agentslist.count}.to_json
+    {:current_user => current_user, :agent => @agents_list, :group => @groups_list, :product => @products_list, :features_list => @features_list, :agents_count => @agents_list.count}.to_json
+  end
+
+
+  def get_portal_store_data
+    ticket_fields = current_account.ticket_fields_from_cache.select do |field| 
+      ['agent','group'].include?(field.name) && field.editable_in_portal
+    end
+    ticket_fields.each { |fd| safe_send("get_#{fd.name}_list") }
+
+    { agent: @agents_list || [], group: @groups_list || [] }.to_json
+  end
+
+
+  def get_agent_list
+    agentslist = current_account.agents_details_from_cache
+    @agents_list ||= agentslist.inject([]) do |res,agent|
+      res << { id: agent.id, name: agent.name, is_account_admin: agent.email == current_account.admin_email}
+    end
+  end
+
+  def get_group_list
+    @groups_list ||= current_account.groups_from_cache.inject([]) do |res,group|
+      res << { id: group.id, name: group.name }
+    end
   end
 end
