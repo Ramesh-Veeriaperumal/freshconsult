0a33aba3b3b7c5b5a9b8cd4c62d5a9debd52c9d9
Balaji Radhakrishnan <balaji.rk@freshdesk.com>
2018-08-03 20:49:54 +0530

    FD-11493 Prestaging(helpkit) (#2444)
    * FD-11493 Twitter MS(helpkit side changes)
    
    * FD-11493 Twitter handle ID as id instead of user_id.
    
    * FD-11493 tst cases fixed for helpkit
    
    * FD-11493 some review comments fixes.
    
    * FD-11493 removing unnecessary logs.
    
    * FD-11493 rubocop fixes latest
    
    * FD-11493 Fix social tweets issue.

--- lib/channel_integrations/common_actions/note.rb
+++ lib/channel_integrations/common_actions/note.rb
@@ -3,34 +3,53 @@ module ChannelIntegrations::CommonActions
     include ChannelIntegrations::Utils::ActionParser
     include ChannelIntegrations::Constants
 
-    def post_note(payload)
+    def create_note(payload)
       data = payload[:data]
-      note = construct_note data
-      note.source = Helpdesk::Note::SOURCE_KEYS_BY_TOKEN['note'] # replace the source from payload for Custom ticket sources.
-      note.save_note
+      note = construct_note(data)
+
+      # we will try to use payload's source. defaults to note.
+      note.source ||= Helpdesk::Note::SOURCE_KEYS_BY_TOKEN['note']
+      note.save_note!
+
+      reply = default_success_format
+      # TODO: Stick with api/v2/{ticket_id}/conversations show response format.
+      reply[:data] = { id: note.id }
+      reply
     end
 
-    def post_reply(payload)
+    def create_reply(payload)
       data = payload[:data]
-      note = construct_note data
-      note.source = Helpdesk::Note::SOURCE_KEYS_BY_TOKEN['email']
-      note.save_note
+      note = construct_note(data)
+
+      # we will try to use payload's source. defaults to email.
+      note.source ||= Helpdesk::Note::SOURCE_KEYS_BY_TOKEN['email']
+      note.save_note!
+
+      reply = default_success_format
+      # TODO: Stick with api/v2/{ticket_id}/conversations show response format.
+      reply[:data] = { id: note.id }
+      reply
     end
 
     private
 
-      def construct_note(data)
-        note = Helpdesk::Note.new
-        ticket = current_account.tickets.find_by_display_id(data[:ticket_id])
+      def construct_note(payload)
+        ticket_id = payload.delete(:ticket_id)
+        raise 'Ticket ID should be present' unless ticket_id
+
+        ticket = load_ticket(ticket_id)
         raise 'Ticket not found' unless ticket
-        note.notable_id = ticket.id
-        note.user_id = data[:user_id]
-        note.account_id = current_account.id
-        note.notable_type = 'Helpdesk::Ticket'
-        note.private = data[:private]
-        note.incoming = false
-        note.build_note_body(body: data[:body], body_html: data[:body])
+
+        body_html = payload.delete(:body)
+
+        note = ticket.notes.build(payload)
+        note.note_body_attributes = { body_html: body_html } if body_html.present?
+
         note
+      end
+
+      def load_ticket(ticket_display_id)
+        current_account.tickets.find_by_display_id(ticket_display_id)
       end
   end
 end
