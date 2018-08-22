d81e4091e17c820cd1bea597918329745eb66dd2
Balaji Radhakrishnan <balaji.rk@freshdesk.com>
2018-08-22 17:23:15 +0530

    FD-13748 validation removed for 400 (#2590)

--- lib/channel_integrations/utils/schema.rb
+++ lib/channel_integrations/utils/schema.rb
@@ -15,7 +15,8 @@ module ChannelIntegrations::Utils
                             command_name: command,
                             command_id: original_payload[:command_id], # returning the original command_id to track the msg.
                             reply_id: SecureRandom.uuid,
-                            context: original_payload[:context])
+                            context: original_payload[:context],
+                            meta: original_payload[:meta])
     end
 
     def default_schema
