c7259a0749b2f926986b1a239db27383f3d29a93
Gokul prabhu <gokul.prabhu@freshworks.com>
2018-08-27 15:08:17 +0530

    Fd 4521 solution visibility issue (#2536)
    * FD-4521-Visibility issue initial changes
    
    * FD-4521 - Style changes
    
    * FD-4521 - Style changes
    
    * FD-4521 - Style changes
    
    * FD-4521 - Link changes
    
    * FD-4521 - Revert solution tab hide changes
    
    * FD-4521 - I18n added
    
    * FD-4521 - I18n added
    
    * FD-4521 - Review changes
    
    * FD-4521 - Review changes
    
    * FD-4521 - Review changes
    
    * Formating changes

--- lib/portal/multilingual.rb
+++ lib/portal/multilingual.rb
@@ -2,22 +2,23 @@ module Portal::Multilingual
 
   def configure_language_switcher
     return unless current_portal.multilingual?
-    current_portal.language_list ||= current_account.all_portal_language_objects.inject([]) do |result,language|
+    current_portal.language_list ||= current_account.visible_language_objects_for_current_user.inject([]) do |result, language|
       result << [language_label(language), route_name(language), nil, language_availability(language)]
       result
     end
   end
 
   def language_label(language)
-    return language.name unless language == Language.current 
-    "<span class='icon-dd-tick-dark'></span>#{language.name}".html_safe
+    visible_label = current_account.agent_only_language?(language) ? "<span class='ficon-eye-disabled fsize-20'></span>" : ""
+    return (language.name + visible_label).html_safe unless language == Language.current
+    "<span class='icon-dd-tick-dark'></span>#{language.name} #{visible_label}".html_safe
   end
 
   def language_availability(language)
     classes = ""
     classes << "active" if language == Language.current
     classes << " unavailable-language" if @solution_item && !@solution_item.safe_send("#{language.to_key}_available?")
-    { :class => classes } 
+    { :class => classes }
   end
 
   # This is being overridden in Support::Solution::ArticlesController
