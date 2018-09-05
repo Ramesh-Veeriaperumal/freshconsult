bd11cc22e6b89a827cca4db15d31f83f7b2b245e
sravya-mallapu <sravya.mallapu@freshworks.com>
2018-09-05 20:56:50 +0530

    [FD-12579] Primary language change automation (#2651)
    Primary language change automation

--- config/routes.rb
+++ config/routes.rb
@@ -3038,6 +3038,7 @@ Helpkit::Application.routes.draw do
           get :check_domain
           put :unblock_outgoing_email
           post :extend_trial
+          put :change_primary_language
         end
       end
 
