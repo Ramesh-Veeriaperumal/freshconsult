56eb8b1ed8072736d4390834dce976eaae59d837
Apk47 <puneet@freshdesk.com>
2018-09-13 13:53:24 +0530

    [FD-12400] Test cases for Facebook direct message worker (#2422)

--- app/workers/social/premium_facebook_worker.rb
+++ app/workers/social/premium_facebook_worker.rb
@@ -1,18 +1,15 @@
 module Social
   class PremiumFacebookWorker < Social::FacebookWorker
-    
-    
-    sidekiq_options :queue => :premium_facebook, :retry => 0, :backtrace => true, :failures => :exhausted
-    
+    sidekiq_options queue: :premium_facebook, retry: 0, backtrace: true, failures: :exhausted
+
     def perform(args)
-      Rails.logger.debug "Premium Facebook Worker starting at #{Time.now} for account #{args['account_id']}"
+      Rails.logger.debug "Premium Facebook Worker starting at #{Time.now.utc} for account #{args['account_id']}"
       Sharding.select_shard_of(args['account_id']) do
         Account.find(args['account_id']).make_current
         super({})
       end
-      rescue ActiveRecord::RecordNotFound, ActiveRecord::AdapterNotSpecified, ShardNotFound => e
-        Rails.logger.debug "#{e.inspect} -- #{args['account_id']}"
+    rescue ActiveRecord::RecordNotFound, ActiveRecord::AdapterNotSpecified, ShardNotFound => e
+      Rails.logger.debug "#{e.inspect} -- #{args['account_id']}"
     end
-    
   end
 end
--- test/api/helpers/facebook_test_helper.rb
+++ test/api/helpers/facebook_test_helper.rb
@@ -6,6 +6,113 @@ module FacebookTestHelper
     fb_page
   end
 
+  def sample_dms(thread_id, user_id, msg_id, time)
+    dm = [
+      {
+        'id' => thread_id.to_s,
+        'updated_time' => Time.now.utc.to_s,
+        'messages' => {
+          'data' => [
+            {
+              'id' => msg_id.to_s,
+              'message' => Faker::Lorem.words(10).join(' ').to_s,
+              'from' => {
+                'name' => Faker::Lorem.words(1).to_s,
+                'id' => user_id.to_s
+              },
+              'created_time' => time.to_s
+            }
+          ]
+        }
+      },
+      {
+        'id' => thread_id.to_s,
+        'updated_time' => Time.now.utc.to_s,
+        'messages' => {
+          'data' => [
+            {
+              'id' => (msg_id + 5).to_s,
+              'message' => Faker::Lorem.words(10).join(' ').to_s,
+              'from' => {
+                'name' => Faker::Lorem.words(1).to_s,
+                'id' => user_id.to_s
+              },
+              'created_time' => (time + 1.hour).to_s
+            }
+          ]
+        }
+      }
+    ]
+  end
+
+  def realtime_dms(page_id, msg_id, user_id, time)
+    {
+      'entry' => {
+        'id' => page_id.to_s,
+        'time' => time.to_i,
+        'messaging' => [
+          {
+            'sender' => {
+              'id' => user_id.to_s
+            },
+            'recipient' => {
+              'id' => page_id.to_s
+            },
+            'timestamp' => time.to_i,
+            'message' => {
+              'mid' => msg_id.to_s
+            }
+          },
+          {
+            'sender' => {
+              'id' => user_id.to_s
+            },
+            'recipient' => {
+              'id' => page_id.to_s
+            },
+            'timestamp' => (time + 1.hour).to_i,
+            'message' => {
+              'mid' => (msg_id + 5).to_s
+            }
+          }
+        ]
+      }
+    }
+  end
+
+  def verify_ticket_properties(ticket, message)
+    fb_user_id = message[:from][:id]
+    dm_created_at = Time.zone.parse(message[:created_time])
+    direct_message_content = message[:message]
+    assert_equal ticket.description, direct_message_content 
+    assert_equal ticket.source, Helpdesk::Ticket::SOURCE_KEYS_BY_TOKEN[:facebook] 
+    assert_equal ticket.requester.fb_profile_id, fb_user_id 
+    assert_equal ticket.created_at, dm_created_at
+  end
+
+  def verify_note_properties(note, message)
+    fb_user_id = message[:from][:id]
+    dm_created_at = Time.zone.parse(message[:created_time])
+    direct_message_content = message[:message]
+    assert_equal note.body, direct_message_content 
+    assert_equal note.source, Helpdesk::Note::SOURCE_KEYS_BY_TOKEN['facebook'] 
+    assert_equal note.user.fb_profile_id, fb_user_id  
+    assert_equal note.created_at, dm_created_at
+  end
+
+  def create_facebook_dm_as_ticket(fb_page, thread_id, user_id)
+    msg_id = rand(10**10)
+    time = Time.now.utc
+
+    dm = sample_dms(thread_id, user_id, msg_id, time)
+    dm.pop
+    Koala::Facebook::API.any_instance.stubs(:get_connections).returns(dm)
+    fb_message = Facebook::KoalaWrapper::DirectMessage.new(fb_page)
+    fb_message.fetch_messages
+    Koala::Facebook::API.any_instance.unstub(:get_connections)
+  end
+
+
   def sample_post_feed(page_id, user_id, feed_id, time)
     fb_feed = [{
       'id'   => "#{page_id}_#{feed_id}",
