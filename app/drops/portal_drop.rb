2542b751cab2445c32f28366b3db2aaa84279357
mallikarjun31 <mallikarjun.susarla@freshworks.com>
2018-09-17 19:54:07 +0530

    FD-14818-In Portal Show author names disable button not working. (#2786)
    FD-14818-In Portal Show author names disable button not working

--- app/drops/portal_drop.rb
+++ app/drops/portal_drop.rb
@@ -285,7 +285,7 @@ class PortalDrop < BaseDrop
   end
 
   def personalized_articles?
-    source.preferences[:personalized_articles].blank? ? false : source.preferences[:personalized_articles].to_bool
+    source.personalized_articles?
   end
 
   def freshid_feature?
