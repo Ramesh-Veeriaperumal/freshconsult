9700210b81b6424a6b296cfd286e552e886bf7b6
premchandars <premchandar.sugumaran@freshworks.com>
2018-09-26 15:08:01 +0530

    FD-13017 removed present check added defined check
    FD-13017 removed present check added defined check

--- lib/helpdesk/initializers/memcache_cleaner.rb
+++ lib/helpdesk/initializers/memcache_cleaner.rb
@@ -9,12 +9,13 @@ module MemcacheCleaner
 
   def clean_memcache_data
     acc_id_hash = { :account_id => self.account_id }
-    if self.class::cache_keys_to_delete.present?
+    if defined? self.class::cache_keys_to_delete
       self.class::cache_keys_to_delete.each do |key|
         MemcacheKeys.delete_from_cache key % acc_id_hash
       end
     end
   end
+  
   module ClassMethods
     def clear_memcache(keys)
       self.class.send :define_method, "cache_keys_to_delete" do
@@ -22,6 +23,7 @@ module MemcacheCleaner
       end
     end
   end
+  
 end
 
 ActiveRecord::Base.send :include, MemcacheCleaner
