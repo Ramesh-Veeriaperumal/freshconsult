40a65ee30e02c64418635bebed37ad5b21787d2e
Subrahmanyam Bhamidipati <41772245+subbu-freshworks@users.noreply.github.com>
2018-10-10 19:33:02 +0530

    FD-14249 : Show Advanced Ticketing for lower plans with upgrade plan … (#2916)
    * FD-14249 : Show Advanced Ticketing for lower plans with upgrade plan option
    
    Feat : Extra fields are added to description whilst raising ticket from product_feedback api.
    
    * FD-14249 : Show Advanced Ticketing for lower plans with upgrade plan option
    
    Fix : getting plan name of an account from memcache rather than from the association itself.
    
    * FD-15679 : Email notification is sent to the user when the user clicks on upgrade button
    
    Fix : removing mrr and agent count from product_feedback api.

--- api/app/controllers/ember/product_feedback_controller.rb
+++ api/app/controllers/ember/product_feedback_controller.rb
@@ -20,7 +20,7 @@ class Ember::ProductFeedbackController < ApiApplicationController
     end
 
     def request_payload
-      description = simple_format(params[cname][:description]) + '<br><br>'.html_safe + account_details.html_safe
+      description = simple_format(params[cname][:description]) + '<br>'.html_safe + account_details.html_safe
       {
         email: current_user.email,
         subject: params[cname][:subject] || "Feedback from #{current_user.name}(#{current_user.email})",
@@ -34,9 +34,15 @@ class Ember::ProductFeedbackController < ApiApplicationController
     end
 
     def account_details
+      "<p>Below are the account details:<p>" \
+      "<p><b>Name</b>: #{current_user.name}</p>" \
+      "<p><b>Email</b>: #{current_user.email}</p>" \
+      "<p><b>Account Name</b>: #{current_account.name}</p>" \
       "<p><b>Account ID</b>: #{current_account.id}</p>" \
       "<p><b>Account URL</b>: #{current_account.full_domain}</p>" \
-      "<p><b>Admin</b>: #{current_user.privilege?(:admin_tasks)}</p>"
+      "<p><b>Plan</b>: #{current_account.subscription.plan_name}</p>" \
+      "<p><b>Admin</b>: #{current_user.privilege?(:admin_tasks)}</p>" \
+      "<br><p>Thanks,</p><p>Freshdesk Product</p>"
     end
 
     # putting validate body params inside validate params so that validation takes place before sanitization
