928dc0b2d4f50c142650c1576b5bc26dd984bc22
Abinaya <abinaya.sarathi@freshworks.com>
2018-10-10 23:07:55 +0530

    [FD-15040] Limit retries for Sla Calculation worker (#3019)
    * [FD-15040] Limit retries for Sla Calculation worker
    
    * [FD-15040] Sla flow test moved out of tickets flow test

--- app/workers/sla/calculation.rb
+++ app/workers/sla/calculation.rb
@@ -7,6 +7,8 @@ module Sla
 
     def perform args
       args.symbolize_keys!
+      # Setting thread variable to restrict requeue of this job beyond a fixed number of retries
+      Thread.current[:ticket_sla_calculation_retries] = args[:retries]
       ticket = Account.current.tickets.find_by_id(args[:ticket_id])
 
       if ticket && ticket.is_in_same_sla_state?(args[:sla_state_attributes])
