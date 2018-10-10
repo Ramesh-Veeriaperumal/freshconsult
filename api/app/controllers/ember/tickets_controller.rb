cad0078b97b448907aadc112086ca9f3bbddf94f
Ragav <ragavendren@freshdesk.com>
2018-10-10 16:09:15 +0530

    [FD-15636] Preloading denormalized flexifields (#2971)

--- api/app/controllers/ember/tickets_controller.rb
+++ api/app/controllers/ember/tickets_controller.rb
@@ -13,7 +13,7 @@ module Ember
 
     SLAVE_ACTIONS = %w(latest_note).freeze
 
-    INDEX_PRELOAD_OPTIONS = [:ticket_states, :tags, :schema_less_ticket, :flexifield, :ticket_status, { requester: [:avatar, :flexifield, :companies, :user_emails, :tags] }, :custom_survey_results].freeze
+    INDEX_PRELOAD_OPTIONS = [:ticket_states, :tags, :schema_less_ticket, :ticket_status, { flexifield: [:denormalized_flexifield]}, { requester: [:avatar, :flexifield, :companies, :user_emails, :tags] }, :custom_survey_results].freeze
     DEFAULT_TICKET_FILTER = :all_tickets.to_s.freeze
     SINGULAR_RESPONSE_FOR = %w(show create update split_note update_properties execute_scenario).freeze
 
