57bb8eae450994ec5c9a7dd65ac188982186b597
Lokesh Agrawal <38247645+lokesh-agrawal@users.noreply.github.com>
2018-10-11 11:40:32 +0530

    FD-14477 export  ticket worker test cases (#3007)
    Test cases for ticket export

---
+++ test/api/sidekiq/tickets_export_test.rb
@@ -0,0 +1,202 @@
+require_relative '../unit_test_helper'
+require 'sidekiq/testing'
+require 'faker'
+
+Sidekiq::Testing.fake!
+
+require Rails.root.join('test', 'core', 'helpers', 'tickets_test_helper.rb')
+require Rails.root.join('spec', 'support', 'agent_helper.rb')
+require Rails.root.join('test', 'api', 'helpers', 'search_test_helper.rb')
+require Rails.root.join('test', 'api', 'helpers', 'archive_ticket_test_helper.rb')
+
+class TicketsExportTest < ActionView::TestCase
+  include AgentHelper
+  include TicketsTestHelper
+  include SearchTestHelper
+  include ArchiveTicketTestHelper
+
+  def setup
+    @account = Account.first.make_current
+    datetime = Time.now
+    @start_time = (datetime - 86400).to_s(:db)
+    @end_time = datetime.to_s(:db)
+    @time = (datetime - 1200).to_s(:db)
+    @ticket_fields = { display_id: 'Ticket ID', subject: 'Subject', description: 'Description', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' }
+  end
+
+  def test_index_ticket_csv
+    @agent = add_agent_to_account(@account, options = { role: 1, active: 1 })
+    @ticket = create_ticket(requester_id: @agent.user_id)
+    @ticket.ticket_states.created_at = @time
+    @ticket.ticket_states.save!
+    args = { format: 'csv', date_filter: 30, ticket_state_filter: 'created_at',
+             query_hash: [{ condition: 'created_at', operator: 'is_greater_than', value: 'last_month' }],
+             start_date: @start_time, end_date: @end_time,
+             ticket_fields: @ticket_fields,
+             contact_fields: {},
+             company_fields: {},
+             filter_name: 'all_tickets',
+             export_fields: { display_id: 'Ticket ID', subject: 'Subject', description: 'Description', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' },
+             data_hash: [{ condition: 'created_at', operator: 'is_greater_than', value: 'last_month' }, { condition: 'spam', operator: 'is', value: false }, { condition: 'deleted', operator: 'is', value: false }],
+             current_user_id: @agent.user_id,
+             portal_url: 'localhost.freshdesk-dev.com' }
+    Export::Util.stubs(:build_attachment).returns(true)
+    Tickets::Export::TicketsExport.new.perform(args)
+    data_export = @account.data_exports.last
+    assert_equal data_export.status, DataExport::EXPORT_STATUS[:completed]
+    assert_equal data_export.source, DataExport::EXPORT_TYPE[:ticket]
+    assert_equal data_export.last_error, nil
+    data_export.destroy
+    @ticket.destroy
+    @agent.destroy
+  ensure
+    Export::Util.unstub(:build_attachment)
+  end
+
+  def test_index_ticket_excel
+    @agent = add_agent_to_account(@account, options = { role: 1, active: 1 })
+    @ticket = create_ticket(requester_id: @agent.user_id)
+    @ticket.ticket_states.created_at = @time
+    @ticket.ticket_states.save!
+    args = { format: 'xls', date_filter: '30', ticket_state_filter: 'created_at',
+             query_hash: [{ condition: 'created_at', operator: 'is_greater_than', value: 'last_month' }],
+             start_date: @start_time, end_date: @end_time,
+             ticket_fields: @ticket_fields,
+             contact_fields: {},
+             company_fields: {},
+             filter_name: 'all_tickets',
+             export_fields: { display_id: 'Ticket ID', subject: 'Subject', description: 'Description', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' },
+             data_hash: [{ condition: 'created_at', operator: 'is_greater_than', value: 'last_month' }, { condition: 'spam', operator: 'is', value: false }, { condition: 'deleted', operator: 'is', value: false }],
+             current_user_id: @agent.user_id,
+             portal_url: 'localhost.freshdesk-dev.com' }
+    Export::Util.stubs(:build_attachment).returns(true)
+    Tickets::Export::TicketsExport.new.perform(args)
+    data_export = @account.data_exports.last
+    assert_equal data_export.status, DataExport::EXPORT_STATUS[:completed]
+    assert_equal data_export.source, DataExport::EXPORT_TYPE[:ticket]
+    assert_equal data_export.last_error, nil
+    data_export.destroy
+    @ticket.destroy
+    @agent.destroy
+  ensure
+    Export::Util.unstub(:build_attachment)
+  end
+
+  def test_index_associated_contact_csv
+    @agent = add_agent_to_account(@account, options = { role: 1, active: 1 })
+    @ticket = create_ticket(requester_id: @agent.user_id)
+    @ticket.ticket_states.created_at = @time
+    @ticket.ticket_states.save!
+    args = { format: 'csv', date_filter: '30', ticket_state_filter: 'created_at',
+             query_hash: [{ condition: 'status', operator: 'is_in', value: [2] }, { condition: 'responder_id', operator: 'is_in', value: ['-1', '0'] }],
+             start_date: @start_time, end_date: @end_time,
+             ticket_fields: @ticket_fields,
+             contact_fields: { name: 'Full name', email: 'Email', phone: 'Work phone', mobile: 'Mobile phone', fb_profile_id: 'Facebook ID', twitter_id: 'Twitter ID', contact_id: 'Contact ID', time_zone: 'Time zone', language: 'Language', job_title: 'Title', unique_external_id: 'Unique External ID' },
+             company_fields: {},
+             filter_name: 'all_tickets',
+             export_fields: { display_id: 'Ticket ID', subject: 'Subject', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' },
+             data_hash: [{ condition: 'status', operator: 'is_in', value: '2' }, { condition: 'responder_id', operator: 'is_in', value: '-1,0' }, { condition: 'spam', operator: 'is', value: false }, { condition: 'deleted', operator: 'is', value: false }],
+             current_user_id: @agent.user_id,
+             portal_url: 'localhost.freshdesk-dev.com' }
+    Export::Util.stubs(:build_attachment).returns(true)
+    Tickets::Export::TicketsExport.new.perform(args)
+    data_export = @account.data_exports.last
+    assert_equal data_export.status, DataExport::EXPORT_STATUS[:completed]
+    assert_equal data_export.source, DataExport::EXPORT_TYPE[:ticket]
+    assert_equal data_export.last_error, nil
+    data_export.destroy
+    @ticket.destroy
+    @agent.destroy
+  ensure
+    Export::Util.unstub(:build_attachment)
+  end
+
+  def test_index_associated_company_csv
+    @agent = add_agent_to_account(@account, options = { role: 1, active: 1 })
+    @ticket = create_ticket(requester_id: @agent.user_id)
+    @ticket.ticket_states.created_at = @time
+    @ticket.ticket_states.save!
+    args = { format: 'csv', date_filter: '30', ticket_state_filter: 'created_at',
+             query_hash: [{ condition: 'status', operator: 'is_in', value: [2] }, { condition: 'responder_id', operator: 'is_in', value: ['-1', '0'] }],
+             start_date: @start_time, end_date: @end_time,
+             ticket_fields: @ticket_fields,
+             contact_fields: {},
+             company_fields: { name: 'Company Name', domains: 'Company Domains', health_score: 'Health score', account_tier: 'Account tier', industry: 'Industry', renewal_date: 'Renewal date' },
+             filter_name: 'all_tickets',
+             export_fields: { display_id: 'Ticket ID', subject: 'Subject', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' },
+             data_hash: [{ condition: 'status', operator: 'is_in', value: '2' }, { condition: 'responder_id', operator: 'is_in', value: '-1,0' }, { condition: 'spam', operator: 'is', value: false }, { condition: 'deleted', operator: 'is', value: false }],
+             current_user_id: @agent.user_id,
+             portal_url: 'localhost.freshdesk-dev.com' }
+    Export::Util.stubs(:build_attachment).returns(true)
+    Tickets::Export::TicketsExport.new.perform(args)
+    data_export = @account.data_exports.last
+    assert_equal data_export.status, DataExport::EXPORT_STATUS[:completed]
+    assert_equal data_export.source, DataExport::EXPORT_TYPE[:ticket]
+    assert_equal data_export.last_error, nil
+    data_export.destroy
+    @ticket.destroy
+    @agent.destroy
+  ensure
+    Export::Util.unstub(:build_attachment)
+  end
+
+  def test_index_associated_contact_and_company_csv
+    @agent = add_agent_to_account(@account, options = { role: 1, active: 1 })
+    @ticket = create_ticket(requester_id: @agent.user_id)
+    @ticket.ticket_states.created_at = @time
+    @ticket.ticket_states.save!
+    args = { format: 'csv', date_filter: '30', ticket_state_filter: 'created_at',
+             query_hash: [{ condition: 'status', operator: 'is_in', value: [2] }, { condition: 'responder_id', operator: 'is_in', value: ['-1', '0'] }],
+             start_date: @start_time, end_date: @end_time,
+             ticket_fields: @ticket_fields,
+             contact_fields: { name: 'Full name', email: 'Email', phone: 'Work phone', mobile: 'Mobile phone', fb_profile_id: 'Facebook ID', twitter_id: 'Twitter ID', contact_id: 'Contact ID', time_zone: 'Time zone', language: 'Language', job_title: 'Title', unique_external_id: 'Unique External ID' },
+             company_fields: { name: 'Company Name', domains: 'Company Domains', health_score: 'Health score', account_tier: 'Account tier', industry: 'Industry', renewal_date: 'Renewal date' },
+             filter_name: 'all_tickets',
+             export_fields: { display_id: 'Ticket ID', subject: 'Subject', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' },
+             data_hash: [{ condition: 'status', operator: 'is_in', value: '2' }, { condition: 'responder_id', operator: 'is_in', value: '-1,0' }, { condition: 'spam', operator: 'is', value: false }, { condition: 'deleted', operator: 'is', value: false }],
+             current_user_id: @agent.user_id,
+             portal_url: 'localhost.freshdesk-dev.com' }
+    Export::Util.stubs(:build_attachment).returns(true)
+    Tickets::Export::TicketsExport.new.perform(args)
+    data_export = @account.data_exports.last
+    assert_equal data_export.status, DataExport::EXPORT_STATUS[:completed]
+    assert_equal data_export.source, DataExport::EXPORT_TYPE[:ticket]
+    assert_equal data_export.last_error, nil
+    data_export.destroy
+    @ticket.destroy
+    @agent.destroy
+  ensure
+    Export::Util.unstub(:build_attachment)
+  end
+
+  def test_index_closed_csv
+    @agent = add_agent_to_account(@account, options = { role: 1, active: 1 })
+    @ticket = create_ticket(requester_id: @agent.user_id, responder_id: 1)
+    args = { format: 'csv', date_filter: '0', ticket_state_filter: 'closed_at',
+             query_hash: [{ condition: 'created_at', operator: 'is_greater_than', value: 'last_month' }],
+             start_date: @start_time, end_date: @end_time,
+             ticket_fields: @ticket_fields,
+             contact_fields: {},
+             company_fields: {},
+             filter_name: 'all_tickets',
+             export_fields: { display_id: 'Ticket ID', subject: 'Subject', description: 'Description', status_name: 'Status', priority_name: 'Priority', source_name: 'Source', ticket_type: 'Type', responder_name: 'Agent', group_name: 'Group', created_at: 'Created time', due_by: 'Due by Time', resolved_at: 'Resolved time', closed_at: 'Closed time', updated_at: 'Last update time', first_response_time: 'Initial response time', time_tracked_hours: 'Time tracked', first_res_time_bhrs: 'First response time (in hrs)', resolution_time_bhrs: 'Resolution time (in hrs)', outbound_count: 'Agent interactions', inbound_count: 'Customer interactions', resolution_status: 'Resolution status', first_response_status: 'First response status', ticket_tags: 'Tags', ticket_survey_results: 'Survey results', product_name: 'Product' },
+             current_user_id: @agent.user_id,
+             portal_url: 'localhost.freshdesk-dev.com' }
+    @ticket.status = Helpdesk::Ticketfields::TicketStatus::CLOSED
+    @ticket.save!
+    @ticket.ticket_states.closed_at = @time
+    @ticket.ticket_states.save!
+    Export::Util.stubs(:build_attachment).returns(true)
+    Tickets::Export::TicketsExport.new.perform(args)
+    data_export = @account.data_exports.last
+    assert_equal data_export.status, DataExport::EXPORT_STATUS[:completed]
+    assert_equal data_export.source, DataExport::EXPORT_TYPE[:ticket]
+    assert_equal data_export.last_error, nil
+    data_export.destroy
+    @ticket.destroy
+    @agent.destroy
+  ensure
+    Export::Util.unstub(:build_attachment)
+  end
+
+end
