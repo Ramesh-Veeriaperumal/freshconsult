9bc80fc0a2cc2e1d4068f5d179971c13380c0671
Yasaswini.B <yasaswini.balasubramaniam@freshworks.com>
2018-10-12 21:12:28 +0530

    Fd 7284 supervisor tickets limitation (#2406)
    * FD-7284 - Supervisor Ticket Limitation
    
    Added Redis key to hold total tickets limitation per rule.
    Added batch size to ensure supervisor rule gets executed on tickets in batches.
    Added launch party feature to whitelist customers to exclude the limitation.
    
    * FD-7284 - Supervisor Ticket Limitation
    
    Added Redis key to hold total tickets limitation per sla worker.
    Added check for sla tickets limitation
    
    * FD-7284 supervisor tickets limitation
    
    Review changes added.
    
    * FD-7284 supervisor tickets limitation
    
    Review changes
    
    * FD-7284 supervisor and sla tickets limitation
    
    Review changes
    
    * FD-7284 supervisor and sla tickets limitation
    
    Review changes
    
    * FD-7284 supervisor and sla tickets limitation
    
    Review changes
    
    * FD-7284 supervisor and sla tickets limitation
    
    * FD-7284 supervisor and sla tickets limitation
    
    * FD-7284 supervisor and sla tickets limitation
    
    * FD-7284 supervisor and sla tickets limitation
    
    * FD-7284 supervisor and sla tickets limitation

--- app/workers/admin/supervisor_worker.rb
+++ app/workers/admin/supervisor_worker.rb
@@ -1,5 +1,7 @@
 module Admin
   class SupervisorWorker < BaseWorker
+    include Redis::RedisKeys
+    include Redis::OthersRedis
 
     sidekiq_options :queue => :supervisor, :retry => 0, :backtrace => true, :failures => :exhausted
     SUPERVISOR_ERROR = 'SUPERVISOR_EXECUTION_FAILED'.freeze
@@ -14,6 +16,10 @@ module Admin
         start_time = Time.now.utc
         rule_type = VAConfig::RULES_BY_ID[VAConfig::RULES[:supervisor]]
         account_negatable_columns = ::VAConfig.negatable_columns(account)
+        supervisor_tickets_limit = get_others_redis_key(SUPERVISOR_TICKETS_LIMIT).to_i unless account.whitelist_supervisor_sla_limitation_enabled?
+        # SUPERVISOR_TICKETS_LIMIT redis key holds the max number of tickets that can be pick in a single rake job
+        # To bypass this max limit check, use 'whitelist_supervisor_sla_limitation' launch party feature
+
         supervisor_rules.each do |rule|
           begin
             rule_start_time = Time.now.utc
@@ -25,8 +31,16 @@ module Admin
             negate_conditions = [""]
             negate_conditions = rule.negation_query(account_negatable_columns)
             joins = rule.get_joins(["#{conditions[0]} #{negate_conditions[0]}"])
-            tickets = account.tickets.where(negate_conditions).where(conditions).updated_in(1.month.ago).visible.joins(joins).select("helpdesk_tickets.*")
-            tickets.each do |ticket|
+            tickets_count = 0
+            ticket_ids = []
+            account.tickets.where(negate_conditions).where(conditions).updated_in(1.month.ago)
+            .visible.joins(joins).readonly(false).find_each do |ticket|
+              tickets_count +=  1
+              Va::Logger::Automation.set_thread_variables(account.id, ticket.id, nil, rule.id)
+              if supervisor_tickets_limit && (tickets_count > supervisor_tickets_limit)
+                Va::Logger::Automation.log "SUPERVISOR: Tickets limit exceeded, exceeded_ticket_id=#{ticket.id}"
+                break
+              end
               begin
                 next if ticket.sent_for_enrichment?
                 execute_on_db("run_on_master") do
@@ -45,15 +59,18 @@ module Admin
                 log_info(account.id, rule.id, ticket.id) { Va::Logger::Automation.log_error(TICKET_SAVE_ERROR, e) }
                 NewRelic::Agent.notice_error(e,{:description => "Error while executing supervisor rule for a tkt :: #{ticket.id} :: account :: #{account.id}" })
                 next
+              ensure
+                ticket_ids.push(ticket.display_id)
+                Va::Logger::Automation.unset_thread_variables
               end
             end
             rule_end_time = Time.now.utc
             rule_total_time = (rule_end_time - rule_start_time )
-            ticket_ids = tickets.map(&:display_id)
             log_format = logging_format(account, ticket_ids, rule, rule_total_time, conditions, negate_conditions, joins)
             custom_logger.info "#{log_format}" unless custom_logger.nil?
             log_info(account.id, rule.id) {
-              Va::Logger::Automation.log "conditions=#{conditions.inspect}, negate_conditons=#{negate_conditions.inspect}, joins=#{joins.inspect}, tickets=#{ticket_ids.inspect}"
+              Va::Logger::Automation.log "conditions=#{conditions.inspect}, 
+              negate_conditons=#{negate_conditions.inspect}, joins=#{joins.inspect}, tickets=#{ticket_ids.inspect}"
               Va::Logger::Automation.log_execution_and_time(rule_total_time, ticket_ids.size, rule_type, rule_start_time, rule_end_time)
             }
           rescue Exception => e
@@ -65,7 +82,9 @@ module Admin
         end
         end_time = Time.now.utc
         total_time = end_time - start_time
-        log_info(account.id) { Va::Logger::Automation.log_execution_and_time(total_time, supervisor_rules.size, rule_type, start_time, end_time) }
+        log_info(account.id) { 
+          Va::Logger::Automation.log_execution_and_time(total_time, supervisor_rules.size, rule_type, start_time, end_time) 
+        }
       }
       ensure
         Account.reset_current_account
@@ -79,9 +98,11 @@ module Admin
       end
 
       def logging_format(account,ticket_ids,rule,rule_total_time, conditions, negate_conditions, joins)
-        "account_id=#{account.id}, account_name=#{account.name}, fullname=#{account.full_domain}, tickets_count=#{ticket_ids.length}, " \
-        "time_taken=#{rule_total_time}, rule_name=#{rule.name}, rule_id=#{rule.id}, host_name=#{Socket.gethostname}, " \
-        "conditions=#{conditions.inspect}, negate_conditons=#{negate_conditions.inspect}, joins=#{joins.inspect}, tickets=#{ticket_ids.inspect}"
+        "account_id=#{account.id}, account_name=#{account.name}, fullname=#{account.full_domain}, 
+        tickets_count=#{ticket_ids.length}, time_taken=#{rule_total_time}, rule_name=#{rule.name}, 
+        rule_id=#{rule.id}, host_name=#{Socket.gethostname}, conditions=#{conditions.inspect}, 
+        negate_conditons=#{negate_conditions.inspect}, joins=#{joins.inspect}, 
+        tickets=#{ticket_ids.inspect}".squish
       end
 
       def log_info(account_id, rule_id=nil, ticket_id=nil)
