e3901a6abebb58c5ac89e5ee9c9d99cbec2b5f33
Lokesh Agrawal <38247645+lokesh-agrawal@users.noreply.github.com>
2018-11-07 16:17:25 +0530

    FD-13730 Account central push (#3112)

---
+++ app/models/account/presenter.rb
@@ -0,0 +1,53 @@
+class Account < ActiveRecord::Base
+  include RepresentationHelper
+  acts_as_api
+  
+  api_accessible :central_publish do |s|
+    s.add :id
+    s.add :name
+    s.add :full_domain
+    s.add :time_zone
+    s.add :helpdesk_name
+    s.add :sso_enabled
+    s.add :sso_options
+    s.add :ssl_enabled
+    s.add :reputation
+    s.add :account_type_hash, as: :account_type
+    s.add proc { |x| x.features_list }, as: :features
+    s.add proc { |x| x.utc_format(x.created_at) }, as: :created_at
+    s.add proc { |x| x.utc_format(x.updated_at) }, as: :updated_at
+  end
+
+  def model_changes_for_central(options = {})
+    return @model_changes if @model_changes.present?
+    changes = self.previous_changes
+    if changes['plan_features']
+      plan_feature = { features: { added: [], removed: [] } }
+      FEATURES_DATA[:plan_features][:feature_list].each do |feature, value|
+        if changes['plan_features'][1].to_i == changes['plan_features'][0].to_i ^ 2**value
+          if self.has_feature?(feature)
+            plan_feature[:features][:added] << feature.to_s
+          else
+            plan_feature[:features][:removed] << feature.to_s
+          end
+        end
+      end
+      changes.delete('plan_features')
+      changes = changes.merge(plan_feature)
+    end
+    changes.delete(:shared_secret) if changes['shared_secret']
+    changes
+  end
+
+  def central_publish_payload
+    as_api_response(:central_publish)
+  end
+
+  def account_type_hash
+    { 
+      id: account_type, 
+      name: ACCOUNT_TYPES.key(account_type)
+    }
+  end
+
+end
---
+++ test/models/presenters/account_test.rb
@@ -0,0 +1,40 @@
+require_relative '../test_helper'
+require 'faker'
+class AccountTest < ActiveSupport::TestCase
+  
+  def test_create
+    CentralPublishWorker::AccountWorker.jobs.clear
+    create_new_account(Faker::Name.first_name, Faker::Internet.email)
+    assert_equal 1, CentralPublishWorker::AccountWorker.jobs.size
+    payload = @account.central_publish_payload.to_json
+    payload.must_match_json_expression(central_publish_account_post(@account))
+  end
+
+  def test_add_bitmap_feature
+    setup
+    @account.revoke_feature(:skill_based_round_robin)
+    CentralPublishWorker::AccountWorker.jobs.clear
+    @account.add_feature(:skill_based_round_robin)
+    assert_equal 1, CentralPublishWorker::AccountWorker.jobs.size
+    payload = @account.central_publish_payload.to_json
+    payload.must_match_json_expression(central_publish_account_post(@account))
+    job = CentralPublishWorker::AccountWorker.jobs.last
+    assert_equal 'account_update', job['args'][0]
+    assert_equal({ 'added' => ["skill_based_round_robin"], 'removed' => [] }, job['args'][1]['model_changes']['features'])
+  ensure
+     @account.revoke_feature(:skill_based_round_robin)
+  end
+  
+  def test_remove_bitmap_feature
+    setup
+    @account.add_feature(:skill_based_round_robin)
+    CentralPublishWorker::AccountWorker.jobs.clear
+    @account.revoke_feature(:skill_based_round_robin)
+    assert_equal 1, CentralPublishWorker::AccountWorker.jobs.size
+    payload = @account.central_publish_payload.to_json
+    payload.must_match_json_expression(central_publish_account_post(@account))
+    job = CentralPublishWorker::AccountWorker.jobs.last
+    assert_equal 'account_update', job['args'][0]
+    assert_equal({ 'added' => [], 'removed' => ["skill_based_round_robin"] }, job['args'][1]['model_changes']['features'])
+  end
+end
