1513207d54feb9d0f4a7afca93caf777d3b473aa
Rahul Gupta <37134617+rahul-gupta-1525@users.noreply.github.com>
2018-11-09 18:28:37 +0530

    [FD-16706] Groups delight metrics cache experiment (#3185)

--- lib/memcache_keys.rb
+++ lib/memcache_keys.rb
@@ -24,6 +24,8 @@ module MemcacheKeys
 
   ACCOUNT_AGENT_GROUPS = "v4/ACCOUNT_AGENT_GROUPS:%{account_id}"
 
+  ACCOUNT_AGENT_GROUPS_HASH = "v4/ACCOUNT_AGENT_GROUPS_HASH:%{account_id}"
+
   ACCOUNT_PRODUCTS = "v1/ACCOUNT_PRODUCTS:%{account_id}"
 
   ACCOUNT_TAGS = "v1/ACCOUNT_TAGS:%{account_id}"
