4d870ba2ff6c163a461b4d656cda44fba4b1a5f7
Balaji Radhakrishnan <balaji.radhakrishnan@freshworks.com>
2019-02-05 11:29:58 +0530

    FD-18443 groups central publish remaining attribute (#3599)
    * FD-18433 groups central publish remaining attribute
    
    * FD-18433 groups destroy model attributes and tests
    
    * FD-18443 bumping up group version
    
    * FD-18443 fixing group tests
    
    * FD-18443 fixing the test case failures
    
    * FD-18443 remove unused helper.

--- test/lib/all_tests/constants.rb
+++ test/lib/all_tests/constants.rb
@@ -15,7 +15,8 @@ INTEGRATION_TESTS = [
 PRESENTER_TESTS = [
   'test/models/presenters/bot/response_test.rb',
   'test/models/presenters/account_test.rb',
-  'test/models/presenters/helpdesk/ticket_test.rb'
+  'test/models/presenters/helpdesk/ticket_test.rb',
+  'test/models/presenters/group_test.rb'
 ]
 SIDEKIQ_TESTS = Dir.glob('test/api/sidekiq/**/*_test.rb')
 SKIP_FILES_SIDEKIQ = Dir.glob('test/api/sidekiq/sandbox/*_test.rb') + Dir.glob('test/api/sidekiq/admin/sla/*_test.rb') + Dir.glob('test/api/sidekiq/community/clear_site_map_test.rb')
--- test/models/presenters/group_test.rb
+++ test/models/presenters/group_test.rb
@@ -7,5 +7,35 @@ class GroupTest < ActiveSupport::TestCase
     group = create_group(@account)
     payload = group.central_publish_payload.to_json
     payload.must_match_json_expression(central_publish_group_pattern(group))
+    assoc_payload = group.associations_to_publish.to_json
+    assoc_payload.must_match_json_expression(group_associations(group))
   end
+
+  def test_central_publish_destroy_payload
+    group = create_group(@account)
+    group.destroy
+
+    job = CentralPublisher::Worker.jobs.last
+    assert_equal 'group_destroy', job['args'][0]
+    assert_equal({}, job['args'][1]['model_changes'])
+    job['args'][1]['model_properties'].must_match_json_expression(central_publish_group_destroy_pattern(group))
+  end
+
+  private
+
+    def group_associations(group)
+      {
+        business_calendar: business_calendar(group)
+      }
+    end
+
+    def business_calendar(group)
+      return nil unless group.business_calendar_id
+      {
+        name: group.name,
+        description: group.description,
+        time_zone: group.time_zone,
+        is_default: group.is_default
+      }
+    end
 end
