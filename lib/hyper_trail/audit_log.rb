746e89585eebbe91ad539c60d4597bfc63ff5a61
HariniKrishnamoorthy <41904767+HariniKrishnamoorthy@users.noreply.github.com>
2019-02-07 02:24:52 +0530

    FD-16161 : Audit log export (#3917)

--- lib/hyper_trail/audit_log.rb
+++ lib/hyper_trail/audit_log.rb
@@ -2,4 +2,12 @@ class HyperTrail::AuditLog < HyperTrail::Base
   def hyper_trail_type
     'audit_log'
   end
+
+  def hyper_trail_filtered_export
+    'audit_log_filtered_export'
+  end
+
+  def hyper_trail_file_export
+    'audit_log_file_export'
+  end
 end
