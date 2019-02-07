e82e90c5e42b9bfb10a5e3f3d20ed0468f5a60dc
Balaji Radhakrishnan <balaji.radhakrishnan@freshworks.com>
2019-02-07 17:10:29 +0530

    FD-16451 API for associated tickets and their count based on ticket type (#3463)
    * FD-16451 Store the ticket_ids by type in additional_info column
    
    * FD-16451 update the child ID’s in parent on ticket type update
    
    * Revert "FD-16451 update the child ID’s in parent on ticket type update"
    
    This reverts commit 5c5f16d3ea59ab8bf93c74496a9505379e45e19e.
    
    * Revert "FD-16451 Store the ticket_ids by type in additional_info column"
    
    This reverts commit a15cd151ec40877a5863f0bcec1f50c0e0bd97c0.
    
    * FD-16451 New API for count.
    
    * FD-16451 associated_tickets by type filter.
    
    * FD-16451 test cases added.
    
    * FD-16451 swagger files for associated tickets api
    
    * FD-16451 review comments and test cases fix
    
    * FD-16451 conflict resolved
    
    * FD-16451 review comment fixes
    
    * FD-16451 fixing the test case failure
    
    * FD-16451 rubocop fix in the test case
    
    * FD-16451 removed the query for no-type filter.
    
    * FD-16451 Reverting to check test case failures

--- config/api_privileges.rb
+++ config/api_privileges.rb
@@ -12,7 +12,7 @@ Authority::Authorization::PrivilegeList.build do
     resource :"ember/canned_response", only: %i(search)
     resource :"ember/ticket", only: %i(index show create execute_scenario spam latest_note create_child_with_template parse_template fetch_errored_email_details suppression_list_alert)
     resource :"ember/tickets/bulk_action", only: %i(bulk_execute_scenario bulk_link bulk_unlink)
-    resource :"ember/tickets/associate", only: [:link, :unlink, :associated_tickets, :prime_association]
+    resource :"ember/tickets/associate", only: [:link, :unlink, :associated_tickets, :prime_association, :associated_tickets_count]
     resource :"ember/ticket_filter", only: [:index, :show, :create, :update, :destroy]
     resource :"ember/attachment", only: [:create, :show]
     resource :"ember/freshcaller/setting", only: %i[index desktop_notification redirect_url]
--- test/api/functional/ember/tickets/associates_controller_test.rb
+++ test/api/functional/ember/tickets/associates_controller_test.rb
@@ -4,6 +4,7 @@ module Ember
     class AssociatesControllerTest < ActionController::TestCase
       include ApiTicketsTestHelper
       include CannedResponsesTestHelper
+      include AdvancedTicketingTestHelper
 
       # Tests for prime association for related/child tickets
       # 1. valid related/child ticket id
@@ -265,6 +266,149 @@ module Ember
         get :associated_tickets, construct_params({ version: 'private', id: @parent_ticket.display_id }, false)
         assert_response 403
         match_json(request_error_pattern(:require_feature, feature: 'Parent Child Tickets and Field Service Management'))
+      end
+
+      def test_associations_with_service_task_type
+        enable_adv_ticketing([:field_service_management]) do
+          enable_fsm do
+            begin
+              perform_fsm_operations
+              Account.any_instance.stubs(:disable_old_ui_enabled?).returns(true)
+              child_ticket_ids = create_advanced_tickets
+              Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+              Helpdesk::Ticket.any_instance.stubs(:associates).returns(child_ticket_ids)
+              get :associated_tickets, construct_params({ version: 'private', id: @parent_ticket.display_id, type: 'Service Task' }, false)
+              assert_response 200
+              match_json(associations_pattern(@parent_ticket))
+              Helpdesk::Ticket.any_instance.unstub(:associates=)
+              Helpdesk::Ticket.any_instance.unstub(:associates)
+            ensure
+              Account.any_instance.unstub(:disable_old_ui_enabled?)
+            end
+          end
+        end
+      end
+
+      def test_associations_with_empty_service_tasks
+        enable_adv_ticketing([:field_service_management]) do
+          enable_fsm do
+            begin
+              Account.any_instance.stubs(:disable_old_ui_enabled?).returns(true)
+              perform_fsm_operations
+              create_parent_child_tickets
+              Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+              Helpdesk::Ticket.any_instance.stubs(:associates).returns([@child_ticket.display_id])
+              get :associated_tickets, construct_params({ version: 'private', id: @parent_ticket.display_id, type: 'Service Task' }, false)
+              assert_response 200
+              Helpdesk::Ticket.any_instance.unstub(:associates=)
+              Helpdesk::Ticket.any_instance.unstub(:associates)
+              match_json([])
+            ensure
+              Account.any_instance.unstub(:disable_old_ui_enabled?)
+            end
+          end
+        end
+      end
+
+      def test_associations_with_invalid_ticket_type
+        enable_adv_ticketing([:field_service_management]) do
+          create_parent_child_tickets
+          Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+          Helpdesk::Ticket.any_instance.stubs(:associates).returns([@child_ticket.display_id])
+          get :associated_tickets, construct_params({ version: 'private', id: @parent_ticket.display_id, type: 'Service Task' }, false)
+          assert_response 400
+          Helpdesk::Ticket.any_instance.unstub(:associates=)
+          Helpdesk::Ticket.any_instance.unstub(:associates)
+        end
+      end
+
+      def test_association_count_with_service_task
+        enable_adv_ticketing([:field_service_management]) do
+          enable_fsm do
+            begin
+              Account.any_instance.stubs(:disable_old_ui_enabled?).returns(true)
+              perform_fsm_operations
+              child_ticket_ids = create_advanced_tickets
+              Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+              Helpdesk::Ticket.any_instance.stubs(:associates).returns(child_ticket_ids)
+              get :associated_tickets_count, construct_params({ version: 'private', id: @parent_ticket.display_id, type: 'Service Task' }, false)
+              assert_response 200
+              Helpdesk::Ticket.any_instance.unstub(:associates=)
+              Helpdesk::Ticket.any_instance.unstub(:associates)
+              match_json('count' => 1)
+            ensure
+              Account.any_instance.unstub(:disable_old_ui_enabled?)
+            end
+          end
+        end
+      end
+
+      def test_association_count_filter_with_multiple_child
+        enable_adv_ticketing([:field_service_management]) do
+          enable_fsm do
+            begin
+              Account.any_instance.stubs(:disable_old_ui_enabled?).returns(true)
+              perform_fsm_operations
+              child_ticket_ids = create_advanced_tickets(fsm: 3)
+              Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+              Helpdesk::Ticket.any_instance.stubs(:associates).returns(child_ticket_ids)
+              get :associated_tickets_count, construct_params({ version: 'private', id: @parent_ticket.display_id, type: 'Service Task' }, false)
+              assert_response 200
+              Helpdesk::Ticket.any_instance.unstub(:associates=)
+              Helpdesk::Ticket.any_instance.unstub(:associates)
+              match_json('count' => 3)
+            ensure
+              Account.any_instance.unstub(:disable_old_ui_enabled?)
+            end
+          end
+        end
+      end
+
+      def test_association_count_without_filter
+        enable_adv_ticketing([:field_service_management]) do
+          enable_fsm do
+            begin
+              Account.any_instance.stubs(:disable_old_ui_enabled?).returns(true)
+              perform_fsm_operations
+              child_ticket_ids = create_advanced_tickets(fsm: 1, pc: 1)
+              Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+              Helpdesk::Ticket.any_instance.stubs(:associates).returns(child_ticket_ids)
+              get :associated_tickets_count, construct_params({ version: 'private', id: @parent_ticket.display_id }, false)
+              assert_response 200
+              Helpdesk::Ticket.any_instance.unstub(:associates=)
+              Helpdesk::Ticket.any_instance.unstub(:associates)
+              match_json('count' => 2)
+            ensure
+              Account.any_instance.unstub(:disable_old_ui_enabled?)
+            end
+          end
+        end
+      end
+
+      def test_association_count_without_the_ticket
+        enable_adv_ticketing([:field_service_management]) do
+          enable_fsm do
+            begin
+              Account.any_instance.stubs(:disable_old_ui_enabled?).returns(true)
+              perform_fsm_operations
+              ticket = create_ticket
+              get :associated_tickets_count, construct_params({ version: 'private', id: ticket.display_id }, false)
+              assert_response 404
+            ensure
+              Account.any_instance.unstub(:disable_old_ui_enabled?)
+            end
+          end
+        end
+      end
+
+      def test_link_tickets_association_count
+        enable_adv_ticketing([:link_tickets]) do
+          create_linked_tickets
+          Helpdesk::Ticket.any_instance.stubs(:associates=).returns(true)
+          Helpdesk::Ticket.any_instance.stubs(:associates).returns([])
+          get :associated_tickets_count, construct_params({ version: 'private', id: @tracker_id, type: 'Question' }, false)
+          assert_response 400
+        end
       end
     end
   end
