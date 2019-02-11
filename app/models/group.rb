6f5cccd7da53a1232ecebcbdc210fc8732f3fdc4
Keerthana Raghavan <keerthana.raghavan@freshworks.com>
2019-02-11 21:25:05 +0530

    [FD-18758] Remove OCR from group on feature removal (#3952)
    **JIRA Id(s)** : FD-18758
    
    **Description** :
    _Why is this change necessary?_
    Steps to Reproduce:
    
    1. Enable OCR for an account
    2. Turn on OCR for a group
    3. Now revoke OCR for the account, and add back the feature
    4. IN the groups page, the OCR toggle is turned on, it should be off
    
    _How does it address the issue?_
    Clear OCR data on revoking OCR feature.

--- app/models/group.rb
+++ app/models/group.rb
@@ -87,6 +87,9 @@ class Group < ActiveRecord::Base
   scope :capping_enabled_groups, :conditions => ["ticket_assign_type = 1 and capping_limit > 0"], :order => :name
   scope :skill_based_round_robin_enabled, :order => :name,
         :conditions => ["ticket_assign_type = #{Group::TICKET_ASSIGN_TYPE[:skill_based]}"]
+  scope :ocr_enabled_groups,
+        order: :name,
+        conditions: ["ticket_assign_type IN (?)", OMNI_CHANNEL_ASSIGNMENT_TYPES]
   
   def excluded_agents(account)      
    return account.users.find(:all , :conditions=>['helpdesk_agent = true and id not in (?)',agents.map(&:id)]) unless agents.blank? 
