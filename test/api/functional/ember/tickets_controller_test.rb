411d42a63e21cc37c4c1c2e093cc563513d20f05
Dhruva Ashok <35563497+fresh-dhruvaashok@users.noreply.github.com>
2019-02-12 18:21:05 +0530

    FD-20296: test cases for lib/sso_util (#3940)
    * FD-20296: test cases for lib/sso_util
    
    * FD-20296: add end empty line
    
    * FD-20296: moving require webmock to the second line
    
    * FD-20296: stubbing external facebook calls
    
    * FD-20296: stubbing more external facebook calls
    
    * FD-20296: ember conv controller test facebook stub
    
    * FD-20296: koala facebook stub
    
    * FD-20296: stub more external requests
    
    * FD-20296: stub status stub
    
    * FD-20296: remove unnecessary change

--- test/api/functional/ember/tickets_controller_test.rb
+++ test/api/functional/ember/tickets_controller_test.rb
@@ -1,5 +1,5 @@
-require 'webmock/minitest'
 require_relative '../../test_helper'
+require 'webmock/minitest'
 ['canned_responses_helper.rb', 'group_helper.rb', 'social_tickets_creation_helper.rb', 'ticket_template_helper.rb'].each { |file| require "#{Rails.root}/spec/support/#{file}" }
 ['account_test_helper.rb', 'shared_ownership_test_helper.rb'].each { |file| require "#{Rails.root}/test/core/helpers/#{file}" }
 ['tickets_test_helper.rb', 'bot_response_test_helper.rb'].each { |file| require "#{Rails.root}/test/api/helpers/#{file}" }
---
+++ test/lib/sso_util_test.rb
@@ -0,0 +1,175 @@
+require_relative '../api/test_helper'
+
+class SsoUtilTestController < ApplicationController
+  include SsoUtil
+  include ActionController::Renderers::All
+
+  def sso_data
+    {
+      email: User.last.email,
+      name: User.last.name,
+      phone: User.last.phone,
+      company: nil,
+      title: nil,
+      external_id: nil
+    }
+  end
+
+  def sso_login_page
+    sso_login_page_redirect
+    head 200
+  end
+
+  def sso_user_fields
+    populate_sso_user_fields(Account.first, User.last, User.last.attributes, {}, false)
+    head 200
+  end
+
+  def sso_response_handling
+    handle_sso_response(sso_data, nil)
+    head 200
+  end
+
+  def saml_response_validation
+    validate_saml_response(Account.first, nil)
+    head 200
+  end
+
+  def user_jwt_sso
+    update_user_for_jwt_sso(Account.first, User.last, User.last.attributes, {}, false)
+    head 200
+  end
+end
+
+class SsoUtilTestControllerTest < ActionController::TestCase
+  include SsoUtil
+
+  def test_valid_saml_on_initialize
+    saml = SAMLResponse.new(true, 'user_name', 'user_name@email.com',
+                            0, nil, nil, nil, {},
+                            'error_message')
+    assert_equal saml.valid?, true
+  end
+
+  def test_sso_login_page
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    @controller.stubs(:action_name).returns('sso_login_page')
+    actual = @controller.send(:sso_login_page)
+    assert_response 200
+  end
+
+  def test_sso_login_with_saml
+    response = ActionDispatch::TestResponse.new
+    Account.any_instance.stubs(:is_saml_sso?).returns(true)
+    OneLogin::RubySaml::Settings.any_instance.stubs(:idp_sso_target_url).returns('')
+    @controller.response = response
+    @controller.stubs(:action_name).returns('sso_login_page')
+    actual = @controller.send(:sso_login_page)
+    assert_response 200
+  end
+
+  def test_populate_sso_user_fields
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    @controller.stubs(:action_name).returns('sso_user_fields')
+    actual = @controller.send(:sso_user_fields)
+    assert_response 200
+  end
+
+  def test_handle_sso_response
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    Account.any_instance.stubs(:sso_enabled?).returns(true)
+    UserSession.any_instance.stubs(:save).returns(true)
+    @controller.stubs(:action_name).returns('sso_response_handling')
+    actual = @controller.send(:sso_response_handling)
+    assert_response 200
+  end
+
+  def test_handle_sso_response_save_fail
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    Account.any_instance.stubs(:sso_enabled?).returns(true)
+    UserSession.any_instance.stubs(:save).returns(false)
+    @controller.stubs(:action_name).returns('sso_response_handling')
+    actual = @controller.send(:sso_response_handling)
+    assert_response 200
+  end
+
+  def test_validate_saml_response_error
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    OneLogin::RubySaml::Response.stubs(:new).returns(User.last)
+    User.any_instance.stubs(:settings).returns(User.last)
+    User.any_instance.stubs(:issuer=).returns(true)
+    User.any_instance.stubs(:is_valid?).returns(false)
+    @controller.stubs(:action_name).returns('saml_response_validation')
+    actual = @controller.send(:saml_response_validation)
+    assert_response 200
+  end
+
+  def test_validate_saml_response_invalid
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    OneLogin::RubySaml::Response.stubs(:new).returns(User.last)
+    User.any_instance.stubs(:settings).returns(User.last)
+    User.any_instance.stubs(:issuer=).returns(true)
+    User.any_instance.stubs(:is_valid?).returns(false)
+    User.any_instance.stubs(:document).returns('')
+    @controller.stubs(:action_name).returns('saml_response_validation')
+    actual = @controller.send(:saml_response_validation)
+    assert_response 200
+  end
+
+  def test_validate_saml_response_valid
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    OneLogin::RubySaml::Response.stubs(:new).returns(User.last)
+    User.any_instance.stubs(:settings).returns(User.last)
+    User.any_instance.stubs(:issuer=).returns(true)
+    User.any_instance.stubs(:is_valid?).returns(true)
+    User.any_instance.stubs(:name_id).returns(User.last.email)
+    @controller.stubs(:action_name).returns('saml_response_validation')
+    actual = @controller.send(:saml_response_validation)
+    assert_response 200
+  end
+
+  def test_update_user_for_jwt_sso
+    response = ActionDispatch::TestResponse.new
+    @controller.response = response
+    @controller.stubs(:action_name).returns('user_jwt_sso')
+    actual = @controller.send(:user_jwt_sso)
+    assert_response 200
+  end
+
+  def test_validate_custom_field_date
+    validation_response = validate_custom_field('field_name', 12, Date)
+  rescue SsoFieldValidationError => e
+    assert_equal validation_response, nil
+  end
+
+  def test_validate_custom_field_boolean
+    validation_response = validate_custom_field('field_name', 'something', 'Boolean')
+  rescue SsoFieldValidationError => e
+    assert_equal validation_response, nil
+  end
+
+  def test_validate_custom_field_url
+    validation_response = validate_custom_field('field_name', 12, 'URL')
+  rescue SsoFieldValidationError => e
+    assert_equal validation_response, nil
+  end
+
+  def test_validate_custom_field_fixnum
+    validation_response = validate_custom_field('field_name', 'abcd', Fixnum)
+  rescue SsoFieldValidationError => e
+    assert_equal validation_response, nil
+  end
+
+  def test_validate_custom_field_others
+    validation_response = validate_custom_field('field_name', 12, 'RandomType')
+  rescue SsoFieldValidationError => e
+    assert_equal validation_response, nil
+  end
+end
