aa2a88ce1f8ce4520b9193758a9398169b1ce314
chockalingam subbiah <chockalingam.subbaiah@freshworks.com>
2019-02-13 13:31:25 +0530

    FD-18891 Enable redis_picklist_id during signups - helpkit backend (#3818)
    https://jira.freshworks.com/browse/FD-18891
    
    Enable redis_picklist_id during signups - helpkit backend

--- app/models/account/constants.rb
+++ app/models/account/constants.rb
@@ -296,7 +296,7 @@ class Account < ActiveRecord::Base
       field_service_management_lp: false, bot_email_channel: false,
       description_by_default: false, ticket_fields_central_publish: false,
       facebook_page_scope_migration: false, agent_group_central_publish: false, custom_fields_search: false,
-      update_billing_info: false, allow_billing_info_update: false, tag_central_publish: false
+      update_billing_info: false, allow_billing_info_update: false, tag_central_publish: false, redis_picklist_id: true
     }, :merge
   )
 
