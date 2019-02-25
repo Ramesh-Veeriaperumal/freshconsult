3933ca4fb76db7a222dea9646d5cd998eefd6ac3
Yuvanesh <yuvanesh.palanisamy@freshworks.com>
2019-02-25 15:40:23 +0530

    [FD-20979] - Fix archive related bugs (#4101)
    * [FD-20979] - Fix archive related bugs
    
    * [FD-20979] - change elements to ids in validation

--- api/app/controllers/validations/archive_validation.rb
+++ api/app/controllers/validations/archive_validation.rb
@@ -2,7 +2,7 @@ class ArchiveValidation < ApiValidation
   attr_accessor :archive_days, :ids
 
   validates :archive_days, custom_numericality: { only_integer: true, allow_nil: false }
-  validates :ids, data_type: { rules: Array },
-                         array: { custom_numericality: { only_integer: true, greater_than: 0, allow_nil: false } },
-                         custom_length: { maximum: ApiConstants::MAX_ITEMS_FOR_BULK_ACTION, message_options: { element_type: :elements } }
+  validates :ids, data_type: { rules: Array, allow_blank: false },
+                  array: { custom_numericality: { only_integer: true, greater_than: 0, allow_blank: false } },
+                  custom_length: { minimum: 1, maximum: ApiConstants::MAX_ITEMS_FOR_BULK_ACTION, message_options: { element_type: :ids } }
 end
--- api/app/delegators/archive_delegator.rb
+++ api/app/delegators/archive_delegator.rb
@@ -8,11 +8,11 @@ class ArchiveDelegator < BaseDelegator
   end
 
   def validate_display_ids
-    valid_ids = Account.current.tickets.visible.where(display_id: @permissible_ticket_ids, status: ApiTicketConstants::CLOSED).pluck(:display_id)
+    valid_ids = Account.current.tickets.visible.not_associated.where(display_id: @permissible_ticket_ids, status: ApiTicketConstants::CLOSED).pluck(:display_id)
     invalid_ids = @display_ids - valid_ids
     if invalid_ids.present?
       errors[:ids] << :invalid_id_list
-      (self.error_options ||= {}).merge!({ ids: { invalid_ids: "#{invalid_ids.join(', ')}" } })
+      (self.error_options ||= {}).merge!(ids: { invalid_ids: "#{invalid_ids.join(', ')}" })
     end
   end
 end
