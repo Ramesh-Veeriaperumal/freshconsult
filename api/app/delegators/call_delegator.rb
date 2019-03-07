0ce10505d879e82c45827d22d0f7a4d8bac53445
Logesh Rajendran <logesh.rajendran@freshworks.com>
2019-03-07 16:08:16 +0530

    [FD-30468]  Changes for integration actions (#3768)
    * [FD-19496]  Changes for integration actions
    
    1. Removed created_at edition since it is creating overdue tickets when a ticket is created for very old calls.
    2. Choosing current user as agent for ticket when agent_email is not sent from freshcaller.
    3. Added test cases when current_user is set from api_key
    
    * [FD-19496]  Changes for integration actions
    
    Rubocop fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496]  Changes for integration actions
    
    Flay fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496]  Changes for integration actions
    
    Flay fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496]  Changes for integration actions
    
    Pushing test fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Fixing failing cases
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Fixing rubocop issues
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    1. Refactoring current_agent private method
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    1. Removing created_at params to create the ticket in its own created_at timestamp
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Separating creator and agent for ticket logic for freshcaller calls
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Rubocop fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Rubocop fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    FRESHCALL-3966 -
    Bug fix for this case
    FC|Attend a call as Agent 2 and create ticket from Agent 1 the conversation between shows Agent 1
    1.Make an incoming call and attend the call as Agent 2.
    2.Now create ticket from callMetrics Page of Agent 1.
    3.Now View the ticket it is showing conversation between customer and Agent 1.
    
    When recording is attached it is changed to conversation between customer and Agent 2.
    
    Expected: It should show conversation between Customer and Agent 2
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Fixing rubocop reviews
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Fixing rubocop changes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Review changes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>
    
    * [FD-19496] Changes for integration actions
    
    Rubocop fixes
    
    Signed-off-by: Logesh Rajendran <logesh.rajendran@freshworks.com>

--- api/app/delegators/call_delegator.rb
+++ api/app/delegators/call_delegator.rb
@@ -1,10 +1,11 @@
 class CallDelegator < BaseDelegator
   include ActiveRecord::Validations
-  attr_accessor :ticket, :agent, :contact
+  attr_accessor :ticket, :agent, :contact, :creator, :call_agent
 
   validate :validate_ticket, if: -> { @ticket_display_id }
-  validate :validate_agent_email, if: -> { @agent_email }
+  validate :validate_agent_email
   validate :validate_contact, if: -> { @contact_id }
+  validate :validate_call_agent_email
 
   def initialize(record, options = {})
     super(record)
@@ -14,7 +15,7 @@ class CallDelegator < BaseDelegator
   end
 
   def validate_ticket
-    ticket = Account.current.tickets.where(display_id: @ticket_display_id).first
+    ticket = current_account.tickets.where(display_id: @ticket_display_id).first
     if ticket
       self.ticket = ticket
     else
@@ -23,19 +24,38 @@ class CallDelegator < BaseDelegator
   end
 
   def validate_agent_email
-    agent = Account.current.technicians.where(email: @agent_email).first
-    if agent
-      agent.make_current
-      self.agent = agent
+    user = current_account.technicians.where(email: @agent_email).first if @agent_email
+    # user with @agent_email will not be present when a ticket creation initiated by freshcaller agent who is not in freshdesk
+    if user
+      user.make_current
+      self.agent = user
+      self.creator = user
+    elsif current_agent
+      # the account admin who linked freshcaller with the freshdesk account is chosen as ticket creator when user with @agent_email is not present
+      self.creator = current_agent
     else
       errors[:agent_email] << :"is invalid"
     end
   end
 
+  def validate_call_agent_email
+    call_agent = current_account.technicians.where(email: @call_agent_email).first if @call_agent_email
+    self.call_agent = call_agent if call_agent
+  end
+
   def validate_contact
     contact = Account.current.users.where(id: @contact_id).first
-    if contact
-      self.contact = contact
-    end
+    self.contact = contact if contact
   end
+
+  private
+
+    def current_account
+      Account.current # the account admin is set as current user when the request comes in, using API key
+    end
+
+    def current_agent
+      current_user = User.current
+      current_user if current_user.try(:agent?)
+    end
 end
