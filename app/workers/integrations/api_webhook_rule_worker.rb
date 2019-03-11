db4dac5f1a597033dd913e8a4c723d96fc12ccf5
Krishnanand nb <krishnanand.balasubramanian@freshworks.com>
2019-03-11 18:51:44 +0530

    FD-18085 : adding logs for web-hook tickets trigger (#4248)
    * FD-18085 : adding logs for web-hook tickets trigger
    
    * FD-18085 : adding logs
    
    * FD-18085 : moving rescue above

--- app/workers/integrations/api_webhook_rule_worker.rb
+++ app/workers/integrations/api_webhook_rule_worker.rb
@@ -7,16 +7,21 @@ module Integrations
       begin
         account = Account.current
         args = args.deep_symbolize_keys
-        evaluate_on = account.safe_send(args[:association].pluralize.to_sym).find args[:evaluate_on_id]
+        association = args[:association].pluralize.to_sym
+        evaluate_on = account.safe_send(association).find args[:evaluate_on_id]
         account.api_webhooks_rules_from_cache.each do |vr|
-          is_a_match = vr.event_matches? args[:current_events], evaluate_on
-          vr.pass_through evaluate_on, nil, nil if is_a_match
+          begin
+            Va::Logger::Automation.set_thread_variables(account.id, args[:evaluate_on_id], nil, vr.id) if association == :tickets
+            is_a_match = vr.event_matches? args[:current_events], evaluate_on
+            vr.pass_through evaluate_on, nil, nil if is_a_match
+          rescue StandardError => error
+            Rails.logger.debug "Error in api_webhook_rule worker - #{error.inspect}"
+            NewRelic::Agent.notice_error(error)
+          ensure
+            Va::Logger::Automation.unset_thread_variables if association == :tickets
+          end
         end
-      rescue Exception => error
-        Rails.logger.debug "Error in api_webhook_rule worker - #{error.inspect}"
-        NewRelic::Agent.notice_error(error)
       end
     end
-
   end
 end
