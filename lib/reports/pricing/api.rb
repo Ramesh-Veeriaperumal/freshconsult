fc62a139ad983340045d5ae1fb2e83a5d3eff81d
Balaji Radhakrishnan <balaji.radhakrishnan@freshworks.com>
2019-03-13 18:14:14 +0530

    FD-19629 Freshvisuals pricing API changes. (#3954)
    * FD-19629 reports pricing config files and API module
    
    * FD-19629 Actual api calls to reports implemented.
    
    * FD-19629 Freshreports -> FreshVisuals config mapping
    
    * FD-19629 rubocop fix of constant file
    
    * FD-19629 Calling the FV API on subscription update
    
    * FD-19629 Listening to reports related BM change.
    
    * FD-19629 mapping configs modified.
    
    * FD-19629 Mapping the features with reports configs
    
    * FD-19629 Adding the reports feature list in account constants.
    
    * FD-19629 rubocop fixes and some constant file changes.
    
    * FD-19629 Fixing the constant references
    
    * FD-19629 new test cases added
    
    * FD-19629 rubocop fixes
    
    * FD-19629 test cases with stubs
    
    * FD-19629 symlink pricing api config file
    
    * FD-19629 Updating the APIs to use new Freshreports config
    
    * FD-19629 Removed merged tickets from resource
    
    * FD-19629 rubocop fixes.
    
    * FD-19629 rubocop fixes 2.
    
    * FD-19629 Review comments #1.
    
    * FD-19629 pricing API more test coverage
    
    * FD-19629 removing the staging keys from yml
    
    * FD-19629 added new features and configs
    
    * FD-19629 added launch party before calling the Freshvisual worker.
    
    * FD-19629 pricing api dependent on freshvisuals yml.
    
    * FD-19629 New features added, resource dependent curated reports config added
    
    * FD-19629 Launchparty check changed as freshvisual bitmap, rubocop fixes.
    
    * FD-19629 Increased test case coverage
    
    * FD-19629 Remove group_performance from euc_hide_agent_metrics check.
    
    * FD-19629 replacing bitmap check with LP check
    
    * FD-19629 analytics_widget_change_chart upgraded to estate
    
    * FD-19629 inverting restricted feature check in pricing API
    
    * FD-19629 Fixing review comments
    
    * FD-19629 analytics_satisfaction_survey_report to garden
    
    * FD-19629 Fixing test case errors
    
    * FD-19629 Added FSM curated reports, CSAT resposes, questions resources added
    
    * FD-19629 A test case fix.

---
+++ lib/reports/pricing/api.rb
@@ -0,0 +1,190 @@
+module Reports::Pricing
+  module Api
+    include HelpdeskReports::Constants::FreshvisualFeatureMapping
+
+    API_RESOURCES = {
+      create_tenant: 'applications/tenants',
+      delete_tenant: 'applications/tenants/%{tenant_id}',
+      update_restriction: 'applications/tenants/%{tenant_id}/restrictions',
+      delete_restriction: 'applications/tenants/%{tenant_id}/restrictions'
+    }.freeze
+
+    SUCCESS_CODES = (200..204).freeze
+
+    def create_tenant
+      url = API_RESOURCES[:create_tenant]
+      body = { tenantId: current_account.id }
+
+      http_request('tenant', 'post', url, body)
+    end
+
+    # def delete_tenant
+    #   url = format(API_RESOURCES[:delete_tenant], tenant_id: current_account.id)
+
+    #   http_request('tenant', 'delete', url)
+    # end
+
+    # This is a upsert query.
+    def update_restrictions
+      url = format(API_RESOURCES[:update_restriction], tenant_id: current_account.id)
+      body = construct_restrictions_payload
+
+      http_request('restriction', 'put', url, body)
+    end
+
+    # def delete_restrictions
+    #   url = format(API_RESOURCES[:delete_restriction], tenant_id: current_account.id)
+
+    #   http_request('restriction', 'delete', url)
+    # end
+
+    private
+
+      def current_account
+        @current_account ||= Account.current
+      end
+
+      def http_request(resource, action, url, body = nil)
+        log_request_and_call(resource, action) do |connection|
+          connection.safe_send(action) do |req|
+            req.url url
+            req.headers['auth'] = jwt_token
+            req.headers['appName'] = FreshVisualsConfig['app_name']
+            req.options.timeout = FreshVisualsConfig['timeout_for_pricing_api']
+            if ['post', 'put'].include?(action)
+              req.headers['Content-Type'] = 'application/json'
+              req.body = body.to_json
+            end
+          end
+        end
+      end
+
+      def log_request_and_call(resource, action)
+        Rails.logger.debug "Reports pricing API called :: #{resource}_#{action}"
+        response = yield(build_connection)
+
+        Rails.logger.debug "Reports pricing API #{SUCCESS_CODES.include?(response.status) ? 'success' : 'error'} :: #{resource}_#{action}, response: #{response.body.inspect}"
+        raise "Pricing API Invalid response, code: #{response.status}" unless SUCCESS_CODES.include?(response.status)
+      rescue StandardError => e
+        Rails.logger.error "Pricing API: Exception in log_request_and_call :: #{resource}_#{action}, error: #{e.message}"
+        NewRelic::Agent.notice_error(e, description: "Pricing API: Exception in log_request_and_call :: #{resource}_#{action} for Account #{current_account.id}")
+      end
+
+      def build_connection
+        Faraday.new(FreshVisualsConfig['end_point']) do |faraday|
+          faraday.response :json, content_type: /\bjson$/
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      def jwt_token
+        JWT.encode freshreports_payload, FreshVisualsConfig['secret_key'], 'HS256', 'alg' => 'HS256', 'typ' => 'JWT'
+      end
+
+      def freshreports_payload
+        {
+          timezone: TimeZone.find_time_zone,
+          sessionExpiration: Time.now.to_i + FreshVisualsConfig['session_expiration_for_pricing_api'],
+          iat: Time.now.to_i,
+          exp: Time.now.to_i + FreshVisualsConfig['session_expiration_for_pricing_api'],
+          tenantId: current_account.id
+        }
+      end
+
+      def construct_restrictions_payload
+        payload = JSON.parse(CONFIG_PAYLOAD).deep_symbolize_keys
+        payload[:curatedRestrictions] = []
+        payload[:resourceRestrictions] = []
+
+        payload = add_features_from_freshvisuals_mapping(payload)
+        payload = add_resource_restriction_for_curated_reports(payload)
+        payload = remove_duplicates(payload)
+        payload = custom_modifications(payload)
+        apply_restrictions(payload)
+      end
+
+      def add_features_from_freshvisuals_mapping(payload)
+        FD_FRESVISUAL_FEATURE_MAPPING.keys.each do |feature|
+          next unless Account.current.safe_send("#{feature}_enabled?")
+
+          config = FD_FRESVISUAL_FEATURE_MAPPING[feature]
+          config_payload = config.class == Array ? construct_payload_for_feature(config) : construct_payload_for_feature([config])
+
+          config_payload[:featureConfig].keys.each do |report_type|
+            payload[:featureConfig][:pages][report_type].merge!(config_payload[:featureConfig][report_type])
+          end
+          payload[:curatedRestrictions].push(*config_payload[:curatedRestrictions])
+          payload[:resourceRestrictions].push(*config_payload[:resourceRestrictions])
+        end
+
+        payload
+      end
+
+      def construct_payload_for_feature(configs)
+        payload = { featureConfig: {}, curatedRestrictions: [], resourceRestrictions: [] }
+
+        configs.each do |config|
+          config_type = config[:config_type]
+
+          if config_type == CONFIG_TYPES[:FEATURE_CONFIGS]
+            report_type = config[:report_type]
+            feature = config[:value]
+
+            payload[:featureConfig][report_type] ||= {}
+            payload[:featureConfig][report_type][feature] = true
+          elsif config_type == CONFIG_TYPES[:CURATED_REPORTS]
+            payload[:curatedRestrictions] << config[:value]
+          elsif config_type == CONFIG_TYPES[:RESOURCE_RESTRICTIONS]
+            payload[:resourceRestrictions] << config[:value]
+          end
+        end
+
+        payload
+      end
+
+      # Certain resources has to be enabled if some curated reports are enabled. This method handles that.
+      def add_resource_restriction_for_curated_reports(payload)
+        RESOURCE_CURATED_REPORTS_MAP.each do |resource, curated_reports|
+          payload[:resourceRestrictions] << resource if payload[:curatedRestrictions].any? { |report| curated_reports.include?(report) }
+        end
+        payload
+      end
+
+      def remove_duplicates(payload)
+        payload[:curatedRestrictions].uniq!
+        payload[:resourceRestrictions].uniq!
+        payload
+      end
+
+      def custom_modifications(payload)
+        payload = custom_addition(payload)
+        payload = custom_deletion(payload)
+        payload
+      end
+
+      def custom_addition(payload)
+        payload
+      end
+
+      def custom_deletion(payload)
+        curated_reports_to_delete = []
+        if Account.current.features?(:euc_hide_agent_metrics)
+          # IN EUC admin should not see agent metrics.
+          curated_reports_to_delete.push(CURATED_REPORTS[:agent_performance][:value])
+        end
+
+        payload[:curatedRestrictions].delete_if { |report| curated_reports_to_delete.include? report }
+        payload
+      end
+
+      # The BM features we are maintaining are positive feature checks.
+      # For example, if analytics_ticket_lifecycle_report is enabled customer can view analytics_ticket_lifecycle_report in Fresh reports.
+      # curatedRestrictions and resourceRestrictions are negative, if we add analytics_ticket_lifecycle_report to curatedRestrictions customer cannot view that report.
+      # So, we need to invert curatedRestrictions, resourceRestrictions we have built so far.
+      def apply_restrictions(payload)
+        payload[:curatedRestrictions] = CURATED_REPORTS_LIST - payload[:curatedRestrictions]
+        payload[:resourceRestrictions] = RESOURCE_RESTRICTION_LIST  - payload[:resourceRestrictions]
+        payload
+      end
+  end
+end
