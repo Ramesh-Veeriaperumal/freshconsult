f316ed9be912cecc10c6102d7146f802b1e79cea
Shridar S <shridar.suryamoorthy@freshworks.com>
2019-03-18 17:49:52 +0530

    FD-30823 supervisor fix (#4440)

--- app/controllers/admin/supervisor_rules_controller.rb
+++ app/controllers/admin/supervisor_rules_controller.rb
@@ -3,12 +3,12 @@ class Admin::SupervisorRulesController < Admin::VaRulesController
   before_filter :check_supervisor_feature
 
   def ticket_field_filters_for_automations
-    fields = current_account.ticket_fields.non_encrypted_custom_fields.preload(:flexifield_def_entry)
+    fields = current_account.ticket_fields.non_encrypted_custom_fields.preload(:flexifield_def_entry).all
     fields.reject! { |tf| TicketFieldData::NEW_DROPDOWN_COLUMN_NAMES.include?(tf.flexifield_def_entry.flexifield_name) }
   end
 
   def ticket_field_actions_for_automations
-    fields = current_account.ticket_fields.non_encrypted_custom_fields.reject(&:fsm_reserved_custom_field?)
+    fields = current_account.ticket_fields.non_encrypted_custom_fields.reject(&:fsm_reserved_custom_field?).all
     fields.reject! { |tf| TicketFieldData::NEW_DROPDOWN_COLUMN_NAMES.include?(tf.flexifield_def_entry.flexifield_name) }
   end
 
