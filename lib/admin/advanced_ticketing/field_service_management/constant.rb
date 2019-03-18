3bc97183368f37d1d638f51b767a6100e754f33a
Supraja Ramesh <40852506+SuprajaR@users.noreply.github.com>
2019-03-18 08:35:07 +0530

    [FD-30713] Re-Adding the field technician logs (#4387)
    * FD-30713 adding back the logs
    
    *  FD-30509 Change FSM labels to sentence case
    
    * FD-30713 renaming fsm fields
    
    * FD-30713 changing field agent to field technician in error_messages.yml
    
    * FD-30713 missed out some camel-cased labels

--- lib/admin/advanced_ticketing/field_service_management/constant.rb
+++ lib/admin/advanced_ticketing/field_service_management/constant.rb
@@ -11,36 +11,36 @@ module Admin::AdvancedTicketing::FieldServiceManagement
         flexifield_alias: 'fsm_contact_name'
       },
       {
-        label: 'Phone Number',
+        label: 'Phone number',
         type: 'text',
         field_type: 'custom_text',
-        label_in_portal: 'Customer Phone Number',
+        label_in_portal: 'Customer phone number',
         name: 'cf_fsm_phone_number',
         required: true,
         flexifield_alias: 'fsm_phone_number'
       },
       {
-        label: 'Service Location',
+        label: 'Service location',
         type: 'text',
         field_type: 'custom_text',
-        label_in_portal: 'Service Location',
+        label_in_portal: 'Service location',
         name: 'cf_fsm_service_location',
         required: true,
         flexifield_alias: 'fsm_service_location'
       },
       {
-        label: 'Appointment Start Time',
+        label: 'Appointment start time',
         type: 'date_time',
         field_type: 'custom_date_time',
-        label_in_portal: 'Appointment Start Time',
+        label_in_portal: 'Appointment start time',
         name: 'cf_fsm_appointment_start_time',
         flexifield_alias: 'fsm_appointment_start_time'
       },
       {
-        label: 'Appointment End Time',
+        label: 'Appointment end time',
         type: 'date_time',
         field_type: 'custom_date_time',
-        label_in_portal: 'Appointment End Time',
+        label_in_portal: 'Appointment end time',
         name: 'cf_fsm_appointment_end_time',
         flexifield_alias: 'fsm_appointment_end_time'
       }
