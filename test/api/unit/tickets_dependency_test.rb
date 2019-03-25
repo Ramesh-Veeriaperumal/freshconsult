1004deafa1019da985f5971be2fa79dfc8806c87
karthikmurugesan <karthik.murugesan@freshworks.com>
2019-03-25 13:01:08 +0530

    [FD-31027] - Test case failure fix Reverse merge 18 march

--- test/api/unit/tickets_dependency_test.rb
+++ test/api/unit/tickets_dependency_test.rb
@@ -11,7 +11,7 @@ class TicketsDependencyTest < ActionView::TestCase
       :force_utf8_params, :persist_user_agent, :set_cache_buster, :remove_pjax_param,
       :set_pjax_url, :set_last_active_time, :reset_language, :remove_rails_2_flash_after,
       :set_affiliate_cookie, :verify_authenticity_token, :build_item, :load_multiple_items,
-      :add_to_history, :load_ticket_item, :build_note_body_attributes, :build_conversation_for_ticket,
+      :load_ticket_item, :build_note_body_attributes, :build_conversation_for_ticket,
       :check_for_from_email, :kbase_email_included, :set_default_source, :prepare_mobile_note_for_send_set,
       :fetch_note_attachments, :traffic_cop_warning, :check_for_public_notes,
       :check_reply_trial_customers_limit, :redirect_to_mobile_url, :portal_check, :verify_format_and_tkt_id,
