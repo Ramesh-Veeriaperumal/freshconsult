5e3e39c356b22344f03c677b8970219451e5f66c
Ragav <ragavendren.ramachandran@freshworks.com>
2019-03-27 14:20:24 +0530

    FD-30213 Ticket create/update APIs optimizations (#4166)
    * FD-109355 Ticket create/update APIs optimizations
    
    * Update methods.rb
    
    * FD-30213 Memoize fields for account and other review comments
    
    * FD-30213 Memoize ticket fields

--- lib/api_webhooks/methods.rb
+++ lib/api_webhooks/methods.rb
@@ -1,5 +1,4 @@
 module ApiWebhooks::Methods
-  
   include ApiWebhooks::Constants
   include Va::Util
 
@@ -7,30 +6,32 @@ module ApiWebhooks::Methods
     (!zendesk_import? && !freshdesk_webhook? && import_id.blank?)
   end
 
-  def subscribe_event_create 
+  def subscribe_event_create
     event_changes = MAP_CREATE_ACTION[self.class.name]
     send_subscribe_events(event_changes) if rule_exists?(MAP_CREATE_ACTION[self.class.name])
   end
 
   def subscribe_event_update
-    event_changes = @model_changes.inject({}) do |filtered, (change_key, change_value)| 
-                                    filter_subscribe_event filtered, change_key, change_value  end
+    event_changes = @model_changes.inject({}) do |filtered, (change_key, change_value)|
+      filter_subscribe_event filtered, change_key, change_value
+    end
 
-    unless event_changes.blank?                                 
-      event_changes.merge! MAP_UPDATE_ACTION[self.class.name] 
+    if event_changes.present?
+      event_changes.merge! MAP_UPDATE_ACTION[self.class.name]
       send_subscribe_events(event_changes) if rule_exists?(MAP_UPDATE_ACTION[self.class.name])
     end
   end
 
-  def filter_subscribe_event filtered, change_key, change_value
+  def filter_subscribe_event(filtered, change_key, change_value)
     change_key = change_key.to_sym
-    ( MAP_SUBSCRIBE_EVENT[self.class.name].include?( change_key ) ||
-      event_flexifields.map(&:flexifield_name).map(&:to_sym).include?(change_key)
-        ) ? filtered.merge!({change_key => change_value}) : filtered
+    include_change_key(change_key) ? filtered.merge!(change_key => change_value) : filtered
   end
 
-  def event_flexifields
+  def include_change_key(change_key)
+    return true if MAP_SUBSCRIBE_EVENT[self.class.name].include?(change_key)
+
     @event_flexifields ||= Account.current.event_flexifields_with_ticket_fields_from_cache
+    @event_flexifields.any? { |event_field| event_field.flexifield_name.to_sym == change_key }
   end
 
   def map_class class_name
