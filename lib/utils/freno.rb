be5073eb06002d4e5a00e4fe67317a11390b2460
Ragav <ragavendren.ramachandran@freshworks.com>
2019-04-10 15:14:44 +0530

    FD-31624 Freno changes for contact delete and spam delete workers (#4671)

---
+++ lib/utils/freno.rb
@@ -0,0 +1,53 @@
+require 'json'
+require 'httparty'
+
+module Utils
+  module Freno
+      FRENO_CHECK_SHARD_API = '/check/appname/mysql/'
+
+      def get_replication_lag_for_shard(application_name, shard_name)
+        # if shard name or application name is empty, replication lag can't be checked for the shard.
+        return  0 if shard_name.blank? || application_name.blank?
+
+        #form the check API endpoint URL
+        check_shard_api_endpoint = FRENO_CHECK_SHARD_API
+        check_shard_api_endpoint.gsub! 'appname', application_name
+        check_shard_api_url = FrenoConfig['freno_base_url'] + check_shard_api_endpoint + shard_name
+
+        # check replication lag API and return based on response code.
+        Rails.cache.fetch(check_shard_api_url,  :expires => 10.second) do
+          response = HTTParty.get(check_shard_api_url, timeout: 1)
+          if response.code == 200 then
+            return  0 # No replication lag found, return 0
+          elsif response.code == 429 then
+            # Replication lag has been found by Freno.
+            return parse_api_response(response)
+          elsif response.code == 417 then
+            # This application has been explicitly throttled, wait for 10 minutes and check again.
+            return 10 * 60
+          else
+            Rails.logger.debug("Warning: Freno API respons code: #{response.code} for shard: #{shard_name}")
+            return -1
+          end
+        end
+
+        # catch any exception from freno API calls and handle them
+        rescue Exception => e
+          NewRelic::Agent.notice_error(e, description: 'Error occurred in making Freno API call: #{e.message} ')
+          Rails.logger.debug("Error occurred while making Freno check API call :: #{e.message}")
+          return -1
+      end
+
+       def parse_api_response(response)
+         parsed_json = JSON.parse(response.body)
+         replag = parsed_json["Value"]
+         if replag.blank? then
+           return 0
+         else
+           return replag.to_i
+         end
+       rescue ArgumentError
+         return 0
+       end
+  end
+end
