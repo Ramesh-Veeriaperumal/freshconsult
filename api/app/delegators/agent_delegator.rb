3411e4844a90e480dade9734d7d8b07b3f7eb081
Karthik Baskaran <42604825+karthikbaskaran1996@users.noreply.github.com>
2019-04-30 12:01:56 +0530

    FD-32390 - Ability to update agent avatar in agents private api (#4815)
    * FD-31638 - Update agent swagger to include avatar_id
    
    * FD-31638 - Added avatar
    
    * FD-31862 - Agent avatar update
    
    * FD-31862 - Code changes
    
    * FD-31862 - Private api test cases
    
    * FD-31862 - Minor changes
    
    * FD-31638 - Remove avatar from private api
    
    * FD-31862 - Remove avatar update feature
    
    * FD-31638 - Rubocop issues addressed
    
    * FD-31638 - Moved to private api
    
    * FD-31638 - Minor fix
    
    * FD-31638 - Test case fix
    
    * FD-31638 - Test case fix
    
    * FD-31638 - Test case fix
    
    * FD-31638 - Minor changes
    
    * FD-31638 - Review comments addressed
    
    * FD-31638 - Minor change
    
    * FD-31638 - Review comments addressed
    
    * FD-31638 - Minor change
    
    * FD-31638 - Rubocop comments
    
    * FD-31638 - Minor change
    
    * FD-31638 - Test case review comments
    
    * FD-31862 - Test case fix
    
    * FD-31638 - Agent contact association support

--- api/app/delegators/agent_delegator.rb
+++ api/app/delegators/agent_delegator.rb
@@ -3,6 +3,13 @@ class AgentDelegator < BaseDelegator
 
   validate :validate_group_ids, if: -> { self[:group_ids] }
   validate :validate_role_ids, if: -> { self[:role_ids] }
+  validate :validate_avatar_extension, if: -> { @avatar_attachment && errors[:attachment_ids].blank? }
+
+  def initialize(record, options = {})
+    options[:attachment_ids] = Array.wrap(options[:avatar_id].to_i) if options[:avatar_id]
+    super(record, options)
+    @avatar_attachment = @draft_attachments.first if @draft_attachments
+  end
 
   def validate_role_ids
     invalid_roles = self[:role_ids].map(&:to_i) - Account.current.roles_from_cache.map(&:id)
@@ -17,6 +24,14 @@ class AgentDelegator < BaseDelegator
     if invalid_groups.present?
       errors[:group_ids] << :invalid_list
       (@error_options ||= {}).merge!(group_ids: { list: invalid_groups.join(', ') })
+    end
+  end
+
+  def validate_avatar_extension
+    valid_extension, extension = ApiUserHelper.avatar_extension_valid?(@avatar_attachment)
+    unless valid_extension
+      errors[:avatar_id] << :upload_jpg_or_png_file
+      (@error_options ||= {}).merge!(avatar_id: { current_extension: extension })
     end
   end
 end
