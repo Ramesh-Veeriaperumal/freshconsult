d8f80b7ee298c8c52f1be4f00e22d8656012095b
Balaji Radhakrishnan <balaji.radhakrishnan@freshworks.com>
2019-05-03 17:50:46 +0530

    FD-31796 Delay Survey destroy when created via portal (#4682)
    * FD-31796 Adding source params to the survey from portal requests.
    
    * FD-31796 remove null from the default parameters
    
    * FD-31796 remove the typo in supports controller.
    
    * FD-31796 Worker to destroy models with a delay.
    
    * FD-31796 moving the constants to the custom survey handle.
    
    * FD-31796 review comment fixes.
    
    * FD-31796 increasing the delay to 20 secs.
    
    * FD-31796 added worker test cases.
    
    * FD-31796 rubocop fixes in three files.

--- app/models/custom_survey/survey_handle/constants.rb
+++ app/models/custom_survey/survey_handle/constants.rb
@@ -3,4 +3,6 @@ class CustomSurvey::SurveyHandle < ActiveRecord::Base
     EmailNotification::TICKET_RESOLVED => CustomSurvey::Survey::RESOLVED_NOTIFICATION,
     EmailNotification::TICKET_CLOSED => CustomSurvey::Survey::CLOSED_NOTIFICATION
   }
+  NEW_VIA_PORTAL = 'new_via_portal'.freeze
+  DESTROY_HANDLE_DELAY_INTERVAL = 20
 end
