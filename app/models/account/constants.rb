924b103ab9da8afc015eb560999b0b12f4c88287
venkatesan <venkatesan.murali@freshworks.com>
2019-05-24 11:26:20 +0530

    FD-33003 enabling denormalzied search in signups

--- app/models/account/constants.rb
+++ app/models/account/constants.rb
@@ -295,7 +295,7 @@ class Account < ActiveRecord::Base
       saml_ecrypted_assertion: false, quoted_text_parsing_feature: false,
       product_central_publish: false, help_widget: false, company_central_publish: false,
       field_service_management_lp: false, bot_email_channel: false, allow_huge_ccs: false,
-      description_by_default: false, ticket_fields_central_publish: false, fluffy: false, disable_field_service_management: false, facebook_page_scope_migration: false, agent_group_central_publish: false, custom_fields_search: false,
+      description_by_default: false, ticket_fields_central_publish: false, fluffy: false, disable_field_service_management: false, facebook_page_scope_migration: false, agent_group_central_publish: false, custom_fields_search: true,
       update_billing_info: false, allow_billing_info_update: false, tag_central_publish: false, redis_picklist_id: true,
       nested_field_revamp: true, freshvisual_configs: false, freshid_org_v2: false, hide_agent_login: false, addon_based_billing: false,
       kbase_mint: false, text_custom_fields_in_etl: false, recalculate_daypass: false
