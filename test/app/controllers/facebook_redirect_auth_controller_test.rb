c846f05b60e4e0f09d01913f923cbb6c974a16cc
ravishankar-gnanaprakasam <39148615+ravishankarfreshworks@users.noreply.github.com>
2019-05-29 18:35:26 +0530

    fb redirect auth controller test case fix (#5097)

--- test/app/controllers/facebook_redirect_auth_controller_test.rb
+++ test/app/controllers/facebook_redirect_auth_controller_test.rb
@@ -11,14 +11,14 @@ class FacebookRedirectAuthControllerTest < ActionController::TestCase
   def test_complete_request_failure
     FacebookRedirectAuthController.any_instance.stubs(:get_others_redis_key).returns('host_url')
     get :complete, controller_params(version: 'private')
-    assert_equal true, response.body.include?('/integrations/applications')
+    assert_equal true, response.body.include?('/admin/home')
     assert_response 302
   end
 
   def test_complete_request_no_redis_key
     FacebookRedirectAuthController.any_instance.stubs(:get_others_redis_key).returns(nil)
     get :complete, controller_params(version: 'private', state: 'state', code: 'dummycode123#')
-    assert_equal true, response.body.include?('/integrations/applications')
+    assert_equal true, response.body.include?('/admin/home')
     assert_response 302
   end
 end
