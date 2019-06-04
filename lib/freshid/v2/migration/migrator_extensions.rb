4e9842ae67677cc0aecb00aca13467f4cd277d07
Ramya Ravi <42969582+ramya-ravi-fd@users.noreply.github.com>
2019-06-04 18:41:25 +0530

    FD-33391 Freshid migration falcon (#5136)
    * changed branch name for freshid-ruby-client
    
    * added freshconnect post_migration changes
    
    * code refactor
    
    * migration code refactor
    
    * migration code refactor
    
    * code refactor
    
    * change tag version freshid-ruby-client
    
    * Migration caller bug fix
    
    * change gemfile version

--- lib/freshid/v2/migration/migrator_extensions.rb
+++ lib/freshid/v2/migration/migrator_extensions.rb
@@ -10,29 +10,29 @@ module Freshid::V2::Migration::MigratorExtensions
   end
 
   def fetch_freshconnect_accounts(account, linked_accounts)
-    product_id = product_account_mapping[@freshconnect_product_name]
+    product_id = product_account_mapping[@freshconnect_product_name].try(:to_s)
     freshconnect_account = account.freshconnect_account
     return if product_id.blank? || freshconnect_account.blank?
     account_info = {}
     unless linked_account_present?(linked_accounts, product_id, freshconnect_account.freshconnect_domain)
       account_info[:domain] = freshconnect_account.freshconnect_domain
-      account_info[:external_id] = freshconnect_account.product_account_id
+      account_info[:external_id] = freshconnect_account.product_account_id.to_s
       account_info[:product_id] = product_id
     end
-    [account_info]
+    account_info.present? ? [account_info] : nil
   end
 
   def fetch_freshcaller_accounts(account, linked_accounts)
-    product_id = product_account_mapping[@freshcaller_product_name]
+    product_id = product_account_mapping[@freshcaller_product_name].try(:to_s)
     freshcaller_account = account.freshcaller_account
     return if product_id.blank? || freshcaller_account.blank?
     account_info = {}
     unless linked_account_present?(linked_accounts, product_id, freshcaller_account.domain)
       account_info[:domain] = freshcaller_account.domain
-      account_info[:external_id] = freshcaller_account.freshcaller_account_id
+      account_info[:external_id] = freshcaller_account.freshcaller_account_id.to_s
       account_info[:product_id] = product_id
     end
-    [account_info]
+    account_info.present? ? [account_info] : nil
   end
 
   def account_admin_emails(account)
