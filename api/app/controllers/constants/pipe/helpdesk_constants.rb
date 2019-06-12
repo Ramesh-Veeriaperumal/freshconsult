4c274ac698f3c3af18bfacc4b697fcaf6ed481da
venkatesan <venkatesan.murali@freshworks.com>
2019-06-12 07:24:04 +0530

    changing pipe max api limit value for freshmover

--- api/app/controllers/constants/pipe/helpdesk_constants.rb
+++ api/app/controllers/constants/pipe/helpdesk_constants.rb
@@ -1,6 +1,6 @@
 module Pipe
   module HelpdeskConstants
     LOAD_OBJECT_EXCEPT = [:toggle_email, :toggle_fast_ticket_creation, :change_api_v2_limit]
-    MAX_API_LIMIT = 25000
+    MAX_API_LIMIT = 100000
   end
 end
