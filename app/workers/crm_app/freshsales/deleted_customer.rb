a6258825996f8db62462a0ef8f7258534a705845
Praveen Pethurajan <40791421+PraveenPethurajan@users.noreply.github.com>
2019-06-13 18:36:59 +0530

    FD-19047-Delete lead from autopilot (#5113)
    * FD-19047-Delete lead from autopilot
    
    * FD-19047-Fixed test case for lead deletion
    
    * Made changes after testing
    
    * Fixing test case
    
    * Rubocop changes
    
    * Test case updated
    
    * updating associated accounts
    
    * Review changes done for lead deletion
    
    * FD-19047- FIxing test case for lead deletion
    
    * Changes after testing
    
    * FD-31868
    
    * Reverting changes

--- app/workers/crm_app/freshsales/deleted_customer.rb
+++ app/workers/crm_app/freshsales/deleted_customer.rb
@@ -2,7 +2,7 @@ class CRMApp::Freshsales::DeletedCustomer < CRMApp::Freshsales::BaseWorker
 
   def perform(args = {})
     args.symbolize_keys!
-    ThirdCRM.new.mark_as_deleted_customer
+    ThirdCRM.new.update_lead_info(args[:admin_email])
     freshsales_utility(args, Account.current).account_cancellation
   rescue => e
     Rails.logger.error "Error while pushing account cancellation to Freshsales 
