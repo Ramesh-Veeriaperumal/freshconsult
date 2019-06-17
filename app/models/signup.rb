12928f73b58694faacd3d91e796a68160d6a9e56
Ramya Ravi <42969582+ramya-ravi-fd@users.noreply.github.com>
2019-06-17 22:38:21 +0530

    FD-31332 freshcaller integration (#5204)
    * FD-31332 freshcaller integration changes
    
    * Freshcaller integration changes
    
    * code refactor
    
    * code refactor
    
    * FD-31332 added review changes

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -5,7 +5,7 @@ class Signup < ActivePresenter::Base
 
   presents :account, :user
   
-  attr_accessor :contact_first_name, :contact_last_name, :org_id, :join_token, :fresh_id_version
+  attr_accessor :contact_first_name, :contact_last_name, :org_id, :join_token, :fresh_id_version, :org_domain
   before_validation :build_primary_email, :build_portal, :build_roles, :build_admin,
     :build_subscription, :build_account_configuration, :set_time_zone, :build_password_policy, :set_freshid_signup_version
   
@@ -45,7 +45,7 @@ class Signup < ActivePresenter::Base
   end
 
   def create_freshid_v2_org_and_account
-    account.create_freshid_v2_account(user, join_token)
+    account.create_freshid_v2_account(user, join_token, org_domain)
   end
   private
     def build_primary_email
