2a527a096dd8759da478e59d2cacdadf01c2f02b
Hrishikesh Premkumar <hrishikesh-p@users.noreply.github.com>
2019-06-18 22:17:32 +0530

    FD-33310 Add quickbooks sso URL Validation (#5067)

--- lib/integrations/quickbooks/constant.rb
+++ lib/integrations/quickbooks/constant.rb
@@ -1,11 +1,12 @@
 module Integrations::Quickbooks::Constant
 
-	ACCESS_TOKEN_RENEWAL_URI = "https://appcenter.intuit.com/api/v1/connection/reconnect"
+  ACCESS_TOKEN_RENEWAL_URI = 'https://appcenter.intuit.com/api/v1/connection/reconnect'.freeze
 
-	DISCONNECT_URI = "https://appcenter.intuit.com/api/v1/connection/disconnect"
+  DISCONNECT_URI = 'https://appcenter.intuit.com/api/v1/connection/disconnect'.freeze
 
- 	OPENID_URL = "https://openid.intuit.com/openid/xrds"
+  OPENID_URL = 'https://openid.intuit.com/openid/xrds'.freeze
 	# renew the access token after 150 days
 	TOKEN_RENEWAL_DAYS = 151
 
+  INTUIT_OPEN_ID_HOST = 'openid.intuit.com'.freeze
 end
