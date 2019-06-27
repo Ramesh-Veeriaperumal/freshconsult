69e4a7b97095cd4aca79f7f7a1c26092e56b8ea9
Krishnanand nb <krishnanand.balasubramanian@freshworks.com>
2019-06-27 16:36:27 +0530

    FD-34160 : teant logger prestaging (#5388)
    * FD-33466 : teant logger prestaging
    
    * FD-33466 : test

--- lib/log_helper.rb
+++ lib/log_helper.rb
@@ -5,12 +5,12 @@ module LogHelper
   def log_format(payload)
     performance_metrics = TimeBandits.metrics
     # for trimming the query params
-    payload[:url] = payload[:url].split('?')[0] unless payload[:url].nil? 
+    payload[:url] = payload[:url].split('?')[0] unless payload[:url].nil?
     payload[:path] = payload[:path].split('?')[0] unless payload[:path].nil?
     "id=#{payload[:uuid]}, e=#{payload[:error]}, ip=#{payload[:ip]}, a=#{payload[:account_id]}, u=#{payload[:user_id]}, s=#{payload[:shard_name]}, "\
      "d=#{payload[:domain]}, url=#{payload[:url]}, p=#{payload[:path]}, c=#{payload[:controller]}, acn=#{payload[:action]}, h=#{payload[:server_ip]}, "\
      "sts=#{payload[:status]}, f=#{payload[:format]}, db=#{payload[:db_runtime]}, vw=#{payload[:view_runtime]}, rc=#{performance_metrics[:redis_calls]}, "\
      "r=#{performance_metrics[:redis_time].round(2)}, mc=#{performance_metrics[:memcache_calls]}, mdr=#{performance_metrics[:memcache_dup_reads]}, "\
-     "m=#{performance_metrics[:memcache_time].round(2)}, oa=#{payload[:oa]}, dur=#{payload[:duration]}"
+     "m=#{performance_metrics[:memcache_time].round(2)}, oa=#{payload[:oa]}, qt=#{payload[:queue_time]}, tdur=#{payload[:total_duration]}, dur=#{payload[:duration]}"
   end
 end
--- lib/middleware/application_logger.rb
+++ lib/middleware/application_logger.rb
@@ -7,26 +7,48 @@ require 'socket'
 
 class Middleware::ApplicationLogger
   include LogHelper
+  X_FD_ACCOUNT_ID = 'X-FD-Account-Id'.freeze
+  X_FD_USER_ID = 'X-FD-User-Id'.freeze
+  X_FD_SHARD = 'X-FD-Shard'.freeze
+  X_FD_QUEUE_TIME = 'X-FD-Queue-Time'.freeze
+  SHARD_PREFIX = 'shard_'.freeze
 
-  def initialize(app, options = {})
+  def initialize(app, _options = {})
     @app = app
     @@logger ||= CustomLogger.new("#{Rails.root}/log/application.log")
-    @@server_ip ||= ( ENV['DOCKER_HOST_IP'] || Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address) )
+    @@server_ip ||= (ENV['DOCKER_HOST_IP'] || Socket.ip_address_list.detect(&:ipv4_private?).try(:ip_address))
     @controller_log_info = nil
   end
 
   def call(env)
     env[:obj_allocation] = object_allocation
-    env[:start_time] = Time.zone.now
-    @app.call(env).tap do |response|
-      status, _headers, _body = *response
-      duration = (Time.zone.now - env[:start_time]) * 1000
-      payload = set_payload(env)
-      payload[:duration] = duration.round(2)
-      payload[:status] = status
-      payload[:oa] = object_allocation - env[:obj_allocation]
-      @@logger.info format_payload(payload)
+    env[:start_time] = Time.zone.now.to_f
+
+    status, headers, body = @app.call(env)
+    begin
+    x_request_start = case env['HTTP_X_REQUEST_START']
+                      when /t=([\d+\.]+)/ then Regexp.last_match(1).to_f
+                      end
+
+    now = Time.zone.now.to_f
+    duration = (now - env[:start_time]) * 1000
+    payload = fetch_payload(env)
+    payload[:duration] = duration.round(2)
+    payload[:status] = status
+    payload[:oa] = object_allocation - env[:obj_allocation]
+    payload[:queue_time] = ((env[:start_time] - x_request_start) * 1000).round(3) if x_request_start.present?
+    payload[:total_duration] = x_request_start.present? ? ((now - x_request_start) * 1000).round(3) : payload[:duration]
+    @@logger.info format_payload(payload)
+
+    headers[X_FD_ACCOUNT_ID] = payload[:account_id].to_s if payload[:account_id].present?
+    headers[X_FD_USER_ID]    = payload[:user_id].to_s if payload[:user_id].present?
+    headers[X_FD_SHARD]      = payload[:shard_name].sub(SHARD_PREFIX, '').to_s if payload[:shard_name].present?
+    headers[X_FD_QUEUE_TIME] = payload[:queue_time].to_s if payload[:queue_time].present?
+    rescue StandardError => e
+      Rails.logger.error "Exception on api logger ::: #{e.message} ::: backtrace ::: #{e.backtrace.join("\n")}"
+      NewRelic::Agent.notice_error(e)
     end
+    [status, headers, body]
   end
 
   private
@@ -47,22 +69,22 @@ class Middleware::ApplicationLogger
 
     # Getting payload keys from request.env is highly preferred since the values got from controller log subscriber uses Thread.current and for errorneous requests and edge cases Thread.current is not reset.
 
-    def set_payload env
+    def fetch_payload(env)
       payload = {}
       request = Rack::Request.new(env)
       payload[:domain] = request.env['HTTP_HOST'] || env['HTTP_HOST']
-      payload[:ip] = request.env['CLIENT_IP'] || env["CLIENT_IP"]
+      payload[:ip] = request.env['CLIENT_IP'] || env['CLIENT_IP']
       payload[:url] = request.url
-      payload[:server_ip] = @@server_ip || request.env['SERVER_ADDR'] || env["SERVER_ADDR"]
+      payload[:server_ip] = @@server_ip || request.env['SERVER_ADDR'] || env['SERVER_ADDR']
       payload[:uuid] = message_uuid(request)
       set_controller_keys(payload, request) if controller_log_info.present?
       payload
     end
 
-    def set_controller_keys payload, request
+    def set_controller_keys(payload, request)
       payload[:path] = controller_log_info[:path]
       payload[:controller] = controller_log_info[:controller]
-      payload[:action] =  controller_action(request)
+      payload[:action] = controller_action(request)
       # Not using controller status as we have the status got from action dispatch itself in above call method.
       # payload[:status] = controller_log_info[:status]
       payload[:format] = controller_log_info[:format]
@@ -70,23 +92,23 @@ class Middleware::ApplicationLogger
       payload[:db_runtime] = controller_log_info[:db_runtime]
       payload[:view_runtime] = controller_log_info[:view_runtime]
       payload[:error] = controller_log_info[:error]
-      set_account_keys(payload)
+      payload_account_keys(payload)
     end
 
-    def set_account_keys payload
+    def payload_account_keys(payload)
       payload[:account_id] = controller_log_info[:account_id]
       payload[:user_id] = controller_log_info[:user_id]
       payload[:shard_name] = controller_log_info[:shard_name]
     end
 
-    def message_uuid request
+    def message_uuid(request)
       request.env['action_dispatch.request_id'] || controller_log_info[:uuid]
     end
 
     # Can get controller name also from request.env but it gives a path based name rather then currently logged class name.
     # Eg. helpdesk/dashboard vs Helpdesk::DashboardController
 
-    def controller_action request
-      request.env["action_dispatch.request.path_parameters"] ? request.env["action_dispatch.request.path_parameters"][:action] : controller_log_info[:action]
+    def controller_action(request)
+      request.env['action_dispatch.request.path_parameters'] ? request.env['action_dispatch.request.path_parameters'][:action] : controller_log_info[:action]
     end
 end
