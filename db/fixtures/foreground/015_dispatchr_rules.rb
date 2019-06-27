9fc59cf3423d433527e0e25d9f7179ebde663744
Ramya Ravi <42969582+ramya-ravi-fd@users.noreply.github.com>
2019-06-27 13:45:20 +0530

    Dispatcher seeding (#5384)

--- db/fixtures/foreground/015_dispatchr_rules.rb
+++ db/fixtures/foreground/015_dispatchr_rules.rb
@@ -13,4 +13,10 @@ VaRule.seed(:account_id, :name, :rule_type) do |s|
   s.active = true
   s.description = 'This is a sample rule to help you understand how Dispatch\'r works. Feel free to edit or delete this rule.
 In this example we will create a rule that will look at all incoming emails that have the text -"return" or "refund" and assigns them to "Billing" group.'
+  s.condition_data = { any: [{ evaluate_on: 'ticket', name: 'subject_or_description',
+                             operator: 'contains', value: 'return' },
+                             { evaluate_on: 'ticket', name: 'subject_or_description',
+                             operator: 'contains', value: 'refund' }] }
+
+
 end
