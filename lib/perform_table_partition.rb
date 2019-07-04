8612c61402a27d9806cfbbeb44360ff3dd08081b
Shridar S <shridar.suryamoorthy@freshworks.com>
2019-07-04 05:31:16 +0530

    FD-34559  Fix test cases (#5444)
    * Fixing failing test case builds
    
    * Extra check
    
    * Fix rubocop errors

--- lib/perform_table_partition.rb
+++ lib/perform_table_partition.rb
@@ -1,13 +1,13 @@
 module PerformTablePartition
 
-  PARTITION_TABLES = ["helpdesk_tickets","helpdesk_notes","users","helpdesk_ticket_states",
-                      "flexifields","helpdesk_attachments","helpdesk_activities","survey_remarks",
-                      "survey_handles","survey_results","survey_result_data","helpdesk_schema_less_tickets",
-                      "helpdesk_schema_less_notes","support_scores","helpdesk_dropboxes","helpdesk_external_notes",
-                      "helpdesk_ticket_bodies","helpdesk_note_bodies","user_emails",
-                      "freshfone_calls","archive_tickets","archive_notes","archive_ticket_associations",
-                      "archive_note_associations","archive_childs","contact_notes","contact_note_bodies",
-                      "company_notes","company_note_bodies","denormalized_flexifields"]
+  PARTITION_TABLES = ['helpdesk_tickets', 'helpdesk_notes', 'users', 'helpdesk_ticket_states',
+                      'flexifields', 'helpdesk_attachments', 'helpdesk_activities', 'survey_remarks',
+                      'survey_handles', 'survey_results', 'survey_result_data', 'helpdesk_schema_less_tickets',
+                      'helpdesk_schema_less_notes', 'support_scores', 'helpdesk_dropboxes',
+                      'helpdesk_external_notes', 'helpdesk_ticket_bodies', 'helpdesk_note_bodies', 'user_emails',
+                      'freshfone_calls', 'archive_tickets', 'archive_notes', 'archive_ticket_associations',
+                      'archive_note_associations', 'archive_childs', 'contact_notes', 'contact_note_bodies',
+                      'company_notes', 'company_note_bodies', 'denormalized_flexifields', 'ticket_field_data'].freeze
   PARTITION_SIZE = 128
 
   PARTITION_COLUMN = "account_id"
