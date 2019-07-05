463560f5bab8ab6b945d64189589f3ca4359329e
Krishnanand nb <krishnanand.balasubramanian@freshworks.com>
2019-07-05 12:32:36 +0530

    FD-34277 : Fix mailbox validation (#5392)
    * FD-34277 : Fix mailbox validation
    
    * FD-34277 : wrapping all the methods inside timeouts
    
    * FD-34277 : imap variable fix
    
    * FD-34277 : Fix mailbox validation

--- lib/mailbox_validator.rb
+++ lib/mailbox_validator.rb
@@ -1,79 +1,115 @@
 module MailboxValidator
-
   class IdleNotSupportedError < StandardError
   end
-  
+
   def validate_mailbox_details
     verify_result = verify_mailbox_details
-    render :json => {:success => verify_result[:success], :msg => verify_result[:msg]}.to_json
+    render json: { success: verify_result[:success], msg: verify_result[:msg] }.to_json
   end
 
   private
 
+    MAIL_VALIDATION_TIMEOUT = 20
+
     def verify_mailbox_details
-      unless params[:email_config][:imap_mailbox_attributes][:_destroy].to_bool
-        imap_verify = verify_imap_details?
-      else
-        imap_verify = { :success => true }
-      end
+      imap_verify = if params[:email_config][:imap_mailbox_attributes][:_destroy].to_bool
+                      { success: true }
+                    else
+                      verify_imap_details?
+                    end
       smtp_verify = verify_smtp_details?
-      verified = imap_verify[:success] && smtp_verify[:success]
-      unless imap_verify[:success] || smtp_verify[:success]
-        msg = [imap_verify[:msg], smtp_verify[:msg]].join("<br />")
-      else
-        msg = imap_verify[:success] ? smtp_verify[:msg] : imap_verify[:msg]
-      end
+      verified    = imap_verify[:success] && smtp_verify[:success]
+      msg         = if imap_verify[:success] || smtp_verify[:success]
+                      imap_verify[:success] ? smtp_verify[:msg] : imap_verify[:msg]
+                    else
+                      [imap_verify[:msg], smtp_verify[:msg]].join('<br />')
+                    end
 
-      {:success => verified, :msg => msg}
+      { success: verified, msg: msg }
     end
-    
+
     def verify_imap_details?
-      args = params[:email_config][:imap_mailbox_attributes]
+      args            = params[:email_config][:imap_mailbox_attributes]
       filtered_params = args.except(:password)
-      verified = false
-      msg = ""
+      verified        = false
+      msg             = ''
+      imap            = nil
       begin
-        imap = Net::IMAP.new(args[:server_name], args[:port], args[:use_ssl].to_bool)
+        options        = {}
+        options[:port] = args[:port]
+        options[:ssl]  = { verify_mode: Net::IMAP::VERIFY_PEER } if args[:use_ssl].to_bool
 
-        if "PLAIN".casecmp(args[:authentication]) == 0
-          imap.login(args[:user_name], args[:password])
-        else
-          imap.authenticate(args[:authentication], args[:user_name], args[:password]) 
+        # We can be blocked/stuck in two scenarios
+        #
+        # 1. During imap connection phase
+        #
+        #   we will wrap it around in a
+        #    timeout block!
+        #
+        # 2. During login/authenticate phase
+        #
+        #    To handle this, there is no read_timeout option. So we will wrap it
+        #    around in a timeout block!
+        #
+        #    Note: this can happen not just when imap server is not responding,
+        #    but can happen when it responds with wrong data, ruby imap
+        #    implementation, will keep looping until it gets expected response,
+        #    ignoring unknown tags (which means, forever)
+        #
+
+        # imap connection
+        Timeout.timeout(MAIL_VALIDATION_TIMEOUT, Net::OpenTimeout) do
+          imap = Net::IMAP.new(args[:server_name], options)
+          if 'PLAIN'.casecmp(args[:authentication]).zero?
+            imap.login(args[:user_name], args[:password])
+          else
+            imap.authenticate(args[:authentication], args[:user_name], args[:password])
+          end
+          # imap login
+          raise IdleNotSupportedError, 'Mailbox server does not support IDLE' unless imap.capability.include?('IDLE')
+
+          imap.logout
+          # msg = I18n.t('mailbox.authetication_success')
         end
-
-        raise IdleNotSupportedError, "Mailbox server does not support IDLE" unless imap.capability.include?("IDLE")
-
-        imap.logout()
-        #msg = I18n.t('mailbox.authetication_success')
         verified = true
       rescue IdleNotSupportedError => error
         msg = I18n.t('mailbox.idle_not_supported')
-        Rails.logger.debug "error while verifying the imap details : #{error} #{filtered_params.inspect}"
-      rescue SocketError => error
+        Rails.logger.error "error while verifying the imap details : #{error} #{filtered_params.inspect}"
+      rescue SocketError, Net::OpenTimeout, Net::ReadTimeout => error
         msg = I18n.t('mailbox.imap_connection_error')
-        Rails.logger.debug "error while verifying the imap details : #{error} #{filtered_params.inspect}"
-      rescue Exception => error
+        Rails.logger.error "error while verifying the imap details : #{error.inspect} #{filtered_params.inspect}"
+      rescue StandardError => error
         msg = I18n.t('mailbox.imap_error')
-        Rails.logger.debug "error while verifying the imap details : #{error} #{filtered_params.inspect}"      
+        Rails.logger.error "error while verifying the imap details : #{error} #{filtered_params.inspect}"
+      ensure
+        # Safe to ignore disconnect errors
+        begin
+          Timeout.timeout(MAIL_VALIDATION_TIMEOUT, Net::OpenTimeout) { imap.disconnect } if imap && !imap.disconnected?
+        rescue StandardError => e
+          Rails.logger.error("IMAP disconnect error: #{e.message} #{e.inspect}")
+        end
       end
-      { :success => verified, :msg => msg }
+
+      { success: verified, msg: msg }
     end
 
     def verify_smtp_details?
-      args = params[:email_config][:smtp_mailbox_attributes]
+      args            = params[:email_config][:smtp_mailbox_attributes]
       filtered_params = args.except(:password)
-      verified = false
-      msg = ""
+      verified        = false
+      msg             = ''
       begin
-        smtp = Net::SMTP.new(args[:server_name], args[:port])
+        smtp              = Net::SMTP.new(args[:server_name], args[:port])
+        smtp.open_timeout = MAIL_VALIDATION_TIMEOUT
+        smtp.read_timeout = MAIL_VALIDATION_TIMEOUT
         if args[:port].to_i == 465
           smtp.enable_ssl
         elsif args[:port].to_i == 587
           smtp.enable_starttls
         end
-        smtp.start(args[:server_name], args[:user_name], args[:password], args[:authentication]) do |smtp|         
+        smtp.start(args[:server_name], args[:user_name], args[:password], args[:authentication]) do |smtp|
         end
-        #flash[:notice] = I18n.t('mailbox.authetication_success')
+        # flash[:notice] = I18n.t('mailbox.authetication_success')
         verified = true
       rescue Timeout::Error => error
         msg = I18n.t('mailbox.smtp_timed_out')
@@ -84,10 +120,10 @@ module MailboxValidator
       rescue Net::SMTPAuthenticationError => error
         msg = I18n.t('mailbox.invalid_credentials')
         Rails.logger.debug "error while verifying the smtp details : #{error} #{filtered_params.inspect}"
-      rescue Exception => error
-        msg = I18n.t('mailbox.smtp_error', :error => error.message)
+      rescue StandardError => error
+        msg = I18n.t('mailbox.smtp_error', error: error.message)
         Rails.logger.debug "error while verifying the smtp details : #{error} #{filtered_params.inspect}"
       end
-      { :success => verified, :msg => msg }
+      { success: verified, msg: msg }
     end
 end
