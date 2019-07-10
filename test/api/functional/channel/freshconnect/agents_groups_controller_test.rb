be768e3910e326c0822eef7fcbc4c2c7c9e2916e
Atul Maheswari <46522401+atulmaheswari@users.noreply.github.com>
2019-07-10 17:31:37 +0530

    FD-34824 Deleting all agents and recreating them before running this … (#5517)
    * FD-34824 Deleting all agents and recreating them before running this test case
    
    Signed-off-by: atulmaheswari <atul.maheswari@freshworks.com>
    
    * FD-34824 Not deleting default User ID for other test cases
    
    Signed-off-by: atulmaheswari <atul.maheswari@freshworks.com>

--- test/api/functional/channel/freshconnect/agents_groups_controller_test.rb
+++ test/api/functional/channel/freshconnect/agents_groups_controller_test.rb
@@ -2,6 +2,7 @@ require_relative '../../../test_helper'
 class Channel::Freshconnect::AgentsGroupsControllerTest < ActionController::TestCase
   include GroupsTestHelper
   include BootstrapTestHelper
+  include AgentHelper
 
   def setup
     super
@@ -9,6 +10,8 @@ class Channel::Freshconnect::AgentsGroupsControllerTest < ActionController::Test
   end
 
   def test_agents_groups_index
+    @account.users.where(helpdesk_agent: true).each { |user| user.destroy if user.id != 1 }
+    5.times { |i| add_agent_to_account(@account, {:name => "testing#{i}", :email => Faker::Internet.email, :active => 1, :role => 1}) }
     5.times { create_group_with_agents(@account) }
     set_jwt_auth_header('freshconnect')
     get :index, controller_params(version: 'channel')
