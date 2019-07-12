96390c942552ca86ce62242d51aaeb86c99ffff3
vinod kunnakkattil <vinod.kunnakkattil@freshworks.com>
2019-07-12 22:21:30 +0530

    FD-34288 : New FSM role with scheduling dashboard privilege. (#5500)
    * FD-34288 : New FSM role with scheduling dashboard privilege.
    
    * including FD-34732 fix.
    
    * Revert "including FD-34732 fix."
    
    This reverts commit 0f4cebdb4e5bd20798756692926c529adb5863f2.

--- app/helpers/admin/roles_helper.rb
+++ app/helpers/admin/roles_helper.rb
@@ -114,6 +114,12 @@ module Admin::RolesHelper
 
          },
 
+         # *************************** Field Service *******************************
+
+          { dom_type: 'label', id: 'fieldservice', not_display: !(current_account.field_service_management_enabled? && current_account.scheduling_fsm_dashboard_enabled?),
+           children:
+            [{ dom_type: 'check_box', id: 'schedule_fsm_dashboard', not_display: !current_account.field_service_management_enabled? }] },
+
          # *************************** Reports **************************
 
          { dom_type: 'label', id: 'reports', children:
--- lib/admin/advanced_ticketing/field_service_management/constant.rb
+++ lib/admin/advanced_ticketing/field_service_management/constant.rb
@@ -54,6 +54,8 @@ module Admin::AdvancedTicketing::FieldServiceManagement
     FSM_FIELDS_PREFIX = 'cf_fsm_'.freeze
     SUPPORT_AGENT_TYPE = 1
     SUPPORT_GROUP_TYPE = 1
+    FIELD_SERVICE_MANAGER_ROLE_NAME = 'Field service manager'.freeze
+    FIELD_SERVICE_MANAGER_ROLE_PRIVILEGES = Helpdesk::Roles::AGENT + [:schedule_fsm_dashboard]
     FSM_APPOINTMENT_START_TIME = 'cf_fsm_appointment_start_time'.freeze
     FSM_APPOINTMENT_END_TIME = 'cf_fsm_appointment_end_time'.freeze
     SERVICE_TASK_SECTION = 'Service task section'.freeze
