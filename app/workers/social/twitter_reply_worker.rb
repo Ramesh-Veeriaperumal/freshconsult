9039a7b1ac854302ffb88ad2cdac7497188985b6
Dhruva Ashok <35563497+fresh-dhruvaashok@users.noreply.github.com>
2019-07-12 19:11:28 +0530

    FD-33361: publishing tweet replies from helpkit (#5432)
    * FD-33361: publishing tweet replies from helpkit
    
    * FD-33361: adding newline to test file
    
    * FD-33361: rubo cop fix
    
    * FD-33361: test case fix
    
    * FD-33361: test case fix
    
    * FD-33361: test case fix
    
    * FD-33361: refactoring test and error_code logic
    
    * FD-33361: review changes made
    
    * FD-33361: minor code refactor
    
    * FD-33361: test case fix
    
    * FD-33361: adding tests for payload
    
    * FD-33361: test case fix
    
    * FD-33361: using existing generate msg id
    
    * FD-33361: skipping twitter commands if tweet type mention
    
    * FD-33361: skipping twitter commands if tweet type mention
    
    * FD-33361: missed out parameter
    
    * FD-33361: rubo cop fix
    
    * FD-33361: code refactor
    
    * FD-33361: test case for message parser
    
    * FD-33361: final newline addition
    
    * FD-33361: test case modifications

--- app/workers/social/twitter_reply_worker.rb
+++ app/workers/social/twitter_reply_worker.rb
@@ -2,6 +2,7 @@ module Social
   class TwitterReplyWorker < BaseWorker
     include Conversations::Twitter
     include Social::Twitter::Util
+    include Social::Twitter::CentralUtil
 
     sidekiq_options queue: :twitter_reply, retry: 0,  failures: :exhausted
 
@@ -16,8 +17,9 @@ module Social
       tweet_body = note.body.strip
       allow_attachments = true
       error_message, reply_twt, error_code = safe_send("send_tweet_as_#{args[:tweet_type]}", args[:twitter_handle_id], ticket, note, tweet_body, allow_attachments)
+      post_success_or_failure_command(error_message, reply_twt, error_code, note.tweet.try(:stream_id), args)
       Rails.logger.info "Reply to twitter ticket sent successfully :: ticket id :: #{ticket.display_id} :: note id :: #{note.id} :: tweet id :: #{reply_twt}" unless error_message
-      if error_message.present? 
+      if error_message.present?
         Rails.logger.info "Reply to twitter ticket failed :: ticket id :: #{ticket.display_id}, note id :: #{note.id}"
         error_code ||= 0 # if no error code is returned, having zero as dummy error
         error_response = { code: error_code, message: error_message }
@@ -29,12 +31,24 @@ module Social
 
     private
 
+      def post_success_or_failure_command(error_message, reply_twt, error_code, stream_id, args)
+        command_payload = construct_payload(error_message, reply_twt, error_code, stream_id, args)
+        msg_id = Social::Util.generate_msg_id(command_payload)
+        Rails.logger.info "Command from Twitter, Command: #{STATUS_UPDATE_COMMAND_NAME}, Msg_id: #{msg_id}"
+        Channel::CommandWorker.perform_async({
+                                               override_payload_type: ChannelIntegrations::Constants::PAYLOAD_TYPES[:command_to_helpkit],
+                                               payload: command_payload
+                                             }, msg_id)
+      rescue StandardError => error
+        ::Rails.logger.error("Error while posting twitter success/failure command to central :: #{error.message}")
+      end
+
       def update_errors_in_schema_less_notes(error, note_id)
         schema_less_notes = Account.current.schema_less_notes.find_by_note_id(note_id)
         schema_less_notes.note_properties[:errors] ||= {}
         twitter_errors = { twitter: { error_code: error[:code], error_message: error[:message] } }
         schema_less_notes.note_properties[:errors].merge!(twitter_errors)
-        schema_less_notes.save!      
+        schema_less_notes.save!
       end
   end
 end
---
+++ test/api/shoryuken/message_parser_test.rb
@@ -0,0 +1,20 @@
+require_relative '../../api/unit_test_helper'
+
+class MessageParserTest < ActionView::TestCase
+  include ChannelIntegrations::Utils::MessageParser
+
+  def test_allowed_twitter_payload
+    return_value = valid_twitter_update?(command_name: 'update_twitter_message', context: { tweet_type: 'dm' })
+    assert_equal true, return_value
+  end
+
+  def test_mismatch_command_twitter_payload
+    return_value = valid_twitter_update?(command_name: 'random_command', context: { tweet_type: 'dm' })
+    assert_equal true, return_value
+  end
+
+  def test_disallowed_twitter_payload
+    return_value = valid_twitter_update?(command_name: 'update_twitter_message', context: { tweet_type: 'mention' })
+    assert_equal false, return_value
+  end
+end
