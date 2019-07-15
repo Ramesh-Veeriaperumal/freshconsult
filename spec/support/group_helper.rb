89cb985176295cf6de859bdb66fd0edfe1dd4a2c
Supraja Ramesh <40852506+SuprajaR@users.noreply.github.com>
2019-07-15 17:17:14 +0530

    fixing test cases (#5558)
    * fixing the test case
    
    * fixing the test case
    
    * fixing the test case

--- spec/support/group_helper.rb
+++ spec/support/group_helper.rb
@@ -1,63 +1,62 @@
 ['shared_ownership_test_helper.rb'].each { |file| require "#{Rails.root}/test/core/helpers/#{file}" }
 
 module GroupHelper
-    include ::SharedOwnershipTestHelper
+  include ::SharedOwnershipTestHelper
 
-	def create_group(account, options= {},group_type= 1)
+  def create_group(account, options = {})
     group = account.groups.find_by_name(options[:name])
     return group if group
     name = options[:name] || Faker::Name.name
-		group = FactoryGirl.build(:group,:name=> name)
-		group.account_id = account.id
-        group.group_type = group_type
-		group.ticket_assign_type  = options[:ticket_assign_type] if options[:ticket_assign_type]
-        group.toggle_availability = options[:toggle_availability] if options[:toggle_availability]
-        group.capping_limit = options[:capping_limit] if options[:capping_limit]
-		group.save!
-		group
-	end
+    group = FactoryGirl.build(:group, name: name)
+    group.account_id = account.id
+    group.group_type = options[:group_type] || GroupConstants::SUPPORT_GROUP_ID
+    group.ticket_assign_type = options[:ticket_assign_type] if options[:ticket_assign_type]
+    group.toggle_availability = options[:toggle_availability] if options[:toggle_availability]
+    group.capping_limit = options[:capping_limit] if options[:capping_limit]
+    group.save!
+    group
+  end
 
-  def create_group_with_agents(account, options= {},group_type= 1)
+  def create_group_with_agents(account, options = {})
     group = account.groups.find_by_name(options[:name])
     return group if group && group.agents.present?
-    unless group
-        name = options[:name] || Faker::Name.name
-        group = FactoryGirl.build(:group,:name=> name)
-        group.group_type = group_type
-        group.account_id = account.id
-        group.ticket_assign_type = options[:ticket_assign_type] if options[:ticket_assign_type]
-        options[:agent_list].each { |agent| group.agent_groups.build(:user_id =>agent) } unless options[:agent_list].blank?
-        group.save!
+    if group
+      add_agent_to_group(group_id = group.id, ticket_permission = 3, role_id = @account.roles.first.id)
     else
-        add_agent_to_group(group_id = group.id, ticket_permission = 3, role_id = @account.roles.first.id)
+      name = options[:name] || Faker::Name.name
+      group = FactoryGirl.build(:group, name: name)
+      group.group_type = options[:group_type] || GroupConstants::SUPPORT_GROUP_ID
+      group.account_id = account.id
+      group.ticket_assign_type = options[:ticket_assign_type] if options[:ticket_assign_type]
+      options[:agent_list].each { |agent| group.agent_groups.build(user_id: agent) } if options[:agent_list].present?
+      group.save!
     end
     group
   end
 
-  def create_groups(account, options= {:count => 2})
+  def create_groups(account, options = { count: 2 })
     groups = []
     options[:count].times do |no|
-        group = create_group(account)
-        groups << group
+      group = create_group(account)
+      groups << group
     end
     groups
   end
 
-  def create_group_private_api(account,options={},group_type= 1)
+  def create_group_private_api(account, options = {})
     name = Faker::Name.name
-    group = FactoryGirl.build(:group,:name=> name)
+    group = FactoryGirl.build(:group, name: name)
     group.account_id = account.id
-    group.description=Faker::Lorem.paragraph   
-    group.escalate_to=1
-    group.group_type= group_type
-    group.agent_ids=options[:agent_ids]
-    group.agent_ids=[1,2] if !options[:agent_ids]
-    group.business_calendar_id=1
-    group.ticket_assign_type=options[:ticket_assign_type] if options[:ticket_assign_type]
-    group.ticket_assign_type=options[:round_robin_type] if options[:round_robin_type]
-    group.capping_limit=options[:capping_limit] if options[:capping_limit]  
+    group.description = Faker::Lorem.paragraph
+    group.escalate_to = 1
+    group.group_type = options[:group_type] || GroupConstants::SUPPORT_GROUP_ID
+    group.agent_ids = options[:agent_ids]
+    group.agent_ids = [1, 2] unless options[:agent_ids]
+    group.business_calendar_id = 1
+    group.ticket_assign_type = options[:ticket_assign_type] if options[:ticket_assign_type]
+    group.ticket_assign_type = options[:round_robin_type] if options[:round_robin_type]
+    group.capping_limit = options[:capping_limit] if options[:capping_limit]
     group.save!
     group
-  end 
-
+  end
 end
