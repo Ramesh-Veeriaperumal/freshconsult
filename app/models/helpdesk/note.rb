0c7c02d2447c9997b6f41c29c936f7abac03104b
Rahul Gupta <37134617+rahul-gupta-1525@users.noreply.github.com>
2019-07-18 20:46:46 +0530

    FD-35265 thank_you checks to reduce ml api calls (#5489)
    * thank_you checks to reduce ml api calls
    
    * review changes
    
    * adding attribute accessor
    
    * error fix
    
    * code review change
    
    * sla_timer_off_status_check
    
    *  add stack logs
    
    * error fix
    
    * remove error logs
    
    * hit ml service only if thank you is configured in rules
    
    * hit ml service only if thank you is configured in rules
    
    * error fix
    
    * changing include redis file
    
    * changing account private method to public
    
    * changing method from protected to public
    
    * error fixes
    
    * adding config file in before_migrate.rb
    
    * handle delete rule redis set problem
    
    * handle destroy of automation rules in redis set
    
    * code review
    
    * rubucop

--- app/models/helpdesk/note.rb
+++ app/models/helpdesk/note.rb
@@ -26,7 +26,7 @@ class Helpdesk::Note < ActiveRecord::Base
   
   attr_accessor :nscname, :disable_observer, :send_survey, :include_surveymonkey_link, :quoted_text, 
                 :skip_notification, :changes_for_observer, :disable_observer_rule, :post_to_forum_topic,
-                :model_changes
+                :model_changes, :import_note
 
   attr_protected :attachments, :notable_id
 
