b092381e4430379baee3a01a43c7463672f098b5
ravishankar-gnanaprakasam <39148615+ravishankarfreshworks@users.noreply.github.com>
2019-07-22 19:42:37 +0530

    [FD-35284] Prevent wildcard ticket create (#5611)
    * prevent wildcard ticket create
    
    * rubocop fixes

--- app/models/account/constants.rb
+++ app/models/account/constants.rb
@@ -294,8 +294,8 @@ class Account < ActiveRecord::Base
       nested_field_revamp: true, freshvisual_configs: false, freshid_org_v2: false, hide_agent_login: false, addon_based_billing: false,
       kbase_mint: false, text_custom_fields_in_etl: false, recalculate_daypass: false,  email_spoof_check: false,
       disable_email_spoof_check: false, attachment_redirect_expiry: false, solutions_agent_portal: false, solutions_agent_metrics: false,
-      sandbox_single_branch: false, ticket_type_filter_in_trends_widget: true, prevent_fwd_email_ticket_create: false, enable_wildcard_ticket_create: false,
-      check_wc_fwd: false, requester_privilege: false, fsm_landing_page: true, scheduling_fsm_dashboard: false
+      sandbox_single_branch: false, ticket_type_filter_in_trends_widget: true, prevent_wc_ticket_create: true, allow_wildcard_ticket_create: false,
+      requester_privilege: false, fsm_landing_page: true, scheduling_fsm_dashboard: false
     }, :merge
   )
 
