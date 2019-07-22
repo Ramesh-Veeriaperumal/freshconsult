1e47034b91f3f07ccca54b74d754c47d4db11bb4
Yuvanesh <yuvanesh.palanisamy@freshworks.com>
2019-07-22 18:17:18 +0530

    [FD-34985] - Upgrade dnsruby to fix persistent 5xx and Time sheet changes (#5620)

--- app/models/account.rb
+++ app/models/account.rb
@@ -855,6 +855,10 @@ class Account < ActiveRecord::Base
     get_members_from_redis_set(automation_rules_with_thank_you_configured).present?
   end
 
+  def time_sheets_with_join
+    time_sheets.joins('AND helpdesk_time_sheets.account_id = helpdesk_tickets.account_id').readonly(false)
+  end
+
   protected
 
     def external_url_is_valid?(url)
