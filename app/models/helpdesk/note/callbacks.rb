6017eb64c9d550dc01f05fc83072fff2c3031f57
Rahul Gupta <37134617+rahul-gupta-1525@users.noreply.github.com>
2019-07-23 22:06:28 +0530

    revert commit (#5656)

--- app/models/helpdesk/note/callbacks.rb
+++ app/models/helpdesk/note/callbacks.rb
@@ -44,7 +44,7 @@ class Helpdesk::Note < ActiveRecord::Base
 
   # For using Redis related functionalities
   include Redis::RedisKeys
-  include Redis::OthersRedis  
+  include Redis::OthersRedis
 
   def construct_note_old_body_hash
     {
@@ -477,21 +477,7 @@ class Helpdesk::Note < ActiveRecord::Base
     end
 
     def detect_thank_you_note?
-      account.detect_thank_you_note_enabled? && !BLACKLISTED_THANK_YOU_DETECTOR_NOTE_SOURCES.include?(source) &&
-      user.customer? && (closed_or_resolved_ticket? || sla_timer_off_status?) && !import_note && recently_created_note? &&
-      body.present? && account.thank_you_configured_in_automation_rules?
-    end
-
-    def sla_timer_off_status?
-      Helpdesk::TicketStatus.onhold_statuses(account).include? notable.status
-    end
-
-    def closed_or_resolved_ticket?
-      (notable.status == Helpdesk::Ticketfields::TicketStatus::CLOSED || notable.status == Helpdesk::Ticketfields::TicketStatus::RESOLVED)
-    end
-
-    def recently_created_note?
-      Time.now - created_at < 1.hour
+      account.detect_thank_you_note_enabled? && !BLACKLISTED_THANK_YOU_DETECTOR_NOTE_SOURCES.include?(source) && user.customer?
     end
 
     def trigger_detect_thank_you_note_worker
