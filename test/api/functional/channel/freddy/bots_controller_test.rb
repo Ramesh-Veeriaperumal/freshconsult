f92ab75ec877d05917aefb13863b0975f9b028f0
kamalkumard <40134785+kamalkumard@users.noreply.github.com>
2019-07-24 23:57:10 +0530

    FD-35454 Widget config (#5668)
    * FD-35285 Agent article suggest revamp
    
    * FD-35285 test case update
    
    * FD-35285 test case update
    
    * update test case
    
    * test case update
    
    * Update agent_suggest_articles.rb
    
    * test case update
    
    * test case update
    
    * test case update

--- test/api/functional/channel/freddy/bots_controller_test.rb
+++ test/api/functional/channel/freddy/bots_controller_test.rb
@@ -30,10 +30,15 @@ class Channel::Freddy::BotsControllerTest < ActionController::TestCase
   def test_create_bot_without_freshchat
     enable_autofaq do
       set_jwt_auth_header(FREDDY)
-      stub_request(:post, %r{^#{Freshchat::Account::CONFIG[:signup][:host]}.*?$}).to_return(body: freshchat_response, status: 200)
+      stub_request(:post, %r{^#{Freshchat::Account::CONFIG[:signup][:host]}.*?$}).to_return(body: freshchat_response, headers: { 'Content-Type' => 'application/json' }, status: 200)
       post :create, controller_params({ version: 'private' }.merge(wrap_cname(Portal.first.id)), false)
       assert_response 200
+      Account.current.reload
+      assert_equal Account.current.freshchat_account.token, '0c046241-743f-4859-bd62-4de2fed42134'
+      assert_equal Account.current.freshchat_account.app_id, '061ae53e-e0b0-4c4c-8bd9-aaadae1d1007'
     end
+  ensure
+    Account.current.freshchat_account.destroy
   end
 
   def test_create_bot_with_freshchat_enabled
@@ -62,10 +67,13 @@ class Channel::Freddy::BotsControllerTest < ActionController::TestCase
     enable_autofaq do
       set_jwt_auth_header(FREDDY)
       portal = create_portal
+      freshchat_account = Freshchat::Account.create(app_id: '', portal_widget_enabled: false, token: '', enabled: false)
       stub_request(:post, %r{^#{Freshchat::Account::CONFIG[:signup][:host]}.*?$}).to_return(body: freshchat_response, status: 200)
       freddy_bot = create_freddy(portal.id)
       put :update, controller_params({ version: 'private', id: freddy_bot.cortex_id }.merge(wrap_cname(portal.id)), false)
       assert_response 200
     end
+  ensure
+    Account.current.freshchat_account.destroy
   end
 end
