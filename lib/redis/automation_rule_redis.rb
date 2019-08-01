117d9e67477a20f96d41409f135a00e08feff9e5
Rahul Gupta <37134617+rahul-gupta-1525@users.noreply.github.com>
2019-08-01 21:43:33 +0530

    FD-35843 Thank you reduce ml calls (#5663)
    * thank_you checks to reduce ml api calls
    
    * review changes
    
    * error fix
    
    * test cases
    
    * code review change
    
    * add logs
    
    * sla_timer_off_status_check
    
    * error fix
    
    * hit ml service only if thank you is configured in rules
    
    * code review changes
    
    * changing include redis file
    
    * changing account private method to public
    
    * removing private method
    
    * changing method from protected to public
    
    * error fixes
    
    * rubocop changes
    
    * handle delete rule redis set problem
    
    * handle destroy of automation rules in redis set
    
    * code review
    
    * rubucop
    
    * code review changes
    
    * appending automation_ in methods
    
    * revert method name change
    
    * Removing redundant check
    
    * in sync changes with helpkit
    
    * changing note callback sla condition
    
    * change url

--- lib/redis/automation_rule_redis.rb
+++ lib/redis/automation_rule_redis.rb
@@ -1,19 +1,34 @@
 module Redis::AutomationRuleRedis
   include Redis::Keys::AutomationRules
 
-
   def inc_automation_rule_exec_count(rule_id, incr_by = 1)
     rule_key = automation_rule_id(rule_id)
-    incr_redis_key_in_hash(account_automation_key, rule_key, incr_by)
+    incr_automation_redis_key_in_hash(account_automation_key, rule_key, incr_by)
   end
 
   def set_expiry_of_account_rule(expiry_time = 86400) # 1 day
-    return false if get_expiry_of_hash(account_automation_key) >= 0
-    set_expiry_of_hash(account_automation_key, expiry_time)
+    return false if get_expiry_of_automation_hash(account_automation_key) >= 0
+    set_expiry_of_automation_hash(account_automation_key, expiry_time)
   end
 
   def get_multi_automation_key_values(rule_ids = [], options = {})
-    get_multi_redis_key_in_hash(account_automation_key(options), rule_ids.map { |id| automation_rule_id(id) })
+    get_multi_automation_redis_key_in_hash(account_automation_key(options), rule_ids.map { |id| automation_rule_id(id) })
+  end
+
+  def automation_rules_with_thank_you_configured
+    format(AUTOMATION_RULES_WITH_THANK_YOU_CONFIGURED, { account_id: Account.current.id })
+  end
+
+  def add_element_to_automation_redis_set(redis_key, element)
+    $redis_automation_rule.perform_redis_op('sadd', redis_key, element)
+  end
+
+  def remove_element_from_automation_redis_set(redis_key, element)
+    $redis_automation_rule.perform_redis_op('srem', redis_key, element)
+  end
+
+  def get_members_from_automation_redis_set(redis_key)
+    $redis_automation_rule.perform_redis_op('smembers', redis_key)
   end
 
   private
@@ -29,44 +44,44 @@ module Redis::AutomationRuleRedis
       format(AUTOMATION_RULE_ID, { rule_id: rule_id })
     end
 
-    def redis_key_exists?(key)
+    def automation_redis_key_exists?(key)
       newrelic_begin_rescue do
         $redis_automation_rule.perform_redis_op('exists', key)
       end
     end
 
-    def get_expiry_of_hash(redis_key)
+    def get_expiry_of_automation_hash(redis_key)
       newrelic_begin_rescue do
         $redis_automation_rule.perform_redis_op('ttl', redis_key)
       end
     end
 
-    def set_expiry_of_hash(redis_key, expiry_time)
+    def set_expiry_of_automation_hash(redis_key, expiry_time)
       newrelic_begin_rescue do
         $redis_automation_rule.perform_redis_op('expire', redis_key, expiry_time)
       end
     end
 
-    def set_redis_keys_in_hash(redis_key, keys)
+    def set_redis_keys_in_automation_hash(redis_key, keys)
       newrelic_begin_rescue do
         $redis_automation_rule.perform_redis_op('hmset', redis_key, keys)
       end
     end
 
-    def incr_redis_key_in_hash(redis_key, key, incr_by)
+    def incr_automation_redis_key_in_hash(redis_key, key, incr_by)
       newrelic_begin_rescue do
         $redis_automation_rule.perform_redis_op('hincrby', redis_key, key, incr_by)
       end
     end
 
-    def get_multi_redis_key_in_hash(redis_key, keys)
+    def get_multi_automation_redis_key_in_hash(redis_key, keys)
       keys = *keys
       newrelic_begin_rescue do
         $redis_automation_rule.perform_redis_op('hmget', redis_key, keys)
       end
     end
 
-    def perform_in_redis_transaction
+    def automation_perform_in_redis_transaction
       newrelic_begin_rescue do
         $redis_automation_rule.multi do
           yield
