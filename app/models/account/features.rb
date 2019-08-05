4c098b0a1fc1b43193e090dd24019378d740c3d4
Syam Phanindra <40861364+SyamPhanindraChavva@users.noreply.github.com>
2019-08-05 21:34:03 +0530

    FD-18465 Adding feature in prestaging (#5648)
    Adding feature in prestaging

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -30,7 +30,7 @@ class Account < ActiveRecord::Base
     :fb_page_api_improvement, :attachment_redirect_expiry, :solutions_agent_portal, :solutions_agent_metrics, :fuzzy_search,
     :delete_trash_daily, :ticket_type_filter_in_trends_widget, :automation_revamp, :prevent_wc_ticket_create, 
     :allow_wildcard_ticket_create, :requester_privilege, :sso_unique_session, :fsm_landing_page,
-    :scheduling_fsm_dashboard, :enable_customer_journey, :csat_translations
+    :scheduling_fsm_dashboard, :enable_customer_journey, :csat_translations, :supervisor_custom_status
   ].freeze
 
   DB_FEATURES = [:custom_survey, :requester_widget, :archive_tickets, :sitemap, :freshfone].freeze
