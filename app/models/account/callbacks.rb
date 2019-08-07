7187f719e56fdccbc4fc93c370001f5d769db058
arvinthj <arvinth.jagannathan@freshworks.com>
2019-08-07 22:33:50 +0530

    FD-34734 Remove dependency on collaboration feature (#5483)
    Stop collaboration feature addition and account registration on freshdesk account signup

--- app/models/account/callbacks.rb
+++ app/models/account/callbacks.rb
@@ -40,7 +40,7 @@ class Account < ActiveRecord::Base
   after_commit ->(obj) { obj.clear_cache }, on: :destroy
 
   after_commit :enable_fresh_connect, on: :create, unless: :is_anonymous_account
-  after_commit :enable_searchv2, :enable_count_es, :enable_collab, :set_falcon_preferences, on: :create
+  after_commit :enable_searchv2, :enable_count_es, :set_falcon_preferences, on: :create
   after_commit :disable_searchv2, :disable_count_es, on: :destroy
   after_commit :update_sendgrid, on: :create
   after_commit :remove_email_restrictions, on: :update , :if => :account_verification_changed?
@@ -514,10 +514,6 @@ class Account < ActiveRecord::Base
 
     def update_sendgrid
       SendgridDomainUpdates.perform_async({:action => 'create', :domain => full_domain, :vendor_id => Account::MAIL_PROVIDER[:sendgrid]})
-    end
-
-    def enable_collab
-      CollabPreEnableWorker.perform_async(true)
     end
 
     def set_falcon_preferences
