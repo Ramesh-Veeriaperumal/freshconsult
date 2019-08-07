6ffd142c4e5453b537396292b7adc6354bb02ead
Shubham Sahu <39123737+freshpirate@users.noreply.github.com>
2019-08-07 22:25:37 +0530

    FD-35691: Add feature for autoincrement collision (#5764)

--- app/models/account/constants.rb
+++ app/models/account/constants.rb
@@ -295,7 +295,8 @@ class Account < ActiveRecord::Base
       kbase_mint: false, text_custom_fields_in_etl: false, recalculate_daypass: false,  email_spoof_check: false,
       disable_email_spoof_check: false, attachment_redirect_expiry: false, solutions_agent_portal: false, solutions_agent_metrics: false,
       sandbox_single_branch: false, prevent_wc_ticket_create: true, allow_wildcard_ticket_create: false,
-      requester_privilege: false, scheduling_fsm_dashboard: false, sso_unique_session: false, supervisor_custom_status: false
+      requester_privilege: false, scheduling_fsm_dashboard: false, sso_unique_session: false, supervisor_custom_status: false,
+      sandbox_temporary_offset: false
     }, :merge
   )
 
