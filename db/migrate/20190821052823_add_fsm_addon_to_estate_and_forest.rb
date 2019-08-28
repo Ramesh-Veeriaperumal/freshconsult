b09247b8eeac93121901cb3500e92a03c0eac5e6
Apk47 <puneet.annavarapu@freshworks.com>
2019-08-28 19:04:36 +0530

    [FD-36347] Support FSM for old plans (#5968)
    * FD-36347 Support FSM for old plans
    
    * FD-36347 remove addon bug fix

---
+++ db/migrate/20190821052823_add_fsm_addon_to_estate_and_forest.rb
@@ -0,0 +1,22 @@
+class AddFsmAddonToEstateAndForest < ActiveRecord::Migration
+  shard :all
+  
+  def up
+    execute <<-SQL
+      INSERT INTO subscription_plan_addons
+        (subscription_plan_id, subscription_addon_id) VALUES
+        ((SELECT id FROM subscription_plans WHERE name = 'Estate'),
+          (SELECT id FROM subscription_addons WHERE name = 'Field Service Management')),
+        ((SELECT id FROM subscription_plans WHERE name = 'Forest'),
+          (SELECT id FROM subscription_addons WHERE name = 'Field Service Management'))
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DELETE FROM subscription_plan_addons WHERE subscription_addon_id
+        IN (SELECT id FROM subscription_addons WHERE name = "Field Service Management") AND subscription_plan_id
+        IN (SELECT id FROM subscription_plans WHERE name = 'Estate' or name = 'Forest')
+    SQL
+  end
+end
