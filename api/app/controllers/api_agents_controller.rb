7f1f0f32b790998c280f5560ceb7f32fce0591fb
Divya <divya.dhanasekar@freshworks.com>
2019-08-28 12:08:41 +0530

    FD-36371 Allow customers to update agent through API if the feature is enabled (#5997)

--- api/app/controllers/api_agents_controller.rb
+++ api/app/controllers/api_agents_controller.rb
@@ -4,7 +4,7 @@ class ApiAgentsController < ApiApplicationController
   end
 
   def check_edit_privilege
-    if current_account.freshid_enabled?
+    if current_account.freshid_integration_enabled? && !current_account.allow_update_agent_enabled?
       AgentConstants::RESTRICTED_PARAMS.any? do |key|
         if @item.user_changes.key?(key)
           @item.errors[:base] << :cannot_edit_inaccessible_fields
