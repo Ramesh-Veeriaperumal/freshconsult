1ae53c6feb7803bc85a92a4ab0fe5cc6f19430b4
Anandweb <40201935+Anandweb@users.noreply.github.com>
2019-09-09 17:26:27 +0530

    FD-37066 transulation file changes falcon  (#6118)
    * FD-36954 yml file code changes
    
    * FD-36954 yml file code changes en.yml
    
    * FD-36788 review comment fixes

--- public/javascripts/app/select_plan.js
+++ public/javascripts/app/select_plan.js
@@ -106,7 +106,7 @@ window.App = window.App || {};
     },
     handleCancellationSuccess: function(cancel_type) {
       this.pending_cancellation_request = false;
-      var message = cancel_type === this.subscription_cancel ? 'common_js_translations.subscription_cancel_request_success' : 'common_js_translations.cancel_request_success'
+      var message = cancel_type === this.subscription_cancel ? 'downgrade_policy.subscription_cancel_request_success' : 'downgrade_policy.request_cancel_success'
       $('#cancellation-wrapper').removeClass('sloading inner-form-hide');
       $('.request-change-wrapper').addClass('hide');
       $('.request-change-info').addClass('hide');
@@ -118,7 +118,7 @@ window.App = window.App || {};
       }, 5000);
     },
     handleCancellationFailure: function(cancel_type) {
-      var message = cancel_type === this.subscription_cancel ? 'common_js_translations.subscription_cancel_request_failure' : 'common_js_translations.cancel_request_failure'
+      var message = cancel_type === this.subscription_cancel ? 'downgrade_policy.subscription_cancel_request_failure' : 'downgrade_policy.cancel_request_failure'
       $('#cancellation-button').removeClass('disabled');
       $('#cancellation-wrapper').removeClass('sloading inner-form-hide');
       $('.request-change-status').addClass('failure').removeClass('hide');
@@ -444,7 +444,7 @@ window.App = window.App || {};
               $(".features-billing-edit").removeClass('hide');
             }
             billing_template.find(".billing-cancel").show();
-            var submitText = request_change ? I18n.t('common_js_translations.request_change') : I18n.t('common_js_translations.update_plan');
+            var submitText = request_change ? I18n.t('downgrade_policy.change_request') : I18n.t('common_js_translations.update_plan');
             billing_template.find(".billing-submit").val(submitText).addClass('btn-primary')
             billing_template.find(".billing-submit").html(submitText).addClass('btn-primary');
             if(request_change){
@@ -464,8 +464,8 @@ window.App = window.App || {};
           }
           else {
             if(request_change) {
-              billing_template.find(".billing-submit").val(I18n.t('common_js_translations.request_change'));
-              billing_template.find(".billing-submit").html(I18n.t('common_js_translations.request_change'));
+              billing_template.find(".billing-submit").val(I18n.t('downgrade_policy.change_request'));
+              billing_template.find(".billing-submit").html(I18n.t('downgrade_policy.change_request'));
               $('.upgrade-same-plan').css('display','none');
               $('.upgrade-same-plan-header').css('display','block');
             }
