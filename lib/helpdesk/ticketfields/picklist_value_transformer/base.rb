e55082829361740253022db30f24aa4e0b541108
Yuvanesh <yuvanesh.palanisamy@freshworks.com>
2019-09-18 18:18:24 +0530

    Revert "[FD-36233] - Supervisor process Crash fix (#5868)" (#6231)
    This reverts commit 6736cda18afdee5662ec9183b9411032184df5ea.

--- lib/helpdesk/ticketfields/picklist_value_transformer/base.rb
+++ lib/helpdesk/ticketfields/picklist_value_transformer/base.rb
@@ -9,15 +9,33 @@ class Helpdesk::Ticketfields::PicklistValueTransformer::Base
   private
 
     def ticket_field_by_flexifield_name_hash
-      account.ticket_field_by_flexifield_name
+      @ticket_field_by_flexifield_name_hash ||= {}.tap do |h|
+        ticket_fields.each do |ticket_field|
+          h[ticket_field.flexifield_name] = ticket_field
+        end
+      end
+    end
+
+    def ticket_fields
+      @ticket_fields ||= account.ticket_fields_from_cache.select do |ticket_field|
+        ticket_field.field_type == 'custom_dropdown' || ticket_field.field_type == 'nested_field'
+      end
     end
 
     def values_by_id_from_cache
-      account.picklist_values_by_id_cache
+      @values_by_id_from_cache ||= MemcacheKeys.get_multi_from_cache(ticket_fields.map(&:picklist_values_by_id_key))
     end
 
     def ids_by_value_from_cache
-      account.picklist_ids_by_value_cache
+      @ids_by_value_from_cache ||= MemcacheKeys.get_multi_from_cache(ticket_fields.map(&:picklist_ids_by_value_key))
+    end
+
+    def values_by_id_from_cache
+      @values_by_id_from_cache ||= MemcacheKeys.get_multi_from_cache(ticket_fields.map(&:picklist_values_by_id_key))
+    end
+
+    def ids_by_value_from_cache
+      @ids_by_value_from_cache ||= MemcacheKeys.get_multi_from_cache(ticket_fields.map(&:picklist_ids_by_value_key))
     end
 
     def account
