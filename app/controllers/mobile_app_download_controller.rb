3249814b11ec2ce52782e44b18aa5b45cdf455d3
Jyoti Dubey <jyoti.dubey@freshworks.com>
2019-09-19 17:04:14 +0530

    [FD-30006]  Feature mobile Onboarding falcon (#5408)
    * [FD-30023]Add user activiation logic
    
    * [FD-30023]Add user activiation screen
    
    * [FD-30023]Add user activation helper
    
    * [FD-30023]Add mobile agent check
    
    * [FD-30023]Add user activation helper
    
    * [FD-30023]Add mobile agent check
    
    * [FD-30023] Add review changes for phase one
    
    * FD-30005 : Add image
    
    * FD-30005 : Add mobile agent check
    
    * FD-30005 : Remove unwanted methods
    
    * FD-30005 : Add fix for review comments
    
    * FD-30005 : Add image files to cdn
    
    * FD-30005 : Add review changes
    
    * FD-30005 : Add review changes
    
    * FD-30005 : Add review changes
    
    * FD-30005 : Add review changes
    
    * FD-30005 : Remove unwanted methods
    
    * FD-30005 : Add review changes
    
    * FD-30005 : Revert unwanted changes
    
    * FD-30006 : Add reduced file size
    
    * FD-30006 : Removed unwanted line
    
    * FD-30006 : Add rubocop changes
    
    * FD-30006 : Change en.yml
    
    * FD-30006 : Change controller
    
    * FD-30006 : Remove unwanted methods
    
    * FD-30006 : Remove unwanted methods
    
    * FD-30006 : Remove unwanted methods
    
    * FD-30006 : Add svg
    
    * FD-30006 : Add svg path
    
    * FD-30006 : Add svg path
    
    * FD-30006 : Add svg path
    
    * [FD-30006] adding asset host url
    
    adding asset host url
    
    * [FD-30006] adding image tag
    
    adding image_tag
    
    * [FD-30006]- Deleting mobile_app_download java script
    
    [FD -30006 ] Deleting mobile_app_download java script

---
+++ app/controllers/mobile_app_download_controller.rb
@@ -0,0 +1,9 @@
+class MobileAppDownloadController < ApplicationController
+  def index
+    @mobile_params = {
+      url: current_account.full_url,
+      full_domain: current_account.full_domain
+    }
+    render layout: false
+  end
+end
