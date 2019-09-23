f2abef4bfce129a20072803e03327f44c6c12f3e
selvakumar784 <selvakumar.ruban@freshworks.com>
2019-09-23 19:39:50 +0530

    [FD-36571] - Do not unsubscribe twitter when account is suspended (#6241)
    * FD-36571: Do not unsubscribe twitter when suspended
    
    * FD-36571: Fix test cases
    
    * FD-36571: Fix failing tests
    
    * FD-36571: Rename a method
    
    * FD-36571: Update test and fix random failure
    
    * FD-36571: Improve test case
    
    * FD-36571: Fix failing test
    
    * FD-36571: Add missing changes

--- test/app/controllers/accounts_controller_test.rb
+++ test/app/controllers/accounts_controller_test.rb
@@ -611,10 +611,6 @@ class AccountsControllerTest < ActionController::TestCase
     Account.any_instance.stubs(:onboarding_applicable?).returns(true)
     anonymous_signup_key = ANONYMOUS_ACCOUNT_SIGNUP_ENABLED
     set_others_redis_key(anonymous_signup_key, true)
-    onboarding_v2_key = ONBOARDING_V2_ENABLED
-    onboarding_i18n_enabled_key = ONBOARDING_I18N_ENABLED
-    set_others_redis_key(onboarding_v2_key, true)
-    set_others_redis_key(onboarding_i18n_enabled_key, true)
     Account.any_instance.expects(:add_to_billing).never
     Account.any_instance.expects(:enable_fresh_connect).never
     @request.env['CONTENT_TYPE'] = 'application/json'
@@ -624,7 +620,6 @@ class AccountsControllerTest < ActionController::TestCase
     assert_response 200
     account = Account.last
     assert_equal account.anonymous_account?, true
-    assert_equal account.launched?(:onboarding_i18n), true
     assert_equal account.admin_first_name, 'Demo'
     assert_equal account.admin_last_name, 'Account'
     assert_match(/freshdeskdemo[0-9]{13}@example.com/, account.admin_email)
@@ -638,8 +633,6 @@ class AccountsControllerTest < ActionController::TestCase
     Account.any_instance.unstub(:anonymous_account?)
     Account.any_instance.unstub(:onboarding_applicable?)
     remove_others_redis_key(anonymous_signup_key)
-    remove_others_redis_key(onboarding_v2_key)
-    remove_others_redis_key(onboarding_i18n_enabled_key)
     account.destroy if account.present?
   end
 
--- test/models/unit/subscription_test.rb
+++ test/models/unit/subscription_test.rb
@@ -1,8 +1,9 @@
 require_relative '../test_helper'
 require Rails.root.join('test', 'core', 'helpers', 'account_test_helper.rb')
-
+['social_tickets_creation_helper.rb'].each { |file| require "#{Rails.root}/spec/support/#{file}" }
 class SubscriptionTest < ActiveSupport::TestCase
   include AccountTestHelper
+  include SocialTicketsCreationHelper
 
   def test_update_should_not_change_onboarding_state_for_active_accounts
     create_new_account('test1234', 'test1234@freshdesk.com')
@@ -74,5 +75,32 @@ class SubscriptionTest < ActiveSupport::TestCase
     subscription = account.subscription
     description = subscription.omni_channel_ticket_params(account, subscription, nil)[:description]
     assert_equal description, "Customer has switched to / purchased an Omni-channel Freshdesk plan. <br>     <b>Account ID</b> : #{account.id}<br><b>Domain</b> : #{account.full_domain}<br><b>Current plan</b>     : #{subscription.plan_name}<br><b>Currency</b> : #{account.subscription.currency.name}<br><b>Previous plan</b> :     #{subscription.plan_name}<br><b>Contact</b> : <br>Ensure plan is set correctly in chat and caller."
+  end
+
+  def test_account_suspended_do_not_unsubscribe_twitter
+    create_test_account
+    create_social_streams
+    subscription = @account.subscription
+    @old_state = subscription.state
+    subscription.state = 'suspended'
+    subscription.save
+    assert_equal @account.twitter_handles.present?, true
+    assert_equal @account.twitter_streams.present?, true
+    assert_equal @account.custom_twitter_streams.present?, true
+  ensure
+    subscription = @account.subscription
+    subscription.state = @old_state
+    subscription.save
+  end
+
+  def create_social_streams
+    twitter_handle = FactoryGirl.build(:seed_twitter_handle)
+    twitter_handle.account_id = @account.id
+    twitter_handle.save!
+    custom_stream = FactoryGirl.build(:seed_twitter_stream)
+    custom_stream.account_id = @account.id
+    custom_stream.social_id = nil
+    custom_stream.save!
+    custom_stream.populate_accessible(Helpdesk::Access::ACCESS_TYPES_KEYS_BY_TOKEN[:all])
   end
 end
