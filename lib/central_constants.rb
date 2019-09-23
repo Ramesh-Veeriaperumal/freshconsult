9cb3e25c879d2d376243e349f1e674c9e77b1922
selvakumar784 <selvakumar.ruban@freshworks.com>
2019-09-23 19:16:11 +0530

    [FD-36571] - Add pod under event info for subscription central payload (#6234)
    * FD-36571: Include pod in event info
    
    * FD-36571: Change central constant value

--- lib/central_constants.rb
+++ lib/central_constants.rb
@@ -12,7 +12,7 @@ module CentralConstants
     'Bot::FeedbackMapping' => '0.0',
     'User' => '0.1',
     'Company' => '1.1',
-    'Subscription' => '1.0',
+    'Subscription' => '1.1',
     'Agent' => '2.1',
     'VaRule' => '0.1',
     'Group' => '3.0',
