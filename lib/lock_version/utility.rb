4ce4ffa10c939e09f827734bff117080cebaf9fe
mahalakshmisomasundaram <mahalakshmi.somasundaram@freshworks.com>
2019-10-04 20:32:19 +0530

    [FD-35127]: First response race condition in Schema Less ticket  (#6296)
    * FD-35127: Adding locking version
    
    * FD-35127: Addressing review comments
    
    * FD-35127: Adding test cases and adding lock_version condition for old tickets
    
    * FD-35127: Code refactor and testcases
    
    * FD-35127: Adding testcase for destroy
    
    * FD-35127 - Addressing review comments
    
    * FD-35127: Changing max retry count to 5
    
    * FD-35127:  Addressing review comments
    
    * FD-35127: Adding new relic and logs

---
+++ lib/lock_version/utility.rb
@@ -0,0 +1,42 @@
+module LockVersion::Utility
+  MAX_RETRY_COUNT = 5
+
+  def optimistic_rails_lock(action)
+    retry_count = 0
+    begin
+      yield
+    rescue ActiveRecord::StaleObjectError => e
+      retry_count += 1
+      Rails.logger.info "#{self.class.name} raised StaleObjectError::AccountId::#{account_id}::Ticket::#{ticket_id}::LockVersion::#{int_tc05}::RetryCount::#{retry_count}"
+      if retry_count <= MAX_RETRY_COUNT
+        self.update_column(:int_tc05, 0) if int_tc05.zero?  # satisfies if only in-case of exceptions on updating old tickets for the first time with locking column
+        safe_send("#{action}_on_lock") # This invokes update_on_lock or destroy_on_lock depending on the action passed
+        retry
+      else
+        Rails.logger.debug "#{self.class.name} #{action} failed. #{inspect}"
+        NewRelic::Agent.notice_error(e, description: "#{self.class.name} #{action} failed for AccountId::#{account_id} and TicketId::#{ticket_id}")
+        raise e
+      end
+    end
+  end
+
+  private
+
+    def update_on_lock
+      retry_changes = attribute_changes # override attribute_changes method in the concerned model and that should return serialised and non-serialised changes
+      lock!
+      reapply_values(retry_changes) # override reapply_values method in the concerned model
+    end
+
+    def destroy_on_lock
+      reload
+    end
+
+    def reapply_values(*)
+      raise NoMethodError, 'reapply_values method must be overridden'
+    end
+
+    def attribute_changes
+      raise NoMethodError, 'attribute_changes method must be overridden'
+    end
+end
--- test/api/sidekiq/tickets/update_ticket_states_test.rb
+++ test/api/sidekiq/tickets/update_ticket_states_test.rb
@@ -6,6 +6,7 @@ require Rails.root.join('spec', 'support', 'automations_helper.rb')
 require Rails.root.join('test', 'core', 'helpers', 'tickets_test_helper.rb')
 require Rails.root.join('test', 'core', 'helpers', 'controller_test_helper.rb')
 require Rails.root.join('test', 'core', 'helpers', 'note_test_helper.rb')
+['test_case_methods.rb'].each { |file| require "#{Rails.root}/test/api/helpers/#{file}" }
 
 class UpdateTicketStatesTest < ActionView::TestCase
   include CoreTicketsTestHelper
@@ -13,6 +14,7 @@ class UpdateTicketStatesTest < ActionView::TestCase
   include ControllerTestHelper
   include AutomationsHelper
   include NoteTestHelper
+  include TestCaseMethods
 
   def setup
     Account.stubs(:current).returns(Account.first)
@@ -24,6 +26,20 @@ class UpdateTicketStatesTest < ActionView::TestCase
   def teardown
     Account.unstub(:current)
     super
+  end
+
+  def get_update_ticket_states_worker_payload
+    user = add_new_user(@account)
+    User.stubs(:current).returns(user)
+    @ticket = create_ticket
+    User.unstub(:current)
+    @note = create_note(notable_id: @ticket.id, private: false, incoming: false, source: Helpdesk::Note::SOURCE_KEYS_BY_TOKEN['email'])
+    {
+      id: @note.id,
+      model_changes: nil,
+      freshdesk_webhook: false,
+      current_user_id: @agent.id
+    }
   end
 
   def test_ticket_state_worker_update_response_time
@@ -177,5 +193,71 @@ class UpdateTicketStatesTest < ActionView::TestCase
     ticket_state = note1.notable.ticket_states
     assert ticket_state.requester_responded_at == note1.updated_at
     assert ticket_state.inbound_count == 2
+  end
+
+  def test_update_first_response_with_rails_lock
+    Tickets::UpdateTicketStatesWorker.new.perform(get_update_ticket_states_worker_payload)
+    @ticket.update_attributes(status: 3)
+    reports_hash = @ticket.schema_less_ticket.reports_hash
+    assert_equal 3, @ticket.status
+    assert_equal @note.id, reports_hash['first_response_id']
+    assert_equal @agent.id, reports_hash['first_response_agent_id']
+  end
+
+  def test_update_first_response_with_rails_lock_exception
+    input = get_update_ticket_states_worker_payload
+    status = @ticket.status
+    Tickets::UpdateTicketStatesWorker.new.perform(input)
+    stub_const(LockVersion::Utility, 'MAX_RETRY_COUNT', 0) do
+      assert_raises ActiveRecord::StaleObjectError do
+        @ticket.update_attributes(status: 3)
+      end
+    end
+    @ticket.reload
+    reports_hash = @ticket.schema_less_ticket.reports_hash
+    assert_equal status, @ticket.status
+    assert_equal @note.id, reports_hash['first_response_id']
+    assert_equal @agent.id, reports_hash['first_response_agent_id']
+  end
+
+  def test_update_schema_less_ticket_with_lock_column_null_in_database
+    Helpdesk::SchemaLessTicket.lock_optimistically = false
+    get_update_ticket_states_worker_payload
+    Helpdesk::SchemaLessTicket.lock_optimistically = true
+    @ticket.schema_less_ticket.save!
+    @ticket.reload
+    assert_equal 1, @ticket.schema_less_ticket.int_tc05
+  end
+
+  def test_destroy_ticket_with_rails_lock
+    Tickets::UpdateTicketStatesWorker.new.perform(get_update_ticket_states_worker_payload)
+    @ticket.destroy
+    assert_raises ActiveRecord::RecordNotFound do
+      @ticket.reload
+    end
+  end
+
+  def test_destroy_ticket_with_rails_lock_exception
+    Tickets::UpdateTicketStatesWorker.new.perform(get_update_ticket_states_worker_payload)
+    stub_const(LockVersion::Utility, 'MAX_RETRY_COUNT', 0) do
+      assert_raises ActiveRecord::StaleObjectError do
+        @ticket.destroy
+      end
+    end
+    assert_nothing_raised { @ticket.reload }
+    reports_hash = @ticket.schema_less_ticket.reports_hash
+    assert_equal @note.id, reports_hash['first_response_id']
+    assert_equal @agent.id, reports_hash['first_response_agent_id']
+  end
+
+  def test_destroy_ticket_with_lock_column_null_in_database
+    Helpdesk::SchemaLessTicket.lock_optimistically = false
+    get_update_ticket_states_worker_payload
+    Helpdesk::SchemaLessTicket.lock_optimistically = true
+    @ticket = @account.tickets.find(@ticket.id)
+    @ticket.destroy
+    assert_raises ActiveRecord::RecordNotFound do
+      @ticket.reload
+    end
   end
 end
