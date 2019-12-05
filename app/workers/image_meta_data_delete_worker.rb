6c861021ee6c14cc800fbf77011a0fd1a33b8cb9
arjun-naduvakkat <53033629+arjun-naduvakkat@users.noreply.github.com>
2019-12-05 21:52:04 +0530

    FD-37478 : EXIF Geolocation Data Not Stripped From Uploaded Images (#6794)
    * FD-37478 : Remove image meta data - initial changes
    
    * FD-37478 : Added feature check using redis key
    
    * FD-37478 : Code clean up
    
    * FD-37478 : Edit all the images (attachment sizes)
    
    * FD-37478 : Test cases added
    
    * FD-37478 : Code review comments
    
    * FD-37478 : Added image type check before removing exif
    
    * FD-37478 : Rubocop changes
    
    * FD-37478 : Rubocop changes
    
    * Revert "FD-37478 : Rubocop changes"
    
    This reverts commit 63b5242a69f45daa3a9ff1879cebeca6e05bc0c8.
    
    * FD-37478 : Test case errors fixed
    
    * FD-37478 : Rubocop changes
    
    * FD-37478 : Code review comments
    
    * FD-37478 : Code review changes
    
    * FD-41483 : Minor bug fixes
    
    * FD-41483 : Rubocop changes
    
    * FD-37478 : Added write options while storing in S3
    
    * FD-37478 : Added logs

---
+++ app/workers/image_meta_data_delete_worker.rb
@@ -0,0 +1,25 @@
+class ImageMetaDataDeleteWorker < BaseWorker
+  require 'open3'
+  sidekiq_options :queue => :image_meta_data_delete, :retry => 0, :failures => :exhausted
+
+  def perform(args)
+    Rails.logger.info "ImageMetaDataDeleteWorker started. args : #{args.inspect}"
+    args.symbolize_keys!
+    s3_paths = args[:s3_paths]
+    s3_bucket = args[:s3_bucket]
+    write_options = { acl: args[:s3_permissions] }
+    s3_paths.each do |s3_path|
+      AwsWrapper::S3Functions.perform_operations_in_s3_attachment(s3_path, s3_bucket, 'image-attachments') do |local_file, path, bucket|
+        _stdout, stderr, status = Open3.capture3("exiftool -all= -tagsFromFile @ -Orientation -overwrite_original #{local_file.path}")
+        raise StandardError, "Error in exiftool, error : #{stderr}" if status.to_s[-1, 1] == '1'
+        File.open(local_file.path) { |file| AwsWrapper::S3Object.store(path, file, bucket, write_options) }
+      end
+    end
+  rescue StandardError => e
+    Rails.logger.error "Exception in removing image meta-data \
+      acc_id: #{Account.current.try(:id)}, args: #{args.inspect}, error message: #{e.message}, error: #{e.backtrace.join('\n')}"
+    NewRelic::Agent.notice_error(e, description: "Exception in removing image meta-data \
+      acc_id: #{Account.current.try(:id)}, args: #{args.inspect}")
+    raise e
+  end
+end
---
+++ lib/aws_wrapper/s3_functions.rb
@@ -0,0 +1,27 @@
+module AwsWrapper
+  class S3Functions
+    class << self
+      def perform_operations_in_s3_attachment(s3_path, s3_bucket, temp_file_prefix)
+        file = download_from_s3(s3_path, s3_bucket, temp_file_prefix)
+        yield(file, s3_path, s3_bucket)
+      ensure
+        File.delete(file.path) if file
+      end
+
+      def fetch_from_s3(s3_path, s3_bucket, temp_file_prefix)
+        file = download_from_s3(s3_path, s3_bucket, temp_file_prefix)
+        file
+      end
+
+      private
+
+        def download_from_s3(s3_path, s3_bucket, temp_file_prefix)
+          file = Tempfile.new(temp_file_prefix)
+          file.binmode
+          file.write(AwsWrapper::S3Object.read(s3_path, s3_bucket))
+          file.flush.seek(0, IO::SEEK_SET) # flush data to file and set RW pointer to beginning
+          file
+        end
+    end
+  end
+end
