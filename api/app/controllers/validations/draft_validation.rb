0219041d689a5e66197a3631aca158701d515c88
Kubendran-Mariappan <56542221+Kubendran-Mariappan@users.noreply.github.com>
2019-12-17 20:53:17 +0530

    FD-36418 - Metrics for articles used in tickets. (#7059)

--- api/app/controllers/validations/draft_validation.rb
+++ api/app/controllers/validations/draft_validation.rb
@@ -1,13 +1,28 @@
 class DraftValidation < ApiValidation
-  attr_accessor :cc_emails, :bcc_emails, :body, :quoted_text, :from_email
+  attr_accessor :cc_emails, :bcc_emails, :body, :quoted_text, :from_email, :articles_suggested
 
   validates :body, data_type: { rules: String, required: true, allow_nil: false }, on: :save_draft
   validates :quoted_text, data_type: { rules: String, allow_nil: true }, on: :save_draft
   validates :cc_emails, :bcc_emails, data_type: { rules: Array }, array: { data_type: { rules: String, allow_nil: false }, custom_length: { maximum: ApiConstants::MAX_LENGTH_STRING } }
   validates :cc_emails, :bcc_emails, custom_length: { maximum: ApiTicketConstants::MAX_EMAIL_COUNT, message_options: { element_type: :values } }
   validates :from_email, data_type: { rules: String, allow_nil: true }
+  validates :articles_suggested,
+            data_type: { rules: Array },
+            array: {
+              data_type: { rules: Hash },
+              hash: {
+                validatable_fields_hash: proc { |x| x.articles_suggested_fields_validation }
+              }
+            }
 
   def initialize(request_params, item, allow_string_param = false)
     super(request_params, item, allow_string_param)
+  end
+
+  def articles_suggested_fields_validation
+    {
+      language: { data_type: { rules: String }, custom_inclusion: { in: Language.all_codes, required: true } },
+      article_id: { data_type: { rules: Integer, allow_nil: false }, custom_numericality: { only_integer: true, greater_than: 0, allow_nil: false, ignore_string: :allow_string_param, required: true } }
+    }
   end
 end
--- app/models/solution/article.rb
+++ app/models/solution/article.rb
@@ -40,6 +40,7 @@ class Solution::Article < ActiveRecord::Base
   
   attr_accessor :highlight_title, :highlight_desc_un_html, :tags_changed, :prev_tags, :latest_tags, :session, :unpublishing, :false_delete_attachment_trigger, :attachment_added, :version_through
   alias_attribute :body, :article_body
+  alias_attribute :suggested, :int_01
   attr_accessible :title, :description, :user_id, :status, :import_id, :seo_data, :outdated
 
   validates_presence_of :title, :description, :user_id , :account_id
@@ -263,6 +264,10 @@ class Solution::Article < ActiveRecord::Base
       solution_article_meta.safe_send("#{method}=", (solution_article_meta.safe_send("#{method}") + value))
       super(value) 
     end
+  end
+
+  def suggested!
+    self.class.increment_counter('int_01', self.id)
   end
 
   def reset_ratings
