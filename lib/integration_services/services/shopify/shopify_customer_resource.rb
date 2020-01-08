06d14ac2bf9f82df107f974c97ca606537b286b8
nipun-gupta-e3183 <57524055+nipun-gupta-e3183@users.noreply.github.com>
2020-01-08 19:27:16 +0530

    [User Story] FD-16725 Shopify - check for orders using phone number if email is not present (#7205)
    * FD-16725 -Added changes for fetching orders from phone
    
    * FD-16725-Added a syntax fix
    
    * FD-16725 : Added some loggers
    
    Added some loggers
    
    * FD-16725 : Removed loggers
    
    * FD-16725 - Added test cases
    
    * FD-16725 - Added test case in installed_applications_conrollers.rb
    
    * FD-16725 - Implemented review comments and added regex to remove special characters from phone if present
    
    * FD-16725 - Added logger message if both email and phone are absent.

--- lib/integration_services/services/shopify/shopify_customer_resource.rb
+++ lib/integration_services/services/shopify/shopify_customer_resource.rb
@@ -1,14 +1,22 @@
 module IntegrationServices::Services
   module Shopify
     class ShopifyCustomerResource < IntegrationServices::Services::Shopify::ShopifyGenericResource
-      
-      def get_customer_id(email)
-        return {} if email.blank?
-        
-        request_url = "#{server_url}/admin/customers/search.json?query=email:#{email}&fields=email,id"
+      def get_customer_id(email, phone)
+        if email.present?
+          request_url = "#{server_url}/admin/customers/search.json?query=email:#{email}&fields=email,id"
+        elsif phone.present?
+          # removing any special characters other than digits
+          phone.gsub!(/\D/, '')
+          request_url = "#{server_url}/admin/customers/search.json?query=phone:#{phone}&fields=phone,id"
+        else
+          Rails.logger.info("No email or phone is present to fetch the shopify customer id.")
+          return {}
+        end
         response = http_get request_url
         process_response(response, 200) do |customers|
-          if customer_id(customers) && customer_email(customers).casecmp(email).zero?
+          if email.present? && customer_id(customers) && customer_email(customers).casecmp(email).zero?
+            return customer_id(customers)
+          elsif phone.present? && customer_id(customers)
             return customer_id(customers)
           else
             return nil
