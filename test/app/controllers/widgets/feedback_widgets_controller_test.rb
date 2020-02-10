24a3f4be89dae72e5c8d7ef68cb43ff740b66908
PadmashriRam <padmashri.ramkumar@freshworks.com>
2020-02-10 17:39:26 +0530

    FD-45484 test case for type nil feedback controller

--- test/app/controllers/widgets/feedback_widgets_controller_test.rb
+++ test/app/controllers/widgets/feedback_widgets_controller_test.rb
@@ -27,6 +27,17 @@ class Widgets::FeedbackWidgetsControllerTest < ActionController::TestCase
     user.destroy
   end
 
+  def test_feedback_widget_create_with_correct_ticket_type
+    user = add_new_user(Account.current, active: true)
+    user.make_current
+    login_as(user)
+    post :create, version: :private, helpdesk_ticket: { email: user.email, ticket_type: 'Question' }
+    assert_response 200
+  ensure
+    log_out
+    user.destroy
+  end
+
   def test_feedback_widget_create_with_empty_ticket_type
     user = add_new_user(Account.current, active: true)
     user.make_current
@@ -38,15 +49,12 @@ class Widgets::FeedbackWidgetsControllerTest < ActionController::TestCase
     user.destroy
   end
 
-  def test_feedback_widget_create_with_wrong_ticket_type
+  def test_feedback_widget_create_with_nil_ticket_type
     user = add_new_user(Account.current, active: true)
     user.make_current
     login_as(user)
     post :create, version: :private, helpdesk_ticket: { email: user.email, ticket_type: nil }
     assert_response 200
-    error_response = JSON.parse(response.body)
-    assert_equal error_response['success'], false
-    assert_equal error_response['error'], 'Invalid ticket type'
   ensure
     log_out
     user.destroy
