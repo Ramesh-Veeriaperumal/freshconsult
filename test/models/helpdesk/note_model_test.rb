8c55216965f4e696bdd86f9c31a471b077ead71f
sridhar-dheenadhayalan <50397624+sridhar-dheenadhayalan@users.noreply.github.com>
2020-02-25 15:28:30 +0530

    [L4 Bug] [FD-13572]Fixed issue of inline images with same cid not displaying. (#7401)
    * Fixed issue of inline images with same cid not displaying.
    
    * confilict issue
    
    * removed unwanted change
    
    * space issue.

--- test/models/helpdesk/note_model_test.rb
+++ test/models/helpdesk/note_model_test.rb
@@ -1,16 +1,23 @@
+require_relative '../test_helper'
 require_relative '../../api/unit_test_helper'
 require_relative '../../../spec/support/note_helper'
+require_relative '../../api/helpers/test_class_methods.rb'
+require_relative '../../api/helpers/tickets_test_helper.rb'
+['account_helper.rb', 'user_helper.rb'].each { |file| require Rails.root.join('spec', 'support', file) }
 
 class NoteModelTest < ActiveSupport::TestCase
+  include AccountHelper
   include EmailHelper
   include NoteHelper
+  include UsersHelper
+  include TicketsTestHelper
 
   def setup
-    Account.stubs(:current).returns(Account.first || create_test_account)
+    @account = create_test_account if @account.nil?
+    @account.make_current
   end
 
   def teardown
-    Account.unstub(:current)
     super
   end
 
@@ -22,5 +29,24 @@ class NoteModelTest < ActiveSupport::TestCase
     note = create_note
     note.update_email_received_at(parsed_date)
     assert_equal true, note.schema_less_note.note_properties.key?(:received_at)
+  end
+
+  # Replacing the cid for all places using gsub is the fix.
+  def test_content_id_for_more_than_one_inline_image_is_same_then_all_images_should_display
+    ticket = Account.current.tickets.last
+    attachment = Helpdesk::Attachment.new
+    attachment.attachable_type = 'Ticket::Inline'
+    attachment.content_file_name = 'testattach'
+    attachment.content_content_type = 'text/binary'
+    attachment.content_file_size = 80
+    Helpdesk::Note.any_instance.stubs(:inline_attachments).returns([attachment])
+    note = create_note(notable_id: ticket.id, private: false)
+    note.note_body.body_html = "<img src='cid:abc' class='inline-image'> This is content <img src='cid:abc' class='inline-image'> "
+    note.note_body.full_text_html = "<img src='cid:abc' class='inline-image'> This is content <img src='cid:abc' class='inline-image'> "
+    note.header_info = { content_ids: { note.inline_attachments[0].content_file_name + '0' => 'abc' } }
+    note.safe_send(:update_content_ids)
+    assert_not_equal note.note_body.body_html, "<img src='cid:abc' class='inline-image'> This is content <img src='cid:abc' class='inline-image'> "
+    assert_not_equal note.note_body.full_text_html, "<img src='cid:abc' class='inline-image'> This is content <img src='cid:abc' class='inline-image'> "
+    Helpdesk::Note.any_instance.unstub(:inline_attachments)
   end
 end
--- test/models/helpdesk/ticket_model_test.rb
+++ test/models/helpdesk/ticket_model_test.rb
@@ -1,4 +1,6 @@
 require_relative '../test_helper'
+require_relative '../../api/helpers/test_class_methods.rb'
+require_relative '../../api/helpers/tickets_test_helper.rb'
 ['account_helper.rb', 'user_helper.rb'].each { |file| require Rails.root.join('spec', 'support', file) }
 
 class TicketModelTest < ActiveSupport::TestCase
@@ -37,5 +39,21 @@ class TicketModelTest < ActiveSupport::TestCase
     ticket = Account.current.tickets.last
     ticket.update_email_received_at(nil)
     assert_equal false, ticket.schema_less_ticket.header_info.key?(:received_at)
+  end
+
+  # Replacing the cid for all places using gsub is the fix.
+  def test_content_id_for_more_than_one_inline_image_is_same_then_all_images_should_display
+    attachment = Helpdesk::Attachment.new
+    attachment.attachable_type = 'Ticket::Inline'
+    attachment.content_file_name = 'testattach'
+    attachment.content_content_type = 'text/binary'
+    attachment.content_file_size = 80
+    Helpdesk::Ticket.any_instance.stubs(:inline_attachments).returns([attachment])
+    ticket = Account.current.tickets.last
+    ticket.ticket_body.description_html = "<img src='cid:abc' class='inline-image'> This is content <img src='cid:abc' class='inline-image'> "
+    ticket.header_info = { content_ids: { ticket.inline_attachments[0].content_file_name + '0' => 'abc' } }
+    ticket.safe_send(:update_content_ids)
+    assert_not_equal ticket.ticket_body.description_html, "<img src='cid:abc' class='inline-image'> This is content <img src='cid:abc' class='inline-image'> "
+    Helpdesk::Ticket.any_instance.unstub(:inline_attachments)
   end
 end
