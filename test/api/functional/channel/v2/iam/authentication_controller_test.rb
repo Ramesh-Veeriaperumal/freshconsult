f74768fa4c2c74b66bfb9f1b55ccb02ec62c0a57
kalyanasundaram <54533394+kalyanasundaram-venkatesan@users.noreply.github.com>
2020-03-11 22:46:34 +0530

    FD-47433, Changes to accept GET, POST and DELETE calls. (#7875)
    - Renaming the show action of IAM authentication controller to authenticate to accept GET, POST and DELETE calls.
    - Disabling SSL validation on authentication endpoint.

--- test/api/functional/channel/v2/iam/authentication_controller_test.rb
+++ test/api/functional/channel/v2/iam/authentication_controller_test.rb
@@ -8,6 +8,12 @@ class Channel::V2::Iam::AuthenticationControllerTest < ActionController::TestCas
   def setup
     super
     @current_account = Account.first.make_current
+    @controller.stubs(:api_current_user).returns(nil)
+  end
+
+  def tear_down
+    Channel::V2::Iam::AuthenticationController.any_instance.unstub(:private_api?)
+    @controller.unstub(:api_current_user)
   end
 
   def test_auth_by_freshid_api
@@ -15,19 +21,15 @@ class Channel::V2::Iam::AuthenticationControllerTest < ActionController::TestCas
     @current_account.launch(:api_jwt_auth)
     token = generate_jwt_token(1, 1, Time.now.to_i, Time.now.to_i)
     request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(token)
-    @controller.stubs(:api_current_user).returns(nil)
-    get :show, controller_params(version: 'private')
+    get :authenticate, controller_params(version: 'private')
     assert_equal JSON.parse(response.body)['success'], true
     assert response.headers.key?('Authorization')
     assert_response 200
   ensure
-    Channel::V2::Iam::AuthenticationController.any_instance.unstub(:private_api?)
     @current_account.rollback(:api_jwt_auth)
-    @controller.unstub(:api_current_user)
   end
 
   def test_session_auth
-    @controller.stubs(:api_current_user).returns(nil)
     Channel::V2::Iam::AuthenticationController.any_instance.stubs(:private_api?).returns(true)
     privileges = ['manage_tickets', 'mange_contacts', 'view_contacts']
     role_without_manage_companies = create_role(name: Faker::Name.name,
@@ -37,43 +39,53 @@ class Channel::V2::Iam::AuthenticationControllerTest < ActionController::TestCas
                                          role_ids: [role_without_manage_companies.id],
                                          privileges: role_without_manage_companies.privileges)
     create_session
-    get :show, controller_params(version: 'private')
+    get :authenticate, controller_params(version: 'private')
     assert_equal JSON.parse(response.body)['success'], true
     assert response.headers.key?('Authorization')
     assert_response 200
-  ensure
-    Channel::V2::Iam::AuthenticationController.any_instance.unstub(:private_api?)
-    @controller.unstub(:api_current_user)
   end
 
   def test_basic_auth
-    @controller.stubs(:api_current_user).returns(nil)
     Channel::V2::Iam::AuthenticationController.any_instance.stubs(:private_api?).returns(false)
     agent = add_test_agent(@account)
     request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(agent.email, 'test')
-    get :show, controller_params(version: 'private')
+    get :authenticate, controller_params(version: 'private')
     assert_equal JSON.parse(response.body)['success'], true
     assert response.headers.key?('Authorization')
     assert_response 200
-  ensure
-    Channel::V2::Iam::AuthenticationController.any_instance.unstub(:private_api?)
-    @controller.unstub(:api_current_user)
+  end
+
+  def test_basic_auth_for_post_method
+    Channel::V2::Iam::AuthenticationController.any_instance.stubs(:private_api?).returns(false)
+    agent = add_test_agent(@account)
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(agent.email, 'test')
+    post :authenticate, controller_params(version: 'private')
+    assert_equal JSON.parse(response.body)['success'], true
+    assert response.headers.key?('Authorization')
+    assert_response 200
+  end
+
+  def test_basic_auth_for_delete_method
+    Channel::V2::Iam::AuthenticationController.any_instance.stubs(:private_api?).returns(false)
+    agent = add_test_agent(@account)
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(agent.email, 'test')
+    delete :authenticate, controller_params(version: 'private')
+    assert_equal JSON.parse(response.body)['success'], true
+    assert response.headers.key?('Authorization')
+    assert_response 200
   end
 
   def test_auth_for_unauthorised_user
-    @controller.stubs(:api_current_user).returns(nil)
     Channel::V2::Iam::AuthenticationController.any_instance.stubs(:private_api?).returns(false)
     CustomRequestStore.store[:private_api_request] = false
     @current_account.launch(:api_jwt_auth)
     token = generate_jwt_token(1, 1, Time.now.to_i, 2 * Time.now.to_i)
     request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(token)
-    get :show, controller_params(version: 'private')
+    get :authenticate, controller_params(version: 'private')
     assert_equal 401, response.status
     match_json(request_error_pattern(:invalid_credentials))
   ensure
     CustomRequestStore.store[:private_api_request] = true
-    Channel::V2::Iam::AuthenticationController.any_instance.unstub(:private_api?)
     @current_account.rollback(:api_jwt_auth)
-    @controller.unstub(:api_current_user)
   end
 end
