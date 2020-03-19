374957c76873ab37cf3d9562faea4ae6d0f16ef6
HariniKrishnamoorthy <41904767+HariniKrishnamoorthy@users.noreply.github.com>
2020-03-19 17:21:52 +0530

    [Technical Story]  FD-47928: adding query params (#7901)
    * adding query params
    
    * lower case of states
    
    * removing validations
    
    * automations change for ooo
    
    * rubocop fixes
    
    * rubocop fixes-1
    
    * changing constant
    
    * changes for ooo automations
    
    * constant change
    
    * random test case fix

--- api/app/controllers/constants/out_of_office_constants.rb
+++ api/app/controllers/constants/out_of_office_constants.rb
@@ -3,5 +3,5 @@ module OutOfOfficeConstants
 
   OUT_OF_OFFICE_SHOW = 'api/v1/out-of-offices/%{out_of_office_id}'.freeze
 
-  ACTIVE_QUERY_PARAM = '?active=true'.freeze
+  QUERY_PARAM = '?state=%{state_value}'.freeze
 end
