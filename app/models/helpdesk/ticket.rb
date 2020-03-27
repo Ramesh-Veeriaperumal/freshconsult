2a52a5f37f785ea769f1b03ba478a92c0402e862
seema-singh-E3339 <59820009+seema-singh-E3339@users.noreply.github.com>
2020-03-27 10:13:50 +0530

    [L4 Bug] FD-42728 Fix cc email caps issue (#7993)
    * FD-41445 Removing create option when multiple email feature not available
    
    * FD-42728 email cc fix
    
    * FD-42728 email cc issue
    
    * FD-42728 rubocop error fix

--- app/models/helpdesk/ticket.rb
+++ app/models/helpdesk/ticket.rb
@@ -701,8 +701,8 @@ class Helpdesk::Ticket < ActiveRecord::Base
   end
 
   def included_in_cc?(from_email)
-    (cc_email_hash) and  ((cc_email_hash[:cc_emails].any? {|email| email.include?(from_email.downcase) }) or
-                     (cc_email_hash[:fwd_emails].any? {|email| email.include?(from_email.downcase) }) or
+    cc_email_hash && ((cc_email_hash[:cc_emails].any? { |email| email.downcase.include?(from_email.downcase) }) ||
+                     (cc_email_hash[:fwd_emails].any? { |email| email.downcase.include?(from_email.downcase) }) ||
                      included_in_to_emails?(from_email))
   end
 
