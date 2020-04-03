a46c16963280e2df2a4c2eda17b09dfcaac6e589
Keerthi Priya <keerthi.priya@freshworks.com>
2020-04-03 15:53:39 +0530

    enable customer journey cleanup (#8109)

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -36,7 +36,7 @@ class Account < ActiveRecord::Base
     :solutions_agent_portal, :solutions_agent_metrics, :fuzzy_search, :delete_trash_daily,
     :prevent_wc_ticket_create, :allow_wildcard_ticket_create, :requester_privilege,
     :prevent_parallel_update, :sso_unique_session, :delete_trash_daily_schedule, :retrigger_lbrr, :fsm_admin_automations,
-    :csat_email_scan_compatibility, :mint_portal_applicable, :quoted_text_parsing_feature, :enable_customer_journey,
+    :csat_email_scan_compatibility, :mint_portal_applicable, :quoted_text_parsing_feature,
     :email_mailbox, :sandbox_temporary_offset, :downgrade_policy, :article_es_search_by_filter,
     :fluffy_min_level, :allow_update_agent, :help_widget_solution_categories, :launch_fsm_geolocation,
     :ticket_field_revamp, :facebook_dm_outgoing_attachment, :skip_posting_to_fb, :hide_mailbox_error_from_agents, :hide_og_meta_tags,
--- test/api/unit/pricing_plan_change_2019_test.rb
+++ test/api/unit/pricing_plan_change_2019_test.rb
@@ -6,35 +6,16 @@ class PricingPlanChange2019Test < ActionView::TestCase
     @account.make_current
   end
 
-  def test_to_features_list_without_launchparty_feature
-    @account.rollback(:enable_customer_journey)
-    features = (Account::PRICING_PLAN_MIGRATION_FEATURES_2020.to_a - @account.features_list)
-    assert features.blank?
-  end
-
-  def test_to_features_list_with_launchparty_feature
+  def test_to_features_list
     added = [:customer_journey]
     added.each { |feature| @account.add_feature(feature) }
-    @account.launch(:enable_customer_journey)
     features_list = Account.current.reload.features_list
     assert ((added & features_list).count == 1), 'Should contain :customer_journey'
   end
 
-  def test_has_feature_without_launchparty_feature
-    @account.rollback(:enable_customer_journey)
-    Account::PRICING_PLAN_MIGRATION_FEATURES_2020.each do |feature|
-      assert Account.current.has_feature?(feature)
-    end
-    @account.revoke_feature(:add_watcher)
-    refute @account.reload.has_feature?(:add_watcher)
-    @account.add_feature(:add_watcher)
-    assert @account.reload.has_feature?(:add_watcher)
-  end
-
-  def test_has_feature_with_feature_launched
+  def test_has_feature
     added = [:customer_journey]
     added.each { |feature| @account.add_feature(feature) }
-    @account.launch(:enable_customer_journey)
     added.each { |feature| assert @account.has_feature?(feature) }
     @account.revoke_feature(:add_watcher)
     refute @account.reload.has_feature?(:add_watcher)
@@ -54,8 +35,7 @@ class PricingPlanChange2019Test < ActionView::TestCase
     assert @account.reload.has_features?(:add_watcher, :advanced_facebook, :unlimited_multi_product)
   end
 
-  def test_has_features_with_launchparty_feature
-    @account.launch(:enable_customer_journey)
+  def test_has_features
     @account.revoke_feature(:add_watcher)
     @account.revoke_feature(:advanced_facebook)
     refute @account.reload.has_features?(:add_watcher, :advanced_facebook)
