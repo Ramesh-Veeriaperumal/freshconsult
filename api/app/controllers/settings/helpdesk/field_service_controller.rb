040db5cea41fa293f20f2a874bd46caeee94d7b8
umapathy-babu <53554274+umapathy-babu@users.noreply.github.com>
2020-05-04 10:44:49 +0530

    [Technical Story] FD-49683 Modify the FSM API settings to new namespace (#8226)
    * FD-49683 Modify the FSM API settings to new namespace
    
    modify endpoint for field service settings under settings/helpdesk controller
    
    * FD-49683 Modify the FSM API settings to new namespace
    
    added before_filter to check for empty params and wrote test cases
    
    * FD-49683 Modify the FSM API settings to new namespace
    
    written test cases for handling null values
    
    * FD-49683 Modify the FSM API settings to new namespace
    
    Wrote a new controller field_service under namespace, SETTINGS:HELPDESK:FIELD_SERVICE
    
    * FD-49683 Modify the FSM API settings to new namespace
    
    Fixed rubocop errors and fixed helpdesk controller validate_body-params
    
    * FD-49683 Modify the FSM API settings to new namespace
    
    changed end point in api routes
    
    * Update helpdesk_controller_test.rb
    
    * FD-49683 changed api end point from field_service to field-service
    
    * FD-49683 updated swagger for field-service
    
    * FD-49683 updated swagger for field-service
    
    added geo_location_enabled to save_settings.yaml

---
+++ api/app/controllers/settings/helpdesk/field_service_controller.rb
@@ -0,0 +1,36 @@
+module Settings::Helpdesk
+  class FieldServiceController < ApiApplicationController
+    include HelperConcern
+    include FieldServiceManagementHelper
+
+    skip_before_filter :load_object
+    before_filter :validate_fsm_enabled
+    before_filter :check_params, :validate_body_params, only: [:update_settings]
+
+    def update_settings
+      scoper.account_additional_settings.save_field_service_management_settings(params[cname])
+      @fsm_settings = fetch_fsm_settings(current_account.account_additional_settings)
+      render 'show_settings'
+    end
+
+    def show_settings
+      @fsm_settings = fetch_fsm_settings(current_account.account_additional_settings)
+    end
+
+    private
+
+    def validate_fsm_enabled
+      render_request_error(:require_feature, 403, feature: 'field_service_management'.titleize) unless current_account.field_service_management_enabled?
+    end
+
+    def scoper
+      current_account
+    end
+
+    def validate_body_params
+      @validation_klass = FieldServiceManagementValidation.to_s.freeze
+      @constants_klass = Admin::AdvancedTicketing::FieldServiceManagement::Constant.to_s.freeze
+      validate_request(nil, params[cname], nil)
+    end
+  end
+end
---
+++ test/api/functional/settings/field_service_controller_test.rb
@@ -0,0 +1,120 @@
+require_relative '../../../test_helper'
+
+module Settings::Helpdesk
+  class FieldServiceControllerTest < ActionController::TestCase
+    include PrivilegesHelper
+    include FieldServiceManagementHelper
+
+    def wrap_cname(params = {})
+      { field_service: params }
+    end
+
+    def test_update_settings
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      add_privilege(User.current, :admin_tasks) unless User.current.privilege?(:admin_tasks)
+      params = FSM_SETTINGS_DEFAULT_VALUES.dup
+      params = params.each { |key, value| params[key] = !value }
+      put :update_settings, construct_params({}, params)
+      params.map { |key, value| [key.to_sym, value] }
+      assert params == Account.current.account_additional_settings.additional_settings[:field_service]
+      match_json(fetch_fsm_settings(Account.current.account_additional_settings))
+      assert_response 200
+      params = FSM_SETTINGS_DEFAULT_VALUES.dup
+      put :update_settings, construct_params({}, params)
+      assert Account.current.account_additional_settings.additional_settings[:field_service].empty?
+      match_json(fetch_fsm_settings(Account.current.account_additional_settings))
+      assert_response 200
+    ensure
+      Account.any_instance.unstub(:field_service_management_enabled?)
+    end
+
+    def test_rename_and_update_hash_key_as_field_service
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      add_privilege(User.current, :admin_tasks) unless User.current.privilege?(:admin_tasks)
+      Account.current.account_additional_settings.additional_settings.delete(:field_service)
+      Account.current.account_additional_settings.additional_settings[:field_service_management] = {}
+      Account.current.account_additional_settings.save!
+      params = FSM_SETTINGS_DEFAULT_VALUES.dup
+      put :update_settings, construct_params({}, params)
+      assert_response 200
+      assert Account.current.account_additional_settings.additional_settings.key?(:field_service)
+      refute Account.current.account_additional_settings.additional_settings.key?(:field_service_management)
+    ensure
+      Account.any_instance.unstub(:field_service_management_enabled?)
+    end
+
+    def test_update_settings_when_fsm_disabled
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(false)
+      add_privilege(User.current, :admin_tasks) unless User.current.privilege?(:admin_tasks)
+      params = FSM_SETTINGS_DEFAULT_VALUES
+      put :update_settings, construct_params({}, params)
+      match_json(request_error_pattern(:require_feature, feature: 'field_service_management'.titleize))
+      assert_response 403
+    ensure
+      Account.any_instance.unstub(:field_service_management_enabled?)
+    end
+
+    def test_delete_key_from_additional_settings_hash_for_default_value
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      add_privilege(User.current, :admin_tasks) unless User.current.privilege?(:admin_tasks)
+      params = { UPDATE_SETTINGS_FIELDS[0] => !FSM_SETTINGS_DEFAULT_VALUES[UPDATE_SETTINGS_FIELDS[0]] }
+      put :update_settings, construct_params({}, params)
+      assert_equal true, Account.current.account_additional_settings.additional_settings[:field_service].key?(UPDATE_SETTINGS_FIELDS[0].to_sym)
+      params = { UPDATE_SETTINGS_FIELDS[0] => FSM_SETTINGS_DEFAULT_VALUES[UPDATE_SETTINGS_FIELDS[0]] }
+      put :update_settings, construct_params({}, params)
+      assert_equal false, Account.current.account_additional_settings.additional_settings[:field_service].key?(UPDATE_SETTINGS_FIELDS[0].to_sym)
+    end
+
+    def test_restrict_update_for_users_without_admin_privilege
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      remove_privilege(User.current, :admin_tasks) if User.current.privilege?(:admin_tasks)
+      params = FSM_SETTINGS_DEFAULT_VALUES
+      put :update_settings, construct_params({}, params)
+      match_json(request_error_pattern(:access_denied))
+      assert_response 403
+    ensure
+      Account.any_instance.unstub(:field_service_management_enabled?)
+      add_privilege(User.current, :admin_tasks) unless User.current.privilege?(:admin_tasks)
+    end
+
+    def test_show_settings
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      add_privilege(User.current, :admin_tasks) unless User.current.privilege?(:admin_tasks)
+      get :show_settings, construct_params({})
+      match_json(fetch_fsm_settings(Account.current.account_additional_settings))
+      assert_response 200
+    ensure
+      Account.any_instance.unstub(:field_service_management_enabled?)
+    end
+
+    def test_show_settings_without_admin_privilege
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      remove_privilege(User.current, :admin_tasks) if User.current.privilege?(:admin_tasks)
+      get :show_settings, construct_params({})
+      match_json(request_error_pattern(:access_denied))
+      assert_response 403
+    end
+
+    def test_show_settings_when_fsm_disabled
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(false)
+      get :show_settings, construct_params({})
+      match_json(request_error_pattern(:require_feature, feature: 'field_service_management'.titleize))
+      assert_response 403
+    ensure
+      Account.any_instance.unstub(:field_service_management_enabled?)
+    end
+
+    def test_update_field_service_with_invalid_params
+      Account.any_instance.stubs(:field_service_management_enabled?).returns(true)
+      User.any_instance.stubs(:privilege?).with(:admin_tasks).returns(true)
+      params = {}
+      put :update_settings, construct_params({}, params)
+      assert_response 400
+      match_json(request_error_pattern(:missing_params))
+      params = { field_agents_can_manage_appointments: nil }
+      put :update_settings, construct_params({}, params)
+      assert_response 400
+      match_json([bad_request_error_pattern('field_agents_can_manage_appointments', :datatype_mismatch, expected_data_type: 'Boolean', prepend_msg: :input_received, given_data_type: 'Null')])
+    end
+  end
+end
