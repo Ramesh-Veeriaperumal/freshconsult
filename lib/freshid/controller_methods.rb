ee8956a9b6aac71305565f39eb44ee60de4e90fe
HariniKrishnamoorthy <41904767+HariniKrishnamoorthy@users.noreply.github.com>
2020-06-16 13:12:05 +0530

    [L4 Bug] FD-54116: added https in url (#8639)
    * FD-54116: added https in url
    
    * Update controller_methods.rb

--- lib/freshid/controller_methods.rb
+++ lib/freshid/controller_methods.rb
@@ -64,7 +64,7 @@ module Freshid::ControllerMethods
   end
 
   def freshid_v2_mobile_authorize_url(account, mobile_scheme = nil)
-    Freshid::V2::UrlGenerator.freshid_authorize(freshid_authorize_callback_url(host: account.full_domain, params: { mobile_login: true, scheme: mobile_scheme }), account.full_domain, false)
+    Freshid::V2::UrlGenerator.freshid_authorize(freshid_authorize_callback_url(host: account.full_domain, params: { mobile_login: true, scheme: mobile_scheme }, protocol: 'https'), account.full_domain, false)
   end
   
   def freshid_integration_enabled_and_not_logged_in?
