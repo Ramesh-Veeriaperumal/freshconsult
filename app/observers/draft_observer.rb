c1eb9d20405424d29c63b8eea83278eaf144b63a
Tharakai Natesan <tharakai.natesan@freshworks.com>
2020-06-18 17:51:51 +0530

    [Bug] FD-54887 approved_by / approved_at and published_by / published_at should be part of the model changes (#8734)

--- app/observers/draft_observer.rb
+++ app/observers/draft_observer.rb
@@ -35,7 +35,7 @@ class DraftObserver < ActiveRecord::Observer
     end
 
     def clear_approvals(item)
-       # on deleting approved article, the approval record will already be deleted because of dependent destroy
+      # on deleting approved article, the approval record will already be deleted because of dependent destroy
       item.article.reload && item.article.clear_approvals if Account.current.article_approval_workflow_enabled?
     end
 
