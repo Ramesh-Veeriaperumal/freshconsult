36512a9427a4e8fa6287f875880d4d0510a82872
Ramesh-Veeriaperumal <ramesh.veeriaperumal@freshworks.com>
2020-07-08 18:07:29 +0530

    FD-56016 Resync worker for helpkit

---
+++ app/workers/central_publish/resync_worker.rb
@@ -0,0 +1,14 @@
+module CentralPublish
+  class ResyncWorker < BaseWorker
+    include CentralReSyncConstants
+    include CentralLib::CentralReSync
+
+    sidekiq_options queue: :central_publish_resync, retry: 0, failures: :exhausted
+
+    def perform(args)
+      args.symbolize_keys!
+
+      Account.current.safe_send(args[:relation_with_account]).sync_entity(args[:meta_info])
+    end
+  end
+end
