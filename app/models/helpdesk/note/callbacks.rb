1e3c4738dc54861987e9d5cf7ca9b762fec81cfa
murugaperumal-ramasamy <murugaperumal.ramasamy@freshworks.com>
2020-07-09 17:26:17 +0530

    [Bug] [FD-58694] Update full_text and full_text_html update on every note body and body_html update (#8908)

--- app/models/helpdesk/note/callbacks.rb
+++ app/models/helpdesk/note/callbacks.rb
@@ -53,12 +53,8 @@ class Helpdesk::Note < ActiveRecord::Base
   include Redis::OthersRedis
 
   def load_full_text
-    note_body.full_text ||= note_body.body if note_body.body.present?
-    note_body.full_text_html ||= note_body.body_html if note_body.body_html.present?
-    if self.note && self.note.note? && !self.note.incoming  # for updating full_text content if body_html is edited
-      note_body.full_text = note_body.body if note_body.body.present?
-      note_body.full_text_html = note_body.body_html if note_body.body_html.present?
-    end
+    note_body.full_text = note_body.body if note_body.body.present?
+    note_body.full_text_html = note_body.body_html if note_body.body_html.present?
   end
 
   def remove_activity
