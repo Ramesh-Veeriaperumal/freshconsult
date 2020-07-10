1425ff7e1f0aba6f251174fa0427311c9a8dcb7a
Ramesh-Veeriaperumal <ramesh.veeriaperumal@freshworks.com>
2020-07-10 19:20:26 +0530

    FD-56016 Rubocop fixes

--- lib/central_lib/central_resync.rb
+++ lib/central_lib/central_resync.rb
@@ -2,7 +2,6 @@ module CentralLib
   module CentralReSync
     RESYNC_DATA_ENTITIES = ['Helpdesk::Ticket', 'Helpdesk::Note'].freeze
     RESYNC_CONFIG_ENTITIES = ['Helpdesk::TicketField', 'Agent', 'Group'].freeze
-
     RESYNC_CONFIG_BATCH_SIZE = 100
     RESYNC_DATA_BATCH_SIZE = 300
 
