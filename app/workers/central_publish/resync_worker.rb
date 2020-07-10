d966eb47fced57324869b9335248300be7848dc0
Ramesh-Veeriaperumal <ramesh.veeriaperumal@freshworks.com>
2020-07-10 19:16:21 +0530

    FD-56016 Modify the worker

--- app/workers/central_publish/resync_worker.rb
+++ app/workers/central_publish/resync_worker.rb
@@ -1,13 +1,26 @@
+require Rails.root.join('lib', 'central_lib', 'central_resync.rb')
+
 module CentralPublish
   class ResyncWorker < BaseWorker
     include CentralLib::CentralReSync
 
     sidekiq_options queue: :central_publish_resync, retry: 0, failures: :exhausted
 
+    attr_accessor :relation_with_account, :meta_info, :conditions
+
     def perform(args)
       args.symbolize_keys!
+      @relation_with_account = args[:relation_with_account].to_sym
+      @meta_info = args[:meta_info]
+      @conditions = args[:conditions]
 
-      Account.current.safe_send(args[:relation_with_account]).sync_entity(args[:meta_info])
+      publish_entity_to_central
+    end
+
+    def publish_entity_to_central
+      return unless Account.current.respond_to?(@relation_with_account)
+
+      Account.current.safe_send(@relation_with_account).where(@conditions).sync_entity(@meta_info)
     end
   end
 end
