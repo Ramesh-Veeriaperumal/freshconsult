16c044ad00fb5d6f5352cf66771c64b00409b5f1
chockalingam subbiah <chockalingam.subbaiah@freshworks.com>
2020-08-03 10:48:37 +0530

    [L4 Bug] FD-57261 Update rate-limiting gem version (#8905)
    * FD-57261 signup throttling should happen only in production
    
    IP based signup throttling should happen only in production
    
    * FD-57261 IP throttling for signup - only in production
    
    * FD-57261 change rate-limit gem version & set per_ip
    
    * FD57261 Update rate-limiting gem version

--- config/application.rb
+++ config/application.rb
@@ -143,7 +143,7 @@ module Helpkit
       r.define_rule(match: "^(/[a-z]{2}(-[A-Z]{2})?)?\/support\/tickets\/check_email", type: :fixed, metric: :rpm, limit: 10, per_ip: true, per_url: true, include_host: true)
       r.define_rule(match: "^(/[a-z]{2}(-[A-Z]{2})?)?\/password_resets", type: :fixed, metric: :rpm, limit: 10, per_ip: true, per_url: true, include_host: true)
       r.define_rule(match: "^/(([a-z]{2}(-[A-Z]{2})?\/)?support(?!\/(theme)))/.*", type: :fixed, metric: :rpm, limit: 200, per_ip: true, per_url: true, include_host: true)
-      r.define_rule( :match => "^/(accounts\/new_signup_free).*", :type => :fixed, :metric => :rpd, :limit => 5,:per_xff_ip => true)
+      r.define_rule(match: "^/(accounts\/new_signup_free).*", type: :fixed, metric: :rpd, limit: 5, per_ip: false, per_xff_ip: true)
       r.define_rule( :match => "^/(accounts\/email_signup).*", :type => :fixed, :metric => :rpd, :limit => 5,:per_ip => true)
       r.define_rule( :match => "^/(accounts\/anonymous_signup).*", :type => :fixed, :metric => :rpd, :limit => 5,:per_ip => true)
       r.define_rule( :match => "^/(public\/tickets)/.*", :type => :fixed, :metric => :rpm, :limit => 10,:per_ip => true)
