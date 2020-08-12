21f0660fc86f1431dc081331810e6e0ec29a9119
Ramachandran Nagaraj <64406059+ramachandran-nagaraj@users.noreply.github.com>
2020-08-12 08:30:06 +0530

    FD-60610 removed translations_cdn lp from features.rb (#9169)
    Co-authored-by: Dhanapal.Kamaraj <dhanapal.kamaraj@freshworks.com>

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -42,7 +42,7 @@ class Account < ActiveRecord::Base
     :sane_restricted_helpdesk, :hiding_confidential_logs, :sla_policy_revamp, :help_widget_log, :freshdesk_freshsales_bundle,
     :requester_widget_timeline,
     :out_of_office, :enable_secure_login_check, :public_api_filter_factory, :marketplace_gallery,
-    :translations_proxy, :translations_cdn, :facebook_public_api, :twitter_public_api, :emberize_agent_form, :retry_emails, :disable_beamer, :fb_message_echo_support, :portal_prototype_update,
+    :translations_proxy, :facebook_public_api, :twitter_public_api, :emberize_agent_form, :retry_emails, :disable_beamer, :fb_message_echo_support, :portal_prototype_update,
     :bot_banner, :idle_session_timeout, :solutions_dashboard,
     :observer_race_condition_fix, :contact_graphical_avatar, :omni_bundle_2020, :article_versioning_redis_lock, :freshid_sso_sync, :fw_sso_admin_security, :cre_account, :cdn_attachments, :shopify_api_revamp,
     :omni_chat_agent, :portal_frameworks_update, :ticket_filters_central_publish, :new_email_regex, :auto_refresh_revamp, :agent_statuses, :omni_reports,
