b98893927bfa7eef8a3e5f42de0501ce03c28efc
Ashok-Kalam <65676047+Ashok-Kalam@users.noreply.github.com>
2020-08-12 14:19:02 +0530

    [Security Bug] FD-55362 Csp reports API Calling for Agent Portal (#8971)
    * FD-55362 Csp reports API Calling
    
    * [FD-55362] updated the path for cspreports URI
    
    * FD-55362 added redis based key for getting CSP
    
    * add log statements FD-55362
    
    * FD-55362  staging env check
    
    * FD-55362 commenting the env check
    
    * enable env staging and prod and remove redis keys FD-55362
    
    * local cache impl for the csp redis key FD-55362
    
    * FD-55362 local cache key
    
    * test file for csp FD-55362
    
    * updated the test case for lp FD-55362
    
    * test case update FD-55362
    
    * env check and fall back mechanism incase redis key is not set FD-55362
    
    * environment check and empty redis key FD-55362
    
    * remove gemfile.lock file changes FD-55362
    
    * discard gem file changes FD-55362
    
    * CSP Fallback refactored
    
    * using rails env interpolation FD-55362
    
    * test case updated FD-55362
    
    * test logic changed FD-59977
    
    * test code changes FD-59977
    
    * final test cases FD-59977
    
    * rubocop issue fix FD-59977
    
    * rubo cop issues fix FD-59977
    
    * Revert "rubo cop issues fix FD-59977"
    
    This reverts commit d64cbb8632ff66be6a8db39357fdfb702434c301.
    
    * rubocop issues fix FD-59977
    
    Co-authored-by: Thanashyam Raj <thanashyam.raj@freshworks.com>

--- test/app/controllers/home_controller_test.rb
+++ test/app/controllers/home_controller_test.rb
@@ -1,9 +1,15 @@
 require_relative '../../api/test_helper'
 require_relative '../../core/helpers/controller_test_helper'
+require_relative '../../core/helpers/account_test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   include ControllerTestHelper
+  include Redis::Keys::Others
+  include Redis::OthersRedis
+
   def setup
+    @account = Account.first.presence || create_test_account
+    @user = Account.current.technicians.first.make_current
     super
   end
 
@@ -42,5 +48,36 @@ class HomeControllerTest < ActionController::TestCase
     assert_equal response.body, html_content
   ensure
     Net::HTTP.unstub(:get)
+  end
+
+  def test_csp_policy_header_if_no_feature
+    html_content = '<html><body>Hey buddy.. test works!!</body></html>'
+    Net::HTTP.stubs(:get).with(URI(AppConfig['falcon_ui']['index_page'])).returns(html_content)
+    get :index_html
+    assert_response 200
+    assert_equal response.headers['Content-Security-Policy-Report-Only'], nil
+  end
+
+  def test_csp_policy_header_if_redis_key_exist
+    Account.current.launch(:csp_reports)
+    csp = "default-src 'self'; script-src https://*.cloudfront.net cdn.headwayapp.co *.freshcloud.io https://assets.freshpo.com https://cdn.heapanalytics.com; style-src https://wchat.freshchat.com; img-src https://*.cloudfront.net https://heapanalytics.com; connect-src https://*.freshworksapi.com https://*.freshpo.com; report-uri /api/_/cspreports"
+    set_others_redis_key(CONTENT_SECURITY_POLICY_AGENT_PORTAL, csp, 60)
+    html_content = '<html><body>Hey buddy.. test works!!</body></html>'
+    Net::HTTP.stubs(:get).with(URI(AppConfig['falcon_ui']['index_page'])).returns(html_content)
+    get :index_html
+    assert_response 200
+    assert_equal response.headers['Content-Security-Policy-Report-Only'], csp
+  end
+
+  def test_csp_policy_header_if_redis_key_not_set
+    # test code
+    Account.current.launch(:csp_reports)
+    staging_csp = "default-src 'self'; script-src https://*.cloudfront.net cdn.headwayapp.co *.freshcloud.io https://assets.freshpo.com https://cdn.heapanalytics.com; style-src https://wchat.freshchat.com; img-src https://*.cloudfront.net https://heapanalytics.com; connect-src https://*.freshworksapi.com https://*.freshpo.com; report-uri /api/_/cspreports"
+    set_others_redis_key(CONTENT_SECURITY_POLICY_AGENT_PORTAL, '', 60)
+    html_content = '<html><body>Hey buddy.. test works!!</body></html>'
+    Net::HTTP.stubs(:get).with(URI(AppConfig['falcon_ui']['index_page'])).returns(html_content)
+    get :index_html
+    assert_response 200
+    assert_equal response.headers['Content-Security-Policy-Report-Only'], staging_csp
   end
 end
