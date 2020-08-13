bc2221ca1847c1f386703f01a269d381a2358f8b
Nipun Gupta <nipun.gupta@freshworks.com>
2020-08-13 17:41:58 +0530

    FD-59788 add shard :all and move rename column outside block

--- db/migrate/20200807113406_add_encrypted_tokens_to_imap_mailboxes.rb
+++ db/migrate/20200807113406_add_encrypted_tokens_to_imap_mailboxes.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 class AddEncryptedTokensToImapMailboxes < ActiveRecord::Migration
+  shard :all
   def up
     Lhm.change_table :imap_mailboxes, atomic_switch: true do |m|
       m.add_column :encrypted_access_token, :text
-      m.rename_column :refresh_token, :encrypted_refresh_token
     end
+    rename_column :imap_mailboxes, :refresh_token, :encrypted_refresh_token
   end
 
   def down
     Lhm.change_table :imap_mailboxes do |m|
       m.remove_column :encrypted_access_token
-      m.rename_column :encrypted_refresh_token, :refresh_token
     end
+    rename_column :imap_mailboxes, :encrypted_refresh_token, :refresh_token
   end
 end
--- db/migrate/20200807115324_add_encrypted_tokens_to_smtp_mailboxes.rb
+++ db/migrate/20200807115324_add_encrypted_tokens_to_smtp_mailboxes.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 class AddEncryptedTokensToSmtpMailboxes < ActiveRecord::Migration
+  shard :all
   def up
     Lhm.change_table :smtp_mailboxes, atomic_switch: true do |m|
       m.add_column :encrypted_access_token, :text
-      m.rename_column :refresh_token, :encrypted_refresh_token
     end
+    rename_column :smtp_mailboxes, :refresh_token, :encrypted_refresh_token
   end
 
   def down
     Lhm.change_table :smtp_mailboxes do |m|
       m.remove_column :encrypted_access_token
-      m.rename_column :encrypted_refresh_token, :refresh_token
     end
+    rename_column :smtp_mailboxes, :encrypted_refresh_token, :refresh_token
   end
 end
