6fcb534e88ce1be53fe5a5224460008bb5bf86ea
Vishnu Mohan <vishnu.mohan@freshworks.com>
2020-08-27 16:52:45 +0530

    FD-61657 Adressing new validations

--- api/app/controllers/channel/v2/agents_controller.rb
+++ api/app/controllers/channel/v2/agents_controller.rb
@@ -3,8 +3,9 @@ module Channel::V2
     include ChannelAuthentication
     include CentralLib::CentralResyncHelper
 
-    skip_before_filter :check_privilege, only: :sync
-    before_filter :channel_client_authentication, :validate_sync_params, only: :sync
+    skip_before_filter :check_privilege, if: :skip_privilege_check?
+    before_filter :channel_client_authentication
+    before_filter :validate_sync_params, only: :sync
 
     def verify_agent_privilege
       @items = { admin: User.current.privilege?(:admin_tasks),
@@ -13,13 +14,31 @@ module Channel::V2
     end
 
     def sync
-      if resync_worker_limit_reached?(@source)
+      channel_source = @source
+      if resync_worker_limit_reached?(channel_source)
         head 429
       else
-        persist_job_info_and_start_entity_publish(@source, request.uuid, CentralLib::CentralResyncConstants::RESYNC_ENTITIES[:agent], @args[:sync_meta], nil, @args[:primary_key_offset])
+        persist_job_info_and_start_entity_publish(channel_source, request.uuid, CentralLib::CentralResyncConstants::RESYNC_ENTITIES[:agent], @args[:meta], nil, @args[:primary_key_offset])
         @response = { job_id: request.uuid }
         render status: 202
       end
     end
+
+    private
+
+      def validate_sync_params
+        @args = params.symbolize_keys
+        agent_validation = Channel::V2::AgentValidation.new(@args)
+        unless agent_validation.valid?(action_name.to_sym)
+          render_custom_errors(agent_validation, true)
+        end
+      end
+
+      def skip_privilege_check?
+        TICKET_FIELDS_ALLOWED_SOURCE.each do |source|
+          return true if channel_source?(source.to_sym)
+        end
+        false
+      end
   end
 end
--- api/app/controllers/validations/channel/v2/agent_validation.rb
+++ api/app/controllers/validations/channel/v2/agent_validation.rb
@@ -1,6 +1,6 @@
 module Channel::V2
   class AgentValidation < ::AgentValidation
-    attr_accessor :sync_meta
-    validates :sync_meta, presence: true, on: :sync
+    attr_accessor :meta
+    validates :meta, presence: true, on: :sync
   end
 end
