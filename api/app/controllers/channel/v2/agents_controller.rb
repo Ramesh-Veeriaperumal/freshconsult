21e12360d444fac8ad647ea64157a5336f93720a
Vishnu Mohan <vishnu.mohan@freshworks.com>
2020-09-01 10:43:19 +0530

    FD-61657 Adressing requested chnages

--- api/app/controllers/channel/v2/agents_controller.rb
+++ api/app/controllers/channel/v2/agents_controller.rb
@@ -4,8 +4,7 @@ module Channel::V2
     include CentralLib::CentralResyncHelper
 
     skip_before_filter :check_privilege, if: :skip_privilege_check?
-    before_filter :channel_client_authentication
-    before_filter :validate_sync_params, only: :sync
+    before_filter :channel_client_authentication, :validate_sync_params, only: :sync
 
     def verify_agent_privilege
       @items = { admin: User.current.privilege?(:admin_tasks),
@@ -18,7 +17,7 @@ module Channel::V2
       if resync_worker_limit_reached?(channel_source)
         head 429
       else
-        persist_job_info_and_start_entity_publish(channel_source, request.uuid, CentralLib::CentralResyncConstants::RESYNC_ENTITIES[:agent], @args[:meta], nil, @args[:primary_key_offset])
+        persist_job_info_and_start_entity_publish(channel_source, request.uuid, RESYNC_ENTITIES[:agent], @args[:meta], nil, @args[:primary_key_offset])
         @response = { job_id: request.uuid }
         render status: 202
       end
@@ -29,16 +28,11 @@ module Channel::V2
       def validate_sync_params
         @args = params.symbolize_keys
         agent_validation = Channel::V2::AgentValidation.new(@args)
-        unless agent_validation.valid?(action_name.to_sym)
-          render_custom_errors(agent_validation, true)
-        end
+        render_custom_errors(agent_validation, true) unless agent_validation.valid?(action_name.to_sym)
       end
 
       def skip_privilege_check?
-        TICKET_FIELDS_ALLOWED_SOURCE.each do |source|
-          return true if channel_source?(source.to_sym)
-        end
-        false
+        RESYNC_ALLOWED_SOURCES.any? { |source| channel_source?(source.to_sym) }
       end
   end
 end
