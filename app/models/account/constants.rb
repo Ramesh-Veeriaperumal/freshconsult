0e5326d35bcebadc0c7859c6fbf3474470c1cfb6
Lalit Yadav <lalit.yadav@freshworks.com>
2020-09-03 16:21:49 +0530

    Removed autorefreshnode (#9464)

--- app/models/account/constants.rb
+++ app/models/account/constants.rb
@@ -126,7 +126,7 @@ class Account < ActiveRecord::Base
   # Default feature when creating account has been made true :surveys & ::survey_links $^&WE^%$E
   TEMPORARY_FEATURES = {
     :bi_reports => false, :contact_merge_ui => false, :social_revamp => true, :multiple_user_emails => false,
-    :facebook_realtime => false, :autorefresh_node => false, :tokenize_emoji => false,
+    :facebook_realtime => false, :tokenize_emoji => false,
     :custom_dashboard => false, :updated_twilio_client => false,
     :report_field_regenerate => false, :reports_regenerate_data => false,
     :chat_enable => false, :saml_old_issuer => false, :spam_dynamo => true,
