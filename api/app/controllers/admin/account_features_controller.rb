d6b81e5644435b60fbb071739ec1e54f1a9c11b2
Ramachandran Nagaraj <ramachandran.nagaraj@freshworks.com>
2020-09-10 16:37:50 +0530

    FD-63348 cascade_dispatcher initial commit

--- api/app/controllers/admin/account_features_controller.rb
+++ api/app/controllers/admin/account_features_controller.rb
@@ -1,18 +1,37 @@
+
+require './lib/helpdesk/initializers/account_settings'
 class Admin::AccountFeaturesController < ApiApplicationController
   include HelperConcern
   include Fdadmin::FeatureMethods
   include Admin::AccountFeatureConstants
+  include ::AccountSettings
 
   before_filter :validate_query_params, only: ALLOWED_METHOD_FOR_PRIVATE_API
   skip_before_filter :build_object, :load_object, only: ALLOWED_METHOD_FOR_PRIVATE_API
   before_filter :get_feature_type, only: ALLOWED_METHOD_FOR_PRIVATE_API
 
   def create
-    modify_feature :enable
-    head 204
+    puts "------------- CREATE -------------------------------"
+    if Account.current.launched?(:feature_based_settings) && AccountSettings::SettingsConfig[params[:name]]
+      if can_admin_modify_setting?(@feature_name)
+        enable_setting(@feature_name)
+      end
+    else
+      p "param: #{params[:name]}"
+      p "settings: #{AccountSettings::SettingsConfig[params[:name]]}"
+      p "launched: #{Account.current.launched?(:feature_based_settings)}"
+      p "can modify #{can_admin_modify_setting?(@feature_name)}"
+      modify_feature :enable
+      head 204
+    end
   end
 
   def destroy
+    puts "------------- DESTROY -------------------------------"
+    p "param: #{params[:name]}"
+    p "settings: #{AccountSettings::SettingsConfig[params[:name]]}"
+    p "launched: #{Account.current.launched?(:feature_based_settings)}"
+    p "can modify #{can_admin_modify_setting?(@feature_name)}"
     modify_feature :disable
     head 204
   end
@@ -20,7 +39,9 @@ class Admin::AccountFeaturesController < ApiApplicationController
   private
 
     def modify_feature(method_type)
+      p @feature_type
       @feature_type.each do |type|
+        puts "#{method_type}_#{type}_feature #{@feature_name}"
         safe_send(:"#{method_type}_#{type}_feature", @feature_name)
       end
     end
