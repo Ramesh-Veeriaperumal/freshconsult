737814456e469e2751c1864cacee64458e665828
Dhruva Ashok <35563497+fresh-dhruvaashok@users.noreply.github.com>
2020-09-10 15:23:43 +0530

    [Technical Story] [FD-64088,  FD-64097]: Phase 1 LP to Bitmap for fetch_ticket_from_ref_first, skip_ticket_threading (#9501)
    * FD-64088: Phase 1 LP to Bitmap for fetch_ticket_from_ref_first, skip_ticket_threading
    
    * FD-64097: rubo cop fixes

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -44,7 +44,7 @@ class Account < ActiveRecord::Base
     :omni_plans_migration_banner, :parse_replied_email, :wf_comma_filter_fix, :composed_email_check, :omni_channel_dashboard, :csat_for_social_surveymonkey, :fresh_parent, :trim_special_characters, :kbase_omni_bundle,
     :omni_agent_availability_dashboard, :twitter_api_compliance, :silkroad_export, :silkroad_shadow, :silkroad_multilingual, :group_management_v2, :symphony, :invoke_touchstone, :explore_omnichannel_feature, :hide_omnichannel_toggle,
     :dashboard_java_fql_performance_fix, :emberize_business_hours, :chargebee_omni_upgrade, :ticket_observer_race_condition_fix, :csp_reports, :show_omnichannel_nudges, :whatsapp_ticket_source, :chatbot_ui_revamp, :response_time_null_fix, :cx_feedback, :export_ignore_primary_key, :archive_ticket_central_publish,
-    :archive_on_missing_associations, :mailbox_ms365_oauth, :pre_compute_ticket_central_payload, :security_revamp
+    :archive_on_missing_associations, :mailbox_ms365_oauth, :pre_compute_ticket_central_payload, :security_revamp, :skip_ticket_threading
   ].freeze
 
   BITMAP_FEATURES = [
@@ -80,7 +80,8 @@ class Account < ActiveRecord::Base
     :fb_ad_posts, :suggested_articles_count, :unlimited_multi_product, :freddy_self_service, :freddy_ultimate,
     :help_widget_article_customisation, :agent_assist_lite, :sla_reminder_automation, :article_interlinking, :pci_compliance_field, :kb_increased_file_limit,
     :twitter_field_automation, :robo_assist, :triage, :advanced_article_toolbar_options, :advanced_freshcaller, :email_bot, :agent_assist_ultimate, :canned_response_suggest, :robo_assist_ultimate, :advanced_ticket_scopes,
-    :custom_objects, :quality_management_system, :kb_allow_base64_images, :triage_ultimate, :autofaq_eligible, :whitelisted_ips, :solutions_agent_metrics, :forums_agent_portal, :solutions_agent_portal
+    :custom_objects, :quality_management_system, :kb_allow_base64_images, :triage_ultimate, :autofaq_eligible, :whitelisted_ips, :solutions_agent_metrics, :forums_agent_portal, :solutions_agent_portal,
+    :fetch_ticket_from_ref_first, :skip_ticket_threading
   ].concat(ADVANCED_FEATURES + ADVANCED_FEATURES_TOGGLE + HelpdeskReports::Constants::FreshvisualFeatureMapping::REPORTS_FEATURES_LIST).uniq
   # Doing uniq since some REPORTS_FEATURES_LIST are present in Bitmap. Need REPORTS_FEATURES_LIST to check if reports related Bitmap changed.
 
@@ -446,6 +447,14 @@ class Account < ActiveRecord::Base
 
   def solutions_agent_metrics_enabled?
     launched?(:solutions_agent_metrics)
+  end
+
+  def fetch_ticket_from_ref_first_enabled?
+    launched?(:fetch_ticket_from_ref_first)
+  end
+
+  def skip_ticket_threading_enabled?
+    launched?(:skip_ticket_threading)
   end
 
   def forums_agent_portal_enabled?
