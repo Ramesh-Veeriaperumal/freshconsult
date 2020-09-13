30032f09dd88f202fda79224c34ef4b8802046c0
praveenkumar.soundarapandian <praveenkumar.soundarapandian@freshworks.com>
2020-09-13 11:16:16 +0530

    FD-62595 putting back freshfone_number and app/kissmetrics

--- public/javascripts/cdn/livechat_reports_pdf.js
+++ public/javascripts/cdn/livechat_reports_pdf.js
@@ -1,5 +1,6 @@
 //= require adv_reports/report_charts.js
 //= require adv_reports/adv_reports.js
+//= require ../app/kissmetrics.js
 //= require ../helpdesk_reports/helpdeskreports_metrics.js
 //= require app/common_saved_report_util.js
 //= require livechat/reports
--- public/javascripts/cdn/reports.js
+++ public/javascripts/cdn/reports.js
@@ -1,4 +1,5 @@
 //= require adv_reports/report_charts.js
 //= require adv_reports/adv_reports.js
 //= require_tree ../helpdesk_reports/templates
+//= require ../app/kissmetrics.js
 //= require ../helpdesk_reports/helpdeskreports_metrics.js
