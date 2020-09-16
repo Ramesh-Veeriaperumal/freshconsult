3a7092b27119b0749f2e4410af305f562b8b7d20
Ramachandran Nagaraj <ramachandran.nagaraj@freshworks.com>
2020-09-16 23:26:28 +0530

    FD-63348 cascade dispatcher changes

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -69,7 +69,7 @@ class Account < ActiveRecord::Base
     :canned_forms, :customize_table_view, :public_url_toggle, :solutions_templates,
     :add_to_response, :agent_scope, :performance_report, :custom_password_policy,
     :social_tab, :unresolved_tickets_widget_for_sprout, :scenario_automation,
-    :ticket_volume_report, :omni_channel, :sla_management_v2, :api_v2, :cascade_dispatcher,
+    :ticket_volume_report, :omni_channel, :sla_management_v2, :api_v2,
     :personal_canned_response, :marketplace, :reverse_notes, :field_service_geolocation,
     :location_tagging, :freshreports_analytics, :disable_old_reports, :article_filters, :adv_article_bulk_actions,
     :auto_article_order, :detect_thank_you_note, :detect_thank_you_note_eligible, :autofaq, :proactive_spam_detection,
---
+++ app/models/account/settings.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+class Account < ActiveRecord::Base
+
+    AccountSettings::SettingsConfig.each_key do |setting|
+      define_method "#{setting}_enabled?" do
+        has_setting?(setting)
+      end
+    end
+  
+    # Need to modify methods when we move all LPs to Bitmaps and validate settings throw error for invalid settings
+    def admin_setting_for_account?(setting)
+      settings_hash = AccountSettings::SettingsConfig[setting]
+      settings_hash && !settings_hash[:internal] && has_feature?(settings_hash[:feature_dependency])
+    end
+  
+    def internal_setting_for_account?(setting)
+      settings_hash = AccountSettings::SettingsConfig[setting]
+      settings_hash && settings_hash[:internal] && has_feature?(settings_hash[:feature_dependency])
+    end
+  
+    # Move feature dependency check inside the valid_setting once, all Settings migrate to bitmap from LP
+    def enable_setting(setting)
+      if valid_setting(setting)
+        has_feature?(AccountSettings::SettingsConfig[setting][:feature_dependency]) ? add_feature(setting) : raise_invalid_setting_error(setting)
+      elsif Account::LP_TO_BITMAP_MIGRATION_FEATURES.include?(setting)
+        launch(setting)
+        add_feature(setting)
+      end
+    end
+  
+    def set_setting(setting)
+      if valid_setting(setting)
+        has_feature?(AccountSettings::SettingsConfig[setting][:feature_dependency]) ? set_feature(setting) : raise_invalid_setting_error(setting)
+      end
+    end
+  
+    def disable_setting(setting)
+      if valid_setting(setting)
+        has_feature?(AccountSettings::SettingsConfig[setting][:feature_dependency]) ? revoke_feature(setting) : raise_invalid_setting_error(setting)
+      elsif Account::LP_TO_BITMAP_MIGRATION_FEATURES.include?(setting)
+        rollback(setting)
+        revoke_feature(setting)
+      end
+    end
+  
+    def reset_setting(setting)
+      if valid_setting(setting)
+        has_feature?(AccountSettings::SettingsConfig[setting][:feature_dependency]) ? reset_feature(setting) : raise_invalid_setting_error(setting)
+      end
+    end
+  
+    private
+  
+      # Can combine this with valid_setting once LP's are migrated to BITMAP
+      def has_setting?(setting)
+        AccountSettings::SettingsConfig[setting][:feature_dependency] && has_feature?(setting)
+      end
+  
+      def valid_setting(setting)
+        AccountSettings::SettingsConfig[setting].present?
+      end
+  
+      def raise_invalid_setting_error(setting)
+        raise "Invalid setting #{setting} for the account"
+      end
+  end
