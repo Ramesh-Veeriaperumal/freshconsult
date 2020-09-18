f8047393116563bd75940bf94bd43cfaf67bb48e
Vishnu Mohan <vishnu.mohan@freshworks.com>
2020-09-18 12:15:33 +0530

    FD-61657 Updating the validation base class

--- api/app/controllers/validations/channel/v2/agent_validation.rb
+++ api/app/controllers/validations/channel/v2/agent_validation.rb
@@ -1,5 +1,5 @@
 module Channel::V2
-  class AgentValidation < ::AgentValidation
+  class AgentValidation < ::ApiValidation
     attr_accessor :meta
     validates :meta, presence: true, on: :sync
   end
