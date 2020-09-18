1a00d5c9b8525363884585ffe536633a3d823e3c
Vishnu Mohan <vishnu.mohan@freshworks.com>
2020-09-18 15:30:27 +0530

    FD-61657 Resolving rubocop errors

--- api/app/controllers/constants/channel/v2/agent_constants.rb
+++ api/app/controllers/constants/channel/v2/agent_constants.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Channel::V2::AgentConstants
   LOAD_OBJECT_EXCEPT = ['sync'].freeze
 end
--- api/app/controllers/validations/channel/v2/agent_validation.rb
+++ api/app/controllers/validations/channel/v2/agent_validation.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module Channel::V2
   class AgentValidation < ::ApiValidation
     attr_accessor :meta
