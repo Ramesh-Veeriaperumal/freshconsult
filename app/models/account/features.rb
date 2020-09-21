6d1c566ebca56983a4120fd20eba8b0f2c318db9
Ashwin Balaji Shekher <61041894+ashwin-shekher@users.noreply.github.com>
2020-09-21 08:22:07 +0530

    [Technical Story] [FD-64431]: Phase 1 LP to Bitmap for supervisor_con (#9626)
    * [Technical Story] [FD-64431]: Phase 1 LP to Bitmap for supervisor_contact_field
    
    * [Technical Story] [FD-64431]: Phase 1 LP to Bitmap for supervisor_contact_field : LP_FEATURES

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -44,7 +44,7 @@ class Account < ActiveRecord::Base
     :omni_plans_migration_banner, :parse_replied_email, :wf_comma_filter_fix, :composed_email_check, :omni_channel_dashboard, :csat_for_social_surveymonkey, :fresh_parent, :trim_special_characters, :kbase_omni_bundle,
     :omni_agent_availability_dashboard, :twitter_api_compliance, :silkroad_export, :silkroad_shadow, :silkroad_multilingual, :group_management_v2, :symphony, :invoke_touchstone, :explore_omnichannel_feature, :hide_omnichannel_toggle,
     :dashboard_java_fql_performance_fix, :emberize_business_hours, :chargebee_omni_upgrade, :ticket_observer_race_condition_fix, :csp_reports, :show_omnichannel_nudges, :whatsapp_ticket_source, :chatbot_ui_revamp, :response_time_null_fix, :cx_feedback, :export_ignore_primary_key, :archive_ticket_central_publish,
-    :archive_on_missing_associations, :mailbox_ms365_oauth, :pre_compute_ticket_central_payload, :security_revamp, :skip_ticket_threading, :channel_command_reply_to_sidekiq, :ocr_to_mars_api
+    :archive_on_missing_associations, :mailbox_ms365_oauth, :pre_compute_ticket_central_payload, :security_revamp, :skip_ticket_threading, :channel_command_reply_to_sidekiq, :ocr_to_mars_api, :supervisor_contact_field
   ].freeze
 
   BITMAP_FEATURES = [
@@ -81,13 +81,14 @@ class Account < ActiveRecord::Base
     :help_widget_article_customisation, :agent_assist_lite, :sla_reminder_automation, :article_interlinking, :pci_compliance_field, :kb_increased_file_limit,
     :twitter_field_automation, :robo_assist, :triage, :advanced_article_toolbar_options, :advanced_freshcaller, :email_bot, :agent_assist_ultimate, :canned_response_suggest, :robo_assist_ultimate, :advanced_ticket_scopes,
     :custom_objects, :quality_management_system, :kb_allow_base64_images, :triage_ultimate, :autofaq_eligible, :whitelisted_ips, :solutions_agent_metrics, :forums_agent_portal, :solutions_agent_portal,
-    :fetch_ticket_from_ref_first, :skip_ticket_threading, :helpdesk_tickets_by_product, :skip_invoice_due_warning, :allow_wildcard_ticket_create
+    :fetch_ticket_from_ref_first, :skip_ticket_threading, :helpdesk_tickets_by_product, :skip_invoice_due_warning, :allow_wildcard_ticket_create, :supervisor_contact_field
   ].concat(ADVANCED_FEATURES + ADVANCED_FEATURES_TOGGLE + HelpdeskReports::Constants::FreshvisualFeatureMapping::REPORTS_FEATURES_LIST).uniq
   # Doing uniq since some REPORTS_FEATURES_LIST are present in Bitmap. Need REPORTS_FEATURES_LIST to check if reports related Bitmap changed.
 
   LP_TO_BITMAP_MIGRATION_FEATURES = [
     :solutions_agent_metrics, :forums_agent_portal, :solutions_agent_portal, :helpdesk_tickets_by_product,
-    :skip_ticket_threading, :fetch_ticket_from_ref_first, :skip_invoice_due_warning, :allow_wildcard_ticket_create
+    :skip_ticket_threading, :fetch_ticket_from_ref_first, :skip_invoice_due_warning, :allow_wildcard_ticket_create,
+    :supervisor_contact_field
   ].freeze
 
   COMBINED_VERSION_ENTITY_KEYS = [
@@ -451,6 +452,10 @@ class Account < ActiveRecord::Base
 
   def skip_invoice_due_warning_enabled?
     launched?(:skip_invoice_due_warning)
+  end
+
+  def supervisor_contact_field_enabled?
+    launched?(:supervisor_contact_field)
   end
 
   def features
