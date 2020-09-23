ee8040dd9d953f469e8cb9b7038aec6a8bceebff
Karthik Murugesan <35285447+karthikmurugesan@users.noreply.github.com>
2020-09-23 18:20:33 +0530

    [User Story] [FD-60162] - New Ticket slider for Freshcaller ticket creation (#9199)
    * [FD-60162] - New Ticket slider for Freshcaller ticket creation
    
    * [FD-60162] - Rubocop error fixes
    
    * [FD-60162] Rubocop issue fix
    
    * [FD-60162] Review comment fixes
    
    * [FD-60162] - Rubocop issues fix
    
    * [FD-60162] - Review comment fixes
    
    * [FD-60162] Update note when note already present
    
    * [FD-60162] additional test cases
    
    * [FD-60162] - Rubocop error fix
    
    * [FD-62511] - Bug fix
    
    * [FD-62511] - Bug fix
    
    * [FD-62511] - changing version in schema.rb
    
    * [FD-62511] Schema.rb version change
    
    * [FD-62511] - test cases fix
    
    * [FD-62511] - call controller test case fixes
    
    * [FD-62511] -  Migrations fix
    
    * [FD-62511] - schema.rb version change
    
    * [FD-62511] - schema.rb version change
    
    * [FD-62511] - schema migration change
    
    * [FD-62511] - schema migration change
    
    * [FD-62511] - schema.rb fic
    
    * [FD-62511] - Additional test cases for coverage
    
    * [FD-62511] Test case fixes
    
    * [FD-62511] test case error fixes
    
    * [FD-62511] Test case changes
    
    * test fix
    
    * test fix 2
    
    * test fix 3
    
    Co-authored-by: Amit Mallick <amit.mallick@freshworks.com>

--- app/models/account/features.rb
+++ app/models/account/features.rb
@@ -44,7 +44,7 @@ class Account < ActiveRecord::Base
     :omni_plans_migration_banner, :parse_replied_email, :wf_comma_filter_fix, :composed_email_check, :omni_channel_dashboard, :csat_for_social_surveymonkey, :fresh_parent, :trim_special_characters, :kbase_omni_bundle,
     :omni_agent_availability_dashboard, :twitter_api_compliance, :silkroad_export, :silkroad_shadow, :silkroad_multilingual, :group_management_v2, :symphony, :invoke_touchstone, :explore_omnichannel_feature, :hide_omnichannel_toggle,
     :dashboard_java_fql_performance_fix, :emberize_business_hours, :chargebee_omni_upgrade, :ticket_observer_race_condition_fix, :csp_reports, :show_omnichannel_nudges, :whatsapp_ticket_source, :chatbot_ui_revamp, :response_time_null_fix, :cx_feedback, :export_ignore_primary_key, :archive_ticket_central_publish,
-    :archive_on_missing_associations, :mailbox_ms365_oauth, :pre_compute_ticket_central_payload, :security_revamp, :skip_ticket_threading, :channel_command_reply_to_sidekiq, :ocr_to_mars_api, :supervisor_contact_field, :forward_to_phone, :disable_freshchat, :html_to_plain_text
+    :archive_on_missing_associations, :mailbox_ms365_oauth, :pre_compute_ticket_central_payload, :security_revamp, :skip_ticket_threading, :channel_command_reply_to_sidekiq, :ocr_to_mars_api, :supervisor_contact_field, :forward_to_phone, :disable_freshchat, :html_to_plain_text, :freshcaller_ticket_revamp
   ].freeze
 
   BITMAP_FEATURES = [
