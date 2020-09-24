178ccc2871baa73c5e4f65ee28e29fbcca1873a7
ramananr <ramanan.raguraman@freshworks.com>
2020-09-24 12:16:18 +0530

    rubocop

--- api/app/controllers/email/settings_controller.rb
+++ api/app/controllers/email/settings_controller.rb
@@ -61,7 +61,7 @@ module Email
 
       def validate_settings
         params[cname].each_key do |setting|
-          unless Account.current.admin_setting_for_account?(EmailSettingsConstants::EMAIL_SETTINGS_PARAMS_NAME_CHANGES[setting.to_sym] || setting.to_sym)
+          unless Account.current.admin_setting_for_account?(EMAIL_SETTINGS_PARAMS_NAME_CHANGES[setting.to_sym] || setting.to_sym)
             return render_request_error(:require_feature, 403, feature: setting)
           end
         end
