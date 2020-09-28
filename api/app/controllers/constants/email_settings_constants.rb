e06517a1627f5f9e7bfd09702d37bf50beed194c
ramananr <ramanan.raguraman@freshworks.com>
2020-09-28 14:43:32 +0530

    removed delegator constant

--- api/app/controllers/constants/email_settings_constants.rb
+++ api/app/controllers/constants/email_settings_constants.rb
@@ -1,6 +1,5 @@
 module EmailSettingsConstants
   VALIDATION_CLASS = 'Email::SettingsValidation'.freeze
-  DELEGATOR_CLASS = 'Email::SettingsDelegator'.freeze
   UPDATE_FIELDS = %w[personalized_email_replies create_requester_using_reply_to allow_agent_to_initiate_conversation original_sender_as_requester_for_forward allow_wildcard_ticket_create skip_ticket_threading].freeze
   UPDATE_FIELDS_WITHOUT_NEW_SETTINGS = %w[personalized_email_replies create_requester_using_reply_to allow_agent_to_initiate_conversation original_sender_as_requester_for_forward].freeze
   COMPOSE_EMAIL = :compose_email
