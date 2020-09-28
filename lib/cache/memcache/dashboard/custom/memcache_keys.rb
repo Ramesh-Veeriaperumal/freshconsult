2517917798a2f0cb4d8102b742d55710f93f9428
bagavathi <36401127+bagavathisundaramt@users.noreply.github.com>
2020-09-28 14:05:32 +0530

    [Technical Story] FD-61492  As a System, need to accomodate dashboard CRUD operation - freshchat (#9462)
    * FD-61435 As a System, need to accomodate dashboard CRUD operation - freshdesk & freshcaller
    
    * removing title and some rubocop fixes
    
    * adding constants
    
    * adding some test cases and validation changes
    
    * adding some test cases and rubocop fixes
    
    * dashboard widget limit code changes
    
    * fix conflict for now
    
    * fix test cases
    
    * fix test cases
    
    * code changes with proper validations
    
    * code changes for completeness
    
    * add code for debugging test case failures
    
    * add more test cases and fix some cases
    
    * add more test cases and fix syntax error
    
    * rubocop fixes
    
    * rubocop fixes
    
    * rubocop fixes & testcase fixes
    
    * FD-61435 testcase fixes
    
    * FD-61435 testcase failures debugging
    
    * FD-61435 testcase failures fix
    
    * FD-61435 ruocop fixes and changing scorecard to time trends
    
    * FD-61435 changing widget limit struture
    
    * FD-61435 changing widget limit struture testcase fixes
    
    * FD-61435 test case fix
    
    * FD-61435 Analysing test case failures
    
    * FD-61435 test case fixes
    
    * FD-61435 test case fixes
    
    * FD-61435 test case loggers addition and fix for some failures
    
    * FD-61435 fix for some testcase failures and review comments
    
    * FD-61435 resolve conflicts
    
    * FD-61435 resolve conflicts
    
    * FD-61435 fix tests
    
    * FD-61435 rubocop fixes after ruby version change
    
    * FD-61435 rubocop fixes and testcase changes
    
    * FD-61435 testcase fix changes
    
    * FD-61435 testcase fixes
    
    * FD-61435 removing loggers
    
    * FD-61435 test fix
    
    * FD-61492 As a System, need to accomodate dashboard CRUD operation - freshchat
    
    * FD-61435 calculate url changes
    
    * FD-61435 calculate url changes
    
    * FD-61492 review changes & testcase fixes
    
    * FD-61435 review comments fixes
    
    * FD-61435 review comments fixes
    
    * FD-61435 revert valid_config changes
    
    * FD-61492 review comments
    
    * FD-61492 test case fixes
    
    * FD-61435 review Comments and additional test cases
    
    * FD-61435 test case fix
    
    * FD-61492 review comments and additional testcases
    
    * FD-61435 fix URL pattern fix for update requests
    
    * FD-61435 fix freshdesk widgets case
    
    * FD-61435 remove presense validation, remove loggers and review comment
    
    * FD-61435 review comments
    
    * FD-61435 review comments
    
    * FD-61492 review comments
    
    * FD-61492 addressing review comments
    
    * FD-61492 test case fixes
    
    * FD-61492 rubocop errors fix

--- lib/cache/memcache/dashboard/custom/memcache_keys.rb
+++ lib/cache/memcache/dashboard/custom/memcache_keys.rb
@@ -9,7 +9,10 @@ module Cache::Memcache::Dashboard::Custom::MemcacheKeys
   CUSTOM_DASHBOARD_TICKET_TREND_CARDS             = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:5'
   CUSTOM_DASHBOARD_TIME_TREND_CARDS               = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:6'
   CUSTOM_DASHBOARD_SLA_TREND_CARDS                = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:7'
+  CUSTOM_DASHBOARD_MS_SCORECARDS                  = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:11'.freeze
+  CUSTOM_DASHBOARD_MS_BAR_CHARTS                  = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:12'.freeze
   CUSTOM_DASHBOARD_MS_AVAILABILITYS               = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:13'.freeze
+  CUSTOM_DASHBOARD_MS_CSATS                       = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:14'.freeze
   CUSTOM_DASHBOARD_MS_TIME_TRENDS                 = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:16'.freeze
   CUSTOM_DASHBOARD_MS_SLA_TRENDS                  = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:17'.freeze
   CUSTOM_DASHBOARD_MS_CALL_TRENDS                 = 'v1/custom/CUSTOM_DASHBOARD:%{account_id}:%{dashboard_id}:18'.freeze
@@ -25,7 +28,10 @@ module Cache::Memcache::Dashboard::Custom::MemcacheKeys
   CUSTOM_DASHBOARD_TICKET_TREND_CARD_DATA         = 'v1/CUSTOM_DASHBOARD:TICKET_TREND_CARD_DATA:%{account_id}:%{dashboard_id}'
   CUSTOM_DASHBOARD_TIME_TREND_CARD_DATA           = 'v1/CUSTOM_DASHBOARD:TIME_TREND_CARD_DATA:%{account_id}:%{dashboard_id}'
   CUSTOM_DASHBOARD_SLA_TREND_CARD_DATA            = 'v1/CUSTOM_DASHBOARD:SLA_TREND_CARD_DATA:%{account_id}:%{dashboard_id}'
+  CUSTOM_DASHBOARD_MS_SCORECARD_DATA              = 'v1/CUSTOM_DASHBOARD:MS_SCORECARD_DATA:%{account_id}:%{dashboard_id}'.freeze
+  CUSTOM_DASHBOARD_MS_BAR_CHART_DATA              = 'v1/CUSTOM_DASHBOARD:MS_BAR_CHART_DATA:%{account_id}:%{dashboard_id}'.freeze
   CUSTOM_DASHBOARD_MS_AVAILABILITY_DATA           = 'v1/CUSTOM_DASHBOARD:MS_AVAILABILITY_DATA:%{account_id}:%{dashboard_id}'.freeze
+  CUSTOM_DASHBOARD_MS_CSAT_DATA                   = 'v1/CUSTOM_DASHBOARD:MS_CSAT_DATA:%{account_id}:%{dashboard_id}'.freeze
   CUSTOM_DASHBOARD_MS_TIME_TREND_DATA             = 'v1/CUSTOM_DASHBOARD:MS_TIME_TREND_DATA:%{account_id}:%{dashboard_id}'.freeze
   CUSTOM_DASHBOARD_MS_SLA_TREND_DATA              = 'v1/CUSTOM_DASHBOARD:MS_SLA_TREND_DATA:%{account_id}:%{dashboard_id}'.freeze
   CUSTOM_DASHBOARD_MS_CALL_TREND_DATA             = 'v1/CUSTOM_DASHBOARD:MS_CALL_TREND_DATA:%{account_id}:%{dashboard_id}'.freeze
