cfafe04eafa36ece538cf15dc4c5a061bdea2afa
ramananr <ramanan.raguraman@freshworks.com>
2020-09-29 18:41:19 +0530

    FD-65507 - remove redundant constant

--- api/app/controllers/constants/email_settings_constants.rb
+++ api/app/controllers/constants/email_settings_constants.rb
@@ -1,6 +1,5 @@
 module EmailSettingsConstants
   VALIDATION_CLASS = 'Email::SettingsValidation'.freeze
-  DELEGATOR_CLASS = 'Email::SettingsDelegator'.freeze
   UPDATE_FIELDS = %w[personalized_email_replies create_requester_using_reply_to allow_agent_to_initiate_conversation original_sender_as_requester_for_forward].freeze
   COMPOSE_EMAIL = :compose_email
   DISABLE_AGENT_FORWARD = :disable_agent_forward
