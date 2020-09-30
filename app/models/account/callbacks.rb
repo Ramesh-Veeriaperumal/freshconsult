85831f0bf8e4a8c2ccaeccbb0d3104f62b005689
ravishankar-gnanaprakasam <39148615+ravishankar-gnanaprakasam@users.noreply.github.com>
2020-09-30 14:46:38 +0530

    [Technical Story] [FD-63404] Cleanup (:service_writes) feature (#9698)
    * [FD-63405] Cleanup (:service_reads) feature
    
    * [FD-63404] Cleanup (:service_writes) feature

--- app/models/account/callbacks.rb
+++ app/models/account/callbacks.rb
@@ -626,9 +626,6 @@ class Account < ActiveRecord::Base
     
     def enable_searchv2
       if self.features?(:es_v2_writes)
-        if redis_key_exists?(SEARCH_SERVICE_SIGNUP)
-          self.launch(:service_writes)
-        end
         SearchV2::Manager::EnableSearch.perform_async
         self.launch(:es_v2_reads)
       end
