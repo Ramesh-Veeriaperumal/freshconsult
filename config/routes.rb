6e90ae1c643fd538552b132223285a5d65e06283
Venkateshan N <61772829+venkateshan-niladri@users.noreply.github.com>
2020-10-15 16:03:50 +0530

    [Technical Story]  FD-65409: New Relic cleanup for ActivationsController#create (#9747)
    * FD-65409: Initial commit to fix newrelic errors
    
    * FD-65409: refactoring for code block
    
    * FD-65409: adding UTs for nil params
    
    * FD-65409: modified condition to OR
    
    * FD-65409: modifying the UT
    
    * FD-65409: modified UT and addressed review comments
    
    * FD-65409: resolving rubocop warnings
    
    * FD-65409: adding UTs for activations_controller
    
    * FD-65409: resolving rubocop for update_attribute
    
    * FD-65409: modified the UTs
    
    * FD-65409: removed ensure block
    
    * FD-65409: restricted to POST call for activate URL
    
    * FD-65409: changed POST to PUT

--- config/routes.rb
+++ config/routes.rb
@@ -451,7 +451,7 @@ Helpkit::Application.routes.draw do
   match '/disable_skip_mandatory' => 'admin/account_additional_settings#disable_skip_mandatory', :as => :disable_skip_mandatory, via: :post
   match '/register/:activation_code' => 'activations#new', :as => :register
   match 'register_new_email/:activation_code' => 'activations#new_email', :as => :register_new_email
-  match '/activate/:perishable_token' => 'activations#create', :as => :activate
+  match '/activate/:perishable_token' => 'activations#create', :as => :activate, via: :put
   match '/set_notes_order' => 'users#set_conversation_preference', :as => :set_notes_order, via: :put
 
   resources :activations do
