724e31218abb090dacc203e29bc59fcd722d91f4
Lalit Yadav <lalit.yadav@freshworks.com>
2020-10-16 14:36:08 +0530

    [Bug] [FD-65856] Remove account choice id validation for custom source (#9912)
    * Removed custom source validation
    
    * Added message
    
    * Rubocop Fix
    
    * Update validations.rb
    
    * Update error_messages.yml
    
    * Update validations.rb
    
    * Fix rubocop

--- app/models/helpdesk/source/validations.rb
+++ app/models/helpdesk/source/validations.rb
@@ -3,6 +3,7 @@ class Helpdesk::Source < Helpdesk::Choice
   validates :name, length: { in: 1..50 }
   validates :name, uniqueness: { scope: :account_id, case_sensitive: false }
   xss_sanitize only: [:name], plain_sanitizer: [:name]
-  # Need to revisit during custom sources
-  validates :account_choice_id, numericality: { less_than_or_equal_to: MAXIMUM_NUMBER_OF_SOURCES, scope: [:account_id, :type] }
+  validates :account_choice_id, numericality: { less_than_or_equal_to: MAXIMUM_NUMBER_OF_SOURCES,
+                                                scope: [:account_id, :type],
+                                                message: 'Exceeded the number of choices which can be created' }
 end
