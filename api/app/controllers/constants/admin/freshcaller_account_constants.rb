a1b00b16d9c4553632d7fa101be8b41d86476176
Prabhu-Kathiresan <56917956+Prabhu-Kathiresan@users.noreply.github.com>
2020-10-30 15:57:52 +0530

    [User Story] FD-66840 Allow a customer to enable/disable freshcaller contact search with country code (#9965)
    * FD-66840 adding search_without_country_code to settings column in freshcaller account
    
    * FD-66840 minor bug fix

--- api/app/controllers/constants/admin/freshcaller_account_constants.rb
+++ api/app/controllers/constants/admin/freshcaller_account_constants.rb
@@ -2,7 +2,8 @@ module Admin::FreshcallerAccountConstants
   VALIDATION_CLASS = 'Admin::FreshcallerAccountValidation'.freeze
   LINK_FIELDS = %w[url email password].freeze
   AUTOMATIC_TICKET_CREATION_SETTINGS = [automatic_ticket_creation: %w[missed_calls abandoned_calls connected_calls]].freeze
-  SETTINGS_FIELDS = [settings: AUTOMATIC_TICKET_CREATION_SETTINGS].freeze
+  FRESHCALLER_SEARCH_SETTINGS = ['search_without_country_code'].freeze
+  SETTINGS_FIELDS = [settings: AUTOMATIC_TICKET_CREATION_SETTINGS + FRESHCALLER_SEARCH_SETTINGS].freeze
   UPDATE_FIELDS = %w[agent_ids] | SETTINGS_FIELDS
   UPDATE_ARRAY_FIELDS = %w[agent_ids].freeze
 end
