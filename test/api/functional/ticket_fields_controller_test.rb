78e15b5b98baa457811cfb6731147f8c9911ddd3
murugaperumal-ramasamy <murugaperumal.ramasamy@freshworks.com>
2020-11-18 11:07:58 +0530

    [Technical Story] [FD-66713] Add coverage for TicketFieldsController (#10244)
    * Add coverage for TicketFieldsController

--- test/api/functional/ticket_fields_controller_test.rb
+++ test/api/functional/ticket_fields_controller_test.rb
@@ -317,6 +317,105 @@ class TicketFieldsControllerTest < ActionController::TestCase
     payload.must_match_json_expression(ticket_field_publish_pattern(custom_field_new))
   end
 
+  def test_delete_a_custom_dependent_field
+    flexifield_def_entry = []
+    labels = %w[Country State City]
+    (0..2).each do |nested_field_id|
+      flexifield_def_entry[nested_field_id] = FactoryGirl.build(:flexifield_def_entry,
+                                                                flexifield_def_id: @account.flexi_field_defs.find_by_name("Ticket_#{@account.id}").id,
+                                                                flexifield_alias: "#{labels[nested_field_id].downcase}_#{@account.id}",
+                                                                flexifield_name: "ffs_0#{nested_field_id + 7}",
+                                                                flexifield_order: 6,
+                                                                flexifield_coltype: 'dropdown',
+                                                                account_id: @account.id)
+      flexifield_def_entry[nested_field_id].save
+    end
+
+    parent_custom_field = FactoryGirl.build(:ticket_field, account_id: @account.id,
+                                                           name: "#{labels[0].downcase}_#{@account.id}",
+                                                           label: labels[0],
+                                                           label_in_portal: labels[0],
+                                                           field_type: 'nested_field',
+                                                           description: '',
+                                                           flexifield_def_entry_id: flexifield_def_entry[0].id)
+    parent_custom_field.save
+
+    nested_field_vals = []
+    (1..2).each do |nf|
+      nested_field_vals[nf - 1] = FactoryGirl.build(:nested_ticket_field, account_id: @account.id,
+                                                                          name: "#{labels[nf].downcase}_#{@account.id}",
+                                                                          flexifield_def_entry_id: flexifield_def_entry[nf].id,
+                                                                          label: labels[nf],
+                                                                          label_in_portal: labels[nf],
+                                                                          ticket_field_id: parent_custom_field.id,
+                                                                          level: nf + 1)
+      nested_field_vals[nf - 1].save
+    end
+
+    field_choices = [['Australia', '0',
+                      [['New South Wales', '0', [%w[Sydney 0]]],
+                       ['Queensland', '0', [%w[Brisbane 0]]]]],
+                     ['USA', '0',
+                      [['California', '0', [%w[Burlingame 0], ['Los Angeles', '0']]],
+                       ['Texas', '0', [%w[Houston 0], %w[Dallas 0]]]]]]
+    field_choices_del = [['Australia', '0',
+                          [['New South Wales', '0'], %w[Queensland 0]]],
+                         ['USA', '0',
+                          [%w[California 0], %w[Texas 0]]]]
+    picklist_vals_l1 = []
+    picklist_vals_l2 = []
+    picklist_vals_l3 = []
+    field_choices.map(&:first).each_with_index do |l1_val, index1|
+      picklist_vals_l1 << FactoryGirl.build(:picklist_value, account_id: @account.id,
+                                                             pickable_type: 'Helpdesk::TicketField',
+                                                             pickable_id: parent_custom_field.id,
+                                                             position: index1 + 1,
+                                                             value: l1_val)
+      picklist_vals_l1.last.save
+
+      field_choices[index1][2].map(&:first).each_with_index do |l2_val, index2|
+        picklist_vals_l2 << FactoryGirl.build(:picklist_value, account_id: @account.id,
+                                                               pickable_type: 'Helpdesk::PicklistValue',
+                                                               pickable_id: picklist_vals_l1[picklist_vals_l1.length - 1].id,
+                                                               position: index2 + 1,
+                                                               value: l2_val)
+        picklist_vals_l2.last.save
+        field_choices[index1][2][index2][2].map(&:first).each_with_index do |l3, index3|
+          picklist_vals_l3 << FactoryGirl.build(:picklist_value, account_id: @account.id,
+                                                                 pickable_type: 'Helpdesk::PicklistValue',
+                                                                 pickable_id: picklist_vals_l2[picklist_vals_l2.length - 1].id,
+                                                                 position: index3 + 1,
+                                                                 value: l3)
+          picklist_vals_l3.last.save
+        end
+      end
+    end
+    put :update, jsonData: @default_fields.push(field_type: 'nested_field',
+                                                id: parent_custom_field.id,
+                                                name: "#{labels[0].downcase}_#{@account.id}",
+                                                label: labels[0],
+                                                label_in_portal: labels[0],
+                                                description: '',
+                                                position: 6,
+                                                active: true,
+                                                required: false,
+                                                required_for_closure: false,
+                                                visible_in_portal: true,
+                                                editable_in_portal: true,
+                                                required_in_portal: false,
+                                                choices: field_choices_del,
+                                                levels: [{ label: 'State', label_in_portal: 'State', description: '',
+                                                           level: 2, id: nested_field_vals[0].id, position: 6, type: 'dropdown', action: 'edit' },
+                                                         { label: '', label_in_portal: '', description: '',
+                                                           level: 3, id: nested_field_vals[1].id, position: 6, type: 'dropdown', action: 'delete' }],
+                                                field_options: nil,
+                                                type: 'dropdown',
+                                                action: 'edit').to_json
+    parent_custom_field = @account.ticket_fields.find_by_label(labels[0])
+    assert_instance_of(Helpdesk::TicketField, parent_custom_field)
+    refute parent_custom_field.level_three_present
+  end
+
   def test_edit_a_custom_dependent_field
     flexifield_def_entry = []
     labels = ['Nation', 'Memorial']
